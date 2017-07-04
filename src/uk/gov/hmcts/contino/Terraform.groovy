000a5e80a00794eac3c7b92f922b09e5409632e0
Hibri Marzook <hibri.marzook@contino.io>
2017-07-04 16:40:00 +0100

    setup terraform in each method

--- src/uk/gov/hmcts/contino/Terraform.groovy
+++ src/uk/gov/hmcts/contino/Terraform.groovy
@@ -9,10 +9,6 @@ class Terraform implements Serializable {
 
     this.steps = steps
     this.product = product
-
-    def tfHome = this.steps.tool name: 'Terraform', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
-
-    this.steps.env.PATH = "${tfHome}:${this.steps.env.PATH}"
   }
 
   /***
@@ -24,6 +20,7 @@ class Terraform implements Serializable {
    * @return
    */
   def init(env, state_store_resource_group, state_store_account, state_store_container ) {
+    setupTerraform()
     return steps.withCredentials([
                         [$class: 'StringBinding', credentialsId: 'sp_password', variable: 'ARM_CLIENT_SECRET'],
                         [$class: 'StringBinding', credentialsId: 'tenant_id', variable: 'ARM_TENANT_ID'],
@@ -31,17 +28,18 @@ class Terraform implements Serializable {
                         [$class: 'StringBinding',credentialsId: 'subscription_id', variable: 'ARM_SUBSCRIPTION_ID'],
                         [$class: 'StringBinding', credentialsId: 'object_id', variable: 'ARM_CLIENT_ID']]) {
 
-        return steps.sh("terraform init -backend-config \"storage_account_name=${state_store_account}\" -backend-config \"container_name=${state_store_container}\" -backend-config \"resource_group_name=${state_store_resource_group}\" -backend-config \"key=${this.product}/${env}/terraform.tfstate\"")
+         steps.sh("terraform init -backend-config \"storage_account_name=${state_store_account}\" -backend-config \"container_name=${state_store_container}\" -backend-config \"resource_group_name=${state_store_resource_group}\" -backend-config \"key=${this.product}/${env}/terraform.tfstate\"")
       }
   }
 
-  /***
+
+/***
    *
    * @param env
    * @return
    */
   def plan(env) {
-
+    setupTerraform()
     steps.sh("terraform get -update=true")
     return steps.sh("terraform plan -var 'env=${env}'")
 
@@ -53,9 +51,15 @@ class Terraform implements Serializable {
    * @return
    */
   def apply(env){
-
+    setupTerraform()
     if (env.BRANCH_NAME == 'master' ) {
       return steps.sh("terraform apply -var 'env=${env}'")
     }
+  }
+
+  private setupTerraform() {
+    def tfHome = this.steps.tool name: 'Terraform', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+
+    this.steps.env.PATH = "${tfHome}:${this.steps.env.PATH}"
   }
 }
