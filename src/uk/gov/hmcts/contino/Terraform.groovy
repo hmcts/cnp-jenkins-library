32be67bb069c3c890037a8da6f79a0b9236d710f
Hibri Marzook <hibri.marzook@contino.io>
2017-07-04 15:44:12 +0100

    add plan and apply

--- src/uk/gov/hmcts/contino/Terraform.groovy
+++ src/uk/gov/hmcts/contino/Terraform.groovy
@@ -26,6 +26,29 @@ class Terraform implements Serializable {
   def init(env, state_store_resource_group, state_store_account, state_store_container ) {
 
       return this.steps.sh("terraform init -backend-config \"storage_account_name=${state_store_account}\" -backend-config \"container_name=${state_store_container}\" -backend-config \"resource_group_name=${state_store_resource_group}\" -backend-config \"key=${this.product}/${env}/terraform.tfstate\"")
+  }
 
+  /***
+   *
+   * @param env
+   * @return
+   */
+  def plan(env) {
+
+    this.steps.sh("terraform get -update=true")
+    return this.steps.sh("terraform plan -var 'env=${env}'")
+
+  }
+
+  /***
+   *
+   * @param env
+   * @return
+   */
+  def apply(env){
+
+    if (env.BRANCH_NAME == 'master' ) {
+      return this.step.sh("terraform apply -var 'env=${env}'")
+    }
   }
 }
