faabbca31b4f9aa98cce10ebfeccf2cdf5880b7d
Hibri Marzook <hibri.marzook@contino.io>
2017-07-06 13:46:11 +0100

    pull init into plan

--- src/uk/gov/hmcts/contino/Terraform.groovy
+++ src/uk/gov/hmcts/contino/Terraform.groovy
@@ -8,15 +8,7 @@ class Terraform implements Serializable {
   def steps
   def product
 
-  def stateStorage = [
-          "dev" : [
-                  resource_group: "",
-
-          ]
-  ]
-
-
-  Terraform(steps, product){
+  Terraform(steps, product) {
 
     this.steps = steps
     this.product = product
@@ -30,29 +22,14 @@ class Terraform implements Serializable {
    * @param state_store_container
    * @return
    */
-  def init(env) {
-
-    def stateStores =  new JsonSlurper().parseText(steps.libraryResource('uk/gov/hmcts/contino/products.json'))
-
-    if(env in stateStores) {
-
-      def stateStoreConfig = stateStores[env]
-      def state_store_resource_group = stateStoreConfig["resourceGroup"]
-      def state_store_account = stateStoreConfig["storageAccount"]
-      def state_store_container = stateStoreConfig["container"]
-
-      return runTerraformWithCreds("init -backend-config \"storage_account_name=${state_store_account}\" -backend-config \"container_name=${state_store_container}\" -backend-config \"resource_group_name=${state_store_resource_group}\" -backend-config \"key=${this.product}/${env}/terraform.tfstate\"")
-    }
-    error "$env does not have state storage configured"
-  }
-
 
 /***
-   *
-   * @param env
-   * @return
-   */
+ *
+ * @param env
+ * @return
+ */
   def plan(env) {
+    init(env)
     setupTerraform()
     runTerraformWithCreds("get -update=true")
     return runTerraformWithCreds("plan -var 'env=${env}'")
@@ -64,15 +41,30 @@ class Terraform implements Serializable {
    * @param env
    * @return
    */
-  def apply(env){
+  def apply(env) {
 
-    if (steps.env.BRANCH_NAME == 'master' ) {
+    if (steps.env.BRANCH_NAME == 'master') {
       return runTerraformWithCreds("apply -var 'env=${env}'")
     }
   }
 
+  private def init(env) {
 
-  private runTerraformWithCreds(args){
+    def stateStores = new JsonSlurper().parseText(steps.libraryResource('uk/gov/hmcts/contino/products.json'))
+
+    if (env in stateStores) {
+
+      def stateStoreConfig = stateStores[env]
+      def state_store_resource_group = stateStoreConfig["resourceGroup"]
+      def state_store_account = stateStoreConfig["storageAccount"]
+      def state_store_container = stateStoreConfig["container"]
+
+      return runTerraformWithCreds("init -backend-config \"storage_account_name=${state_store_account}\" -backend-config \"container_name=${state_store_container}\" -backend-config \"resource_group_name=${state_store_resource_group}\" -backend-config \"key=${this.product}/${env}/terraform.tfstate\"")
+    }
+    error "$env does not have state storage configured"
+  }
+
+  private runTerraformWithCreds(args) {
 
     setupTerraform()
 
@@ -80,7 +72,7 @@ class Terraform implements Serializable {
       [$class: 'StringBinding', credentialsId: 'sp_password', variable: 'ARM_CLIENT_SECRET'],
       [$class: 'StringBinding', credentialsId: 'tenant_id', variable: 'ARM_TENANT_ID'],
       [$class: 'StringBinding', credentialsId: 'contino_github', variable: 'TOKEN'],
-      [$class: 'StringBinding',credentialsId: 'subscription_id', variable: 'ARM_SUBSCRIPTION_ID'],
+      [$class: 'StringBinding', credentialsId: 'subscription_id', variable: 'ARM_SUBSCRIPTION_ID'],
       [$class: 'StringBinding', credentialsId: 'object_id', variable: 'ARM_CLIENT_ID']]) {
 
       steps.sh("terraform ${args}")
