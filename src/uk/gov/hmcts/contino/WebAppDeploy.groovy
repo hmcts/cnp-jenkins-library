445709239868a9cb5f6b31c48ae9d9cf744d6230
Hibri Marzook <hibri.marzook@contino.io>
2017-07-07 16:23:32 +0100

    move credentials inside

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -15,8 +15,14 @@ class WebAppDeploy implements Serializable {
 
   def deploy(env) {
 
-    steps.sh("\"git remote add ${defaultRemote} \"https://$GIT_DEPLPOY_USERNAME:$GIT_DEPLPOY_PASSWORD@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
+    steps.withCredentials([
+      usernamePassword(credentialsId: 'WebAppDeployCredentials',
+        usernameVariable: 'GIT_DEPLOY_USERNAME',
+        passwordVariable: 'GIT_DEPLOY_PASSWORD')]) {
 
-    return steps.sh("git push ${defaultRemote} master")
+      steps.sh("\"git remote add ${defaultRemote} \"https://$GIT_DEPLPOY_USERNAME:$GIT_DEPLPOY_PASSWORD@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
+
+      return steps.sh("git push ${defaultRemote} master")
+    }
   }
 }
