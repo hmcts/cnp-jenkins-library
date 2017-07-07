ed8a76991de328f3a5f779b3fc487f4d91494525
Hibri Marzook <hibri.marzook@contino.io>
2017-07-07 16:32:50 +0100

    use username password from steps

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -16,11 +16,13 @@ class WebAppDeploy implements Serializable {
   def deploy(env) {
 
     return steps.withCredentials([
-        [$class: 'UsernamePasswordBinding', credentialsId: 'WebAppDeployCredentials', usernameVariable: 'GIT_DEPLOY_USERNAME', passwordVariable: 'GIT_DEPLOY_PASSWORD']]) {
+        steps.usernamePassword(credentialsId: 'WebAppDeployCredentials',
+        usernameVariable: 'GIT_DEPLOY_USERNAME',
+        passwordVariable: 'GIT_DEPLOY_PASSWORD')]) {
 
-        steps.sh("\"git remote add ${defaultRemote} \"https://$GIT_DEPLPOY_USERNAME:$GIT_DEPLPOY_PASSWORD@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
+      steps.sh("\"git remote add ${defaultRemote} \"https://$GIT_DEPLPOY_USERNAME:$GIT_DEPLPOY_PASSWORD@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
 
-        steps.sh("git push ${defaultRemote} master")
+       teps.sh("git push ${defaultRemote} master")
     }
   }
 }
