db79c5636fffa37c69c99372537286c99b95f09d
Hibri Marzook <hibri.marzook@contino.io>
2017-07-07 16:48:16 +0100

    remove escape char

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -21,7 +21,7 @@ class WebAppDeploy implements Serializable {
           usernameVariable: 'GIT_USERNAME',
           passwordVariable: 'GIT_PASSWORD']]) {
 
-        steps.sh("\"git remote add ${defaultRemote} \"https://${GIT_USERNAME}:${GIT_PASSWORD}@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
+        steps.sh("git remote add ${defaultRemote} \"https://$GIT_USERNAME:$GIT_PASSWORD@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
 
         steps.sh("git push ${defaultRemote} master")
     }
