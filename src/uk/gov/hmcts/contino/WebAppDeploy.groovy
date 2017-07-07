a4e7fbaa63ee786109bffdbfde02db3d6df26289
Hibri Marzook <hibri.marzook@contino.io>
2017-07-07 16:55:38 +0100

    see if we can get it from the env

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -22,7 +22,7 @@ class WebAppDeploy implements Serializable {
           passwordVariable: 'GIT_PASSWORD']]) {
 
         steps.echo("${steps.env}")
-        steps.sh("git remote add ${defaultRemote} \"https://$GIT_USERNAME:$GIT_PASSWORD@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
+        steps.sh("git remote add ${defaultRemote} \"https://${steps.env.GIT_USERNAME}:${steps.env.GIT_PASSWORD}@${product}-${env}.scm.${product}-${env}.p.azurewebsites.net/${product}-${env}.git\"")
 
         steps.sh("git push ${defaultRemote} master")
     }
