dc794b262b267d4f5c50a816b69e3d17f5cfbcaf
Hibri Marzook <hibri.marzook@contino.io>
2017-07-11 15:41:58 +0100

    append env to remote name

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -24,9 +24,9 @@ class WebAppDeploy implements Serializable {
           passwordVariable: 'GIT_PASSWORD']]) {
 
         def appUrl = "${product}-${app}-${env}"
-        steps.sh("git remote add ${defaultRemote} \"https://${steps.env.GIT_USERNAME}:${steps.env.GIT_PASSWORD}@${appUrl}.scm.${hostingEnv}.p.azurewebsites.net/${appUrl}.git\"")
+        steps.sh("git remote add ${defaultRemote}-${env} \"https://${steps.env.GIT_USERNAME}:${steps.env.GIT_PASSWORD}@${appUrl}.scm.${hostingEnv}.p.azurewebsites.net/${appUrl}.git\"")
         steps.sh("git checkout ${steps.env.BRANCH_NAME}")
-        steps.sh("git push ${defaultRemote} master")
+        steps.sh("git push ${defaultRemote}-${env}  master")
     }
   }
 }
