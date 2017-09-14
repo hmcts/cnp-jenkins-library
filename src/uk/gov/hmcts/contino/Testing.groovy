7567e885323824905247a8a02441b718eb2a6118
Calin Marina <calin.marina@gmail.com>
2017-09-14 09:51:37 +0100

    added unit tests to library

--- src/uk/gov/hmcts/contino/Testing.groovy
+++ src/uk/gov/hmcts/contino/Testing.groovy
@@ -5,21 +5,32 @@ class Testing implements Serializable {
 
   private String PREPARE_ENVIRONMENT = 'export PATH=$PATH:/usr/local/bundle/bin:/usr/local/bin && export HOME="$WORKSPACE"'
   def pipe
-  def gitUrl
+  def String gitUrl
 
   Testing(pipe){
     this.pipe = pipe
     this.gitUrl = "${pipe.GITHUB_PROTOCOL}://${pipe.TOKEN}@${pipe.GITHUB_REPO}"
   }
 
+  Testing(pipe, String gitUrl){
+    this.pipe = pipe
+    this.gitUrl = gitUrl
+  }
+
+  def unitTest() {
+    return pipe.docker.image('dsanabria/terraform_validate:latest').inside {
+      pipe.sh 'cd tests/unit && python tests.py'
+    }
+  }
   /* Running integration tests for a module with Chef Kitchen, spinning up temporary infrastructure
   * running tests and removing the infratructure at the end
   */
   def moduleIntegrationTests() {
     String RANDOM_STRING = RandomStringUtils.random(6, true, true)
+    def envSuffix = (pipe.env.BRANCH_NAME == 'master') ? 'dev' : pipe.env.BRANCH_NAME
 
     return runWithDocker("cd tests/int && kitchen test azure",
-                         [TF_VAR_random_name:"inspec-${pipe.env.BRANCH_NAME}-${RANDOM_STRING}",
+                         [TF_VAR_random_name:"inspec-${envSuffix}-${RANDOM_STRING}",
                           TF_VAR_branch_name:pipe.env.BRANCH_NAME])
   }
 
