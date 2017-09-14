85797baef3f29e8bfea8012fe47e92910c30fc1f
Calin Marina <calin.marina@gmail.com>
2017-09-14 10:44:26 +0100

    repaired test for Terraform lib

--- test/uk/gov/hmcts/contino/TerraformTests.groovy
+++ test/uk/gov/hmcts/contino/TerraformTests.groovy
@@ -5,28 +5,62 @@ import spock.lang.Shared
 import spock.lang.Ignore
 import spock.lang.Specification
 
-class TerraformTests  extends  Specification {
+class TerraformTests extends Specification {
 
-  @Shared steps
-  @Shared terraform
+  @Shared
+      steps
+  @Shared
+      terraform
 
   def setup() {
     steps = Mock(JenkinsStepMock)
-    steps.libraryResource(_) >> new File("./resources/uk/gov/hmcts/contino/state-storage.json").text
-    steps.env >> [ "PATH" : ""]
+    steps.libraryResource(_) >> new File("./resources/uk/gov/hmcts/contino/state-storage-template.json").text
+    steps.env >> ["PATH": ""]
 //    steps.ansiColor(_, _) >> { format, closure -> println "Format: "+ format+ "; Chained command"+ closure.metaClass.classNode.getDeclaredMethods("doCall")[0].code.text  }
 
     terraform = new Terraform(steps, "test")
   }
 
-  def "should run init"() {
-    when:
-      terraform.plan("dev")
+  def "should run init on master branch with [dev, test, prod] env"() {
+    given:
+    steps.env.BRANCH_NAME = 'master'
 
+    when:
+    terraform.plan("dev")
     then:
-      1 * steps.fileExists(_)
-      3 * steps.ansiColor('xterm', _ )
+    1 * steps.fileExists(_)
+    3 * steps.ansiColor('xterm', _)
+
+    when:
+    terraform.plan("test")
+    then:
+    1 * steps.fileExists(_)
+    3 * steps.ansiColor('xterm', _)
+
   }
+
+  def "should not run init on other branch with [dev, test, prod] env"() {
+    given:
+    steps.env.BRANCH_NAME = 'some_branch'
+
+    when:
+    terraform.plan("dev")
+    then:
+    thrown(Exception)
+
+    when:
+    terraform.plan("test")
+    then:
+    thrown(Exception)
+
+    when:
+    terraform.plan("prod")
+    then:
+    thrown(Exception)
+
+  }
+
+
 }
 
 
