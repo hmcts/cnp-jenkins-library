de5354bc084f37d38e7156f53f1e0b3ee4077f82
Hibri Marzook <hibri.marzook@contino.io>
2017-10-23 12:33:07 +0100

    use terraform 10

--- vars/terraformSetup.groovy
+++ vars/terraformSetup.groovy
@@ -1,5 +1,5 @@
 def call() {
-  def tfHome = steps.tool name: 'Terraform', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+  def tfHome = steps.tool name: 'Terraform 10', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
   env.PATH = "${tfHome}:${env.PATH}"
   env.TERRAFORM_INITIALIZED = true
 }
