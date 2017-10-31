34c1ea49579ea92c8a59ae7d0f1bed5855a2221c
Neil Richardson <njrich28@gmail.com>
2017-10-31 13:42:03 +0000

    Add --fore and --pure-lockfile to yarn install

--- resources/uk/gov/hmcts/contino/yarn-install/deploy.cmd
+++ resources/uk/gov/hmcts/contino/yarn-install/deploy.cmd
@@ -105,7 +105,7 @@ call :ExecuteCmd !NPM_CMD! install yarn -g
 echo Installing Yarn Packages.
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd yarn install --production
+  call :ExecuteCmd yarn install --production --force --pure-lockfile --verbose
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
