7c717765470fd93f6284532e3ff53e724568be10
Calin Marina <calin.marina@gmail.com>
2017-11-02 14:45:56 +0000

    parametrize

--- vars/withSubscription.groovy
+++ vars/withSubscription.groovy
@@ -1,22 +1,15 @@
 import groovy.json.JsonSlurperClassic
 
-def call(String servicePrincipal, String vaultName, Closure body) {
+def call(String servicePrincipal, String vaultName, String secretName, Closure body) {
 
   withCredentials([azureServicePrincipal(servicePrincipal)]) {
 
-    resp = sh(script: "az keyvault secret list --vault-name '${vaultName}'", returnStdout: true).trim()
+    //resp = sh(script: "az keyvault secret list --vault-name '${vaultName}'", returnStdout: true).trim()
     //resp = sh(script: "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID", returnStdout: true).trim()
-    echo "returned: $resp"
-/*
-    def varName = 'tenant-id'
-    def subscription = 'nonprod'
-    if (env == 'prod')
-      subscription = 'prod'
-
-    resp = steps.sh(script: "az keyvault secret show --vault-name 'jenkins-vault' --name ${subscription}-${varName}", returnStdout: true).trim()
+    //resp = sh(script: "az keyvault secret list --vault-name '${vaultName}'", returnStdout: true).trim()
+    resp = steps.sh(script: "az keyvault secret show --vault-name '${vaultName}' --name ${secretName}", returnStdout: true).trim()
     secrets = new JsonSlurperClassic().parseText(resp)
     echo "TOKEN: '${secrets}'"
-*/
 
     body.call()
 
