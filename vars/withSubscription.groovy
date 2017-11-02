f4c08e1899a5c67d284e7e85954a92827023498a
Calin Marina <calin.marina@gmail.com>
2017-11-02 12:01:56 +0000

    add withSubscription step

---
+++ vars/withSubscription.groovy
@@ -0,0 +1,24 @@
+import groovy.json.JsonSlurperClassic
+
+def call(servicePrincipal, vaultName) {
+
+  withCredentials([azureServicePrincipal(servicePrincipal)]) {
+
+    resp = sh(script: "az keyvault secret list --vault-name '${vaultName}'", returnStdout: true).trim()
+    //resp = sh(script: "az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID", returnStdout: true).trim()
+    secrets = new JsonSlurperClassic().parseText(resp)
+    echo "TOKEN: '${secrets}'"
+
+  }
+
+//  steps.withCredentials([azureServicePrincipal('jenkinsSP')]) {
+//    def subscription = 'nonprod'
+//    if (env == 'prod')
+//      subscription = 'prod'
+//
+//    resp = steps.sh(script: "az keyvault secret show --vault-name 'jenkins-vault' --name ${subscription}-${varName}", returnStdout: true).trim()
+//    secret = new JsonSlurperClassic().parseText(resp)
+//  }
+//  return secret.value
+
+}
