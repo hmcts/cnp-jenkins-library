e003a36c0dd35b3cf7d5e860db9d3b0312f6fc59
James Whinn <jameswhinn89@gmail.com>
2017-11-06 21:28:18 +0000

    add waf cert step

---
+++ vars/createwafcert.groovy
@@ -0,0 +1,11 @@
+import uk.gov.hmcts.contino.*
+
+def call(String appName) {
+  echo "Running SSL certificate creation script"
+  String pfxPass = org.apache.commons.lang.RandomStringUtils.random(9, true, true)
+
+  def functions = libraryResource 'uk/gov/hmcts/contino/createwafcert.sh'
+  writeFile file: 'createwafcert.sh', text: functions
+
+  result = sh "bash createwafcert.sh ${pfxPass}"
+}
