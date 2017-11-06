7b2d8b01762d613513b40b6c6d08dd5e9a8ce13f
Calin Marina <calin.marina@gmail.com>
2017-11-06 16:25:36 +0000

    fix vars inside step

--- vars/createssl.groovy
+++ vars/createssl.groovy
@@ -3,10 +3,9 @@ import uk.gov.hmcts.contino.*
 def call(String appName) {
   echo "Running SSL certificate creation script"
   String pfxPass = org.apache.commons.lang.RandomStringUtils.random(9, true, true)
-  domain=appName
 
   def functions = libraryResource 'uk/gov/hmcts/contino/create-cert.sh'
   writeFile file: 'create-cert.sh', text: functions
 
-  result = sh "bash create-cert.sh ${product} ${pfxPass}"
+  result = sh "bash create-cert.sh ${appName} ${pfxPass}"
 }
