fce0c55bbb9facfb0f1872b66320802ccc029b85
Calin Marina <calin.marina@gmail.com>
2017-11-06 16:00:50 +0000

    add script as separate file in resources and try to run it via sh

---
+++ resources/uk/gov/hmcts/contino/create-cert.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+domain=$1
+pxfPass=$2 #$(cat /dev/random | LC_CTYPE=C tr -dc "[:alpha:]" | head -c 8)
+
+echo "Creating Self-Signed cert for $domain"
+
+
+cat > $domain.conf <<-EOF
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+[ dn ]
+C=GB
+ST=LONDON
+L=LONDON
+O=MOJ
+OU=Engineering
+emailAddress=support@moj
+CN = commonNameVar.service.internal
+[ req_ext ]
+subjectAltName = @alt_names
+[ alt_names ]
+DNS.1 = scm.commonNameVar.service.internal
+EOF
+
+sed -i "s/commonNameVar/$domain/g" $domain.conf
+
+openssl req -new -sha256 -nodes -out \*.$domain.csr -newkey rsa:2048 -keyout \*.$domain.key -config <( cat $domain.conf )
+
+openssl x509 -req -in \*.$domain.csr -signkey \*.$domain.key -out $domain.cer
+
+openssl pkcs12 -export -in $domain.cer -inkey \*.$domain.key -out $domain.pfx -password pass:$pfxPass
+
+rm -f \*.$domain.key \*.$domain.csr $domain.conf
+
+az keyvault certificate import --vault-name infra-vault -n $domain -f $domain.pfx --password $pfxPass
--- vars/createssl.groovy
+++ vars/createssl.groovy
@@ -5,51 +5,11 @@ def call(String appName) {
   String pfxPass = org.apache.commons.lang.RandomStringUtils.random(9, true, true)
   domain=appName
 
-  result = sh script: """
-#!/bin/bash
-set +xe
 
-echo "Creating Self-Signed cert for $domain"
+  def functions = libraryResource 'uk/gov/hmcts/contino/create-cert.sh'
+  writeFile file: 'create-cert.sh', text: functions
 
+  result = sh script: "bash -c create-sh.sh ${appName} ${pfxPass}"
 
-cat > ${domain}.conf <<-EOF
-[req]
-default_bits = 2048
-prompt = no
-default_md = sha256
-req_extensions = req_ext
-distinguished_name = dn
-[ dn ]
-C=GB
-ST=LONDON
-L=LONDON
-O=MOJ
-OU=Engineering
-emailAddress=support@moj
-CN = commonNameVar.service.internal
-[ req_ext ]
-subjectAltName = @alt_names
-[ alt_names ]
-DNS.1 = scm.commonNameVar.service.internal
-EOF
-
-pwd
-ls
-cat ${domain}.conf
-
-sed -i "s/commonNameVar/${domain}/g" ${domain}.conf
-
-cat ${domain}.conf
-openssl req -new -sha256 -nodes -out ${domain}.csr -newkey rsa:2048 -keyout ${domain}.key -config < cat ./${domain}.conf
-ls
-openssl x509 -req -in ./${domain}.csr -signkey ./${domain}.key -out ./${domain}.cer
-
-openssl pkcs12 -export -in ./${domain}.cer -inkey ./${domain}.key -out ./${domain}.pfx -password pass:$pfxPass
-
-rm -f ./${domain}.key ./${domain}.csr ./${domain}.conf
-
-az keyvault certificate import --vault-name infra-vault -n ./${domain} -f ./${domain}.pfx --password $pfxPass
-"""
-  //script: libraryResource('uk/gov/hmcts/contino/create-cert'), returnStatus: true
   echo "Script return status: $result"
 }
