941464aabd55071c3cb95a8cc5b5ca0c9515e9d5
James Whinn <jameswhinn89@gmail.com>
2017-11-07 11:39:10 +0000

    adding back min pass creation

--- resources/uk/gov/hmcts/contino/createwafcert.sh
+++ resources/uk/gov/hmcts/contino/createwafcert.sh
@@ -17,6 +17,6 @@ emailAddress=support@moj
 
 # Generate our Private Key, CSR and Certificate
 openssl genrsa -out "waf.key" 2048
-openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "waf.key" -out "waf.csr" -passin pass:$pxfPass
+openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "waf.key" -out "waf.csr" -passin pass:$pfxPass
 openssl x509 -req -days 365 -in "waf.csr" -signkey "waf.key" -out "waf.crt"
-openssl pkcs12 -export -out waf.pfx -inkey waf.key -in waf.crt -passout pass:$pxfPass
+openssl pkcs12 -export -out waf.pfx -inkey waf.key -in waf.crt -passout pass:$pfxPass
--- vars/createwafcert.groovy
+++ vars/createwafcert.groovy
@@ -2,14 +2,11 @@ import uk.gov.hmcts.contino.*
 
 def call(String appName) {
   echo "Running SSL certificate creation script"
-  String pfxPass = "FddsD35272"//org.apache.commons.lang.RandomStringUtils.random(9, true, true)
+  String pfxPass = org.apache.commons.lang.RandomStringUtils.random(9, true, true)
   env.TF_VAR_pfxPass="${pfxPass}"
   def functions = libraryResource 'uk/gov/hmcts/contino/createwafcert.sh'
   writeFile file: 'createwafcert.sh', text: functions
 
   result = sh "bash createwafcert.sh ${pfxPass}"
 
-  //sh "echo pfxPass=${pfxPass} >> terraform.tfvars"
-
-  //sh "sed -i 's/${pfxPass}/\"${pfxPass}\"/g' terraform.tfvars"
 }
