67d785a95e9c191e52b78e4fbf66dd4378b74f03
Gianluca Ciocci <gianluca.ciocci@hmcts.net>
2017-11-09 23:15:47 +0000

    git ssl verify false 2

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -226,7 +226,7 @@ class WebAppDeploy implements Serializable {
 
   private def gitPushToService(serviceDeploymentHost, serviceName, env) {
     steps.sh("git -c http.sslVerify=false remote add ${defaultRemote}-${env} \"https://${steps.env.GIT_USERNAME}:${steps.env.GIT_PASSWORD}@${serviceDeploymentHost}/${serviceName}.git\"")
-    steps.sh("git push ${defaultRemote}-${env} master -f")
+    steps.sh("git -c http.sslVerify=false push ${defaultRemote}-${env} master -f")
   }
 
   private def configureGit() {
