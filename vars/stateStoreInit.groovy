ad36ac5e7bff4653af8c60f943f8be9f73872493
Calin Marina <calin.marina@gmail.com>
2017-11-24 02:41:28 +0000

    add az login before running withSubscription closure

--- vars/stateStoreInit.groovy
+++ vars/stateStoreInit.groovy
@@ -14,24 +14,4 @@ def call(String env) {
   sh "bash stateStoreInit.sh $__rg $sa_name $sacontainer_name $__location"
   echo "$result"
 
-  //  check if the storage account exists. Creates it if not.
-/*
-  if  (! sh("az group exists --name $sa_rg_name", returnStdout: true).toBoolean()) {
-
-    __isCreated = sh("az group create --name $__rg --location $__location --output json")
-    __isCreated = "$(az group create --name $__rg --location $__location --output json | jq -r .properties.provisioningState)"
-
-    if
-    ["${__isCreated}" == "Succeeded"]; then
-    echo "The resource $__rg has been created with no error"
-    else
-    fail "The resource group $__rg hasn't been created successfully"
-    return 0
-  }
-  fi
-  else
-  echo "The resource $__rg already exists."
-  fi
-*/
-
 }
