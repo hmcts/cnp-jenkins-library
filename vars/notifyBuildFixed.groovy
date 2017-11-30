5e57804a6f2b75b953333a94fb88432a9cc5068c
Tim Jacomb <t.jacomb@kainos.com>
2017-11-30 10:17:40 +0000

    Add slack integration (#45)

---
+++ vars/notifyBuildFixed.groovy
@@ -0,0 +1,41 @@
+import uk.gov.hmcts.contino.slack.SlackChannelRetriever
+
+/**
+ * Send build fixed notification
+ * <p>
+ * When build happens on master branch then specified team channel is notified. In other cases change author is notified instead.
+ * <p>
+ * When change author does not have Slack account or uses other Slack username than one registered in LDAP then notification won't be sent.
+ *
+ * @param args arguments:
+ *  <ul>
+ *      <li>channel - (string; required) name of the slack channel for team notifications</li>
+ *  </ul>
+ */
+def call(Map args = [:]) {
+  validate(args)
+
+  String changeAuthor = env.CHANGE_AUTHOR
+
+  String channel
+  if (env.BRANCH_NAME == 'master') {
+    channel = args.channel
+  } else {
+    channel = new SlackChannelRetriever(this).retrieve(args.channel as String, changeAuthor)
+  }
+
+  try {
+    if (currentBuild.getPreviousBuild()?.getResult() == 'FAILURE') {
+      slackSend(
+        channel: channel,
+        color: 'good',
+        message: "${env.JOB_NAME}: <${env.RUN_DISPLAY_URL}|Build ${env.BUILD_DISPLAY_NAME}> is Fixed")
+    }
+  } catch (Exception ex) {
+    echo "ERROR: Failed to notify ${channel} due to the following error: ${ex}"
+  }
+}
+
+private static validate(Map args) {
+  if (args.channel == null) throw new Exception('Slack channel is required')
+}
--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -1,7 +1,6 @@
 import uk.gov.hmcts.contino.*
 
 def call(type, String product, String app, Closure body) {
-
   def pipelineTypes = [
     java: new SpringBootPipelineType(this, product, app),
     nodejs: new NodePipelineType(this, product, app)
@@ -26,87 +25,99 @@ def call(type, String product, String app, Closure body) {
   body.delegate = pl
   body.call() // register callbacks
 
-  node {
-
-    stage('Checkout') {
-      pl.callAround('checkout') {
-        deleteDir()
-        checkout scm
+  try {
+    node {
+      stage('Checkout') {
+        pl.callAround('checkout') {
+          deleteDir()
+          checkout scm
+        }
       }
-    }
 
-    stage("Build") {
-      pl.callAround('build') {
-        builder.build()
+      stage("Build") {
+        pl.callAround('build') {
+          builder.build()
+        }
       }
-    }
 
-    stage("Test") {
-      pl.callAround('test') {
-        builder.test()
+      stage("Test") {
+        pl.callAround('test') {
+          builder.test()
+        }
       }
-    }
 
-    stage("Security Checks") {
-      pl.callAround('securitychecks') {
-        builder.securityCheck()
+      stage("Security Checks") {
+        pl.callAround('securitychecks') {
+          builder.securityCheck()
+        }
       }
-    }
 
-    stage("Sonar Scan") {
-      pl.callAround('sonarscan') {
-        if (Jenkins.instance.getPluginManager().getPlugins().find { it.getShortName() == 'sonar' } != null) {
-          withSonarQubeEnv("SonarQube") {
-            builder.sonarScan();
-          }
-
-          timeout(time: 1, unit: 'MINUTES') {
-            def qg = steps.waitForQualityGate()
-            if (qg.status != 'OK') {
-              error "Pipeline aborted due to quality gate failure: ${qg.status}"
+      stage("Sonar Scan") {
+        pl.callAround('sonarscan') {
+          if (Jenkins.instance.getPluginManager().getPlugins().find { it.getShortName() == 'sonar' } != null) {
+            withSonarQubeEnv("SonarQube") {
+              builder.sonarScan();
             }
+
+            timeout(time: 1, unit: 'MINUTES') {
+              def qg = steps.waitForQualityGate()
+              if (qg.status != 'OK') {
+                error "Pipeline aborted due to quality gate failure: ${qg.status}"
+              }
+            }
+          } else {
+            echo "Sonarqube plugin not installed. Skipping static analysis."
           }
         }
-        else {
-          echo "Sonarqube plugin not installed. Skipping static analysis."
+      }
+
+      stage('Deploy Dev') {
+        pl.callAround('deploy:dev') {
+          deployer.deploy('dev')
+          deployer.healthCheck('dev')
         }
       }
-    }
 
-    stage('Deploy Dev') {
-      pl.callAround('deploy:dev') {
-        deployer.deploy('dev')
-        deployer.healthCheck('dev')
-      }
-    }
-
-    stage('Smoke Tests - Dev') {
-      withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('dev')}"]) {
-        pl.callAround('smoketest:dev') {
-          builder.smokeTest()
+      stage('Smoke Tests - Dev') {
+        withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('dev')}"]) {
+          pl.callAround('smoketest:dev') {
+            builder.smokeTest()
+          }
         }
       }
-    }
 
-    stage("OWASP") {
+      stage("OWASP") {
 
-    }
-
-    stage('Deploy Prod') {
-      pl.callAround('deploy:prod') {
-        deployer.deploy('prod')
-        deployer.healthCheck('prod')
       }
-    }
 
-    stage('Smoke Tests - Prod') {
-      withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('prod')}"]) {
-        pl.callAround('smoketest:prod') {
-          builder.smokeTest()
+      stage('Deploy Prod') {
+        pl.callAround('deploy:prod') {
+          deployer.deploy('prod')
+          deployer.healthCheck('prod')
         }
       }
+
+      stage('Smoke Tests - Prod') {
+        withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('prod')}"]) {
+          pl.callAround('smoketest:prod') {
+            builder.smokeTest()
+          }
+        }
+      }
+
+    }
+  } catch (err) {
+    if (pl.slackChannel) {
+      notifyBuildFailure channel: pl.slackChannel
     }
 
+    pl.call('onFailure')
+    throw err
   }
 
+  if (pl.slackChannel) {
+    notifyBuildFixed channel: pl.slackChannel
+  }
+
+  pl.call('onSuccess')
 }
