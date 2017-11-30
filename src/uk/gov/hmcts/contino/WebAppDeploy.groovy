2d8aec6e6be5f90113b2dce2ed0d35f5009027c9
Tim Jacomb <tim.jacomb@hmcts.net>
2017-11-30 15:36:32 +0000

    hi rafal

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -226,7 +226,7 @@ class WebAppDeploy implements Serializable {
   }
 
   private def gitPushToService(serviceDeploymentHost, serviceName, env) {
-    steps.sh("Hi rafal")
+    steps.sh("echo Hi rafal")
     steps.sh("echo ${branch}")
     steps.sh("git remote add ${defaultRemote}-${env} \"https://${steps.env.GIT_USERNAME}:${steps.env.GIT_PASSWORD}@${serviceDeploymentHost}/${serviceName}.git\"")
     steps.sh("echo '>>> push command will be: git push ${defaultRemote}-${env} ${branch} -f'")
