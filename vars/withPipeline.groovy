2a1b39cc7450d914229fee1b324bd5ac3369312f
Tim Jacomb <t.jacomb@kainos.com>
2017-11-30 10:42:03 +0000

    Add slack integration (#45) (#57)

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -26,86 +26,100 @@ def call(type, String product, String app, Closure body) {
   body.delegate = pl
   body.call() // register callbacks
 
-  node {
-   platformSetup {
-     withSubscription("jenkinsServicePrincipal", "infra-vault") {
-        stage('Checkout') {
-          deleteDir()
-          checkout scm
-        }
-
-        stage("Build") {
-          pl.callAround('build') {
-            builder.build()
-          }
-        }
-
-       stage("Test") {
-         pl.callAround('test') {
-           builder.test()
-         }
-       }
-
-       stage("Security Checks") {
-         pl.callAround('securitychecks') {
-           builder.securityCheck()
-         }
-       }
-
-       stage("Sonar Scan") {
-         pl.callAround('sonarscan') {
-        if (Jenkins.instance.getPluginManager().getPlugins().find { it.getShortName() == 'sonar' } != null) {
-          withSonarQubeEnv("SonarQube") {
-            builder.sonarScan();
+  try {
+    node {
+      platformSetup {
+        withSubscription("jenkinsServicePrincipal", "infra-vault") {
+          stage('Checkout') {
+            deleteDir()
+            checkout scm
           }
 
-          timeout(time: 1, unit: 'MINUTES') {
-            def qg = steps.waitForQualityGate()
-            if (qg.status != 'OK') {
-              error "Pipeline aborted due to quality gate failure: ${qg.status}"
+          stage("Build") {
+            pl.callAround('build') {
+              builder.build()
+            }
+          }
+
+          stage("Test") {
+            pl.callAround('test') {
+              builder.test()
+            }
+          }
+
+          stage("Security Checks") {
+            pl.callAround('securitychecks') {
+              builder.securityCheck()
+            }
+          }
+
+          stage("Sonar Scan") {
+            pl.callAround('sonarscan') {
+              if (Jenkins.instance.getPluginManager().getPlugins().find { it.getShortName() == 'sonar' } != null) {
+                withSonarQubeEnv("SonarQube") {
+                  builder.sonarScan();
+                }
+
+                timeout(time: 1, unit: 'MINUTES') {
+                  def qg = steps.waitForQualityGate()
+                  if (qg.status != 'OK') {
+                    error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                  }
+                }
+              } else {
+                echo "Sonarqube plugin not installed. Skipping static analysis."
+              }
+            }
+          }
+
+          stage('Deploy Dev') {
+            pl.callAround('deploy:dev') {
+              deployer.deploy('dev')
+              deployer.healthCheck('dev')
+            }
+          }
+
+          stage('Smoke Tests - Dev') {
+            withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('dev')}"]) {
+              pl.callAround('smoketest:dev') {
+                builder.smokeTest()
+              }
+            }
+          }
+
+          stage("OWASP") {
+
+          }
+
+          stage('Deploy Default') {
+            pl.callAround('deploy:default') {
+              deployer.deploy('default')
+              deployer.healthCheck('default')
+            }
+          }
+
+          stage('Smoke Tests - Prod') {
+            withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('prod')}"]) {
+              pl.callAround('smoketest:prod') {
+                builder.smokeTest()
+              }
             }
           }
         }
-        else {
-          echo "Sonarqube plugin not installed. Skipping static analysis."
-        }
-         }
-       }
-
-       stage('Deploy Dev') {
-         pl.callAround('deploy:dev') {
-           deployer.deploy('dev')
-           deployer.healthCheck('dev')
-         }
-       }
-
-       stage('Smoke Tests - Dev') {
-         withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('dev')}"]) {
-           pl.callAround('smoketest:dev') {
-             builder.smokeTest()
-           }
-         }
-       }
-
-       stage("OWASP") {
-
-       }
-
-        stage('Deploy Default') {
-          pl.callAround('deploy:default') {
-            deployer.deploy('default')
-            deployer.healthCheck('default')
-          }
-        }
-
-       stage('Smoke Tests - Prod') {
-         withEnv(["SMOKETEST_URL=${deployer.getServiceUrl('prod')}"]) {
-           pl.callAround('smoketest:prod') {
-             builder.smokeTest()
-           }
-         }
-       }
       }
     }
- }
+  } catch (err) {
+    if (pl.slackChannel) {
+      notifyBuildFailure channel: pl.slackChannel
+    }
+
+    pl.call('onFailure')
+    throw err
+  }
+
+  if (pl.slackChannel) {
+    notifyBuildFixed channel: pl.slackChannel
+  }
+
+  pl.call('onSuccess')
 }
