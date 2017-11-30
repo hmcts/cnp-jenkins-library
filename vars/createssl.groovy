84e0661e3930e763694fd7f330f655ba705aca0e
James Whinn <jameswhinn89@gmail.com>
2017-11-30 12:07:54 +0000

    fix params for createssl

--- vars/createssl.groovy
+++ vars/createssl.groovy
@@ -1,12 +1,12 @@
 import uk.gov.hmcts.contino.*
-import groovy.json.JsonSlurperClassic
+import groovy.json.JsonSlurper
 
 def call(String appName) {
 
   def response = httpRequest httpMode: 'POST', requestBody: "grant_type=client_credentials&resource=https%3A%2F%2Fmanagement.core.windows.net%2F&client_id=$ARM_CLIENT_ID&client_secret=$ARM_CLIENT_SECRET", acceptType: 'APPLICATION_JSON', url: "https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token"
-  TOKEN = new JsonSlurperClassic().parseText(response.content).access_token
-  def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/applications-infra-mo-dev/providers/Microsoft.Web/hostingEnvironments/applications-compute-4-dev/capacities/virtualip?api-version=2016-09-01"
-  def internalip = new JsonSlurperClassic().parseText(vip.content).internalIpAddress
+  TOKEN = new JsonSlurper().parseText(response.content).access_token
+  def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-${platform}/providers/Microsoft.Web/hostingEnvironments/core-compute-${platform}/capacities/virtualip?api-version=2016-09-01"
+  def internalip = new JsonSlurper().parseText(vip.content).internalIpAddress
   println internalip
 
   echo "Running SSL certificate creation script"
@@ -17,4 +17,3 @@ def call(String appName) {
 
   result = sh "bash webapp-configure.sh ${appName} ${pfxPass} core-infra-${platform} ${internalip}"
 }
-
