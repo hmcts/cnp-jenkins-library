6290e68cf0d4671bc272d070573df94b4665ae52
James Whinn <jameswhinn89@gmail.com>
2017-12-04 10:57:13 +0000

    .

--- resources/uk/gov/hmcts/contino/ilbSSL.sh
+++ resources/uk/gov/hmcts/contino/ilbSSL.sh
@@ -31,11 +31,11 @@ EOF
 
 sed -i "s/commonNameVar/$domain/g" $domain.conf
 
-sudo openssl req -new -sha256 -nodes -out \*.$domain.csr -newkey rsa:2048 -keyout \*.$domain.key -config <( cat $domain.conf )
+openssl req -new -sha256 -nodes -out \*.$domain.csr -newkey rsa:2048 -keyout \*.$domain.key -config <( cat $domain.conf )
 
-sudo openssl x509 -req -in \*.$domain.csr -signkey \*.$domain.key -out $domain.cer
+openssl x509 -req -in \*.$domain.csr -signkey \*.$domain.key -out $domain.cer
 
-sudo openssl pkcs12 -export -in $domain.cer -inkey \*.$domain.key -out $domain.pfx -password pass:$pfxPass
+openssl pkcs12 -export -in $domain.cer -inkey \*.$domain.key -out $domain.pfx -password pass:$pfxPass
 
 rm -f \*.$domain.key \*.$domain.csr $domain.conf
 
