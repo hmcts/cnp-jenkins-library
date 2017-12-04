f096aac7cc3f4e0e9da205212c8386dbfe0c1b02
Neil Richardson <njrich28@gmail.com>
2017-12-04 11:27:58 +0000

    Change env to nonprod

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -79,7 +79,7 @@ def call(type, String product, String app, Closure body) {
            dir('infrastructure') {
              lock("${product}-test") {
                stage('getIlbIp') {
-                 def envSuffix = 'test'
+                 def envSuffix = 'nonprod'
                  def response = httpRequest httpMode: 'POST', requestBody: "grant_type=client_credentials&resource=https%3A%2F%2Fmanagement.core.windows.net%2F&client_id=$ARM_CLIENT_ID&client_secret=$ARM_CLIENT_SECRET", acceptType: 'APPLICATION_JSON', url: "https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token"
                  TOKEN = new JsonSlurperClassic().parseText(response.content).access_token
                  def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-$envSuffix/providers/Microsoft.Web/hostingEnvironments/core-compute-$envSuffix/capacities/virtualip?api-version=2016-09-01"
