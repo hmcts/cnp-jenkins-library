0ab471f86b476f55d1500ff053b819c7726f8dcf
Neil Richardson <njrich28@gmail.com>
2017-12-07 10:20:24 +0000

    Comment out nonprod build and add prod infrastructure stages

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -79,7 +79,7 @@ def call(type, String product, String app, Closure body) {
         }
 
         onMaster {
-
+/*
           folderExists('infrastructure') {
             terraform.ini("${product}-${app}", this)
             withSubscription('nonprod') {
@@ -119,9 +119,34 @@ def call(type, String product, String app, Closure body) {
               }
             }
           }
-
+*/
           stage("OWASP") {
 
+          }
+
+          folderExists('infrastructure') {
+            terraform.ini("${product}-${app}", this)
+            withSubscription('prod') {
+              dir('infrastructure') {
+                lock("${product}-${app}-prod") {
+                  stage('getIlbIp') {
+                    def envSuffix = 'prod'
+                    def response = httpRequest httpMode: 'POST', requestBody: "grant_type=client_credentials&resource=https%3A%2F%2Fmanagement.core.windows.net%2F&client_id=$ARM_CLIENT_ID&client_secret=$ARM_CLIENT_SECRET", acceptType: 'APPLICATION_JSON', url: "https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token"
+                    TOKEN = new JsonSlurperClassic().parseText(response.content).access_token
+                    def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-$envSuffix/providers/Microsoft.Web/hostingEnvironments/core-compute-$envSuffix/capacities/virtualip?api-version=2016-09-01"
+                    def internalip = new JsonSlurperClassic().parseText(vip.content).internalIpAddress
+                    println internalip
+                    env.TF_VAR_ilbIp = internalip
+                  }
+                  stage('Infrastructure Plan - prod') {
+                    terraform.plan('prod')
+                  }
+                  stage('Infrastructure Build - prod') {
+                    terraform.apply('prod')
+                  }
+                }
+              }
+            }
           }
 
           stage('Deploy Prod') {
