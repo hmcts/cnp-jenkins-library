f1721fb05eaa54917546d4c8b4b86a73cfc15fac
Calin Marina <calin.marina@gmail.com>
2017-12-11 14:42:45 +0000

    trying

--- vars/stateStoreInit.groovy
+++ vars/stateStoreInit.groovy
@@ -1,7 +1,8 @@
 #!groovy
+import uk.gov.hmcts.contino.*
 
 //can be run only inside withSubscription
-def call(environment) {
+def call(String environment) {
   stage("Check/Init state store for env '${environment}'") {
     def functions = libraryResource 'uk/gov/hmcts/contino/stateStoreInit.sh'
 //    writeFile file: 'stateStoreInit.sh', text: functions
@@ -13,6 +14,6 @@ def call(environment) {
     sacontainer_name = "${env.STORE_sa_container_name_template}${environment}"
 
     echo "before running bash"
-    result = sh "bash stateStoreInit.sh $__rg $sa_name $sacontainer_name $__location"
+    sh "bash stateStoreInit.sh $__rg $sa_name $sacontainer_name $__location"
   }
 }
