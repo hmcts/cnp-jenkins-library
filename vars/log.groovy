b64b23140e4e5966332c415ad50abe1420cb3eee
Calin Marina <calin.marina@gmail.com>
2017-12-11 15:16:20 +0000

    small fix for logging

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,12 +6,12 @@ def info(message) {
 
 def warning(message) {
   ansiColor('xterm') {
-    echo "\\e[35mWARNING: ${message}\\e[0m"
+    echo "\\u001B[35mWARNING: ${message}\\u001B[0m"
   }
 }
 
 def error(message) {
   ansiColor('xterm') {
-    echo "\\e[31mERROR: ${message}\\e[0m"
+    echo "\\u001B[31mERROR: ${message}\\u001B[0m"
   }
 }
