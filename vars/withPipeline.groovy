e813a47d9c6b181a3d837d1f421d2fcdd0ba19c4
Neil Richardson <960806+njrich28@users.noreply.github.com>
2017-12-11 16:10:06 +0000

    Remove getilbip stage (#73)
    * Push ILB IP retrieval into separate method
    
    * Update variable name to avoid clash

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -4,7 +4,6 @@ import uk.gov.hmcts.contino.NodePipelineType
 import uk.gov.hmcts.contino.PipelineCallbacks
 import uk.gov.hmcts.contino.PipelineType
 import uk.gov.hmcts.contino.SpringBootPipelineType
-import groovy.json.JsonSlurperClassic
 
 def call(type, String product, String app, Closure body) {
   def pipelineTypes = [
@@ -129,20 +128,13 @@ def call(type, String product, String app, Closure body) {
             withSubscription('prod') {
               dir('infrastructure') {
                 lock("${product}-${app}-prod") {
-                  stage('getIlbIp') {
-                    def envSuffix = 'prod'
-                    def response = httpRequest httpMode: 'POST', requestBody: "grant_type=client_credentials&resource=https%3A%2F%2Fmanagement.core.windows.net%2F&client_id=$ARM_CLIENT_ID&client_secret=$ARM_CLIENT_SECRET", acceptType: 'APPLICATION_JSON', url: "https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token"
-                    TOKEN = new JsonSlurperClassic().parseText(response.content).access_token
-                    def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-$envSuffix/providers/Microsoft.Web/hostingEnvironments/core-compute-$envSuffix/capacities/virtualip?api-version=2016-09-01"
-                    def internalip = new JsonSlurperClassic().parseText(vip.content).internalIpAddress
-                    println internalip
-                    env.TF_VAR_ilbIp = internalip
-                  }
-                  stage('Infrastructure Plan - prod') {
-                    terraform.plan('prod')
-                  }
-                  stage('Infrastructure Build - prod') {
-                    terraform.apply('prod')
+                  withIlbIp('prod') {
+                    stage('Infrastructure Plan - prod') {
+                      terraform.plan('prod')
+                    }
+                    stage('Infrastructure Build - prod') {
+                      terraform.apply('prod')
+                    }
                   }
                 }
               }
