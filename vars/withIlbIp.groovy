873392a85b3d80a6fa0a6f38daf82d84934ff701
Neil Richardson <njrich28@gmail.com>
2017-12-11 15:51:43 +0000

    Update variable name to avoid clash

--- vars/withIlbIp.groovy
+++ vars/withIlbIp.groovy
@@ -1,10 +1,10 @@
 #!groovy
 import groovy.json.JsonSlurperClassic
 
-def call(String env, Closure body) {
+def call(String environment, Closure body) {
     def response = httpRequest httpMode: 'POST', requestBody: "grant_type=client_credentials&resource=https%3A%2F%2Fmanagement.core.windows.net%2F&client_id=$ARM_CLIENT_ID&client_secret=$ARM_CLIENT_SECRET", acceptType: 'APPLICATION_JSON', url: "https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token"
     TOKEN = new JsonSlurperClassic().parseText(response.content).access_token
-    def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-$env/providers/Microsoft.Web/hostingEnvironments/core-compute-$env/capacities/virtualip?api-version=2016-09-01"
+    def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-$environment/providers/Microsoft.Web/hostingEnvironments/core-compute-$environment/capacities/virtualip?api-version=2016-09-01"
     def internalip = new JsonSlurperClassic().parseText(vip.content).internalIpAddress
     println internalip
     env.TF_VAR_ilbIp = internalip
