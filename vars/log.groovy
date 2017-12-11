18e8569b21c7e8c0fda2d803876a3943eedc42a0
Calin Marina <calin.marina@gmail.com>
2017-12-11 12:11:31 +0000

    try ansicolor directly in log

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,9 +5,13 @@ def info(message) {
 }
 
 def warning(message) {
-  echo "\\e[35mWARNING: ${message}\\e[0m"
+  ansiColor {
+    echo "\\e[35mWARNING: ${message}\\e[0m"
+  }
 }
 
 def error(message) {
-  echo "\\e[31mERROR: ${message}\\e[0m"
+  ansiColor {
+    echo "\\e[31mERROR: ${message}\\e[0m"
+  }
 }
