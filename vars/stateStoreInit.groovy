6a278ddf9b51b785378bc747acbefafbd2ee3154
Calin Marina <calin.marina@gmail.com>
2017-12-11 11:19:19 +0000

    fix env overwritten by local var

--- vars/stateStoreInit.groovy
+++ vars/stateStoreInit.groovy
@@ -1,15 +1,15 @@
 import uk.gov.hmcts.contino.*
 
 //can be run only inside withSubscription
-def call(String env) {
+def call(String environment) {
 
   def functions = libraryResource 'uk/gov/hmcts/contino/stateStoreInit.sh'
   writeFile file: 'stateStoreInit.sh', text: functions
 
   __location = 'uksouth'
-  __rg= env.STORE_rg_name_template+ "-"+ env
-  sa_name= STORE_sa_name_template+ env
-  sacontainer_name = STORE_sa_container_name_template+ env
+  __rg= env.STORE_rg_name_template+ "-"+ environment
+  sa_name= env.STORE_sa_name_template+ environment
+  sacontainer_name = env.STORE_sa_container_name_template+ environment
 
   sh "bash stateStoreInit.sh $__rg $sa_name $sacontainer_name $__location"
 }
