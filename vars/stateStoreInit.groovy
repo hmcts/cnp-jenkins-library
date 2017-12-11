1c8ec270434c73c2137091a4c4aef6f256df2236
Calin Marina <calin.marina@gmail.com>
2017-12-11 14:57:09 +0000

    put back writeFile to have an sh to work with

--- vars/stateStoreInit.groovy
+++ vars/stateStoreInit.groovy
@@ -5,15 +5,13 @@ import uk.gov.hmcts.contino.*
 def call(String environment) {
   stage("Check/Init state store for env '${environment}'") {
     def functions = libraryResource 'uk/gov/hmcts/contino/stateStoreInit.sh'
-//    writeFile file: 'stateStoreInit.sh', text: functions
+    writeFile file: 'stateStoreInit.sh', text: functions
 
-    echo "got past writeFile"
     __location = 'uksouth'
     __rg = "${env.STORE_rg_name_template}-${environment}"
     sa_name = "${env.STORE_sa_name_template}${environment}"
     sacontainer_name = "${env.STORE_sa_container_name_template}${environment}"
 
-    echo "before running bash"
     sh "bash stateStoreInit.sh $__rg $sa_name $sacontainer_name $__location"
   }
 }
