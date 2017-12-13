92bb5090ed1878f274547e1d0fe070b594f975d4
Neil Richardson <njrich28@gmail.com>
2017-12-13 15:52:43 +0000

    Use correct credentials

--- src/uk/gov/hmcts/contino/WebAppDeploy.groovy
+++ src/uk/gov/hmcts/contino/WebAppDeploy.groovy
@@ -133,7 +133,7 @@ class WebAppDeploy implements Serializable {
   def deployJavaWebApp(env) {
     return steps.withCredentials(
       [[$class: 'UsernamePasswordMultiBinding',
-        credentialsId: 'jenkins-github-api-token',
+        credentialsId: 'WebAppDeployCredentials',
         usernameVariable: 'GIT_USERNAME',
         passwordVariable: 'GIT_PASSWORD']]) {
 
