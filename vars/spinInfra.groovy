b8b5dff03b9f54a447909d3221f1db2982f526e7
Daniel Sanabria <sanabria.d@gmail.com>
2017-12-20 10:57:41 +0000

    using environment and subscriptions to look for containers and storage accounts respectively

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -1,46 +1,21 @@
 #!groovy
 
-/*
- @param productName
- @param environment
-        the suffix of storage account group and rest of created resources
- @param planOnly
-        will only run terraform plan, apply will be skipped
-        Default: false
-*/
-def call(productName, subscription, planOnly = false) {
-/*  //TODO: make it work with optoinal parameters for planOnly
-  // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
+def call(productName, environment, planOnly = false, subscription) {
 
-  if (!config.containsKey("productName"))
-    throw new Exception("You have not specified a productName therefore not sure what to build! Stopping execution...")
-  if (!config.containsKey("environment"))
-    throw new Exception("You have not specified a environment therefore not sure what to build! Stopping execution...")
-  //default subscription = nonprod if not otherwise specified
-  if (!config.containsKey("subscription"))
-    config.subscription = "nonprod"
-  //default planOnly = false
-  if (!config.containsKey("planOnly"))
-    config.planOnly = false
-*/
   if (env.SUBSCRIPTION_NAME == null)
     throw new Exception("There is no SUBSCRIPTION_NAME environment variable, are you running inside a withSubscription block?")
 
-  stateStoreInit(subscription)
+  stateStoreInit(environment)
 
-  lock("${productName}-${subscription}") {
-    stage("Plan ${productName}-${subscription} in ${subscription}") {
+  lock("${productName}-${environment}") {
+    stage("Plan ${productName}-${environment} in ${environment}") {
       if (env.STORE_rg_name_template != null &&
         env.STORE_sa_name_template != null &&
         env.STORE_sa_container_name_template != null) {
         log.warning("Using following stateStore={" +
-          "'rg_name': '${env.STORE_rg_name_template}-${subscription}', " +
+          "'rg_name': '${env.STORE_rg_name_template}-${environment}', " +
           "'sa_name': '${env.STORE_sa_name_template}${subscription}', " +
-          "'sa_container_name': '${env.STORE_sa_container_name_template}${subscription}'}")
+          "'sa_container_name': '${env.STORE_sa_container_name_template}${environment}'}")
       } else
         throw new Exception("State store name details not found in environment variables?")
 
@@ -48,18 +23,18 @@ def call(productName, subscription, planOnly = false) {
 
       sh "terraform init -reconfigure -backend-config " +
         "\"storage_account_name=${env.STORE_sa_name_template}${subscription}\" " +
-        "-backend-config \"container_name=${STORE_sa_container_name_template}${subscription}\" " +
-        "-backend-config \"resource_group_name=${env.STORE_rg_name_template}-${subscription}\" " +
-        "-backend-config \"key=${productName}/${subscription}/terraform.tfstate\""
+        "-backend-config \"container_name=${STORE_sa_container_name_template}${environment}\" " +
+        "-backend-config \"resource_group_name=${env.STORE_rg_name_template}-${environment}\" " +
+        "-backend-config \"key=${productName}/${environment}/terraform.tfstate\""
 
       sh "terraform get -update=true"
-      sh "terraform plan -var 'env=${subscription}' -var 'name=${productName}'" +
-        (fileExists("${subscription}.tfvars") ? " var-file=${subscription}.tfvars" : "")
+      sh "terraform plan -var 'env=${environment}' -var 'name=${productName}'" +
+        (fileExists("${environment}.tfvars") ? " var-file=${environment}.tfvars" : "")
 
       if (!planOnly) {
-        stage("Apply ${productName}-${subscription} in ${subscription}") {
-          sh "terraform apply -auto-approve -var 'env=${subscription}' -var 'name=${productName}'" +
-            (fileExists("${subscription}.tfvars") ? " var-file=${subscription}.tfvars" : "")
+        stage("Apply ${productName}-${environment} in ${environment}") {
+          sh "terraform apply -auto-approve -var 'env=${environment}' -var 'name=${productName}'" +
+            (fileExists("${environment}.tfvars") ? " var-file=${environment}.tfvars" : "")
         }
       } else
         log.warning "Skipping apply due to planOnly flag set"
--- vars/stateStoreInit.groovy
+++ vars/stateStoreInit.groovy
@@ -2,14 +2,14 @@
 import uk.gov.hmcts.contino.*
 
 //can be run only inside withSubscription
-def call(String environment) {
+def call(String environment, String subscription) {
   stage("Check/Init state store for env '${environment}'") {
     def functions = libraryResource 'uk/gov/hmcts/contino/stateStoreInit.sh'
     writeFile file: 'stateStoreInit.sh', text: functions
 
     __location = 'uksouth'
     __rg = "${env.STORE_rg_name_template}-${environment}"
-    sa_name = "${env.STORE_sa_name_template}${environment}"
+    sa_name = "${env.STORE_sa_name_template}${subscription}"
     sacontainer_name = "${env.STORE_sa_container_name_template}${environment}"
 
     sh "bash stateStoreInit.sh $__rg $sa_name $sacontainer_name $__location"
