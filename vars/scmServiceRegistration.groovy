f24c24c00c583eefb358addd469386ff658555f5
sanabriad <sanabria.d@gmail.com>
2018-01-24 13:29:20 +0000

    increasing to 60 secs for all methods

--- vars/scmServiceRegistration.groovy
+++ vars/scmServiceRegistration.groovy
@@ -36,9 +36,9 @@ def call(environment) {
 // Get Auth Token
   println "Getting access token from management.azure.com ..."
   OkHttpClient client = new OkHttpClient.Builder()
-    .connectTimeout(10, TimeUnit.SECONDS)
-    .writeTimeout(10, TimeUnit.SECONDS)
-    .readTimeout(30, TimeUnit.SECONDS)
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .writeTimeout(60, TimeUnit.SECONDS)
+    .readTimeout(60, TimeUnit.SECONDS)
     .build()
 
   MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded")
