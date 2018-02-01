72863e5bb103748e0771f5d54cc6a2a7d7c07384
ChrisAnn <christine.ann.odell@gmail.com>
2018-02-01 15:16:12 +0000

    Instantiate property classes in the constructors

--- src/uk/gov/hmcts/contino/SpringBootPipelineType.groovy
+++ src/uk/gov/hmcts/contino/SpringBootPipelineType.groovy
@@ -5,13 +5,15 @@ class SpringBootPipelineType implements PipelineType, Serializable {
   def product
   def app
 
+  Builder builder
+  Deployer deployer
+
   SpringBootPipelineType(steps, product, app) {
     this.steps = steps
     this.product = product
     this.app = app
+
+    builder = new GradleBuilder(steps, product)
+    deployer = new JavaDeployer(steps, product, app)
   }
-
-  Builder builder = new GradleBuilder(steps, product)
-
-  Deployer deployer = new JavaDeployer(steps, product, app)
 }
