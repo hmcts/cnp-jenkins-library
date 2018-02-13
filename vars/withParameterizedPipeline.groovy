40440be82039c9e1e656eae922d4dd69bdc3b2f3
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-13 15:34:40 +0000

    Try out smoke and functional tests in parameterised pipeline

--- vars/withParameterizedPipeline.groovy
+++ vars/withParameterizedPipeline.groovy
@@ -77,6 +77,24 @@ def call(type, String product, String component, String environment, String subs
           }
         }
 
+        stage("Smoke test - ${environment}") {
+          withEnv(["TEST_URL=${deployer.getServiceUrl(environment)}"]) {
+            pl.callAround('smoketest:${environment}') {
+              echo "Using TEST_URL: '$TEST_URL'"
+              builder.smokeTest()
+            }
+          }
+        }
+
+        stage("Functional Test - ${environment}") {
+          withEnv(["TEST_URL=${deployer.getServiceUrl(environment)}"]) {
+            pl.callAround('functionaltest:${environment}') {
+              echo "Using TEST_URL: '$TEST_URL'"
+              builder.functionalTest()
+            }
+          }
+        }
+
         stage("Promote - ${environment} (staging -> production slot)") {
           withSubscription(subscription) {
             sh "az webapp deployment slot swap --name \"${product}-${component}-${environment}\" --resource-group \"${product}-${component}-${environment}\" --slot staging --target-slot production"
