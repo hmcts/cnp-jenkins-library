415497756ae2f1cd20d9d499fb63beed249c41e4
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-19 08:07:40 +0000

    Reuse deploy to env for parameterised pipeline
    It keeps getting out of sync with the path to live pipeline and
    dupicated, I can't see a reason why the code can't be reused

--- vars/withParameterizedPipeline.groovy
+++ vars/withParameterizedPipeline.groovy
@@ -1,10 +1,9 @@
 import uk.gov.hmcts.contino.Builder
-import uk.gov.hmcts.contino.Deployer
+import uk.gov.hmcts.contino.MetricsPublisher
 import uk.gov.hmcts.contino.NodePipelineType
 import uk.gov.hmcts.contino.PipelineCallbacks
 import uk.gov.hmcts.contino.PipelineType
 import uk.gov.hmcts.contino.SpringBootPipelineType
-import uk.gov.hmcts.contino.MetricsPublisher
 
 def call(type, String product, String component, String environment, String subscription, Closure body) {
   def pipelineTypes = [
@@ -22,7 +21,6 @@ def call(type, String product, String component, String environment, String subs
 
   assert pipelineType != null
 
-  Deployer deployer = pipelineType.deployer
   Builder builder = pipelineType.builder
 
   MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild, product, component)
@@ -54,66 +52,13 @@ def call(type, String product, String component, String environment, String subs
           }
         }
 
-        folderExists('infrastructure') {
-          withSubscription(subscription) {
-            dir('infrastructure') {
-              withIlbIp(environment) {
-                tfOutput = spinInfra("${product}-${component}", environment, false, subscription)
-                scmServiceRegistration(environment)
-              }
-            }
-          }
-          if (pl.migrateDb) {
-            stage("DB Migration - ${environment}") {
-              builder.dbMigrate(tfOutput.vaultName.value)
-            }
-          }
-        }
-
-        stage("Deploy $environment") {
-          pl.callAround("deploy:$environment") {
-            deployer.deploy(environment)
-            deployer.healthCheck(environment, "staging")
-          }
-        }
-
-        withSubscription(subscription) {
-          wrap([
-            $class: 'AzureKeyVaultBuildWrapper',
-            keyVaultURLOverride: tfOutput?.vaultUri?.value,
-            azureKeyVaultSecrets: pl.vaultSecrets
-          ]) {
-            echo ">>> Vault URI ${tfOutput?.vaultUri?.value}"
-            sh 'echo ">>> Listing injected secrets"'
-            sh 'echo $AAT_TEST_USER_USERNAME'
-            sh 'echo $AAT_TEST_USER_PASSWORD'
-            sh 'echo $AAT_TEST_USER_EMAIL_PATTERN'
-            stage("Smoke test - ${environment}") {
-              withEnv(["TEST_URL=${deployer.getServiceUrl(environment)}"]) {
-                pl.callAround('smoketest:${environment}') {
-                  echo "Using TEST_URL: '$TEST_URL'"
-                  builder.smokeTest()
-                }
-              }
-            }
-
-            stage("Functional Test - ${environment}") {
-              withEnv(["TEST_URL=${deployer.getServiceUrl(environment)}"]) {
-                pl.callAround('functionaltest:${environment}') {
-                  echo "Using TEST_URL: '$TEST_URL'"
-                  builder.functionalTest()
-                }
-              }
-            }
-          }
-        }
-
-
-        stage("Promote - ${environment} (staging -> production slot)") {
-          withSubscription(subscription) {
-            sh "az webapp deployment slot swap --name \"${product}-${component}-${environment}\" --resource-group \"${product}-${component}-${environment}\" --slot staging --target-slot production"
-          }
-        }
+        sectionDeployToEnvironment(
+          pipelineCallbacks: pl,
+          pipelineType: pipelineType,
+          subscription: subscription,
+          environment: environment,
+          product: product,
+          component: component)
 
       }
     } catch (err) {
