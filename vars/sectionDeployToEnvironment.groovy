01d31802251f622253f9a4e360db6af05d4be921
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-19 13:47:37 +0000

    More debug

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -42,15 +42,16 @@ def call(params) {
 
   wrap([
     $class              : 'AzureKeyVaultBuildWrapper',
-    keyVaultURLOverride : tfOutput?.vaultUri?.value,
-    azureKeyVaultSecrets: pl.vaultSecrets
+    azureKeyVaultSecrets: pl.vaultSecrets,
+    keyVaultURLOverride : tfOutput?.vaultUri?.value
   ]) {
     echo ">>> Vault URI ${tfOutput?.vaultUri?.value}"
-    echo ">>> TF_VAR_jenkins_AAD_objectId ${env.TF_VAR_jenkins_AAD_objectId}"
     sh 'echo ">>> Listing injected secrets"'
+    sh 'echo $pl.vaultSecrets'
     sh 'echo $AAT_TEST_USER_USERNAME'
     sh 'echo $AAT_TEST_USER_PASSWORD'
     sh 'echo $AAT_TEST_USER_EMAIL_PATTERN'
+    echo pl.vaultSecrets.toString()
 
     stage("Smoke Test - ${environment} (staging slot)") {
       withEnv(["TEST_URL=${deployer.getServiceUrl(environment, "staging")}"]) {
