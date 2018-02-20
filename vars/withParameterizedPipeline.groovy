9fe0952a6f97a426dc45ebbd4acf87790cfca5d3
Valentin Laurin <vlaurin@Valentins-MacBook-Pro-2.local>
2018-02-20 12:40:13 +0000

    Angular: Debug error

--- vars/withParameterizedPipeline.groovy
+++ vars/withParameterizedPipeline.groovy
@@ -27,7 +27,7 @@ def call(type, String product, String component, String environment, String subs
   Deployer deployer = pipelineType.deployer
   Builder builder = pipelineType.builder
 
-  println "!!!! Deployer: " + deployer.getClass().getName()
+  println "!!!! Builder: " + builder.getClass().getName()
 
   MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild, product, component)
   def pl = new PipelineCallbacks(metricsPublisher)
