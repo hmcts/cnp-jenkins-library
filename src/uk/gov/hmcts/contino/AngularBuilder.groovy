bfba1a4c05489de6120e407bbe7ad223ccbad9ae
Valentin Laurin <vlaurin@Valentins-MacBook-Pro-2.local>
2018-02-20 14:42:34 +0000

    Angular: Rewrite AngularBuilder as decorator

--- src/uk/gov/hmcts/contino/AngularBuilder.groovy
+++ src/uk/gov/hmcts/contino/AngularBuilder.groovy
@@ -1,16 +1,50 @@
 package uk.gov.hmcts.contino
 
-class AngularBuilder extends YarnBuilder {
+class AngularBuilder implements Builder, Serializable {
+
+  def steps
+
+  YarnBuilder builder
 
   AngularBuilder(steps) {
-    super(steps)
+    this.steps = steps
+    this.builder = new YarnBuilder(steps)
   }
 
   @Override
   def build() {
     println "!!! inside build"
-    super.build()
+    builder.build()
+    builder.yarn("build:ssr")
+  }
 
-    yarn("build:ssr")
+  @Override
+  def test() {
+    builder.test()
+  }
+
+  @Override
+  def sonarScan() {
+    builder.sonarScan()
+  }
+
+  @Override
+  def smokeTest() {
+    builder.smokeTest()
+  }
+
+  @Override
+  def functionalTest() {
+    builder.functionalTest()
+  }
+
+  @Override
+  def securityCheck() {
+    builder.securityCheck()
+  }
+
+  @Override
+  def addVersionInfo() {
+    builder.addVersionInfo()
   }
 }
