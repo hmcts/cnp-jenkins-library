b78c23c1f5a86aba07c7c02b35e99cecba5b6cbf
Calin Marina <calin.marina@gmail.com>
2018-03-01 12:09:42 +0000

    updated for az cli command race condition

--- resources/uk/gov/hmcts/contino/peerVnets.sh
+++ resources/uk/gov/hmcts/contino/peerVnets.sh
@@ -11,49 +11,49 @@ resourceB="core-infra-$3"
 subscriptionB=$4
 nameB="CNPAppProdToCNPMgmtProd"
 
-az account set --subscription ${subscriptionA}
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az account set --subscription ${subscriptionA}
 
 # Peer VNetA to VNetB
-az network vnet peering create \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering create \
  --name ${nameA} \
  --resource-group ${resourceA} \
  --vnet-name ${networkA} \
  --remote-vnet-id /subscriptions/${subscriptionB}/resourceGroups/${resourceB}/providers/Microsoft.Network/VirtualNetworks/${networkB} \
  --allow-vnet-access
-az network vnet peering update \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering update \
  --vnet-name ${networkA} \
  --name ${nameA} \
  --resource-group ${resourceA} \
  --set allowForwardedTraffic==true
-az network vnet peering update \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering update \
  --vnet-name ${networkA} \
  --name ${nameA} \
  --resource-group ${resourceA} \
  --set allowVirtualNetworkAccess==true
-az network vnet peering list \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering list \
  --resource-group ${resourceA} \
  --vnet-name ${networkA} \
  --output table
-az account set --subscription ${subscriptionB}
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az account set --subscription ${subscriptionB}
 
 # Peer VNetB to VNetA
-az network vnet peering create \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering create \
   --name ${nameB} \
   --resource-group ${resourceB} \
   --vnet-name ${networkB} \
   --remote-vnet-id /subscriptions/${subscriptionA}/resourceGroups/${resourceA}/providers/Microsoft.Network/VirtualNetworks/${networkA} \
   --allow-vnet-access
-az network vnet peering update \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering update \
   --vnet-name ${networkB} \
   --name ${nameB} \
   --resource-group ${resourceB} \
   --set allowForwardedTraffic==true
-az network vnet peering update \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering update \
   --vnet-name ${networkB} \
   --name ${nameB} \
   --resource-group ${resourceB} \
   --set allowVirtualNetworkAccess==true
-az network vnet peering list \
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az network vnet peering list \
   --resource-group ${resourceB} \
   --vnet-name ${networkB} \
   --output table
