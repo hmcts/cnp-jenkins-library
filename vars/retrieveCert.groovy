043b0211d15bd3c8f5544a3fb3b0f85cdd13d011
James Whinn <jameswhinn89@gmail.com>
2018-03-06 14:38:00 +0000

    update cert name

--- vars/retrieveCert.groovy
+++ vars/retrieveCert.groovy
@@ -26,7 +26,7 @@ def call(String environment) {
       // Setting environment vars consumed by TF
       sh "az keyvault certificate show --vault-name $TF_VAR_vaultName --name ${environment} --query x509ThumbprintHex --output tsv > thumbhex.txt"
       env.TF_VAR_certificateThumbprint = readFile('thumbhex.txt')
-      env.TF_VAR_certificateName = "${environment}"
+      env.TF_VAR_certificateName = "core-compute-${environment}"
       // if(["sandbox","saat","sprod"]).contains("${environment}"){
       //   env.TF_VAR_vaultName = "infra-vault-sandbox"
       // }
