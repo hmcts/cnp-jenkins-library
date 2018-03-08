d5966250b8981957c9dd1abed042c6b797abcc90
Calin Marina <calin.marina@gmail.com>
2018-03-08 15:46:46 +0000

    try again with old code

--- vars/folderExists.groovy
+++ vars/folderExists.groovy
@@ -13,13 +13,13 @@ import java.nio.file.FileSystems
 def call(String folderPath, Closure block) {
   def localPath = sh(script: 'pwd', returnStdout: true).trim()
 
-  path = FileSystems.getDefault().getPath(localPath, folderPath).toString()
+  /*path = FileSystems.getDefault().getPath(localPath, folderPath).toString()
   log.info("looking for path: $path")
   if (fileExists(path))
+    return block.call()*/
+  if (Files.exists(FileSystems.getDefault().getPath(localPath, folderPath))) {
     return block.call()
-  /*if (Files.exists(FileSystems.getDefault().getPath(localPath, folderPath))) {
-    return block.call()
-  }*/
+  }
   else
     log.info("$folderPath not found => There is no infrastructure to build")
 }
