68f86adfaff65b19d7ef2752bdac9db3b3f12be8
Calin Marina <calin.marina@gmail.com>
2018-03-21 23:26:39 +0000

    update cert creation bash script

--- resources/uk/gov/hmcts/contino/ilbSSL.sh
+++ resources/uk/gov/hmcts/contino/ilbSSL.sh
@@ -5,10 +5,12 @@ pfxPass=$2 #$(cat /dev/random | LC_CTYPE=C tr -dc "[:alpha:]" | head -c 8)
 platform=$3
 subscription=$4
 
-echo "Creating Self-Signed cert for $domain"
+certjson=$(env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az keyvault certificate show --vault-name app-vault-${subscription} -n $domain)
+if [[ $certjson = *"not found"* ]]; then
+  echo "Creating Self-Signed cert for $domain"
 
-echo "workspace for script = ${WORKSPACE}"
-cat > $domain.conf <<-EOF
+  echo "workspace for script = ${WORKSPACE}"
+  cat > $domain.conf <<-EOF
 [req]
 default_bits = 2048
 prompt = no
@@ -29,13 +31,9 @@ subjectAltName = @alt_names
 DNS.1 = *.scm.service.commonNameVar.internal
 
 EOF
-commonName=core-compute-$platform
-sed -i "s/commonNameVar/$commonName/g" $domain.conf
+  commonName=core-compute-$platform
+  sed -i "s/commonNameVar/$commonName/g" $domain.conf
 
-
-certjson=$(env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az keyvault certificate show --vault-name app-vault-${subscription} -n $domain)
-if [[ $certjson = *"not found"* ]]; then
-  echo "Cert doesn't exist... will create one"
   openssl req -new -sha256 -nodes -out $domain.csr -newkey rsa:2048 -keyout $domain.key -config <( cat $domain.conf )
   openssl x509 -req -in $domain.csr -signkey $domain.key -out $domain.cer
   openssl pkcs12 -export -in $domain.cer -inkey $domain.key -out $domain.pfx -password pass:$pfxPass
