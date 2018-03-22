a44a9da4cb3ac4cacf9075d531c1f4db7e3c419a
James Whinn <jameswhinn89@gmail.com>
2018-03-22 15:32:36 +0000

    add shutter.groovy

---
+++ vars/shutter.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+def call(productName, environment, subscription, status){
+  withSubscription(subscription){
+
+    if ( status == "on") {
+      endpointStatus = "Enabled"
+    }
+    else {
+      endpointStatus = "Disabled"
+    }
+
+    sh "az network traffic-manager endpoint update --resource-group ${productName}-${environment } --profile-name ${productName}-${environment } --name app -type externalEndpoints --endpoint-status ${endpointStatus}"
+
+     }
+
+  }
