47baf4a4e23377c04b922368abc75fdbb755c6f2
Calin Marina <calin.marina@gmail.com>
2018-03-23 12:12:01 +0000

    cleanup

--- vars/retrieveCert.groovy
+++ vars/retrieveCert.groovy
@@ -9,7 +9,7 @@ def call(String environment) {
 
   // Setting environment vars consumed by TF
   env.TF_VAR_certificateName = "core-compute-${environment}"
-  thumbPrint = az(/keyvault certificate list --vault-name ${env.INFRA_VAULT_NAME} --query "[?contains(id,'${env.TF_VAR_certificateName}')]" --query x509ThumbprintHex -o tsv/)
+  thumbPrint = az(/keyvault certificate list --vault-name ${env.INFRA_VAULT_NAME} --query "[?contains(id,'${env.TF_VAR_certificateName}')].x509ThumbprintHex" -o tsv/)
   // check certificate exists
   if (thumbPrint) {
     log.info("Certificate found in vault... will use the same")
@@ -23,7 +23,7 @@ def call(String environment) {
     log.info("Certificate name ${env.TF_VAR_certificateName} does not exist in vault ${env.INFRA_VAULT_NAME}! Creating one right now...")
     az(/keyvault certificate create --vault-name ${env.INFRA_VAULT_NAME} --name ${env.TF_VAR_certificateName} --policy @certificateDefaultPolicy.json/)
     log.info("Retrieving the thumbprint")
-    env.TF_VAR_certificateThumbprint = az "keyvault certificate show --vault-name $env.INFRA_VAULT_NAME --name ${env.TF_VAR_certificateName} --query x509ThumbprintHex --output tsv"
+    env.TF_VAR_certificateThumbprint = az "keyvault certificate show --vault-name $env.INFRA_VAULT_NAME --name ${env.TF_VAR_certificateName} --query x509ThumbprintHex -o tsv"
   }
 
 }
