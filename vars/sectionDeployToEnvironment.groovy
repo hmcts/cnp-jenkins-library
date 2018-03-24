e3e38bc3f1a6ecb4f723fac9ec72608767503239
Neil Richardson <njrich28@gmail.com>
2018-03-24 13:08:32 +0000

    Add suffix param to deploy section

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -25,6 +25,7 @@ def call(params) {
   def environment = params.environment
   def product = params.product
   def component = params.component
+  def suffix = params.suffix
 
   Builder builder = pipelineType.builder
   Deployer deployer = pipelineType.deployer
