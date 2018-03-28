508f8838d6abf51abf3c1a7c6d484a406639d7d1
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-28 16:35:44 +0100

    Log token for debugging

--- vars/githubCreateDeployment.groovy
+++ vars/githubCreateDeployment.groovy
@@ -1,7 +1,7 @@
 def call() {
   withCredentials([string(credentialsId: 'jenkins-github-api-token-secret-text', variable: 'BEARER_TOKEN')]) {
     httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
-      customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST', requestBody: '''{
+      customHeaders: [[maskValue: false, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST', requestBody: '''{
   "ref": "feature/ROC-3443-pr-preview",
   "description": "Deploying feature/ROC-3443-pr-preview",
   "environment": "preview",
