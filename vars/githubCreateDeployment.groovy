dc931e5a195aad8dc47f253c3cbe5a14ba0767a6
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-28 16:52:09 +0100

    Start using vars

--- vars/githubCreateDeployment.groovy
+++ vars/githubCreateDeployment.groovy
@@ -1,14 +1,20 @@
+import groovy.json.JsonSlurperClassic
+
 def call() {
   withCredentials([usernamePassword(credentialsId: 'jenkins-github-hmcts-api-token', passwordVariable: 'BEARER_TOKEN', usernameVariable: 'USERNAME')]) {
-    httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
-      customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST', requestBody: '''{
-  "ref": "feature/ROC-3443-pr-preview",
-  "description": "Deploying feature/ROC-3443-pr-preview",
+  def response = httpRequest consoleLogResponseBody: true, acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
+      customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST', requestBody: """{
+  "ref": "${env.CHANGE_BRANCH}",
+  "description": "Deploying ${env.CHANGE_BRANCH}",
   "environment": "preview",
   "required_contexts": [],
   "auto_merge": false,
   "transient_environment": true
-}''',
+}""",
       timeout: 15, url: 'https://api.github.com/repos/hmcts/cmc-claim-store/deployments', validResponseCodes: '200:201'
+
+    def deploymentId = new JsonSlurperClassic().parseText(response.content).id
+    echo "" + deploymentId
+    return deploymentId
   }
 }
