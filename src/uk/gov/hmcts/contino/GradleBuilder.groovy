5da7123838c782947ecaea9ffbd480301ee2b521
Calin Marina <calin.marina@gmail.com>
2018-03-28 16:19:41 +0100

    try to hide username/pass from jenkins logging

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -53,9 +53,14 @@ class GradleBuilder implements Builder, Serializable {
     try {
       def az = { cmd -> return steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az $cmd", returnStdout: true).trim() }
 
-      def owaspUser = az "keyvault secret show --vault-name '${steps.env.INFRA_VAULT_NAME}' --name 'OWASPDb-Account' --query value -o tsv"
-      def owaspPassword = az "keyvault secret show --vault-name '${steps.env.INFRA_VAULT_NAME}' --name 'OWASPDb-Password' --query value -o tsv"
-      gradle("-DdependencyCheck.failBuild=true -DdependencyCheck.cveValidForHours=24 -DdependencyCheck.data.driver='com.microsoft.sqlserver.jdbc.SQLServerDriver' -DdependencyCheck.data.connectionString='jdbc:sqlserver://owaspdependencycheck.database.windows.net:1433;database=owaspdependencycheck;user=${owaspUser}@owaspdependencycheck;password=${owaspPassword};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;' -DdependencyCheck.data.username='${owaspUser}' -DdependencyCheck.data.password='${owaspPassword}' dependencyCheck")
+      def owaspU = az "keyvault secret show --vault-name '${steps.env.INFRA_VAULT_NAME}' --name 'OWASPDb-Account' --query value -o tsv"
+      def owaspP = az "keyvault secret show --vault-name '${steps.env.INFRA_VAULT_NAME}' --name 'OWASPDb-Password' --query value -o tsv"
+      withCredentials([
+        usernamePassword(credentialsId: owaspUser, usernameVariable: 'OWASP_USER', passwordVariable: owaspU),
+        usernamePassword(credentialsId: owaspPassword, usernameVariable: 'OWASP_PASS', passwordVariable: owaspP) ])
+      {
+        gradle("-DdependencyCheck.failBuild=true -DdependencyCheck.cveValidForHours=24 -DdependencyCheck.data.driver='com.microsoft.sqlserver.jdbc.SQLServerDriver' -DdependencyCheck.data.connectionString='jdbc:sqlserver://owaspdependencycheck.database.windows.net:1433;database=owaspdependencycheck;user=${OWASP_USER}@owaspdependencycheck;password=${OWASP_PASS};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;' -DdependencyCheck.data.username='${OWASP_USER}' -DdependencyCheck.data.password='${OWASP_PASS}' dependencyCheck")
+      }
     }
     finally {
       steps.archiveArtifacts 'build/reports/dependency-check-report.html'
