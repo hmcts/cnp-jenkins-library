d37f235af73224acdec49b038c73b805e6266b69
Tim Jacomb <tim.jacomb@hmcts.net>
2018-03-28 16:47:28 +0100

    Authentication works now

--- vars/githubCreateDeployment.groovy
+++ vars/githubCreateDeployment.groovy
@@ -1,9 +1,7 @@
 def call() {
   withCredentials([usernamePassword(credentialsId: 'jenkins-github-hmcts-api-token', passwordVariable: 'BEARER_TOKEN', usernameVariable: 'USERNAME')]) {
-  echo BEARER_TOKEN.substring(0, 5)
-    echo BEARER_TOKEN.substring(5, BEARER_TOKEN.length() - 1)
     httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
-      customHeaders: [[maskValue: false, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST', requestBody: '''{
+      customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST', requestBody: '''{
   "ref": "feature/ROC-3443-pr-preview",
   "description": "Deploying feature/ROC-3443-pr-preview",
   "environment": "preview",
