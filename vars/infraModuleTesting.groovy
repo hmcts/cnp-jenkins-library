29436f6e8674c6ba344c06e4d82e9fdc7c146b13
Daniel Sanabria <sanabria.d@gmail.com>
2018-04-03 13:09:02 +0100

    Checking rc for each command

--- vars/infraModuleTesting.groovy
+++ vars/infraModuleTesting.groovy
@@ -1,5 +1,5 @@
 def call () {
   withSubscription('sandbox') {
-    sh 'scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/; bundle install --path vendor/bundle; cd tests/int; bundle exec kitchen test"'
+    sh 'scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/ && bundle install --path vendor/bundle && cd tests/int && bundle exec kitchen test"'
   }
 }
