be555082be5df57e6bc35616fc41f206d7b14a1b
Daniel Sanabria <sanabria.d@gmail.com>
2018-04-03 12:38:49 +0100

    fixing library errors

--- vars/infraModuleTesting.groovy
+++ vars/infraModuleTesting.groovy
@@ -1,7 +1,6 @@
-
-def call(String subscription, Closure body) {
+def call(String subscription) {
   withSubscription(subscription) {
-    sudo yum install -y rh-ruby24-ruby-devel
-    scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/; bundle install --path vendor/bundle; cd tests/int; bundle exec kitchen test"
+    sh 'sudo yum install -y rh-ruby24-ruby-devel'
+    sh 'scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/; bundle install --path vendor/bundle; cd tests/int; bundle exec kitchen test"'
   }
 }
