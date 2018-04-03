6ea33cff10f5feed73b5d590148cca74efad109a
sanabriad <sanabria.d@gmail.com>
2018-04-03 15:49:22 +0100

    Adding linting stage

--- vars/infraModuleTesting.groovy
+++ vars/infraModuleTesting.groovy
@@ -1,5 +1,16 @@
 def call () {
-  withSubscription('sandbox') {
-    sh 'scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/ && bundle install --path vendor/bundle && cd tests/int && bundle exec kitchen test"'
+
+  stage('Terraform Init') {
+    sh 'terraform init'
+  }
+
+  stage('Terraform Linting Checks') {
+    sh 'terraform validate -check-variables=false -no-color'
+  }
+
+  stage('Integration tests') {
+    withSubscription('sandbox') {
+      sh 'scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/ && bundle install --path vendor/bundle && cd tests/int && bundle exec kitchen test"'
+    }
   }
 }
