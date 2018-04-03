5acf29975f27d62ed254bac41c30e38390103d14
Daniel Sanabria <sanabria.d@gmail.com>
2018-04-03 12:59:41 +0100

    Fixing lib errors

--- vars/infraModuleTesting.groovy
+++ vars/infraModuleTesting.groovy
@@ -1,4 +1,4 @@
-def call {
+def call () {
   withSubscription('sandbox') {
     sh 'scl enable rh-ruby24 "gem install bundler && export PATH=$PATH:/home/jenkinsssh/bin/; bundle install --path vendor/bundle; cd tests/int; bundle exec kitchen test"'
   }
