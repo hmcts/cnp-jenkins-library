0bfa11fb1ac9e56b7403e902efc9c82629b8a157
danmerron-sig <dmerron@sportingsolutions.com>
2018-04-05 12:08:46 +0100

    added additional if for creating vnets

--- resources/uk/gov/hmcts/contino/peerVnets.sh
+++ resources/uk/gov/hmcts/contino/peerVnets.sh
@@ -15,13 +15,16 @@ nameB="B-CNP${networkB}toCNP${networkA}"
 
 env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az account set --subscription ${subscriptionA}
 
+deletedA=false
+deletedB=false
+
 # Check if VNET peering exists & is in connected state for VNetA
-set -x
 if [ "$(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering show --name ${nameA} --resource-group ${resourceA} --vnet-name ${networkA} --query peeringState)" != "Connected" ] ; then
   AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering delete \
    --name ${nameA} \
    --resource-group ${resourceA} \
    --vnet-name  ${networkA}
+   deletedA=true
 fi
 
 # Check if VNET peering exists & is in connected state for VNetB
@@ -30,50 +33,54 @@ if [ "$(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet 
    --name ${nameB} \
    --resource-group ${resourceB} \
    --vnet-name  ${networkB}
+   deletedB=true
 fi
-set +x
 
 # Peer VNetA to VNetB
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering create \
- --name ${nameA} \
- --resource-group ${resourceA} \
- --vnet-name ${networkA} \
- --remote-vnet-id /subscriptions/${subscriptionB}/resourceGroups/${resourceB}/providers/Microsoft.Network/VirtualNetworks/${networkB} \
- --allow-vnet-access
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
- --vnet-name ${networkA} \
- --name ${nameA} \
- --resource-group ${resourceA} \
- --set allowForwardedTraffic==true
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
- --vnet-name ${networkA} \
- --name ${nameA} \
- --resource-group ${resourceA} \
- --set allowVirtualNetworkAccess==true
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering list \
- --resource-group ${resourceA} \
- --vnet-name ${networkA} \
- --output table
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az account set --subscription ${subscriptionB}
+if [ $deletedA ]; then
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering create \
+   --name ${nameA} \
+   --resource-group ${resourceA} \
+   --vnet-name ${networkA} \
+   --remote-vnet-id /subscriptions/${subscriptionB}/resourceGroups/${resourceB}/providers/Microsoft.Network/VirtualNetworks/${networkB} \
+   --allow-vnet-access
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
+   --vnet-name ${networkA} \
+   --name ${nameA} \
+   --resource-group ${resourceA} \
+   --set allowForwardedTraffic==true
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
+   --vnet-name ${networkA} \
+   --name ${nameA} \
+   --resource-group ${resourceA} \
+   --set allowVirtualNetworkAccess==true
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering list \
+   --resource-group ${resourceA} \
+   --vnet-name ${networkA} \
+   --output table
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az account set --subscription ${subscriptionB}
+fi
 
 # Peer VNetB to VNetA
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering create \
-  --name ${nameB} \
-  --resource-group ${resourceB} \
-  --vnet-name ${networkB} \
-  --remote-vnet-id /subscriptions/${subscriptionA}/resourceGroups/${resourceA}/providers/Microsoft.Network/VirtualNetworks/${networkA} \
-  --allow-vnet-access
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
-  --vnet-name ${networkB} \
-  --name ${nameB} \
-  --resource-group ${resourceB} \
-  --set allowForwardedTraffic==true
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
-  --vnet-name ${networkB} \
-  --name ${nameB} \
-  --resource-group ${resourceB} \
-  --set allowVirtualNetworkAccess==true
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering list \
-  --resource-group ${resourceB} \
-  --vnet-name ${networkB} \
-  --output table
+if [ $deletedB ]; then
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering create \
+    --name ${nameB} \
+    --resource-group ${resourceB} \
+    --vnet-name ${networkB} \
+    --remote-vnet-id /subscriptions/${subscriptionA}/resourceGroups/${resourceA}/providers/Microsoft.Network/VirtualNetworks/${networkA} \
+    --allow-vnet-access
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
+    --vnet-name ${networkB} \
+    --name ${nameB} \
+    --resource-group ${resourceB} \
+    --set allowForwardedTraffic==true
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering update \
+    --vnet-name ${networkB} \
+    --name ${nameB} \
+    --resource-group ${resourceB} \
+    --set allowVirtualNetworkAccess==true
+  env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering list \
+    --resource-group ${resourceB} \
+    --vnet-name ${networkB} \
+    --output table
+fi
