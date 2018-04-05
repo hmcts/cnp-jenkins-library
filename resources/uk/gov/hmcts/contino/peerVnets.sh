0b7e20f53e904681fb3fcf59444209dbe3d31857
danmerron-sig <dmerron@sportingsolutions.com>
2018-04-05 11:09:17 +0100

    syntax fix

--- resources/uk/gov/hmcts/contino/peerVnets.sh
+++ resources/uk/gov/hmcts/contino/peerVnets.sh
@@ -14,7 +14,7 @@ nameB="B-CNP${networkB}toCNP${networkA}"
 env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az account set --subscription ${subscriptionA}
 
 # Check if VNET peering exists & is in connected state for VNetA
-if [ $(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering show --name ${nameA} --resource-group ${resourceA} --vnet-name ${networkA} --query peeringState) != "Connected" ] ; then
+if [ "$(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering show --name ${nameA} --resource-group ${resourceA} --vnet-name ${networkA} --query peeringState)" != "Connected" ] ; then
   AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering delete \
    --name ${nameA} \
    --resource-group ${resourceA} \
@@ -22,7 +22,7 @@ if [ $(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet p
 fi
 
 # Check if VNET peering exists & is in connected state for VNetB
-if [ $(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering show --name ${nameB} --resource-group ${resourceB} --vnet-name ${networkB} --query peeringState) != "Connected" ]  ; then
+if [ "$(AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering show --name ${nameB} --resource-group ${resourceB} --vnet-name ${networkB} --query peeringState)" != "Connected" ]  ; then
   AZURE_CONFIG_DIR=/opt/jenkins/.azure-$SUBSCRIPTION_NAME az network vnet peering delete \
    --name ${nameB} \
    --resource-group ${resourceB} \
