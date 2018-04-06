83dbaf1fe4a8f2c2d1a4cff28908e8871bbf648f
Tim Jacomb <tim.jacomb@hmcts.net>
2018-04-06 17:18:41 +0100

    Update docs

--- src/uk/gov/hmcts/contino/RepositoryUrl.groovy
+++ src/uk/gov/hmcts/contino/RepositoryUrl.groovy
@@ -3,9 +3,9 @@ package uk.gov.hmcts.contino
 class RepositoryUrl {
 
   /**
-   * Provides github short url
+   * Provides a github repository path
    * @param changeUrl the change url env variable from jenkins i.e. https://github.com/hmcts/spring-boot-template/pull/294
-   * @return a url in the form of hmcts/spring-boot-template
+   * @return a repository path in the form of hmcts/spring-boot-template
    */
   String getShort(String changeUrl) {
     return changeUrl.replace("https://github.com/", "").replaceFirst("/pull/\\d+", "")
