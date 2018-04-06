d6c44d602ab7d1360dea2093dbf3ab00b08336f2
Tim Jacomb <tim.jacomb@hmcts.net>
2018-04-06 16:57:03 +0100

    Add quotes around url

--- vars/githubUpdateDeploymentStatus.groovy
+++ vars/githubUpdateDeploymentStatus.groovy
@@ -4,21 +4,12 @@ def call(Long deploymentNumber, String url) {
   String repositoryShortUrl = new RepositoryUrl().getShort(env.CHANGE_URL)
 
   withCredentials([usernamePassword(credentialsId: 'jenkins-github-hmcts-api-token', passwordVariable: 'BEARER_TOKEN', usernameVariable: 'USERNAME')]) {
-    GString requestBody = """{
-      "state": "success",
-      "description": "Deployment finished successfully.",
-      "environment_url": ${url}
-    }"""
-
-    echo requestBody
-    echo url
-
     httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
       customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST',
       requestBody: """{
         "state": "success",
         "description": "Deployment finished successfully.",
-        "environment_url": ${url}
+        "environment_url": "${url}"
       }""",
       timeout: 15, url: "https://api.github.com/repos/${repositoryShortUrl}/deployments/${deploymentNumber}/statuses",
       validResponseCodes: '200:201'
