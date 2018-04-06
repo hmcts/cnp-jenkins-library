9b7031e1b5f77d5fe75607a0f06c12d2b7093e8c
Tim Jacomb <tim.jacomb@hmcts.net>
2018-04-06 15:15:39 +0100

    Remove more hardcoded vars

--- vars/githubCreateDeployment.groovy
+++ vars/githubCreateDeployment.groovy
@@ -1,6 +1,9 @@
 import groovy.json.JsonSlurperClassic
+import uk.gov.hmcts.contino.RepositoryUrl
 
 def call() {
+  String repositoryShortUrl = new RepositoryUrl().getShort(env.CHANGE_URL)
+
   withCredentials([usernamePassword(credentialsId: 'jenkins-github-hmcts-api-token', passwordVariable: 'BEARER_TOKEN', usernameVariable: 'USERNAME')]) {
     def response = httpRequest consoleLogResponseBody: true, acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
       customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST',
@@ -12,7 +15,7 @@ def call() {
         "auto_merge": false,
         "transient_environment": true
       }""",
-      timeout: 15, url: 'https://api.github.com/repos/hmcts/cmc-claim-store/deployments', validResponseCodes: '200:201'
+      timeout: 15, url: "https://api.github.com/repos/${repositoryShortUrl}/deployments", validResponseCodes: '200:201'
 
     def deploymentId = new JsonSlurperClassic().parseText(response.content).id
     return deploymentId
--- vars/githubUpdateDeploymentStatus.groovy
+++ vars/githubUpdateDeploymentStatus.groovy
@@ -1,4 +1,6 @@
 def call(Long deploymentNumber, String url) {
+  String repositoryShortUrl = new RepositoryUrl().getShort(env.CHANGE_URL)
+
   withCredentials([usernamePassword(credentialsId: 'jenkins-github-hmcts-api-token', passwordVariable: 'BEARER_TOKEN', usernameVariable: 'USERNAME')]) {
     httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON',
       customHeaders: [[maskValue: true, name: 'Authorization', value: "Bearer ${BEARER_TOKEN}"]], httpMode: 'POST',
@@ -7,7 +9,7 @@ def call(Long deploymentNumber, String url) {
         "description": "Deployment finished successfully.",
         "environment_url": ${url}
       }""",
-      timeout: 15, url: "https://api.github.com/repos/hmcts/cmc-claim-store/deployments/${deploymentNumber}/statuses",
+      timeout: 15, url: "https://api.github.com/repos/${repositoryShortUrl}/deployments/${deploymentNumber}/statuses",
       validResponseCodes: '200:201'
 
   }
