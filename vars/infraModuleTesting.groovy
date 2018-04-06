ccf46bb1a6a81d044df330a946f5ab2b4f875cc4
Daniel Sanabria <sanabria.d@gmail.com>
2018-04-06 09:16:52 +0100

    typo

--- vars/infraModuleTesting.groovy
+++ vars/infraModuleTesting.groovy
@@ -15,8 +15,9 @@ def call () {
         gem install bundler
         export PATH=$PATH:/home/jenkinsssh/bin/
         export TF_WARN_OUTPUT_ERRORS=1
-        bundle install --path vendor/bundle && cd tests/int
-        bundle exec kitchen testi
+        bundle install --path vendor/bundle
+        cd tests/int
+        bundle exec kitchen test
         "
       '''
     }
