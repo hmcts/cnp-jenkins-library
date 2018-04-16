b257211ca0a5dbb8d69cc54811967d0b4fd1675c
Calin Marina <calin.marina@gmail.com>
2018-04-16 14:15:37 +0300

    re-enableowasp (#188)
    * moving az closure in the context where it has access to all env variables
    
    * try pushing subscription down to buildAndTest
    
    * fix owasp tests to run with jenkins creds
    
    * remove unnecessary param from build and test section
    
    * fix valut name
    
    * add steps for env variable to be found
    
    * try to hide username/pass from jenkins logging
    
    * using locally declared credentials
    
    * try creation of credentials on jenkins repo on the fly
    
    * declare var with def
    
    * log is a property of steps
    
    * add noncps to securityCheck
    
    * where's my logs?
    
    * leave secrets hiding aside for now
    
    * remove unneeded imports
    
    * add owasp to parameterized pipeline for testing
    
    * removed triggering owasp from parameterised pipeline

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -49,15 +49,18 @@ class GradleBuilder implements Builder, Serializable {
   }
 
   def securityCheck() {
+    try {
+      def az = { cmd -> return steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az $cmd", returnStdout: true).trim() }
 
-//    try {
-//       def owaspUser = az "keyvault secret show --vault-name 'https://infra-vault.vault.azure.net/' --name 'OWASPDb-Account' --query value -o tsv"
-//       def owaspPassword = az "keyvault secret show --vault-name 'https://infra-vault.vault.azure.net/' --name 'OWASPDb-Password' --query value -o tsv"      
-//       gradle("-DdependencyCheck.failBuild=true -DdependencyCheck.cveValidForHours=24 -DdependencyCheck.data.driver='com.microsoft.sqlserver.jdbc.SQLServerDriver' -DdependencyCheck.data.connectionString='jdbc:sqlserver://owaspdependencycheck.database.windows.net:1433;database=owaspdependencycheck;user=${owaspUser}@owaspdependencycheck;password=${owaspPassword};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;' -DdependencyCheck.data.username='${owaspUser}' -DdependencyCheck.data.password='${owaspPassword}' dependencyCheck")
-//     } finally {
-//       steps.archiveArtifacts 'build/reports/dependency-check-report.html'
-//     }
+      def owaspU = az "keyvault secret show --vault-name '${steps.env.INFRA_VAULT_NAME}' --name 'OWASPDb-Account' --query value -o tsv"
+      def owaspP = az "keyvault secret show --vault-name '${steps.env.INFRA_VAULT_NAME}' --name 'OWASPDb-Password' --query value -o tsv"
 
+      gradle("-DdependencyCheck.failBuild=true -DdependencyCheck.cveValidForHours=24 -DdependencyCheck.data.driver='com.microsoft.sqlserver.jdbc.SQLServerDriver' -DdependencyCheck.data.connectionString='jdbc:sqlserver://owaspdependencycheck.database.windows.net:1433;database=owaspdependencycheck;user=${owaspU}@owaspdependencycheck;password=${owaspP};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;' -DdependencyCheck.data.username='${owaspU}' -DdependencyCheck.data.password='${owaspP}' dependencyCheckAnalyze")
+
+    }
+    finally {
+      steps.archiveArtifacts 'build/reports/dependency-check-report.html'
+    }
   }
 
   @Override
@@ -82,7 +85,7 @@ EOF
 
   def dbMigrate(String vaultName, String microserviceName) {
     def az = { cmd -> return steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$steps.env.SUBSCRIPTION_NAME az $cmd", returnStdout: true).trim() }
-    
+
     def dbName = az "keyvault secret show --vault-name '$vaultName' --name '${microserviceName}-POSTGRES-DATABASE' --query value -o tsv"
     def dbHost = az "keyvault secret show --vault-name '$vaultName' --name '${microserviceName}-POSTGRES-HOST' --query value -o tsv"
     def dbPass = az "keyvault secret show --vault-name '$vaultName' --name '${microserviceName}-POSTGRES-PASS' --query value -o tsv"
