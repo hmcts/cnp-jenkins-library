c4a1fafa25c6b27e469a760e564e72a717e88f80
Chris O'Dell <chris.odell@contino.io>
2018-04-16 16:04:19 +0100

    WIP (this doesn't work...)

--- test/withPipelineTest.groovy
+++ test/withPipelineTest.groovy
@@ -1,6 +1,9 @@
 import org.junit.Before
+import uk.gov.hmcts.contino.MockBuilder
 
+import static com.lesfurets.jenkins.unit.MethodCall.callArgsToString
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static org.assertj.core.api.Assertions.assertThat
 import static uk.gov.hmcts.contino.ProjectSource.projectSource
 import com.lesfurets.jenkins.unit.BasePipelineTest
 import uk.gov.hmcts.contino.MockJenkins
@@ -45,4 +48,35 @@ class withPipelineTest extends BasePipelineTest {
     printCallStack()
   }
 
+  @Test
+  void testBuildIsCalled() {
+
+
+    def library = library()
+      .name('Infrastructure')
+      .targetPath(projectDir)
+      .retriever(projectSource(projectDir))
+      .defaultVersion("master")
+      .allowOverride(true)
+      .implicit(false)
+      .build()
+    helper.registerSharedLibrary(library)
+    helper.registerAllowedMethod("deleteDir", {})
+    helper.registerAllowedMethod("stash", [Map.class], {})
+    helper.registerAllowedMethod("unstash", [String.class], {})
+    helper.registerAllowedMethod("withEnv", [List.class, Closure.class], {})
+    helper.registerAllowedMethod("ansiColor", [String, Closure], {})
+    helper.registerAllowedMethod("withCredentials", [LinkedHashMap, Closure.class], {})
+    helper.registerAllowedMethod("sh", [Map.class], { return "" })
+    helper.registerAllowedMethod( 'fileExists', [String.class], { c -> true })
+    helper.registerAllowedMethod("timestamps", [Closure.class], { c -> c.call() })
+    helper.registerAllowedMethod("timeout", [Map.class, Closure.class], { c, d -> d.call() })
+    runScript("testResources/examplePipeline.jenkins")
+    printCallStack()
+
+
+    assertThat(helper.callStack.any { call ->
+      call.methodName == "sectionBuildAndTest.stage(Test, groovy.lang.Closure)"
+    }).isTrue()
+  }
 }
