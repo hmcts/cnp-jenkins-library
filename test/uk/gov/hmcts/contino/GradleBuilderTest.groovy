95db964fd83ed08eca40d0cabf8c28efe328e7e2
Neil Richardson <njrich28@gmail.com>
2018-04-19 14:40:58 +0100

    Update tests to make them slightl less brittle

--- test/uk/gov/hmcts/contino/GradleBuilderTest.groovy
+++ test/uk/gov/hmcts/contino/GradleBuilderTest.groovy
@@ -4,27 +4,29 @@ import spock.lang.Specification
 
 class GradleBuilderTest extends Specification {
 
+  static final String GRADLE_CMD = './gradlew'
+
   def steps = Mock(JenkinsStepMock.class)
   def builder = new GradleBuilder(steps, 'test')
 
-  def "build calls './gradlew assemble'"() {
+  def "build calls 'gradle assemble'"() {
     when:
       builder.build()
     then:
-      1 * steps.sh('./gradlew assemble')
+      1 * steps.sh({ it.startsWith(GRADLE_CMD) && it.contains('assemble') })
   }
 
-  def "test calls './gradlew --info check"() {
+  def "test calls 'gradle check'"() {
     when:
       builder.test()
     then:
-      1 * steps.sh('./gradlew --info check')
+      1 * steps.sh({ it.startsWith(GRADLE_CMD) && it.contains('check') })
   }
 
-  def "smokeTest calls './gradlew --info --rerurn-tasks smoke'"() {
+  def "smokeTest calls 'gradle --rerurn-tasks smoke' with '--rerun-tasks' flag"() {
     when:
       builder.smokeTest()
     then:
-      1 * steps.sh('./gradlew --info --rerun-tasks smoke')
+      1 * steps.sh({ it.startsWith(GRADLE_CMD) && it.contains('smoke') && it.contains('--rerun-tasks') })
   }
 }
