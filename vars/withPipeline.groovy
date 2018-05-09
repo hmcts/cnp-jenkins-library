652a95748cf3670bb0b2689321bf75cd2d8bacb4
Chris O'Dell <chris.odell@contino.io>
2018-05-09 13:58:15 +0100

    Do not set success early

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -40,7 +40,6 @@ def call(type, String product, String component, Closure body) {
   pl.onStageFailure() {
     currentBuild.result = "FAILURE"
   }
-  currentBuild.result = "SUCCESS"
 
   Subscription subscription = new Subscription(env)
   Environment environment = new Environment(env)
