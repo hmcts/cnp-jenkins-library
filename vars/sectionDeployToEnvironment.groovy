fac80f2d4456e3c0def0a28a275e31826ff09d07
timwebster9 <tim.webster@gmail.com>
2018-06-06 17:38:05 +0100

    put azure upload back in after it got removed by rebase somehow

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -1,4 +1,5 @@
 #!groovy
+import uk.gov.hmcts.contino.AbstractBuilder
 import uk.gov.hmcts.contino.Builder
 import uk.gov.hmcts.contino.Deployer
 import uk.gov.hmcts.contino.PipelineCallbacks
@@ -118,6 +119,7 @@ def call(params) {
               testEnv(deployer.getServiceUrl(environment, "staging"), tfOutput) {
                 pl.callAround("performanceTest:${environment}") {
                   builder.performanceTest()
+                  azureBlobUpload('buildlog-storage-account', AbstractBuilder.GATLING_REPORTS_DIR, 'performance/${product}-${component}/${environment}')
                 }
               }
             }
