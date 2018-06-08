6d8499514fc43f5c5d06cc1ff363b54668dd793e
timwebster9 <tim.webster@gmail.com>
2018-06-08 11:37:48 +0100

    container permission issues, use tagged images, refactor Gatling stuff

--- src/uk/gov/hmcts/contino/AbstractBuilder.groovy
+++ src/uk/gov/hmcts/contino/AbstractBuilder.groovy
@@ -2,26 +2,17 @@ package uk.gov.hmcts.contino
 
 abstract class AbstractBuilder implements Builder, Serializable {
 
-  public static final String GATLING_REPORTS_DIR  = 'build/reports/gatling'
-  public static final String GATLING_SIMS_DIR     = 'src/gatling/simulations'
-  public static final String GATLING_DATA_DIR     = 'src/gatling/data'
-  public static final String GATLING_BODIES_DIR   = 'src/gatling/bodies'
-  public static final String GATLING_CONF_DIR     = 'src/gatling/conf'
-  public static final String GATLING_IMAGE        = 'hmcts/moj-gatling-image'
-  public static final String GATLING_RUN_ARGS     = '-v $WORKSPACE/'+ GATLING_CONF_DIR + ':/etc/gatling/conf'
-
   def steps
+  def gatling
 
   AbstractBuilder(steps) {
     this.steps = steps
+    this.gatling = new Gatling(this.steps)
   }
 
   @Override
   def performanceTest() {
-    // with Gatling command-line you can't specify a configuration directory, so we need to bind-mount it
-    this.steps.withDocker(GATLING_IMAGE, GATLING_RUN_ARGS) {
-      this.steps.sh "gatling.sh -sf ${GATLING_SIMS_DIR} -df ${GATLING_DATA_DIR} -bdf ${GATLING_BODIES_DIR} -rf ${GATLING_REPORTS_DIR} -m"
-    }
+    this.gatling.execute()
   }
 
 }
---
+++ src/uk/gov/hmcts/contino/Gatling.groovy
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.contino
+
+class Gatling {
+
+  public static final String GATLING_REPORTS_DIR  = 'build/gatling/reports'
+  public static final String GATLING_SIMS_DIR     = 'src/gatling/simulations'
+  public static final String GATLING_DATA_DIR     = 'src/gatling/data'
+  public static final String GATLING_BODIES_DIR   = 'src/gatling/bodies'
+  public static final String GATLING_CONF_DIR     = 'src/gatling/conf'
+  public static final String GATLING_IMAGE        = 'hmcts/moj-gatling-image:2.3.1-1.0'
+
+  // with Gatling command-line you can't specify a configuration directory, so we need to bind-mount it
+  public static final String GATLING_RUN_ARGS     = '-u root -v $WORKSPACE/'+ GATLING_CONF_DIR + ':/etc/gatling/conf'
+
+  def steps
+
+  Gatling(steps) {
+    this.steps = steps
+  }
+
+  def execute() {
+    this.steps.withDocker(GATLING_IMAGE, GATLING_RUN_ARGS) {
+      this.steps.sh "gatling.sh -m -sf ${GATLING_SIMS_DIR} -df ${GATLING_DATA_DIR} -bdf ${GATLING_BODIES_DIR} -rf ${GATLING_REPORTS_DIR}"
+    }
+  }
+
+}
