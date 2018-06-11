041993fe116ec8bcb70fc711e3fcd2c70546d4bb
timwebster9 <tim.webster@gmail.com>
2018-06-11 11:29:58 +0100

    include WORKSPACE in all gatling vars

--- src/uk/gov/hmcts/contino/Gatling.groovy
+++ src/uk/gov/hmcts/contino/Gatling.groovy
@@ -2,15 +2,15 @@ package uk.gov.hmcts.contino
 
 class Gatling implements Serializable {
 
-  public static final String GATLING_REPORTS_DIR  = 'build/gatling/reports'
-  public static final String GATLING_SIMS_DIR     = 'src/gatling/simulations'
-  public static final String GATLING_DATA_DIR     = 'src/gatling/data'
-  public static final String GATLING_BODIES_DIR   = 'src/gatling/bodies'
-  public static final String GATLING_CONF_DIR     = 'src/gatling/conf'
+  public static final String GATLING_REPORTS_DIR  = '$WORKSPACE/build/gatling/reports'
+  public static final String GATLING_SIMS_DIR     = '$WORKSPACE/src/gatling/simulations'
+  public static final String GATLING_DATA_DIR     = '$WORKSPACE/src/gatling/data'
+  public static final String GATLING_BODIES_DIR   = '$WORKSPACE/src/gatling/bodies'
+  public static final String GATLING_CONF_DIR     = '$WORKSPACE/src/gatling/conf'
   public static final String GATLING_IMAGE        = 'hmcts/moj-gatling-image:2.3.1-1.0'
 
   // with Gatling command-line you can't specify a configuration directory, so we need to bind-mount it
-  public static final String GATLING_RUN_ARGS     = '-u root -v $WORKSPACE/'+ GATLING_CONF_DIR + ':/etc/gatling/conf'
+  public static final String GATLING_RUN_ARGS     = '-u root -v ' + GATLING_CONF_DIR + ':/etc/gatling/conf'
 
   def steps
 
