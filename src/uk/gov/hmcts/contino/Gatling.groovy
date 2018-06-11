fb5dda46b3bb9d9180a2218d2aba6df734e7cda9
timwebster9 <tim.webster@gmail.com>
2018-06-11 11:57:25 +0100

    use workspace dir for binaries so don't need to run container as root

--- src/uk/gov/hmcts/contino/Gatling.groovy
+++ src/uk/gov/hmcts/contino/Gatling.groovy
@@ -2,15 +2,16 @@ package uk.gov.hmcts.contino
 
 class Gatling implements Serializable {
 
-  public static final String GATLING_REPORTS_DIR  = '$WORKSPACE/build/gatling/reports'
-  public static final String GATLING_SIMS_DIR     = '$WORKSPACE/src/gatling/simulations'
-  public static final String GATLING_DATA_DIR     = '$WORKSPACE/src/gatling/data'
-  public static final String GATLING_BODIES_DIR   = '$WORKSPACE/src/gatling/bodies'
-  public static final String GATLING_CONF_DIR     = '$WORKSPACE/src/gatling/conf'
-  public static final String GATLING_IMAGE        = 'hmcts/moj-gatling-image:2.3.1-1.0'
+  public static final String GATLING_REPORTS_DIR   = '$WORKSPACE/build/gatling/reports'
+  public static final String GATLING_BINARIES_DIR  = '$WORKSPACE/build/gatling/binaries'
+  public static final String GATLING_SIMS_DIR      = '$WORKSPACE/src/gatling/simulations'
+  public static final String GATLING_DATA_DIR      = '$WORKSPACE/src/gatling/data'
+  public static final String GATLING_BODIES_DIR    = '$WORKSPACE/src/gatling/bodies'
+  public static final String GATLING_CONF_DIR      = '$WORKSPACE/src/gatling/conf'
+  public static final String GATLING_IMAGE         = 'hmcts/moj-gatling-image:2.3.1-1.0'
 
   // with Gatling command-line you can't specify a configuration directory, so we need to bind-mount it
-  public static final String GATLING_RUN_ARGS     = '-u root -v ' + GATLING_CONF_DIR + ':/etc/gatling/conf'
+  public static final String GATLING_RUN_ARGS     = '-v ' + GATLING_CONF_DIR + ':/etc/gatling/conf'
 
   def steps
 
@@ -20,7 +21,7 @@ class Gatling implements Serializable {
 
   def execute() {
     this.steps.withDocker(GATLING_IMAGE, GATLING_RUN_ARGS) {
-      this.steps.sh "gatling.sh -m -sf ${GATLING_SIMS_DIR} -df ${GATLING_DATA_DIR} -bdf ${GATLING_BODIES_DIR} -rf ${GATLING_REPORTS_DIR}"
+      this.steps.sh "gatling.sh -m -sf ${GATLING_SIMS_DIR} -df ${GATLING_DATA_DIR} -bdf ${GATLING_BODIES_DIR} -bf ${GATLING_BINARIES_DIR} -rf ${GATLING_REPORTS_DIR}"
     }
   }
 
