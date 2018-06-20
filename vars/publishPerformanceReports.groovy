28bf11e657b3d7696c8d38297e4a11d6e63d93a9
timwebster9 <tim.webster@gmail.com>
2018-06-20 16:00:10 +0100

    fix params for publish perf reports

--- vars/publishPerformanceReports.groovy
+++ vars/publishPerformanceReports.groovy
@@ -1,7 +1,7 @@
 import uk.gov.hmcts.contino.Gatling
 
 def call(steps, params)  {
-  azureBlobUpload('buildlog-storage-account', Gatling.GATLING_REPORTS_DIR, "performance/${product}-${component}/${environment}")
+  azureBlobUpload('buildlog-storage-account', Gatling.GATLING_REPORTS_DIR, "performance/${params.product}-${params.component}/${params.environment}")
   def reportsPath = "${WORKSPACE}/" + Gatling.GATLING_REPORTS_PATH
   publishToCosmosDb(steps, params, Gatling.COSMOSDB_COLLECTION, reportsPath, '**/*.json')
 }
