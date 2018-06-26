87046d34ae34b024de35347433fa9f9732ae7a07
vakepati <venkata.akepati@hmcts.net>
2018-06-26 23:51:59 +0100

    removed security check abstract method

--- vars/withNightlyPipeline.groovy
+++ vars/withNightlyPipeline.groovy
@@ -19,7 +19,7 @@ def call(type,Closure body) {
 
   assert pipelineType != null
 
-  MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild)
+  MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild,"gjhgjh")
   def pl = new PipelineCallbacks(metricsPublisher)
 
   body.delegate = pl
