f4a069e70afaf6279ae204bd8f5967a8322dabee
timwebster9 <tim.webster@gmail.com>
2018-06-27 21:39:35 +0100

    revert resource name change, and tag resource group with change URL instead

--- src/uk/gov/hmcts/contino/RepositoryUrl.groovy
+++ src/uk/gov/hmcts/contino/RepositoryUrl.groovy
@@ -7,17 +7,7 @@ class RepositoryUrl {
    * @param changeUrl the change url env variable from jenkins i.e. https://github.com/hmcts/spring-boot-template/pull/294
    * @return a repository path in the form of hmcts/spring-boot-template
    */
-  String getFull(String changeUrl) {
-    return changeUrl.replace("https://github.com/", "").replaceFirst("/pull/\\d+", "")
-  }
-
-  /**
-   * Provides a Github repository name
-   *
-   * @param changeUrl the change url env variable from jenkins i.e. https://github.com/hmcts/spring-boot-template/pull/294
-   * @return a repository name in the form of 'spring-boot-template'
-   */
   String getShort(String changeUrl) {
-    return changeUrl.tokenize('/.')[-3]
+    return changeUrl.replace("https://github.com/", "").replaceFirst("/pull/\\d+", "")
   }
 }
