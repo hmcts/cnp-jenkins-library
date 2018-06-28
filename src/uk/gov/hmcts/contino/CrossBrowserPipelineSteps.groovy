c1a12f7cd8fe7c8843ab2f74d912b326791779cf
vakepati <venkata.akepati@hmcts.net>
2018-06-28 16:38:42 +0100

    added sauce configuration

--- src/uk/gov/hmcts/contino/CrossBrowserPipelineSteps.groovy
+++ src/uk/gov/hmcts/contino/CrossBrowserPipelineSteps.groovy
@@ -11,7 +11,7 @@ class CrossBrowserPipelineSteps extends AbstractNightlyBuilder {
   }
 
   def crossBrowserTest() {
-      sauceconnect(options: "-u divorce -K e0067992-049e-412c-9d15-2566a28cfb73 --verbose --tunnel-identifier reformtunnel", verboseLogging: true)
+      //sauceconnect(options: "-u divorce -K e0067992-049e-412c-9d15-2566a28cfb73 --verbose --tunnel-identifier reformtunnel", verboseLogging: true)
       yarn("test:crossbrowser")
 
 
