03d793124f1237759c0d031049d1e7f8b503b883
vakepati <venkata.akepati@hmcts.net>
2018-06-29 14:05:38 +0100

    added sauce configuration

--- vars/withNightlyPipeline.groovy
+++ vars/withNightlyPipeline.groovy
@@ -7,58 +7,69 @@ import uk.gov.hmcts.contino.NightlyPipeline
 
 def call(type,Closure body) {
 
-   String typeOfTesting
+  String typeOfTesting
 
-  forEach(typeOfTesting :type){
-    if (typeOfTesting.equalIgnorecase('crossBrowser')||('PerformanceTest')){
+  /*for(int i=0;i<type.length();i++){
+    if (type[i].equalIgnorecase('crossBrowser')||('PerformanceTest')){
       np  : new NightlyPipeline(this)
     }
-  }
+  }*/
+  def pipelineTypes = [
+    (crossBrowser || PerformanceTest): new NightlyPipeline(this)
+  ]
 
-  PipelineType pipelineType=np
-
-  assert pipelineType != null
-
-  NightlyBuilder builder = pipelineType.nBuilder
-
-
-  MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild,"product","component")
-  def pl = new PipelineCallbacks(metricsPublisher)
-
-  body.delegate = pl
-  body.call() // register callbacks
-
-  pl.onStageFailure() {
-    currentBuild.result = "FAILURE"
-  }
-
-  timestamps {
-    try {
-      node {
-        env.PATH = "$env.PATH:/usr/local/bin"
-        SectionNightlyTest(pl, builder)
-      }
+  type.each {
+    if ($ { it } instanceof PipelineType) {
+      pipelineType = $ { it }
+    } else {
+      pipelineType = pipelineTypes.get($ { it })
     }
-    catch (err) {
+
+    PipelineType pipelineType = np
+
+    assert pipelineType != null
+
+    NightlyBuilder builder = pipelineType.nBuilder
+
+
+    MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild, "product", "component")
+    def pl = new PipelineCallbacks(metricsPublisher)
+
+    body.delegate = pl
+    body.call() // register callbacks
+
+    pl.onStageFailure() {
       currentBuild.result = "FAILURE"
+    }
+
+    timestamps {
+      try {
+        node {
+          env.PATH = "$env.PATH:/usr/local/bin"
+          SectionNightlyTest(pl, builder)
+        }
+      }
+      catch (err) {
+        currentBuild.result = "FAILURE"
+        if (pl.slackChannel) {
+          notifyBuildFailure channel: pl.slackChannel
+        }
+
+        pl.call('onFailure')
+        node {
+          metricsPublisher.publish('Pipeline Failed')
+        }
+        throw err
+      }
+
       if (pl.slackChannel) {
-        notifyBuildFailure channel: pl.slackChannel
+        notifyBuildFixed channel: pl.slackChannel
       }
 
-      pl.call('onFailure')
+      pl.call('onSuccess')
       node {
-        metricsPublisher.publish('Pipeline Failed')
+        metricsPublisher.publish('Pipeline Succeeded')
       }
-      throw err
-    }
-
-    if (pl.slackChannel) {
-      notifyBuildFixed channel: pl.slackChannel
-    }
-
-    pl.call('onSuccess')
-    node {
-      metricsPublisher.publish('Pipeline Succeeded')
     }
   }
 }
