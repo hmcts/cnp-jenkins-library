e7006b204f46336017a86c14f42cca9f9a412ba1
vakepati <venkata.akepati@hmcts.net>
2018-07-03 14:14:35 +0100

    refactored the nighttly Pipeline classes

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -42,7 +42,12 @@ class YarnBuilder extends AbstractBuilder {
   }
 
   def crossBrowserTest() {
+    try{
     yarn("test:crossbrowser")
+    }
+    finally {
+      steps.archiveArtifacts allowEmptyArchive: true, artifacts: 'functional-output/cross-browser/*'
+    }
   }
 
   def securityCheck() {
