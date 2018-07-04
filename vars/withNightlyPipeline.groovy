d753eaa9bef2c9b2f35022e6059dfa646cb26d8c
vakepati <venkata.akepati@hmcts.net>
2018-07-05 00:15:21 +0100

    configured test url to run cross browser and performance tests

--- vars/withNightlyPipeline.groovy
+++ vars/withNightlyPipeline.groovy
@@ -5,6 +5,8 @@ import uk.gov.hmcts.contino.PipelineType
 import uk.gov.hmcts.contino.NodePipelineType
 import uk.gov.hmcts.contino.SpringBootPipelineType
 import uk.gov.hmcts.contino.AngularPipelineType
+import uk.gov.hmcts.contino.Subscription
+import uk.gov.hmcts.contino.Environment
 
 
 def call(type,product,component,Closure body) {
