280c7a609dd096ca9f58bebef8007ed05f6e380c
vakepati <venkata.akepati@hmcts.net>
2018-07-10 12:06:44 +0100

    Added Dependency Checker Task to the NightlyPipeline

--- vars/sectionNightlyTests.groovy
+++ vars/sectionNightlyTests.groovy
@@ -1,64 +1,63 @@
-
 import uk.gov.hmcts.contino.Builder
-import uk.gov.hmcts.contino.Deployer
+import uk.gov.hmcts.contino.PipelineCallbacks
 
+def call(PipelineCallbacks pl, Builder builder) {
 
-def call() {
-
-  Builder builder = pipelineType.builder
-  Deployer deployer = pipelineType.deployer
-
-
-  def TEST_URL = deployer.getServiceUrl(environment, "staging")
-
-  echo "Using TEST_URL....: ${env.TEST_URL}"
-
-      stage('Checkout') {
-        pl.callAround('checkout') {
-          deleteDir()
-          checkout scm
-        }
-      }
-
-      if (pl.crossBrowserTest) {
-        try {
-          stage("Build") {
-            pl.callAround('build') {
-              timeout(time: 15, unit: 'MINUTES') {
-                builder.build()
-              }
-            }
-          }
-          onFunctionalTestEnvironment(environment) {
-            stage("crossBrowserTest") {
-              testEnv(deployer.getServiceUrl(environment, "staging"), tfOutput) {
-                echo "test url is ..... ${TEST_URL}"
-                pl.callAround('crossBrowserTest') {
-                  timeout(time: pl.crossBrowserTestTimeout, unit: 'MINUTES') {
-                    builder.crossBrowserTest()
-                  }
-                }
-              }
-            }
-          }
-        } catch (err) {
-          err.printStackTrace()
-          currentBuild.result = "UNSTABLE"
-        }
-      }
-
-      if (pl.performanceTest) {
-        try {
-          stage("performanceTest") {
-            pl.callAround('PerformanceTest') {
-              timeout(time: pl.perfTestTimeout, unit: 'MINUTES') {
-                builder.performanceTest()
-              }
-            }
-          }
-        } catch (err) {
-          err.printStackTrace()
-          currentBuild.result = "UNSTABLE"
-        }
-      }
+  stage('Checkout') {
+    pl.callAround('checkout') {
+      deleteDir()
+      checkout scm
+    }
   }
+
+  stage("Build") {
+    pl.callAround('build') {
+      timeout(time: 15, unit: 'MINUTES') {
+        builder.build()
+      }
+    }
+  }
+
+  try {
+    stage('DependencyCheckNightly') {
+      pl.callAround('DependencyCheckNightly') {
+        timeout(time: 15, unit: 'MINUTES') {
+          builder.securityCheck()
+        }
+      }
+    }
+  } catch(err) {
+    err.printStackTrace()
+    currentBuild.result = "UNSTABLE"
+  }
+
+  if (pl.crossBrowserTest) {
+    try {
+      stage("crossBrowserTest") {
+        pl.callAround('crossBrowserTest') {
+          timeout(time: pl.crossBrowserTestTimeout, unit: 'MINUTES') {
+            builder.crossBrowserTest()
+          }
+        }
+      }
+    } catch (err) {
+      err.printStackTrace()
+      currentBuild.result = "UNSTABLE"
+    }
+  }
+
+  if (pl.performanceTest) {
+    try {
+      stage("performanceTest") {
+        pl.callAround('PerformanceTest') {
+          timeout(time: pl.perfTestTimeout, unit: 'MINUTES') {
+            builder.performanceTest()
+          }
+        }
+      }
+    } catch (err) {
+      err.printStackTrace()
+      currentBuild.result = "UNSTABLE"
+    }
+  }
+}
