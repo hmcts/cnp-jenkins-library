e0b7d827dd60b2a71c413f51a043cf076a02806c
Janis Berzins <janis.berzins@hmcts.net>
2018-07-24 16:13:32 +0100

    Updated pipeline types

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -1,5 +1,4 @@
 import uk.gov.hmcts.contino.AngularPipelineType
-import uk.gov.hmcts.contino.MsBuildPipelineType
 import uk.gov.hmcts.contino.NodePipelineType
 import uk.gov.hmcts.contino.PipelineCallbacks
 import uk.gov.hmcts.contino.PipelineType
@@ -20,7 +19,6 @@ def call(type, String product, String component, Closure body) {
     java  : new SpringBootPipelineType(this, deploymentProduct, component),
     nodejs: new NodePipelineType(this, deploymentProduct, component),
     angular: new AngularPipelineType(this, deploymentProduct, component),
-    dotnet  : new MsBuildPipelineType(this, deploymentProduct, component)
   ]
 
   PipelineType pipelineType
