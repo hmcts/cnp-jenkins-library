cd7eb485958944aff9b598b0d03e8e798e2509c3
vakepati <venkata.akepati@hmcts.net>
2018-07-25 10:54:43 +0100

    DependencyChecker

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -44,10 +44,10 @@ class YarnBuilder extends AbstractBuilder {
 
   def crossBrowserTest() {
     try {
-      steps.sh """ sauce(reform_tunnel) {
+      steps.sh ''' sauce(\"reform_tunnel\") {
         sauceconnect(options: "--verbose  --logfile sauceconnect.log --tunnel-identifier reformtunnel", useGeneratedTunnelIdentifier: false, verboseLogging: true) {
         }
-      } """
+      } '''
       yarn("test:crossbrowser")
     }
     finally {
