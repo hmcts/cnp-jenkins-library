256f2301d72a4b7e4158d6e9250aeb139bda176a
satyachundur <>
2018-07-30 12:16:44 +0100

    added with sauce connect

---
+++ vars/withSauceConnect.groovy
@@ -0,0 +1,18 @@
+#!groovy
+
+/**
+ * Run a closure sauce connect proxy
+ *
+ * @param sauceid credentialid for accessing sauce tunnel
+ * @param body the closure
+ */
+def call(String sauceid, Closure body) {
+    node {
+        sauce(sauceid) {
+            sauceconnect(options: '--verbose  --tunnel-identifier reformtunnel', useGeneratedTunnelIdentifier: false,
+                    useLatestSauceConnect: true, verboseLogging: true) {
+                body()
+            }
+        }
+    }
+}
