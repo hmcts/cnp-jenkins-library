891df50eba9636ed4981f0b326ac0bccc077f4af
vakepati <37808224+vakepati@users.noreply.github.com>
2018-08-03 14:29:20 +0100

    mutation Tests

--- test/uk/gov/hmcts/contino/YarnBuilderTest.groovy
+++ test/uk/gov/hmcts/contino/YarnBuilderTest.groovy
@@ -72,16 +72,9 @@ class YarnBuilderTest extends Specification {
 
   def "mutationTest calls 'yarn test:mutation'"() {
     when:
-    builder.mutationTest()
+        builder.mutationTest()
     then:
-    1 * steps.sh({ it.startsWith(YARN_CMD) && it.contains('test:mutation') })
-  }
-
-  def "mutationTest calls 'yarn test:mutation'"() {
-    when:
-    builder.mutationTest()
-    then:
-    1 * steps.sh({ it.startsWith(YARN_CMD) && it.contains('test:mutation') })
+        1 * steps.sh({ it.startsWith(YARN_CMD) && it.contains('test:mutation') })
   }
 
   def "securityCheck calls 'yarn test:nsp'"() {
