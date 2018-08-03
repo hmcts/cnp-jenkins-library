49e3520902bb767953321d107fcdbdce4a2015e9
timwebster9 <tim.webster@gmail.com>
2018-08-03 13:36:06 +0100

    remove sections commented out for testing

--- vars/sectionBuildAndTest.groovy
+++ vars/sectionBuildAndTest.groovy
@@ -18,7 +18,6 @@ def call(PipelineCallbacks pl, Builder builder) {
     }
   }
 
-  /**
   stage("Test") {
     pl.callAround('test') {
       timeout(time: 15, unit: 'MINUTES') {
@@ -49,5 +48,4 @@ def call(PipelineCallbacks pl, Builder builder) {
       }
     }
   }
-   */
 }
