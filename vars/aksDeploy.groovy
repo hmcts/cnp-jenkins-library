db9fdff75f51d3a65b1ce6953c1c4c2883723e89
timwebster9 <tim.webster@gmail.com>
2018-08-06 15:37:00 +0100

    handle if secrets provided but no vault name, revert change for if no secrets/vault name provided (plugin won't fail)

--- vars/aksDeploy.groovy
+++ vars/aksDeploy.groovy
@@ -6,49 +6,49 @@ def call(List templateEnvVars, String subscription, PipelineCallbacks pl, String
   withDocker('hmcts/cnp-aks-client:1.2', null) {
     withSubscription(subscription) {
 
-      if (pl.vaultName && pl.vaultSecrets?.size() > 0) {
+      def keyvaultUrl
 
-        def projectKeyvaultName = pl.vaultName + '-' + env.NONPROD_ENVIRONMENT_NAME
-        def keyvaultUrl = "https://${projectKeyvaultName}.vault.azure.net/"
-
-        wrap([
-          $class                   : 'AzureKeyVaultBuildWrapper',
-          azureKeyVaultSecrets     : pl.vaultSecrets,
-          keyVaultURLOverride      : keyvaultUrl,
-          applicationIDOverride    : env.AZURE_CLIENT_ID,
-          applicationSecretOverride: env.AZURE_CLIENT_SECRET
-        ]) {
-          withEnv(templateEnvVars) {
-            deploy(subscription, namespace)
-          }
+      if (pl.vaultSecrets?.size() > 0) {
+        if (pl.vaultName) {
+          def projectKeyvaultName = pl.vaultName + '-' + env.NONPROD_ENVIRONMENT_NAME
+          keyvaultUrl = "https://${projectKeyvaultName}.vault.azure.net/"
+        }
+        else  {
+          error "Please set vault name `setVaultName('rhubarb')` if loading vault secrets"
         }
       }
-      else {
-        deploy(subscription, namespace)
+
+      wrap([
+        $class                   : 'AzureKeyVaultBuildWrapper',
+        azureKeyVaultSecrets     : pl.vaultSecrets,
+        keyVaultURLOverride      : keyvaultUrl,
+        applicationIDOverride    : env.AZURE_CLIENT_ID,
+        applicationSecretOverride: env.AZURE_CLIENT_SECRET
+      ]) {
+        withEnv(templateEnvVars) {
+
+          // authenticate with the cluster
+          def kubectl = new Kubectl(this, subscription, namespace)
+          kubectl.login()
+
+          // create namespace (idempotent)
+          kubectl.createNamespace(env.NAMESPACE)
+
+          // perform template variable substitution
+          sh "envsubst < src/kubernetes/deployment.template.yaml > src/kubernetes/deployment.yaml"
+
+          // deploy the app
+          kubectl.apply('src/kubernetes/deployment.yaml')
+
+          // discover and export the service URL for the next stage
+          env.AKS_TEST_URL = "http://" + kubectl.getServiceLoadbalancerIP(env.SERVICE_NAME)
+          echo "Your AKS service can be reached at: ${env.AKS_TEST_URL}"
+
+          def url = env.AKS_TEST_URL + '/health'
+          def healthChecker = new HealthChecker(this)
+          healthChecker.check(url, 10, 50)
+        }
       }
     }
   }
-}
-
-def deploy(subscription, namespace) {
-  // authenticate with the cluster
-  def kubectl = new Kubectl(this, subscription, namespace)
-  kubectl.login()
-
-  // create namespace (idempotent)
-  kubectl.createNamespace(env.NAMESPACE)
-
-  // perform template variable substitution
-  sh "envsubst < src/kubernetes/deployment.template.yaml > src/kubernetes/deployment.yaml"
-
-  // deploy the app
-  kubectl.apply('src/kubernetes/deployment.yaml')
-
-  // discover and export the service URL for the next stage
-  env.AKS_TEST_URL = "http://" + kubectl.getServiceLoadbalancerIP(env.SERVICE_NAME)
-  echo "Your AKS service can be reached at: ${env.AKS_TEST_URL}"
-
-  def url = env.AKS_TEST_URL + '/health'
-  def healthChecker = new HealthChecker(this)
-  healthChecker.check(url, 5, 10)
 }
