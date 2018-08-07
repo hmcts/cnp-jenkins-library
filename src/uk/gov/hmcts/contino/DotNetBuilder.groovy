df97ddc9d48cb5ce15df8ead2b739868f4ae5e0e
janisBerz <janis.berzins@hmcts.net>
2018-08-07 17:48:45 +0100

    Updated steps

--- src/uk/gov/hmcts/contino/DotNetBuilder.groovy
+++ src/uk/gov/hmcts/contino/DotNetBuilder.groovy
@@ -14,8 +14,8 @@ class DotNetBuilder extends AbstractBuilder {
   }
 
   def build() {
-    steps.bat 'nuget restore HearingsAPI\\HearingsAPI.sln'
-    steps.bat 'powershell $Path = Resolve-Path **\\**.sln; $proc = Start-Process -NoNewWindow -Wait -PassThru -FilePath msbuild -ArgumentList "$Path"; $proc.WaitForExit()'
+    steps.powershell 'nuget restore HearingsAPI\\HearingsAPI.sln'
+    steps.powershell '$Path = Resolve-Path **\\**.sln; $proc = Start-Process -NoNewWindow -Wait -PassThru -FilePath msbuild -ArgumentList "$Path"; $proc.WaitForExit()'
     //steps.msbuild "\"${tool 'MSBuild'}\" HearingsAPI\\HearingsAPI.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
   }
 
