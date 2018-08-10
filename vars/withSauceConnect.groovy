701d365f395dedd6c35e05a182fe5ce9a9b18e55
vakepati <venkata.akepati@hmcts.net>
2018-08-10 17:48:10 +0100

    added proxy configuration

--- vars/withSauceConnect.groovy
+++ vars/withSauceConnect.groovy
@@ -8,7 +8,7 @@
  */
 def call(String sauceid, Closure body) {
     sauce(sauceid) {
-        sauceconnect(options: '--proxy 172.16.0.7:8080 --proxy-tunnel --verbose --no-remove-colliding-tunnels  --tunnel-identifier reformtunnel', useGeneratedTunnelIdentifier: false,
+        sauceconnect(options: '--proxy reformmgmtproxyout.reform.hmcts.net:8080 --proxy-tunnel --verbose --no-remove-colliding-tunnels  --tunnel-identifier reformtunnel', useGeneratedTunnelIdentifier: false,
                 useLatestSauceConnect: true, verboseLogging: true) {
             body().call()
         }
