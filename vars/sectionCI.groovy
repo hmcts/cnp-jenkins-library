a8c1fd5982716320b56c5dde1cf3bff848a4e342
Calin Marina <calin.marina@gmail.com>
2018-09-05 10:54:37 +0300

    CNP-726 Enable functional tests for AKS (#282)

--- vars/sectionCI.groovy
+++ vars/sectionCI.groovy
@@ -1,28 +1,42 @@
+import uk.gov.hmcts.contino.Builder
 import uk.gov.hmcts.contino.PipelineCallbacks
+import uk.gov.hmcts.contino.PipelineType
 import uk.gov.hmcts.contino.ProjectBranch
 import uk.gov.hmcts.contino.DockerImage
 import uk.gov.hmcts.contino.azure.Acr
 
+def testEnv(String testUrl, block) {
+  def testEnvVariables = ["TEST_URL=${testUrl}"]
+
+  withEnv(testEnvVariables) {
+    echo "Using TEST_URL: '$env.TEST_URL'"
+    block.call()
+  }
+}
+
 def call(params) {
   PipelineCallbacks pl = params.pipelineCallbacks
+  PipelineType pipelineType = params.pipelineType
 
   def subscription = params.subscription
   def product = params.product
   def component = params.component
 
+  Builder builder = pipelineType.builder
+
   if (pl.dockerBuild) {
     withSubscription(subscription) {
 
       def registrySecrets = [
-        [ $class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'registry-name', version: '', envVariable: 'REGISTRY_NAME' ],
-        [ $class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'registry-resource-group', version: '', envVariable: 'REGISTRY_RESOURCE_GROUP' ],
-        [ $class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'aks-resource-group', version: '', envVariable: 'AKS_RESOURCE_GROUP' ],
-        [ $class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'aks-cluster-name', version: '', envVariable: 'AKS_CLUSTER_NAME' ],
+        [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'registry-name', version: '', envVariable: 'REGISTRY_NAME'],
+        [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'registry-resource-group', version: '', envVariable: 'REGISTRY_RESOURCE_GROUP'],
+        [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'aks-resource-group', version: '', envVariable: 'AKS_RESOURCE_GROUP'],
+        [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'aks-cluster-name', version: '', envVariable: 'AKS_CLUSTER_NAME'],
       ]
 
-      wrap([$class: 'AzureKeyVaultBuildWrapper',
-            azureKeyVaultSecrets: registrySecrets,
-            keyVaultURLOverride: env.INFRA_VAULT_URL,
+      wrap([$class                   : 'AzureKeyVaultBuildWrapper',
+            azureKeyVaultSecrets     : registrySecrets,
+            keyVaultURLOverride      : env.INFRA_VAULT_URL,
             applicationIDOverride    : env.AZURE_CLIENT_ID,
             applicationSecretOverride: env.AZURE_CLIENT_SECRET
       ]) {
@@ -44,6 +58,29 @@ def call(params) {
               timeout(time: 15, unit: 'MINUTES') {
                 aksUrl = aksDeploy(dockerImage, params, acr)
                 log.info("deployed component URL: ${aksUrl}")
+              }
+            }
+          }
+          stage("Smoke Test - AKS") {
+            testEnv(aksUrl) {
+              pl.callAround("smoketest:aks") {
+                timeout(time: 10, unit: 'MINUTES') {
+                  builder.smokeTest()
+                }
+              }
+            }
+          }
+
+          def environment = subscription == 'nonprod' ? 'preview' : 'saat'
+
+          onFunctionalTestEnvironment(environment) {
+            stage("Functional Test - AKS") {
+              testEnv(aksUrl) {
+                pl.callAround("functionalTest:${environment}") {
+                  timeout(time: 40, unit: 'MINUTES') {
+                    builder.functionalTest()
+                  }
+                }
               }
             }
           }
