a8309a4202e34b72efd19225d834be2ada632524
Tim Webster <tim.webster@gmail.com>
2018-10-05 18:10:15 +0100

    fix ACR build logs  (#319)
    * suppress ACR builds logs due to https://github.com/Azure/azure-cli/issues/6408
    
    * update aks client image, use --no-format in ACR build

--- test/uk/gov/hmcts/contino/azure/AcrTest.groovy
+++ test/uk/gov/hmcts/contino/azure/AcrTest.groovy
@@ -50,7 +50,7 @@ class AcrTest extends Specification {
 
     then:
       1 * steps.sh({it.containsKey('script') &&
-                    it.get('script').contains("az acr build -r ${REGISTRY_NAME} -t ${IMAGE_NAME} -g ${REGISTRY_RESOURCE_GROUP} .") &&
+                    it.get('script').contains("az acr build --no-format -r ${REGISTRY_NAME} -t ${IMAGE_NAME} -g ${REGISTRY_RESOURCE_GROUP} .") &&
                     it.containsKey('returnStdout') &&
                     it.get('returnStdout').equals(true)})
   }
