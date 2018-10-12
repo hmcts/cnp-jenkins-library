801c05ca817c5caa2401a24580d640db9b1d6e17
timwebster9 <tim.webster@gmail.com>
2018-10-12 16:46:29 +0100

    fix mirror url. again.

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -51,7 +51,7 @@ def call(type, String product, String component, Closure body) {
         env.PATH = "$env.PATH:/usr/local/bin"
 
         def mirrorHost = (subscription.nonProdName == "sandbox") ? 'sandbox-artifactory' : 'artifactory'
-        env.ARTIFACT_MIRROR = "https://${mirrorHost}.platform.hmcts.net/maven-remotes"
+        env.ARTIFACT_MIRROR = "https://${mirrorHost}.platform.hmcts.net/artifactory/maven-remotes"
 
         sectionBuildAndTest(pl, pipelineType.builder)
 
