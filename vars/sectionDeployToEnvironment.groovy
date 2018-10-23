6da8a40e3ee0de2a46aa004584763b289a5a5665
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2018-10-23 10:55:18 +0100

    Update sectionDeployToEnvironment.groovy

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -32,23 +32,16 @@ def collectAdditionalInfrastructureVariablesFor(subscription, product, environme
   
   onHMCTSDemo {
     keyVault = new KeyVault(this, subscription, "infra-vault-hmctsdemo")
-  }
     
-  def hmctsdemoTenantId = keyVault.find("security-aad-tenantId")
-  if (hmctsdemoTenantId) {
+    def hmctsdemoTenantId = keyVault.find("security-aad-tenantId")
     environmentVariables.add("TF_VAR_security_aad_tenantId=${hmctsdemoTenantId}")
-  }
 
-  def hmctsdemoClientId = keyVault.find("security-aad-clientId")
-  if (hmctsdemoClientId) {
+    def hmctsdemoClientId = keyVault.find("security-aad-clientId")
     environmentVariables.add("TF_VAR_security_aad_clientId=${hmctsdemoClientId}")
-  }
-  
-  def hmctsdemoClientSecret = keyVault.find("security-aad-clientSecret")
-  if (hmctsdemoClientSecret) {
-    environmentVariables.add("TF_VAR_security_aad_clientSecret=${hmctsdemoClientSecret}")
-  }        
-  
+
+    def hmctsdemoClientSecret = keyVault.find("security-aad-clientSecret")
+    environmentVariables.add("TF_VAR_security_aad_clientSecret=${hmctsdemoClientSecret}")  
+  }     
   return environmentVariables
 }
 
