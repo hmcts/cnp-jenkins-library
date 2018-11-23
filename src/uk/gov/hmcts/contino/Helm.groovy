b1ece58fe010a0c9e1eb5757a8721185ba991974
Luigi Bitonti <luigi.bitonti@hmcts.net>
2018-11-23 11:42:12 +0000

    Use subscription Id

--- src/uk/gov/hmcts/contino/Helm.groovy
+++ src/uk/gov/hmcts/contino/Helm.groovy
@@ -14,8 +14,9 @@ class Helm {
   def init() {
     this.steps.sh(returnStatus: true, script: "helm init --client-only")
     def subscription = this.steps.env.SUBSCRIPTION_NAME
+    def subscriptionId = steps.env.AZURE_SUBSCRIPTION_ID
     this.steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-${subscription} az configure --defaults acr=hmcts", returnStdout: true)
-    this.steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-${subscription} az acr helm repo add  --subscription ${subscription}", returnStdout: true)
+    this.steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-${subscription} az acr helm repo add  --subscription ${subscriptionId}", returnStdout: true)
   }
 
   def installOrUpgradeMulti(List<String> names, List<String> values) {
