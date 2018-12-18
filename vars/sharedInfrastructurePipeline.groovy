a3275418106bf3a9aec496aaee4f6f6cbd35dd2b
danielev04 <daniele@vottero.eu>
2018-12-18 09:58:55 +0000

    added per deploymentTarget

--- vars/sharedInfrastructurePipeline.groovy
+++ vars/sharedInfrastructurePipeline.groovy
@@ -38,3 +38,42 @@ def call(String product, String environment, String subscription, boolean planOn
     }
   }
 }
+
+def call(String product, String environment, String subscription, boolean planOnly, String deploymentTarget) {
+
+  node {
+    env.PATH = "$env.PATH:/usr/local/bin"
+
+    def tfOutput
+
+    stage('Checkout') {
+      deleteDir()
+      checkout scm
+    }
+
+    withSubscription(subscription) {
+      withIlbIp(environment) {
+        tfOutput = spinInfra(product, null, environment, planOnly, subscription)
+        folderExists('deploymentTarget') {
+          dir('deploymentTarget') {
+            spinInfra(product, null, environment, planOnly, subscription, deploymentTarget)
+          }
+        }
+      }
+
+      if (!planOnly) {
+        stage('Store shared product secrets') {
+          if (!tfOutput.vaultName) {
+            throw new IllegalStateException("No vault has been created to store the secrets in")
+          }
+
+          KeyVault keyVault = new KeyVault(this, subscription, tfOutput.vaultName.value)
+
+          if (tfOutput.appInsightsInstrumentationKey) {
+            keyVault.store(ProductVaultEntries.APP_INSIGHTS_INSTRUMENTATION_KEY, tfOutput.appInsightsInstrumentationKey.value)
+          }
+        }
+      }
+    }
+  }
+}
