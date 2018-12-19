1b6d53fca0aab60b4e90c125ca8b041cfb3333cf
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2018-12-19 11:17:12 +0000

    Update Consul.groovy

--- src/uk/gov/hmcts/contino/Consul.groovy
+++ src/uk/gov/hmcts/contino/Consul.groovy
@@ -22,8 +22,15 @@ class Consul {
       return this.consulApiAddr
     }
     this.steps.log.info "Getting consul's IP address ..."
+    
+    def subscriptionResourceGroupMap = [
+        'nonprod'  : 'core-infra-preview',
+        'hmctsdemo'  : 'core-infra-hmctsdemo'
+    ]
 
-    def tempConsulIpAddr = this.az.az "network lb frontend-ip show  -g core-infra-${subscription  == 'nonprod' ? 'preview' : 'saat'}  --lb-name consul-server_dns --name PrivateIPAddress --query privateIpAddress -o tsv"
+    def consulResourceGroup = subscriptionResourceGroupMap.subscription ?: 'core-infra-saat'
+
+    def tempConsulIpAddr = this.az.az "network lb frontend-ip show  -g ${consulResourceGroup}  --lb-name consul-server_dns --name PrivateIPAddress --query privateIpAddress -o tsv"
     this.consulApiAddr = tempConsulIpAddr?.trim()
     if (this.consulApiAddr == null || "".equals(this.consulApiAddr)) {
       throw new RuntimeException("Failed to retrieve Consul LB IP")
