3b41f120679c2d52b3b89a679a36393dbe5d324a
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2019-01-04 16:09:39 +0000

    Update sectionDeployToEnvironment.groovy

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -21,30 +21,6 @@ def testEnv(String testUrl, tfOutput, block) {
   }
 }
 
-def collectAdditionalInfrastructureVariablesFor(subscription, product, environment) {
-  KeyVault keyVault = new KeyVault(this, subscription, "${product}-${environment}")
-  def environmentVariables = []
-
-  def appInsightsInstrumentationKey = keyVault.find(ProductVaultEntries.APP_INSIGHTS_INSTRUMENTATION_KEY)
-  if (appInsightsInstrumentationKey) {
-    environmentVariables.add("TF_VAR_appinsights_instrumentation_key=${appInsightsInstrumentationKey}")
-  }
-
-  onHMCTSDemo {
-    keyVault = new KeyVault(this, subscription, "infra-vault-hmctsdemo")
-
-    def hmctsdemoTenantId = keyVault.find("security-aad-tenantId")
-    environmentVariables.add("TF_VAR_security_aad_tenantId=${hmctsdemoTenantId}")
-
-    def hmctsdemoClientId = keyVault.find("security-aad-clientId")
-    environmentVariables.add("TF_VAR_security_aad_clientId=${hmctsdemoClientId}")
-
-    def hmctsdemoClientSecret = keyVault.find("security-aad-clientSecret")
-    environmentVariables.add("TF_VAR_security_aad_clientSecret=${hmctsdemoClientSecret}")
-  }
-  return environmentVariables
-}
-
 def call(params) {
   PipelineCallbacks pl = params.pipelineCallbacks
   PipelineType pipelineType = params.pipelineType
