f3334dfd01034b2f744cc9733964bd811f6707ca
Tim Jacomb <t.jacomb@kainos.com>
2019-01-30 09:53:51 +0000

    RPE-835 Add lint to PR pipeline (#394)

--- src/uk/gov/hmcts/contino/Helm.groovy
+++ src/uk/gov/hmcts/contino/Helm.groovy
@@ -5,6 +5,7 @@ import uk.gov.hmcts.contino.azure.Acr
 
 class Helm {
 
+  public static final String HELM_RESOURCES_DIR = "charts"
   def steps
   def acr
   def helm = { cmd, name, options -> return this.steps.sh(script: "helm $cmd $name $options", returnStdout: true)}
@@ -13,14 +14,18 @@ class Helm {
   def subscriptionId
   def resourceGroup
   def registryName
+  String chartLocation
+  def chartName
 
-  Helm(steps) {
+  Helm(steps, String chartName) {
     this.steps = steps
     this.subscription = this.steps.env.SUBSCRIPTION_NAME
     this.subscriptionId = this.steps.env.AZURE_SUBSCRIPTION_ID
     this.resourceGroup = this.steps.env.AKS_RESOURCE_GROUP
     this.registryName = (subscription == "sandbox" ? "hmctssandbox" : "hmcts")
     this.acr = new Acr(this.steps, subscription, registryName, resourceGroup)
+    this.chartLocation = "${HELM_RESOURCES_DIR}/${chartName}"
+    this.chartName = chartName
   }
 
   def setup() {
@@ -41,29 +46,25 @@ class Helm {
     this.acr.az "acr helm repo add --subscription ${subscriptionId}"
   }
 
-  def installOrUpgradeMulti(String path, List<String> names, List<String> values) {
-    this.installOrUpgradeMulti(path, names, values, null)
+  def lint(List<String> values) {
+    this.execute("lint", this.chartLocation, values, null)
   }
 
-  def installOrUpgradeMulti(String path, List<String> names, List<String> values, List<String> options) {
-    // zip chart name + related values files and execute
-    [names, values].transpose().each { nv ->
-      this.installOrUpgrade(path, nv[0], nv[1..-1], options)
-    }
-  }
-
-  def installOrUpgrade(String path, String name, List<String> values, List<String> options) {
+  def installOrUpgrade(String imageTag, List<String> values, List<String> options) {
     if (!values) {
       throw new RuntimeException("Helm charts need at least a values file (none given).")
     }
-    this.dependencyUpdate(path)
+
+    dependencyUpdate()
+    lint(values)
+
     def allOptions = ["--install", "--wait", "--timeout 500"] + (options == null ? [] : options)
     def allValues = values.flatten()
-    this.execute("upgrade", "${name} ${path}", allValues, allOptions)
+    this.execute("upgrade", "${this.chartName}-${imageTag} ${this.chartLocation}", allValues, allOptions)
   }
 
-  def dependencyUpdate(String path) {
-    this.execute("dependency update", path)
+  def dependencyUpdate() {
+    this.execute("dependency update", this.chartLocation)
   }
 
   def delete(String name) {
--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -13,13 +13,13 @@ def call(DockerImage dockerImage, Map params) {
   def product = params.product
   def component = params.component
 
-  def helmResourcesDir = "charts"
+  def helmResourcesDir = Helm.HELM_RESOURCES_DIR
 
   def digestName = dockerImage.getDigestName()
   def aksServiceName = dockerImage.getAksServiceName()
   def aksDomain = "${(subscription in ['nonprod', 'prod']) ? 'service.core-compute-preview.internal' : 'service.core-compute-saat.internal'}"
   def serviceFqdn = "${aksServiceName}.${aksDomain}"
-  
+
   def consul = new Consul(this)
   def consulApiAddr = consul.getConsulIP()
 
@@ -29,31 +29,30 @@ def call(DockerImage dockerImage, Map params) {
   def ingressIP = kubectl.getServiceLoadbalancerIP("traefik", "kube-system")
 
   def templateEnvVars = [
-    "NAMESPACE=${aksServiceName}", 
-    "SERVICE_NAME=${aksServiceName}", 
-    "IMAGE_NAME=${digestName}", 
+    "NAMESPACE=${aksServiceName}",
+    "SERVICE_NAME=${aksServiceName}",
+    "IMAGE_NAME=${digestName}",
     "SERVICE_FQDN=${serviceFqdn}",
     "CONSUL_LB_IP=${consulApiAddr}",
     "INGRESS_IP=${ingressIP}"
   ]
-  
+
   withEnv(templateEnvVars) {
 
     if (!fileExists("${helmResourcesDir}")) {
       throw new RuntimeException("No Helm charts directory found at ${helmResourcesDir}")
     }
 
-    def helm = new Helm(this)
-    helm.setup()
-
     def values = []
-    def chart = aksServiceName
-    def chartPath = "${product}-${component}"
+    def chartName = "${product}-${component}"
     def namespace = new TeamNames().getNameNormalizedOrThrow(product)
 
+    def helm = new Helm(this, chartName)
+    helm.setup()
+
     // default values + overrides
-    def templateValues = "${helmResourcesDir}/${chartPath}/values.template.yaml"
-    def defaultValues = "${helmResourcesDir}/${chartPath}/values.yaml"
+    def templateValues = "${helmResourcesDir}/${chartName}/values.template.yaml"
+    def defaultValues = "${helmResourcesDir}/${chartName}/values.yaml"
     if (!fileExists(templateValues) && !fileExists(defaultValues)) {
       throw new RuntimeException("No default values file found at ${templateValues} or ${defaultValues}")
     }
@@ -63,26 +62,27 @@ def call(DockerImage dockerImage, Map params) {
     values << defaultValues
 
     // environment specific values is optional
-    def valuesEnvTemplate = "${helmResourcesDir}/${chartPath}/values.${environment}.template.yaml"
-    def valuesEnv = "${helmResourcesDir}/${chartPath}/values.${environment}.yaml"
+    def valuesEnvTemplate = "${helmResourcesDir}/${chartName}/values.${environment}.template.yaml"
+    def valuesEnv = "${helmResourcesDir}/${chartName}/values.${environment}.yaml"
     if (fileExists(valuesEnvTemplate)) {
       sh "envsubst < ${valuesEnvTemplate} > ${valuesEnv}"
       values << valuesEnv
     }
 
-    def requirementsEnv = "${helmResourcesDir}/${chartPath}/requirements.${environment}.yaml"
-    def requirements = "${helmResourcesDir}/${chartPath}/requirements.yaml"
+    def requirementsEnv = "${helmResourcesDir}/${chartName}/requirements.${environment}.yaml"
+    def requirements = "${helmResourcesDir}/${chartName}/requirements.yaml"
     if (fileExists(requirementsEnv)) {
       sh "envsubst < ${requirementsEnv} > ${requirements}"
     }
 
     def options = [
       "--set global.subscriptionId=${this.env.AZURE_SUBSCRIPTION_ID} ",
-      "--set global.tenantId=${this.env.AZURE_TENANT_ID} ", 
+      "--set global.tenantId=${this.env.AZURE_TENANT_ID} ",
       "--set global.environment=${environment} ",
-      "--namespace ${namespace}" 
+      "--namespace ${namespace}"
     ]
-    helm.installOrUpgrade("${helmResourcesDir}/${chartPath}", chart, values, options)
+
+    helm.installOrUpgrade(dockerImage.getTag(), values, options)
 
     // Register service dns
     consul.registerDns(aksServiceName, ingressIP)
