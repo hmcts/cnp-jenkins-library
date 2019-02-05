06081598b291533e62645ff2fa36890ab5da07e0
Tim Jacomb <tim.jacomb@hmcts.net>
2019-02-05 08:13:29 +0000

    Fix typo

--- src/uk/gov/hmcts/contino/Helm.groovy
+++ src/uk/gov/hmcts/contino/Helm.groovy
@@ -62,8 +62,8 @@ class Helm {
 
       this.steps.sh "helm package ${this.chartLocation}"
       this.acr.az "acr helm push --name ${registryName} ${this.chartName}-${version}.tgz"
-      
-      this.steps.sh "Published ${this.chartName}-${version} to ${registryName}"
+
+      this.steps.echo "Published ${this.chartName}-${version} to ${registryName}"
     } else {
       this.steps.echo "Chart already published, skipping publish, bump the version in ${this.chartLocation}/Chart.yaml if you want it to be published"
     }
