464ac364424c9b29cf7d9ea29939a434a1a56f0c
Neil Richardson <njrich28@gmail.com>
2019-03-11 15:27:27 +0000

    Add deprecation notice to the withInfrastructurePipeline

--- vars/withInfrastructurePipeline.groovy
+++ vars/withInfrastructurePipeline.groovy
@@ -2,6 +2,24 @@
 @Deprecated
 def call(String product, String environment, String subscription, String deploymentTarget = "") {
 
+  echo '''
+================================================================================
+withInfrastructurePipeline is
+______                              _           _ _ 
+|  _  \\                            | |         | | |
+| | | |___ _ __  _ __ ___  ___ __ _| |_ ___  __| | |
+| | | / _ \\ '_ \\| '__/ _ \\/ __/ _` | __/ _ \\/ _` | |
+| |/ /  __/ |_) | | |  __/ (_| (_| | ||  __/ (_| |_|
+|___/ \\___| .__/|_|  \\___|\\___\\__,_|\\__\\___|\\__,_(_)
+          | |                                       
+          |_|                                       
+
+Use withInfraPipeline instead
+
+https://github.com/hmcts/cnp-jenkins-library#opinionated-infrastructure-pipeline
+================================================================================
+'''
+
   def environmentDeploymentTarget = "$environment$deploymentTarget"
 
   node {
