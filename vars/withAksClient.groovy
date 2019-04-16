293696dd657f2436af640de2d52c4643b5b42498
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-04-16 21:42:45 +0100

    Added debug log

--- vars/withAksClient.groovy
+++ vars/withAksClient.groovy
@@ -21,8 +21,9 @@ def call(String subscription, String environment, Closure block) {
     withSubscription(subscription) {
       withRegistrySecrets {
         def envName = environment.toUpperCase()
-        env.AKS_CLUSTER_NAME = env."${envName}_AKS_CLUSTER_NAME" ?: 'cnp-${environment}-cluster'
-        env.AKS_RESOURCE_GROUP = env."${envName}_AKS_RESOURCE_GROUP" ?: 'cnp-${environment}-rg'
+        echo "envname : ${envName} "
+        env.AKS_CLUSTER_NAME = env."${envName}_AKS_CLUSTER_NAME" ?: "cnp-${environment}-cluster"
+        env.AKS_RESOURCE_GROUP = env."${envName}_AKS_RESOURCE_GROUP" ?: "cnp-${environment}-rg"
         block.call()
       }
     }
