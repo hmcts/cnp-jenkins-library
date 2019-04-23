af6fcbd2c2d78ae105baffafdd9f7227d715356c
Tim Jacomb <tim.jacomb@hmcts.net>
2019-04-23 15:37:22 +0100

    Try cleanup gatling options

--- src/uk/gov/hmcts/contino/Gatling.groovy
+++ src/uk/gov/hmcts/contino/Gatling.groovy
@@ -30,9 +30,33 @@ class Gatling implements Serializable {
     this.steps.withDocker(gatlingImage, GATLING_RUN_ARGS) {
       this.steps.sh """
         export JAVA_HOME=''
-        gatling.sh -m -sf ${GATLING_SIMS_DIR} -df ${GATLING_DATA_DIR} -bdf ${GATLING_BODIES_DIR} -bf ${GATLING_BINARIES_DIR} -rf ${GATLING_REPORTS_DIR}
+        gatling.sh -m --simulations-folder ${GATLING_SIMS_DIR} \
+           --binaries-folder ${GATLING_BINARIES_DIR} \
+           --results-folder ${GATLING_REPORTS_DIR}
       """
     }
+
+    """
+
+Usage: gatling [options]
+
+  -h, --help               Show help (this message) and exit
+  -nr, --no-reports        Runs simulation but does not generate reports
+  -ro, --reports-only <directoryName>
+                           Generates the reports for the simulation in <directoryName>
+  -rsf, --resources-folder <directoryPath>
+                           Uses <directoryPath> as the absolute path of the directory where resources are stored
+  -rf, --results-folder <directoryPath>
+                           Uses <directoryPath> as the absolute path of the directory where results are stored
+  -sf, --simulations-folder <directoryPath>
+                           Uses <directoryPath> to discover simulations that could be run
+  -bf, --binaries-folder <directoryPath>
+                           Uses <directoryPath> as the absolute path of the directory where Gatling should produce compiled binaries
+  -s, --simulation <className>
+                           Runs <className> simulation
+  -rd, --run-description <description>
+                           A short <description> of the run to include in the report
+"""
 
     // jenkins gatling plugin
     this.steps.gatlingArchive()
