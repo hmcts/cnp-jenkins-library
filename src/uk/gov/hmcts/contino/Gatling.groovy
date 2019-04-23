f64003e623ca541554753edf2e14eee4440879ea
Tim Jacomb <tim.jacomb@hmcts.net>
2019-04-23 15:39:48 +0100

    Remove garbage

--- src/uk/gov/hmcts/contino/Gatling.groovy
+++ src/uk/gov/hmcts/contino/Gatling.groovy
@@ -36,28 +36,6 @@ class Gatling implements Serializable {
       """
     }
 
-    """
-
-Usage: gatling [options]
-
-  -h, --help               Show help (this message) and exit
-  -nr, --no-reports        Runs simulation but does not generate reports
-  -ro, --reports-only <directoryName>
-                           Generates the reports for the simulation in <directoryName>
-  -rsf, --resources-folder <directoryPath>
-                           Uses <directoryPath> as the absolute path of the directory where resources are stored
-  -rf, --results-folder <directoryPath>
-                           Uses <directoryPath> as the absolute path of the directory where results are stored
-  -sf, --simulations-folder <directoryPath>
-                           Uses <directoryPath> to discover simulations that could be run
-  -bf, --binaries-folder <directoryPath>
-                           Uses <directoryPath> as the absolute path of the directory where Gatling should produce compiled binaries
-  -s, --simulation <className>
-                           Runs <className> simulation
-  -rd, --run-description <description>
-                           A short <description> of the run to include in the report
-"""
-
     // jenkins gatling plugin
     this.steps.gatlingArchive()
   }
