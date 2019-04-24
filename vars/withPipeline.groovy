9c1a901059e93c6311ba5eaeebe240303c8dfab3
Tim Jacomb <t.jacomb@kainos.com>
2019-04-24 15:43:33 +0100

    Update vars/withPipeline.groovy
    Co-Authored-By: adusumillipraveen <47391951+adusumillipraveen@users.noreply.github.com>

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -71,7 +71,7 @@ def call(type, String product, String component, Closure body) {
           component: component
         )
 
-        onPR{
+        onPR {
 
           sectionCI(
             appPipelineConfig: pipelineConfig,
