571cf1ce75d780dfea9904e460aa5f38d4004d18
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-06-12 13:51:43 +0100

    Fix creds

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -85,7 +85,7 @@ class GradleBuilder extends AbstractBuilder {
       ]
       steps.withAzureKeyVault(secrets) {
         try {
-          gradle("-DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name='org.postgresql.Driver' -Ddata.connection_string='jdbc:postgresql://owaspdependency-v5-prod.postgres.database.azure.com/owaspdependencycheck' -Ddata.user='${steps.env.OWASPDB_ACCOUNT}' -Ddata.password='${steps.env.OWASPDB_PASSWORD}' -Dautoupdate='false' dependencyCheckAnalyze")
+          gradle("-DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name='org.postgresql.Driver' -Ddata.connection_string='jdbc:postgresql://owaspdependency-v5-prod.postgres.database.azure.com/owaspdependencycheck' -Ddata.user='${steps.env.OWASPDB_V5_ACCOUNT}' -Ddata.password='${steps.env.OWASPDB_V5_PASSWORD}' -Dautoupdate='false' dependencyCheckAnalyze")
         }
         finally {
           steps.archiveArtifacts 'build/reports/dependency-check-report.html'
