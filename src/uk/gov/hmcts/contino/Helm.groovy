abac63a254eee45eaaf62ce2ac501860b025eaaf
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-07-09 21:59:02 +0100

    Test commit to see if this will avoid new line

--- src/uk/gov/hmcts/contino/Helm.groovy
+++ src/uk/gov/hmcts/contino/Helm.groovy
@@ -52,9 +52,10 @@ class Helm {
   def enableTLS(String aksSubscription, String keyVaultName){
     def az = { cmd -> return steps.sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$steps.env.SUBSCRIPTION_NAME az $cmd", returnStdout: true).trim() }
     def helmRoot = this.helm "home", "", ""
-    az "keyvault secret download --vault-name '$keyVaultName' --name 'helm-pki-ca-cert' --subscription '$aksSubscription' --file '$helmRoot/ca.pem' "
-    az "keyvault secret download --vault-name '$keyVaultName' --name 'helm-pki-helm-cert' --subscription '$aksSubscription' --file '$helmRoot/cert.pem' "
-    az "keyvault secret download --vault-name '$keyVaultName' --name 'helm-pki-helm-key' --subscription '$aksSubscription' --file '$helmRoot/key.pem' "
+    this.steps.echo "Helm root is : ${helmRoot} and this is debug statement "
+    az "keyvault secret download --vault-name '$keyVaultName' --name 'helm-pki-ca-cert' --subscription '$aksSubscription' --file $helmRoot/ca.pem "
+    az "keyvault secret download --vault-name '$keyVaultName' --name 'helm-pki-helm-cert' --subscription '$aksSubscription' --file $helmRoot/cert.pem "
+    az "keyvault secret download --vault-name '$keyVaultName' --name 'helm-pki-helm-key' --subscription '$aksSubscription' --file $helmRoot/key.pem "
     this.tlsOptions = "--tls"
   }
 
