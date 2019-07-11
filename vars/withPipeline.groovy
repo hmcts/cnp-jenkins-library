bbf96677b5d1519a6976b941ccab17bc9bf9c69f
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-07-11 16:40:42 +0100

    Add missing param

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -106,6 +106,7 @@ def call(type, String product, String component, Closure body) {
             pipelineCallbacksRunner: callbacksRunner,
             pipelineType: pipelineType,
             subscription: subscription.nonProdName,
+            aksSubscription: aksSubscription.previewName,
             environment: environment.nonProdName,
             product: product,
             component: component)
