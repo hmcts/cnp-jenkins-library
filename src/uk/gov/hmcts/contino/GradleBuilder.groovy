b2272cd834c0420c52d2801291a6b32640b174b2
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-07-22 15:57:36 +0100

    removing unwanted secrets (#513)

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -80,9 +80,7 @@ class GradleBuilder extends AbstractBuilder {
   def securityCheck() {
     def secrets = [
       [ secretType: 'Secret', name: 'OWASPPostgresDb-v5-Account', version: '', envVariable: 'OWASPDB_V5_ACCOUNT' ],
-      [ secretType: 'Secret', name: 'OWASPPostgresDb-v5-Password', version: '', envVariable: 'OWASPDB_V5_PASSWORD' ],
-      [ secretType: 'Secret', name: 'OWASPPostgresDb-Account', version: '', envVariable: 'OWASPDB_ACCOUNT' ],
-      [ secretType: 'Secret', name: 'OWASPPostgresDb-Password', version: '', envVariable: 'OWASPDB_PASSWORD' ]
+      [ secretType: 'Secret', name: 'OWASPPostgresDb-v5-Password', version: '', envVariable: 'OWASPDB_V5_PASSWORD' ]
     ]
     steps.withAzureKeyvault(secrets) {
       try {
@@ -90,9 +88,7 @@ class GradleBuilder extends AbstractBuilder {
           gradle("-DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name='org.postgresql.Driver' -Ddata.connection_string='jdbc:postgresql://owaspdependency-v5-prod.postgres.database.azure.com/owaspdependencycheck' -Ddata.user='${steps.env.OWASPDB_V5_ACCOUNT}' -Ddata.password='${steps.env.OWASPDB_V5_PASSWORD}' -Dautoupdate='false' -Danalyzer.retirejs.enabled=false dependencyCheckAnalyze")
         } else {
           // NOTE: delete owasp 4 dependency check and its tests in GradleBuilderTest some time after 15/07/2019
-          if (new Date() > new Date().parse("dd.MM.yyyy", "15.07.2019")) {
             throw new RuntimeException("Owasp dependency check version 4 is not available anymore. Please update your build to use version 5.")
-          }
         }
       }
       finally {
