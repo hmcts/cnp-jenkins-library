c0bf35e04447b4effb677eda6fe325c38deb4e3f
Neil Richardson <njrich28@gmail.com>
2019-07-31 12:13:59 +0100

    Rename variable from PR comments

--- src/uk/gov/hmcts/pipeline/EnvironmentApprovals.groovy
+++ src/uk/gov/hmcts/pipeline/EnvironmentApprovals.groovy
@@ -3,14 +3,14 @@ package uk.gov.hmcts.pipeline
 class EnvironmentApprovals {
   def steps
   static final String GITHUB_CREDENTIAL = 'jenkins-github-hmcts-api-token'
-  static def approvalsMap
+  static def environmentApprovals
 
   EnvironmentApprovals(steps) {
     this.steps = steps
   }
 
-  def getEnvironmentApprovalsMap() {
-    if (approvalsMap == null) {
+  def getEnvironmentApprovals() {
+    if (environmentApprovals == null) {
       def response = steps.httpRequest(
         consoleLogResponseBody: true,
         authentication: "${GITHUB_CREDENTIAL}",
@@ -18,19 +18,19 @@ class EnvironmentApprovals {
         url: "https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/environment-approvals.yml",
         validResponseCodes: '200'
       )
-      approvalsMap = steps.readYaml(text: response.content)
+      environmentApprovals = steps.readYaml(text: response.content)
     }
-    return approvalsMap
+    return environmentApprovals
   }
 
   boolean isApproved(String environment, String repository) {
-    def environmentApprovalsMap = getEnvironmentApprovalsMap()
-    if (!environmentApprovalsMap.containsKey(environment)) {
+    def environmentApprovals = getEnvironmentApprovals()
+    if (!environmentApprovals.containsKey(environment)) {
       // if environment is not listed in config then assume no whitelist required
       return true
     }
 
-    def approvals = environmentApprovalsMap.get(environment)
+    def approvals = environmentApprovals.get(environment)
     return approvals.findResult(false) { it.repo.equalsIgnoreCase(repository) ? true : null }
   }
 
