a88db71eed27cf5b0077a4d305f979d3bc56de73
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-08-20 14:01:19 +0100

    Use docker image for tf-utils

--- src/uk/gov/hmcts/pipeline/TerraformInfraApprovals.groovy
+++ src/uk/gov/hmcts/pipeline/TerraformInfraApprovals.groovy
@@ -4,8 +4,10 @@ import uk.gov.hmcts.contino.RepositoryUrl
 
 class TerraformInfraApprovals {
 
-  static final String GITHUB_CREDENTIAL = 'jenkins-github-hmcts-api-token'
   static final String GITHUB_BASE_URL = 'https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/terraform-infra-approvals'
+
+  public static final String TFUTILS_IMAGE = 'tf-utils:dbd9e'
+  public static final String TFUTILS_RUN_ARGS = '--entrypoint ""'
 
   def steps
   def subscription
@@ -20,18 +22,17 @@ class TerraformInfraApprovals {
   def getInfraApprovals() {
     if (!infraApprovals) {
       String repositoryShortUrl = new RepositoryUrl().getShortWithoutOrg(this.steps.env.CHANGE_URL)
-      ["global.json", "${repositoryShortUrl}.json"].each {
+      ["global.json": "200", "${repositoryShortUrl}.json": "200:404"].each { k,v ->
         steps.httpRequest(
           consoleLogResponseBody: true,
-          authentication: "${GITHUB_CREDENTIAL}",
           timeout: 10,
-          url: "${GITHUB_BASE_URL}/${it}",
-          validResponseCodes: '200:404',
-          outputFile: "${it}"
+          url: "${GITHUB_BASE_URL}/${k}",
+          validResponseCodes: v,
+          outputFile: "${k}"
         )
-        def infraApprovalsFile = new File(it)
+        def infraApprovalsFile = new File(k)
         if (infraApprovalsFile.exists() && infraApprovalsFile.length() > 0) {
-          infraApprovals << it
+          infraApprovals << k
         }
       }
     }
@@ -42,14 +43,16 @@ class TerraformInfraApprovals {
   boolean isApproved(String tfInfraPath) {
     def infraApprovals = getInfraApprovals()
     if (!infraApprovals) {
-      this.steps.sh("echo 'WARNING: No Terraform infrastructure whitelist (terraform-infra-approvals.json) found.'")
+      this.steps.sh("echo 'WARNING: No Terraform infrastructure whitelist found.'")
       return true
     }
     if (this.subscription == "sandbox") {
       this.steps.sh("echo 'WARNING: Terraform whitelisting disabled. Please do not forget to check your infrastructure.'")
       return true
     }
-    return this.steps.sh(returnStatus: true, script: "tf-utils --whitelist ${tfInfraPath} ${infraApprovals.join(" ")}")
+    this.steps.withDocker(TFUTILS_IMAGE, TFUTILS_RUN_ARGS) {
+      return this.steps.sh(returnStatus: true, script: "/tf-utils --whitelist ${tfInfraPath} ${infraApprovals.join(" ")}")
+    }
   }
 
 }
