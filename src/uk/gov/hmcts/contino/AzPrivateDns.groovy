49837315456a728766e2e88fc50b3ee9e47d5db4
Gareth Heath <gareth.heath@hmcts.net>
2019-09-05 15:03:03 +0100

    codes updates

--- src/uk/gov/hmcts/contino/AzPrivateDns.groovy
+++ src/uk/gov/hmcts/contino/AzPrivateDns.groovy
@@ -1,9 +1,11 @@
 class AzPrivateDns {
 
-    private subscriptionId = "6aff0bdf-7c12-4850-8f7e-7dd4633b411"     // subscription id of azure private dns zones. Reform-CFT-Sandbox
-    private ResourceGroup = "rdo-private-dns-sbox"                     // resource group of azure dns zones
-    private zone = "service.core-compute-${env}.internal"
+    private subscriptionId = ""     // subscription id of azure private dns zones.
+    private ResourceGroup = ""                     // resource group of azure dns zones
+    private zone = ""
     private aRecordName = "${product}-${component}-${env}"
+
+    // Login to determin which zone to deploy record to
 
     def RegisterAzDns(aRecord, serviceIP) {
     
@@ -31,24 +33,3 @@ class AzPrivateDns {
     }
 
 }
-
-// https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/privateDnsZones/{privateZoneName}/{recordType}/{relativeRecordSetName}?api-version=2018-09-01
-
-/*
-
-https://plum-frontend-aat-staging.service.core-compute-aat.internal
-
-REST API request:
-PUT https://management.azure.com/subscriptions/subscriptionId/resourceGroups/resourceGroup1/providers/Microsoft.Network/privateDnsZones/privatezone1.com/CNAME/recordCNAME?api-version=2018-09-01
-
-JSON payload:
-{
-  "properties": {
-    "ttl": 3600,
-    "cnameRecord": {
-      "cname": "contoso.com"
-    }
-  }
-}
-
-*/
