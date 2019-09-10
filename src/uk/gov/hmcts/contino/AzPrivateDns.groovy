27232f224055029c7da64cf8aadb3f3dc26aad53
Gareth Heath <gareth.heath@hmcts.net>
2019-09-10 14:53:14 +0100

    Authenticate with azure api

--- src/uk/gov/hmcts/contino/AzPrivateDns.groovy
+++ src/uk/gov/hmcts/contino/AzPrivateDns.groovy
@@ -2,22 +2,22 @@ package uk.gov.hmcts.contino
 
 import groovy.json.JsonOutput
 
-class AzPrivateDns {
+class AzPrivateDns extends Az {
 
     def steps
     def environment
     private subscriptionId = "bf308a5c-0624-4334-8ff8-8dca9fd43783"     // Subscription id of azure dns zone
     private resourceGroup = "rdo-private-dns-sbox"                      // Resource group of azure dns zone
     
-    AzPrivateDns(steps, environment) {
+    AzPrivateDns(steps, subscription, environment) {
+        super(this, subscription)
+
         this.steps = steps
         this.environment = environment
-
     }
 
     def registerAzDns(recordName, serviceIP) {
         def zone = "service.core-compute-${environment}.internal"
-       // def json = "{ "properties" : { "ttl" : "3600", "aRecords" : [ { "ipv4Address" : "${serviceIP}" } ] } }" 
 
         def json = JsonOutput.toJson(
             [
@@ -28,6 +28,7 @@ class AzPrivateDns {
         ])
 
         this.steps.echo "Registering DNS for ${recordName} to ${serviceIP}, properties: ${json}"
+        def accessToken = this.az "account get-access-token --query accessToken -o tsv"
 
         def result = this.steps.httpRequest(
             httpMode: 'PUT',
@@ -36,6 +37,9 @@ class AzPrivateDns {
             url: "https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.Network/defDnsZones/${zone}/A/${recordName}?api-version=2018-09-01",
             requestBody: "${json}",
             consoleLogResponseBody: true,
+            customHeaders: [
+                [maskValue: true, name: 'Authorization', value: "Bearer ${accessToken}"]
+            ]
             validResponseCodes: '201'
         )
 
