0dd8742664c4173141a3c8b42713bbcf6743f1d2
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-10-04 22:29:12 +0100

    Disable cron jobs only on PRs

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -85,8 +85,10 @@ def call(DockerImage dockerImage, Map params) {
       sh "envsubst < ${requirementsEnv} > ${requirements}"
     }
 
+    def disableCronJobs = false
     onPR {
       helmOptionEnvironment = new Environment(env).nonProdName
+      disableCronJobs = true
     }
 
     def options = [
@@ -100,7 +102,9 @@ def call(DockerImage dockerImage, Map params) {
 
     if (!config.serviceApp) {
       //Forcing Jobs deployed through Jenkins to be Job to avoid cronJobs being run forever.
-      options.add("--set global.job.kind=Job")
+      if (disableCronJobs) {
+        options.add("--set global.job.kind=Job")
+      }
       //deleting non service apps before installing as K8s doesn't allow editing image of deployed Jobs
       if(helm.exists(dockerImage.getImageTag(), namespace)){
         helm.delete(dockerImage.getImageTag())
