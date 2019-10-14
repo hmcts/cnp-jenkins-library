686099d21288934a8832727e922ed5b400902f6f
Gareth Heath <gareth.heath@hmcts.net>
2019-10-14 18:12:34 +0100

    add logic for subscription

--- src/uk/gov/hmcts/contino/AzPrivateDns.groovy
+++ src/uk/gov/hmcts/contino/AzPrivateDns.groovy
@@ -7,15 +7,16 @@ class AzPrivateDns extends Az {
 
     def steps
     def environment
+    private subscriptionId
     private resourceGroup = "mgmt-intdns-prod"                             // Resource group of azure dns zone
 
-    if (environment == "prod") {
-        private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
-    } else if (environment == "idam-prod") {
-        private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
-    } else {
-        private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
-    }
+    // if (environment == "prod") {
+    //     private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+    // } else if (environment == "idam-prod") {
+    //     private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+    // } else {
+    //     private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+    // }
     
     AzPrivateDns(steps, subscription, environment) {
         super(steps, subscription)
@@ -25,6 +26,15 @@ class AzPrivateDns extends Az {
     }
 
     def registerAzDns(recordName, serviceIP) {
+        
+        if (environment == "prod") {
+            private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+        } else if (environment == "idam-prod") {
+            private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+        } else {
+            private subscriptionId = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+        }
+
         def zone = "service.core-compute-${environment}.internal"
 
         def json = JsonOutput.toJson(
