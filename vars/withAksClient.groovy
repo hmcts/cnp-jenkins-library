5dbdf5aee53455b70a1367796d0906a85a645458
Henry Dobosn <henrydobson@me.com>
2019-10-15 19:47:09 +0100

    feat(aksclient): ensure 'idam-' is not in the environment name

--- vars/withAksClient.groovy
+++ vars/withAksClient.groovy
@@ -4,7 +4,7 @@ def call(String subscription, String environment, Closure block) {
   withDocker('hmcts/cnp-aks-client:az-2.0.61-kubectl-1.13.5-helm-2.14.0', null) {
     withSubscription(subscription) {
       withRegistrySecrets {
-        def envName = environment.toUpperCase()
+        def envName = environment.replace('idam-', '').toUpperCase()
         env.AKS_CLUSTER_NAME = env."${envName}_AKS_CLUSTER_NAME" ?: "cnp-${environment}-cluster"
         env.AKS_RESOURCE_GROUP = env."${envName}_AKS_RESOURCE_GROUP" ?: "cnp-${environment}-rg"
         block.call()
