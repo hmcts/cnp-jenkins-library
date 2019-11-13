2e4f218aca809e242f3357d5316ec88b0aba7ed9
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-13 15:40:42 +0000

    Update withAcrClient.groovy

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -1,5 +1,5 @@
 def call(String subscription, Closure block) {
-  withDocker('hmcts/cnp-aks-client:az-2.0.61-kubectl-1.16.2-helm-3.0.rc2', null) {
+  withDocker('hmcts/cnp-aks-client:az-2.0.61-kubectl-1.16.2-helm-3.0.0', null) {
     withSubscription(subscription) {
       withRegistrySecrets {
         block.call()
