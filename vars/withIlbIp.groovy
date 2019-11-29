064027030c8a99677449bccd35fcd02c5652ce60
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-29 11:18:26 +0000

    JDD

--- vars/withIlbIp.groovy
+++ vars/withIlbIp.groovy
@@ -2,7 +2,7 @@
 import groovy.json.JsonSlurperClassic
 
 def call(String environment, Closure body) {
-    TOKEN = sh(script: "az account get-access-token --query accessToken -o tsv", returnStdOut: true).trim()
+    TOKEN = sh(script: "az account get-access-token --query accessToken -o tsv", returnStdout: true).trim()
     def vip = httpRequest httpMode: 'GET', customHeaders: [[name: 'Authorization', value: "Bearer ${TOKEN}"]], url: "https://management.azure.com/subscriptions/$env.ARM_SUBSCRIPTION_ID/resourceGroups/core-infra-$environment/providers/Microsoft.Web/hostingEnvironments/core-compute-$environment/capacities/virtualip?api-version=2016-09-01"
     def internalip = new JsonSlurperClassic().parseText(vip.content).internalIpAddress
     log.info("Internal IP: $internalip")
