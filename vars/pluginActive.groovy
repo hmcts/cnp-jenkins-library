ea421aad37e93dd9683579943aaf52f84abc8852
Tim Jacomb <tim.jacomb@hmcts.net>
2019-12-01 11:25:29 +0000

    Try non cps pluginActive

--- vars/pluginActive.groovy
+++ vars/pluginActive.groovy
@@ -1,6 +1,11 @@
 
 def call(String pluginId, Closure body) {
-  if (Jenkins.instance.getPluginManager().getPlugins().find { it.getShortName() == pluginId && it.isEnabled() } != null) {
+  if (pluginEnabled(pluginId)) {
     body.call()
   }
+}
+
+@NonCPS
+def pluginEnabled(def pluginId) {
+  Jenkins.instance.getPluginManager().getPlugins().find { it.getShortName() == pluginId && it.isEnabled() } != null
 }
