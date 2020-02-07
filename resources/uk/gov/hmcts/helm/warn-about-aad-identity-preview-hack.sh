f025fd6bb37bc9de3c79ca3e3664e9b5d226ffde
Tim Jacomb <tim.jacomb@hmcts.net>
2020-02-07 17:21:24 +0000

    Formatting

--- resources/uk/gov/hmcts/helm/warn-about-aad-identity-preview-hack.sh
+++ resources/uk/gov/hmcts/helm/warn-about-aad-identity-preview-hack.sh
@@ -8,10 +8,10 @@ grep --quiet "aadIdentityName" values.preview.template.yaml
 if [ $? -gt 0 ]; then
   echo "AAD Identity name not found, this is good"
 else
-  echo "==============================================================================="
-  echo "=====  Please remove aadIdentityName from values.preview.template.yaml    ====="
-  echo "=====  This is required as we are going to start enforcing                ====="
-  echo "=====  authentication with your pod's identity like all other AKS environments and not kvcreds            ====="
-  echo "==============================================================================="
+  echo "====================================================================================================="
+  echo "=====  Please remove aadIdentityName from values.preview.template.yaml                         ======"
+  echo "=====  This is required as we are going to start enforcing                                     ======"
+  echo "=====  authentication with your pod's identity like all other AKS environments and not kvcreds ======"
+  echo "====================================================================================================="
   exit 1
 fi
