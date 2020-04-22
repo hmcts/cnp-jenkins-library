42296a768ddb39497d5d4ba04dba98f535cda3fa
Tim Jacomb <tim.jacomb@hmcts.net>
2020-04-22 11:14:18 +0100

    Add null checks on terraform output

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -53,14 +53,14 @@ def call(params) {
             if (config.migrateDb) {
               stage("DB Migration - ${environment}") {
                 pcr.callAround("dbmigrate:${environment}") {
-                  if (tfOutput.microserviceName) {
+                  if (tfOutput?.microserviceName) {
                     WarningCollector.addPipelineWarning("deprecated_microservice_name_outputted", "Please remove microserviceName from your terraform outputs, if you are not outputting the microservice name (component) and instead outputting something else you will need to migrate the secrets first, example PR: https://github.com/hmcts/ccd-data-store-api/pull/540"
       , new Date().parse("dd.MM.yyyy", "05.09.2019"))
                   }
 
                   builder.dbMigrate(
-                    tfOutput.vaultName ? tfOutput.vaultName.value : "${config.dbMigrationVaultName}-${environment}",
-                    tfOutput.microserviceName? tfOutput.microserviceName.value : component
+                    tfOutput?.vaultName ? tfOutput.vaultName.value : "${config.dbMigrationVaultName}-${environment}",
+                    tfOutput?.microserviceName ? tfOutput.microserviceName.value : component
                   )
                 }
               }
