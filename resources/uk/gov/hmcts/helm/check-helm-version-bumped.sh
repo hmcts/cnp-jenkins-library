866fa24bb1c654b772443e2a0ef594462a43eb9d
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-05-22 16:39:21 +0100

    chart change

--- resources/uk/gov/hmcts/helm/check-helm-version-bumped.sh
+++ resources/uk/gov/hmcts/helm/check-helm-version-bumped.sh
@@ -13,6 +13,15 @@ else
   DIFF_IN_VALUES=false
 fi
 
+git diff -s --exit-code origin/master charts/"${CHART_DIRECTORY}"/Chart.yaml
+
+if [ $? -eq 1 ]; then
+  echo "Diff in Chart.yaml detected"
+  DIFF_IN_CHART=true
+else
+  DIFF_IN_CHART=false
+fi
+
 DIFF_IN_REQUIREMENTS=false
 if [[ -f charts/"${CHART_DIRECTORY}"/requirements.yaml ]]
 then
@@ -23,7 +32,7 @@ then
    fi
 fi
 
-if [[ ${DIFF_IN_VALUES} = 'false' ]] && [[ ${DIFF_IN_REQUIREMENTS} = 'false' ]]; then
+if [[ ${DIFF_IN_VALUES} = 'false' ]] && [[ ${DIFF_IN_REQUIREMENTS} = 'false' ]] && [[ ${DIFF_IN_CHART} = 'false' ]] ; then
   echo 'No differences requiring chart version bump detected'
   exit 0
 fi
