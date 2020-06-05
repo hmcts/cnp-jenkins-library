0e129ebeec7f32c219c87f6a83ee0232a42c7518
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-06-05 15:46:45 +0100

    fix tests

--- vars/checkoutScm.groovy
+++ vars/checkoutScm.groovy
@@ -7,14 +7,14 @@ def call() {
     env.GIT_COMMIT = scmVars.GIT_COMMIT
     env.GIT_URL = scmVars.GIT_URL
   }
-  def credentialsId = SCMSource.SourceByItem.findSource(currentBuild.rawBuild.parent).credentialsId
-  env.GIT_CREDENTIALS_ID = credentialsId
   try {
+    def credentialsId = SCMSource.SourceByItem.findSource(currentBuild.rawBuild.parent).credentialsId
+    env.GIT_CREDENTIALS_ID = credentialsId
     //This code assumes it uses GitHub App Authentication
     def response = steps.httpRequest url: "https://api.github.com/users/$credentialsId%5Bbot%5D", httpMode: 'GET', acceptType: 'APPLICATION_JSON'
     def gitUserId = steps.readYaml(text: response.content).id
     env.GIT_APP_EMAIL_ID = gitUserId + "+" + credentialsId + "[bot]@users.noreply.github.com"
   } catch (err) {
-    steps.echo "Unable to find git email Id for the user' ${err}'"
+    echo "Unable to find git email Id for the user' ${err}'"
   }
 }
