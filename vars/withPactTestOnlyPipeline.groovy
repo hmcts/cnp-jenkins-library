923548cace0645e2939be740741788d7a85d7072
Dan Hamilton <dan@emagineers.co.uk>
2020-06-18 16:46:08 +0100

    Library online Pact Pipeline (#633)
    Co-authored-by: Tim Jacomb <tim.jacomb@hmcts.net>
    Co-authored-by: Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>

---
+++ vars/withPactTestOnlyPipeline.groovy
@@ -0,0 +1,131 @@
+import uk.gov.hmcts.contino.AngularPipelineType
+import uk.gov.hmcts.contino.Builder
+import uk.gov.hmcts.contino.Environment
+import uk.gov.hmcts.contino.MetricsPublisher
+import uk.gov.hmcts.contino.NodePipelineType
+import uk.gov.hmcts.contino.PipelineType
+import uk.gov.hmcts.contino.ProjectBranch
+import uk.gov.hmcts.contino.SpringBootPipelineType
+import uk.gov.hmcts.contino.Subscription
+import uk.gov.hmcts.contino.AppPipelineConfig
+import uk.gov.hmcts.contino.AppPipelineDsl
+import uk.gov.hmcts.contino.PipelineCallbacksConfig
+import uk.gov.hmcts.contino.PipelineCallbacksRunner
+import uk.gov.hmcts.pipeline.TeamConfig
+
+def call(type, String product, String component, Closure body) {
+
+  def branch = new ProjectBranch(env.BRANCH_NAME)
+
+  def deploymentNamespace = branch.deploymentNamespace()
+  def deploymentProduct = deploymentNamespace ? "$deploymentNamespace-$product" : product
+
+  def pipelineTypes = [
+    java  : new SpringBootPipelineType(this, deploymentProduct, component),
+    nodejs: new NodePipelineType(this, deploymentProduct, component),
+    angular: new AngularPipelineType(this, deploymentProduct, component)
+  ]
+
+  Subscription subscription = new Subscription(env)
+
+  PipelineType pipelineType
+
+  if (type instanceof PipelineType) {
+    pipelineType = type
+  } else {
+    pipelineType = pipelineTypes.get(type)
+  }
+
+  assert pipelineType != null
+
+  MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild, product, component, subscription.prodName )
+  def pipelineConfig = new AppPipelineConfig()
+  def callbacks = new PipelineCallbacksConfig()
+  def callbacksRunner = new PipelineCallbacksRunner(callbacks)
+
+  callbacks.registerAfterAll { stage ->
+    metricsPublisher.publish(stage)
+  }
+
+  def dsl = new AppPipelineDsl(this, callbacks, pipelineConfig)
+  body.delegate = dsl
+  body.call() // register pipeline config
+
+  dsl.onStageFailure() {
+    currentBuild.result = "FAILURE"
+  }
+
+  Environment environment = new Environment(env)
+
+  node {
+    def slackChannel = new TeamConfig(this).getBuildNoticesSlackChannel(product)
+    try {
+      env.PATH = "$env.PATH:/usr/local/bin"
+
+      onMaster {
+
+        PipelineCallbacksRunner pcr = callbacksRunner
+        AppPipelineConfig config = pipelineConfig
+        Builder builder = pipelineType.builder
+
+        def pactBrokerUrl = environment.pactBrokerUrl
+        boolean noSkipImgBuild = true
+
+        stage('Checkout') {
+          pcr.callAround('checkout') {
+            checkoutScm()
+          }
+        }
+
+        stage("Build") {
+
+          builder.setupToolVersion()
+        }
+
+        stage("Tests") {
+
+          when(noSkipImgBuild) {
+                pcr.callAround('test') {
+                  timeoutWithMsg(time: 20, unit: 'MINUTES', action: 'test') {
+                    builder.test()
+                  }
+                }
+          }
+        }
+
+        stage("Pact Consumer Verification") {
+
+          def version = env.GIT_COMMIT.length() > 7 ? env.GIT_COMMIT.substring(0, 7) : env.GIT_COMMIT
+          def isOnMaster = new ProjectBranch(env.BRANCH_NAME).isMaster()
+
+          env.PACT_BRANCH_NAME = isOnMaster ? env.BRANCH_NAME : env.CHANGE_BRANCH
+          env.PACT_BROKER_URL = pactBrokerUrl
+
+          /*
+         * These instructions have to be kept in order
+         */
+          pcr.callAround('pact-consumer-tests') {
+            builder.runConsumerTests(pactBrokerUrl, version)
+          }
+      }
+      }
+    } catch (err) {
+      currentBuild.result = "FAILURE"
+      notifyBuildFailure channel: slackChannel
+
+      callbacksRunner.call('onFailure')
+      metricsPublisher.publish('Pipeline Failed')
+      throw err
+    } finally {
+      notifyPipelineDeprecations(slackChannel, metricsPublisher)
+      if (env.KEEP_DIR_FOR_DEBUGGING != "true") {
+        deleteDir()
+      }
+    }
+
+    notifyBuildFixed channel: slackChannel
+
+    callbacksRunner.call('onSuccess')
+    metricsPublisher.publish('Pipeline Succeeded')
+  }
+}
