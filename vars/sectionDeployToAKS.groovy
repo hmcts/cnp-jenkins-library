9609f2f33e99ccc1372433bd317c36b20b19d5c0
Henry Dobson <henrydobson@me.com>
2020-06-25 09:49:54 +0100

    chore(duplicate): remove nested withAksClient

--- vars/sectionDeployToAKS.groovy
+++ vars/sectionDeployToAKS.groovy
@@ -50,12 +50,10 @@ def call(params) {
             onPR {
               deploymentNumber = githubCreateDeployment()
             }
-            withAksClient(subscription, environment) {
-              params.environment = params.environment.replace('idam-', '')
-              log.info("Using AKS environment: ${params.environment}")
-              aksUrl = helmInstall(dockerImage, params)
-              log.info("deployed component URL: ${aksUrl}")
-            }
+            params.environment = params.environment.replace('idam-', '')
+            log.info("Using AKS environment: ${params.environment}")
+            aksUrl = helmInstall(dockerImage, params)
+            log.info("deployed component URL: ${aksUrl}")
             onPR {
               githubUpdateDeploymentStatus(deploymentNumber, aksUrl)
             }
