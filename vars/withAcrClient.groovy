29d9ffa3e19bbd4eec1e66c6cd6530fd64e1a4eb
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-07-16 13:25:37 +0100

    Use local az cli

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -1,11 +1,11 @@
 def call(String subscription, Closure block) {
-  withDocker('hmcts/cnp-aks-client:az-2.3.0-kubectl-1.18.0-helm-3.1.2', null) {
+//  withDocker('hmcts/cnp-aks-client:az-2.3.0-kubectl-1.18.0-helm-3.1.2', null) {
     withSubscriptionLogin(subscription) {
       withRegistrySecrets {
         block.call()
       }
     }
-  }
+//  }
 }
 
 
