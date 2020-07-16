9dd97927a6b6dd0e2f3ea60ee7c940a9f1964cda
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-07-16 14:17:57 +0100

    Check java11 location

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -167,8 +167,13 @@ EOF
       def javaVersion = gradleWithOutput("-q :javaVersion")
       steps.echo "Found java version: ${javaVersion}"
       if (javaVersion == java11) {
-        steps.env.JAVA_HOME = "/usr/share/jdk-11.0.2"
-        steps.env.PATH = "${steps.env.JAVA_HOME}/bin:${steps.env.PATH}"
+        if (steps.fileExists("/usr/share/jdk-11.0.2")) {
+          steps.env.JAVA_HOME = "/usr/share/jdk-11.0.2"
+          steps.env.PATH = "${steps.env.JAVA_HOME}/bin:${steps.env.PATH}"
+        } else if (steps.fileExists("/usr/local/openjdk-11")) {
+          steps.env.JAVA_HOME = "/usr/local/openjdk-11"
+          steps.env.PATH = "${steps.env.JAVA_HOME}/bin:${steps.env.PATH}"
+        }
       } else {
         nagAboutJava11Required()
       }
