73865b01fa053d78db92336477aa8bee38f616aa
Piyush Kumar <piyush.m82@gmail.com>
2020-07-30 11:57:04 +0100

    aks client image with rsync (#661)

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -6,7 +6,7 @@ def call(String subscription, String product, Closure block) {
   if (isDockerBuildAgent) {
     doAcrOrKubectlTask(subscription, block)
   } else {
-    withDocker('hmcts/cnp-aks-client:az-2.3.0-kubectl-1.18.0-helm-3.1.2', null) {
+    withDocker('hmcts/cnp-aks-client:az-2.3.0-kubectl-1.18.0-helm-3.1.2-rsync', null) {
       doAcrOrKubectlTask(subscription, block)
     }
   }
