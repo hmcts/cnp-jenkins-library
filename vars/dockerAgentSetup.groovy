d997570fd83cd4c5255455532cd420ecf3bc8914
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-08-04 12:46:02 +0100

    Fix issue with string conversion (#667)

--- vars/dockerAgentSetup.groovy
+++ vars/dockerAgentSetup.groovy
@@ -1,7 +1,7 @@
 import uk.gov.hmcts.contino.azure.KeyVault
 
 def call(String product) {
-  if (env.IS_DOCKER_BUILD_AGENT) {
+  if (env.IS_DOCKER_BUILD_AGENT && env.IS_DOCKER_BUILD_AGENT.toBoolean()) {
     def envName = env.JENKINS_SUBSCRIPTION_NAME == "DTS-CFTSBOX-INTSVC" ? "sandbox" : "prod"
     withSubscriptionLogin(envName) {
       def infraVaultName = env.INFRA_VAULT_NAME
