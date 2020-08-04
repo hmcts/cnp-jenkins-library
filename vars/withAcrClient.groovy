27414e329c9fd8a894f52f5805965bd5d162d005
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-08-04 12:41:06 +0100

    force condition

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -1,6 +1,6 @@
 
 def call(String subscription, String product, Closure block) {
-  if (env.IS_DOCKER_BUILD_AGENT) {
+  if (env.IS_DOCKER_BUILD_AGENT == "true") {
     doAcrOrKubectlTask(subscription, block)
   } else {
     withDocker('hmcts/cnp-aks-client:az-2.3.0-kubectl-1.18.0-helm-3.1.2-rsync', null) {
