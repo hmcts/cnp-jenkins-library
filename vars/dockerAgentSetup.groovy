9cf00c7aff5f53e1d8ead726fa2b09dcd657e868
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-08-10 10:00:15 +0100

    Add logging

--- vars/dockerAgentSetup.groovy
+++ vars/dockerAgentSetup.groovy
@@ -3,6 +3,7 @@ import uk.gov.hmcts.contino.azure.KeyVault
 def call(String product) {
   if (env.IS_DOCKER_BUILD_AGENT && env.IS_DOCKER_BUILD_AGENT.toBoolean()) {
     def envName = env.JENKINS_SUBSCRIPTION_NAME == "DTS-CFTSBOX-INTSVC" ? "sandbox" : "prod"
+    echo "Using container env: ${envName}"
     String infraVaultName = env.INFRA_VAULT_NAME
     String sshFile = "/home/jenkins/.ssh/id_rsa"
     String vaultSecret = "jenkins-ssh-private-key"
@@ -13,10 +14,14 @@ def call(String product) {
         keyVault.download(vaultSecret, sshFile, "600")
         if (fileExists(sshFile)) {
           env.CURRENT_ID_RSA = idRsa
+          echo "Downloaded ssh id_rsa"
         } else {
           env.CURRENT_ID_RSA = ""
+          throw new Exception("Failed ssh id_rsa download")
         }
       }
+    } else {
+      echo "Using existing ssh id_rsa"
     }
   }
 }
