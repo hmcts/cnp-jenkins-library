0f4d1305c9849787e509b68dcfaf2ef928bd2eef
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-08-10 12:54:41 +0100

    Do not use steps fileExists in container

--- vars/dockerAgentSetup.groovy
+++ vars/dockerAgentSetup.groovy
@@ -1,5 +1,9 @@
 import uk.gov.hmcts.contino.azure.KeyVault
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
 def call(String product) {
   if (env.IS_DOCKER_BUILD_AGENT && env.IS_DOCKER_BUILD_AGENT.toBoolean()) {
     def envName = env.JENKINS_SUBSCRIPTION_NAME == "DTS-CFTSBOX-INTSVC" ? "sandbox" : "prod"
@@ -8,11 +12,11 @@ def call(String product) {
     String sshFile = "/home/jenkins/.ssh/id_rsa"
     String vaultSecret = "jenkins-ssh-private-key"
     String idRsa = "${envName}-${infraVaultName}-${vaultSecret}"
-    if (env.CURRENT_ID_RSA != idRsa || !fileExists(sshFile)) {
+    if (env.CURRENT_ID_RSA != idRsa || !Files.exists(Paths.get(sshFile))) {
       withSubscriptionLogin(envName) {
         KeyVault keyVault = new KeyVault(this, envName, infraVaultName)
         keyVault.download(vaultSecret, sshFile, "600")
-        if (fileExists(sshFile)) {
+        if (Files.exists(Paths.get(sshFile))) {
           env.CURRENT_ID_RSA = idRsa
           echo "Downloaded ssh id_rsa"
         } else {
