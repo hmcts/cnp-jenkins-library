367d5d8cf35650e163dadf69d99a3b063d71186f
Hannah Smith <hannahs@kainos.com>
2020-09-29 14:39:45 +0100

    clean up product-component section

--- vars/helmUninstall.groovy
+++ vars/helmUninstall.groovy
@@ -5,14 +5,12 @@ import uk.gov.hmcts.contino.Helm
 def call(DockerImage dockerImage, Map params) {
   
   def subscription = params.subscription
-  def product = params.product
-  def component = params.component
 
   def aksServiceName = dockerImage.getAksServiceName()
 
   def namespace = env.TEAM_NAMESPACE
 
-  def chartName = "${product}-${component}"
+  def chartName = "${params.product}-${params.component}"
 
   def kubectl = new Kubectl(this, subscription, aksServiceName, params.aksSubscription.name)
   kubectl.login()
