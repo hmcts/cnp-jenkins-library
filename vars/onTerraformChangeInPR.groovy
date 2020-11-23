158f0d3f46055212456cd98053ea3844acb5267c
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-11-23 09:17:29 +0000

    Apply suggestions from code review
    Co-authored-by: Tim Jacomb <tim.jacomb@hmcts.net>

--- vars/onTerraformChangeInPR.groovy
+++ vars/onTerraformChangeInPR.groovy
@@ -2,13 +2,13 @@ def call(Closure block) {
   folderExists('infrastructure') {
     writeFile file: 'check-infrastructure-files-changed.sh', text: libraryResource('uk/gov/hmcts/infrastructure/check-infrastructure-files-changed.sh')
 
-    INFRA_CHANGED = sh(
+    def infraFolderHasChanges = sh(
       script: "chmod +x check-infrastructure-files-changed.sh\n" +
         "    ./check-infrastructure-files-changed.sh",
       returnStatus: true
     )
     sh 'rm check-infrastructure-files-changed.sh'
-    if (INFRA_CHANGED == 1) {
+    if (infraFolderHasChanges == 1) {
       return block.call()
     }
   }
