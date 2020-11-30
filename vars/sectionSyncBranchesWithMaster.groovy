746535599cafa0ad25669505d26a631fab46b6e6
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-11-30 16:19:23 +0000

    Implement git code for sync

--- vars/sectionSyncBranchesWithMaster.groovy
+++ vars/sectionSyncBranchesWithMaster.groovy
@@ -2,7 +2,6 @@
 import uk.gov.hmcts.contino.AppPipelineConfig
 
 def call(params) {
-    
     AppPipelineConfig config = params.appPipelineConfig
     def product = params.product
 
@@ -10,6 +9,24 @@ def call(params) {
         if (!config.branchesToSyncWithMaster.isEmpty()) {
             for (branch in config.branchesToSyncWithMaster) {
                 println('Syncing branch - ' + branch)
+
+                sh '''
+                    set -e
+                    git remote set-url origin $(git config remote.origin.url | sed "s/github.com/${GIT_CREDENTIALS_ID}:${BEARER_TOKEN}@github.com/g")
+
+                    git config --global user.name "${GIT_CREDENTIALS_ID}"
+                    git config --global user.email "${GIT_APP_EMAIL_ID}"
+                '''
+
+                try {
+                    sh '''
+                        git fetch origin ${branch}:${branch}
+                        git push --force origin HEAD:${branch}
+                    '''
+                } catch (err) {
+                    echo "Failed to update $branch branch."
+                    throw err
+                }
             }
         }
     }
