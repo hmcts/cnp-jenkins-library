cdacfdd457744b8355f49ab9cef29792376f5bfe
Hannah Smith <hannahs@kainos.com>
2021-01-22 14:31:55 +0000

    move back

--- resources/uk/gov/hmcts/pipeline/camunda/publish-camunda-processes.sh
+++ resources/uk/gov/hmcts/pipeline/camunda/publish-camunda-processes.sh
@@ -40,8 +40,8 @@ do
     -F "deployment-name=$(basename "${file}")" \
     -F "deploy-changed-only=true" \
     -F "deployment-source=$product" \
-    -F "file=@${filepath}/$(basename "${file}")" \
-    -F "tenant-id=$TENANT_PARAM" )
+    -F "tenant-id=$TENANT_PARAM" \
+    -F "file=@${filepath}/$(basename "${file}")")
 
   upload_http_code=$(echo "$uploadResponse" | tail -n1)
   upload_response_content=$(echo "$uploadResponse" | sed '$d')
