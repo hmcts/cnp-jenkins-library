7853dcbe1fbd991689fbb4eb564fbda98705ece4
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-02-05 13:45:43 +0000

    fix typo

--- vars/warnAboutDeprecatedChartConfig.groovy
+++ vars/warnAboutDeprecatedChartConfig.groovy
@@ -1,6 +1,9 @@
 import uk.gov.hmcts.pipeline.deprecation.WarningCollector
 
-def call() {
+def call(Map<String, String> params) {
+
+  def product = params.product
+  def component = params.component
 
   writeFile file: 'check-helm-api-version.sh', text: libraryResource('uk/gov/hmcts/helm/check-helm-api-version.sh')
   writeFile file: 'check-deprecated-charts.sh', text: libraryResource('uk/gov/hmcts/helm/check-deprecated-charts.sh')
@@ -8,7 +11,7 @@ def call() {
   try {
     sh """
     chmod +x check-helm-api-version.sh
-    ./check-helm-api-version.sh
+    ./check-helm-api-version.sh $product $component
     """
   } catch(ignored) {
     WarningCollector.addPipelineWarning("deprecated_helmapiversion", "Please upgrade helm chart api version to v2. For examples see: https://github.com/hmcts/service-auth-provider-app/pull/255 and https://github.com/hmcts/draft-store/pull/611/files", new Date().parse("dd.MM.yyyy", "22.02.2021"))
@@ -19,7 +22,7 @@ def call() {
   try {
     sh """
     chmod +x check-deprecated-charts.sh
-    ./check-deprecated-charts.sh
+    ./check-deprecated-charts.sh $product $component
     """
   } catch(ignored) {
     WarningCollector.addPipelineWarning("deprecated_helmcharts", "Please upgrade base helm charts to latest. See https://github.com/hmcts/chart-java/releases, https://github.com/hmcts/chart-nodejs/releases", new Date().parse("dd.MM.yyyy", "22.02.2021"))
