d000a7e89ed52d609ac94607a957e0fceb387534
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-02-05 14:44:11 +0000

    remove

--- vars/sectionBuildAndTest.groovy
+++ vars/sectionBuildAndTest.groovy
@@ -43,8 +43,6 @@ def call(params) {
       warnAboutAADIdentityPreviewHack product: product, component: component
     }
 
-    warnAboutDeprecatedChartConfig product: product, component: component
-
     builder.setupToolVersion()
 
     if (!fileExists('Dockerfile')) {
