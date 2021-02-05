b724221c13406af144ce31ea87a2c914c72f450e
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-02-05 15:26:05 +0000

    fix

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -8,12 +8,12 @@ MIN_NODEJS_VERSION="2.3.0"
 JAVA_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "java" |awk '{ print $2}' | sed "s/~//g")
 NODEJS_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "nodejs" |awk '{ print $2}' | sed "s/~//g")
 
-if [[ $JAVA_VERSION < $MIN_JAVA_VERSION ]]; then
+if [[ -n $JAVA_VERSION ]] &&  [[ $JAVA_VERSION < $MIN_JAVA_VERSION ]]; then
     echo "Java chart version $JAVA_VERSION is deprecated, please upgrade"
     exit 1
 fi
 
-if [[ $NODEJS_VERSION < $MIN_NODEJS_VERSION ]]; then
+if [[ -n $NODEJS_VERSION ]] && [[ $NODEJS_VERSION < $MIN_NODEJS_VERSION ]]; then
     echo "Nodejs chart version $NODEJS_VERSION is deprecated, please upgrade"
     exit 1
 fi
