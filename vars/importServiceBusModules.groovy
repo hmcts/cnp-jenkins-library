5477964cbfa0f14db2e973e7cfdb5a623c09d451
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-11 23:47:53 +0000

    Test

--- vars/importServiceBusModules.groovy
+++ vars/importServiceBusModules.groovy
@@ -32,12 +32,14 @@ def call(String subscription) {
     String stateList = sh(script: "terraform state list", returnStdout: true).trim()
 
     String sbNamespaceModuleName = sh(script: "grep '$nsIdentifier' <<< '$stateList' | awk -F '$nsIdentifier' '{print \$1}'", returnStdout: true).trim()
+    String topicModuleName = sh(script: "grep '$topicIdentifier' <<< '$stateList' | awk -F '$topicIdentifier' '{print \$1}'", returnStdout: true).trim()
+    String subsciptionModuleName = sh(script: "grep '$subIdentifier' <<< '$stateList' | awk -F '$subIdentifier' '{print \$1}'", returnStdout: true).trim()
 
     // String sbNamespaceModuleName = sh(script: "echo '$stateList' | grep '$nsIdentifier' | awk -F '$nsIdentifier' '{print \$1}'", returnStdout: true).trim()
     // String topicModuleName = sh(script: "echo $stateList | grep '$topicIdentifier' | awk -F '$topicIdentifier' '{print \$1}'", returnStdout: true).trim()
     // String subsciptionModuleName = sh(script: "echo $stateList | grep '$subIdentifier' | awk -F '$subIdentifier' '{print \$1}'", returnStdout: true).trim()
 
     echo "SERVICE BUS MODULE NAME - ${sbNamespaceModuleName}"
-    // echo "TOPIC MODULE NAME - ${topicModuleName}"
-    // echo "SUBSCTION MODULE NAME - ${subsciptionModuleName}"
+    echo "TOPIC MODULE NAME - ${topicModuleName}"
+    echo "SUBSCTION MODULE NAME - ${subsciptionModuleName}"
 }
