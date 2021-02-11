6ee60020a0e3298df9be9570a132bfc6414b1f5d
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-11 22:50:56 +0000

    Test

--- vars/importServiceBusModules.groovy
+++ vars/importServiceBusModules.groovy
@@ -29,7 +29,7 @@ def call(String subscription) {
     String topicIdentifier = "azurerm_template_deployment.topic"
     String subIdentifier = "azurerm_template_deployment.subscription"
 
-    String sbNamespaceModuleName = sh(script: "terraform state list | grep \$nsIdentifier | awk -F \$nsIdentifier '{print \$1}'", returnStdout: true).trim()
+    String sbNamespaceModuleName = sh(script: "terraform state list | grep \${nsIdentifier} | awk -F \${nsIdentifier} '{print \$1}'", returnStdout: true).trim()
     // topicModuleName = sh(script: "terraform state list | grep ${topicIdentifier} | awk -F ${topicIdentifier} '{print $1}'", returnStdout: true).trim()
     // subsciptionModuleName = sh(script: "terraform state list | grep ${subIdentifier} | awk -F ${subIdentifier} '{print $1}'", returnStdout: true).trim()
 
