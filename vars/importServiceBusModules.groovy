aab9d0733bdbd1bb3804af2237e00df701490bd5
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-12 00:07:08 +0000

    Test

--- vars/importServiceBusModules.groovy
+++ vars/importServiceBusModules.groovy
@@ -31,15 +31,22 @@ def call(String subscription) {
 
     String stateList = sh(script: "terraform state list", returnStdout: true).trim()
 
-    String sbNamespaceModuleName = sh(script: "grep '$nsIdentifier' <<< '$stateList' | awk -F '$nsIdentifier' '{print \$1}'", returnStdout: true).trim()
-    String topicModuleName = sh(script: "grep '$topicIdentifier' <<< '$stateList' | awk -F '$topicIdentifier' '{print \$1}'", returnStdout: true).trim()
-    String subsciptionModuleName = sh(script: "grep '$subIdentifier' <<< '$stateList' | awk -F '$subIdentifier' '{print \$1}'", returnStdout: true).trim()
+    String sbNamespaceModule = sh(script: "grep '$nsIdentifier' <<< '$stateList' | awk -F '$nsIdentifier' '{print \$1}'", returnStdout: true).trim()
+    String topicModule = sh(script: "grep '$topicIdentifier' <<< '$stateList' | awk -F '$topicIdentifier' '{print \$1}'", returnStdout: true).trim()
+    String subModule = sh(script: "grep '$subIdentifier' <<< '$stateList' | awk -F '$subIdentifier' '{print \$1}'", returnStdout: true).trim()
 
-    // String sbNamespaceModuleName = sh(script: "echo '$stateList' | grep '$nsIdentifier' | awk -F '$nsIdentifier' '{print \$1}'", returnStdout: true).trim()
-    // String topicModuleName = sh(script: "echo $stateList | grep '$topicIdentifier' | awk -F '$topicIdentifier' '{print \$1}'", returnStdout: true).trim()
-    // String subsciptionModuleName = sh(script: "echo $stateList | grep '$subIdentifier' | awk -F '$subIdentifier' '{print \$1}'", returnStdout: true).trim()
+    String sbNamespaceModuleName = sbNamespaceModule + "azurerm_servicebus_namespace.servicebus_namespace"
+    String sbNamespaceAuthModuleName = sbNamespaceModule + "azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule"
+    String topicModuleName = topicModule + "azurerm_servicebus_topic.servicebus_topic"
+    String topicAuthModuleName = topicModule + "azurerm_servicebus_topic_authorization_rule.topic_authorization_rule"
+    String subsciptionModuleName = subModule + "azurerm_servicebus_subscription.servicebus_subscription"
 
     echo "SERVICE BUS MODULE NAME - ${sbNamespaceModuleName}"
+    echo "SERVICE BUS AUTH RULE - ${sbNamespaceAuthModuleName}"
     echo "TOPIC MODULE NAME - ${topicModuleName}"
+    echo "TOPIC AUTH RULE - ${topicAuthModuleName}"
     echo "SUBSCTION MODULE NAME - ${subsciptionModuleName}"
+
+    // sh "terraform import -var 'common_tags=${pipelineTags}' -var 'env=${environment}' -var 'product=${product}'" +
+    //     (fileExists("${environment}.tfvars") ? " -var-file=${environment}.tfvars" : "") +
 }
