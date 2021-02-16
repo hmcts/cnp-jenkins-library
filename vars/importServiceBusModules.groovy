cd2d09c9019e9492103a3ffbb1f38eefee5a500a
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-16 22:41:20 +0000

    Test

--- vars/importServiceBusModules.groovy
+++ vars/importServiceBusModules.groovy
@@ -9,13 +9,15 @@ def call(String subscription, String environment, String product, tags) {
     stageWithAgent("Import Service Bus Modules", product) {
         def jsonSlurper = new JsonSlurper()
 
-        az = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az $cmd", returnStdout: true).trim() }
+        importModules = new ImportServiceBusModules(this, environment, product, tags)
+        // az = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az $cmd", returnStdout: true).trim() }
 
         String stateJsonString =  sh(script: "terraform show -json", returnStdout: true).trim()
         def stateJsonObj = jsonSlurper.parseText(stateJsonString)
 
         // Get all modules
         def child_modules = stateJsonObj.values.root_module.child_modules
+
         for (module in child_modules) {
             def resources = module.resources
 
@@ -27,7 +29,8 @@ def call(String subscription, String environment, String product, tags) {
                     echo "${resource.values.name}"
                     echo "${resource.values.resource_group_name}"
 
-                    if (importServiceBusNamespaceModule(resource.values.name, resource.values.resource_group_name, address, environment, product, tags)) {
+                    // if (importServiceBusNamespaceModule(resource.values.name, resource.values.resource_group_name, address, environment, product, tags)) {
+                    if (importModules.importServiceBusNamespaceModule(resource.values.name, resource.values.resource_group_name, address)) {
                         echo "Import of Service Module - ${resource.values.name} is successful"
                     } else {
                         echo "Failed to import Serice Bus Module - ${resource.values.name}"
@@ -39,27 +42,68 @@ def call(String subscription, String environment, String product, tags) {
     }
 }
 
-def importServiceBusNamespaceModule(String serviceBusName, String resource_group_name, String module_reference, String environment, String product, pipelineTags) {
-    try {
+// def importServiceBusNamespaceModule(String serviceBusName, String resource_group_name, String module_reference, String environment, String product, pipelineTags) {
+//     try {
 
-        echo "Importing Service Bus Namespace - ${serviceBusName}"
+//         echo "Importing Service Bus Namespace - ${serviceBusName}"
 
-        String nsModule = module_reference + ".azurerm_servicebus_namespace.servicebus_namespace"
-        String nsAuthRuleModule = module_reference + ".azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule"
+//         String nsModule = module_reference + ".azurerm_servicebus_namespace.servicebus_namespace"
+//         String nsAuthRuleModule = module_reference + ".azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule"
 
-        String serviceBusId = az "servicebus namespace show --name ${serviceBusName} --resource-group ${resource_group_name} --query id -o tsv"
-        String serviceBusAuthRuleID = az "servicebus namespace authorization-rule show --name SendAndListenSharedAccessKey --namespace-name ${serviceBusName} --resource-group ${resource_group_name} --query id -o tsv"
+//         String serviceBusId = az "servicebus namespace show --name ${serviceBusName} --resource-group ${resource_group_name} --query id -o tsv"
+//         String serviceBusAuthRuleID = az "servicebus namespace authorization-rule show --name SendAndListenSharedAccessKey --namespace-name ${serviceBusName} --resource-group ${resource_group_name} --query id -o tsv"
 
-        sh "terraform import -var 'common_tags=${pipelineTags}' -var 'env=${environment}' -var 'product=${product}'" +
-            (fileExists("${environment}.tfvars") ? " -var-file=${environment}.tfvars" : "") + " ${nsModule} ${serviceBusId}"
+//         sh "terraform import -var 'common_tags=${pipelineTags}' -var 'env=${environment}' -var 'product=${product}'" +
+//             (fileExists("${environment}.tfvars") ? " -var-file=${environment}.tfvars" : "") + " ${nsModule} ${serviceBusId}"
 
-        sh "terraform import -var 'common_tags=${pipelineTags}' -var 'env=${environment}' -var 'product=${product}'" +
-            (fileExists("${environment}.tfvars") ? " -var-file=${environment}.tfvars" : "") + " ${nsAuthRuleModule} ${serviceBusAuthRuleID}"
+//         sh "terraform import -var 'common_tags=${pipelineTags}' -var 'env=${environment}' -var 'product=${product}'" +
+//             (fileExists("${environment}.tfvars") ? " -var-file=${environment}.tfvars" : "") + " ${nsAuthRuleModule} ${serviceBusAuthRuleID}"
 
-        return true;
+//         return true;
+//     }
+//     catch (err) {
+//         echo err.getMessage()
+//         return false;
+//     }
+// }
+
+class ImportServiceBusModules {
+    def steps
+    def environment
+    def product
+    def tags
+    def az
+
+    ImportServiceBusModules(steps, environment, product, tags) {
+        this.steps = steps
+        this.environment = environment
+        this.product = product
+        this.tags = tags
+        this.az = new Az(this.steps, this.steps.env.SUBSCRIPTION_NAME)
     }
-    catch (err) {
-        echo err.getMessage()
-        return false;
+
+    def importServiceBusNamespaceModule(String serviceBusName, String resource_group_name, String module_reference) {
+        try {
+
+            this.steps.echo "Importing Service Bus Namespace - ${serviceBusName}"
+
+            String nsModule = module_reference + ".azurerm_servicebus_namespace.servicebus_namespace"
+            String nsAuthRuleModule = module_reference + ".azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule"
+
+            String serviceBusId = this.az.az "servicebus namespace show --name ${serviceBusName} --resource-group ${resource_group_name} --query id -o tsv"
+            String serviceBusAuthRuleID = this.az.az "servicebus namespace authorization-rule show --name SendAndListenSharedAccessKey --namespace-name ${serviceBusName} --resource-group ${resource_group_name} --query id -o tsv"
+
+            this.steps.sh "terraform import -var 'common_tags=${this.tags}' -var 'env=${this.environment}' -var 'product=${this.product}'" +
+                (fileExists("${this.environment}.tfvars") ? " -var-file=${this.environment}.tfvars" : "") + " ${nsModule} ${serviceBusId}"
+
+            this.steps.sh "terraform import -var 'common_tags=${this.tags}' -var 'env=${this.environment}' -var 'product=${this.product}'" +
+                (fileExists("${this.environment}.tfvars") ? " -var-file=${this.environment}.tfvars" : "") + " ${nsAuthRuleModule} ${serviceBusAuthRuleID}"
+
+            return true;
+        }
+        catch (err) {
+            this.steps.echo err.getMessage()
+            return false;
+        }
     }
 }
