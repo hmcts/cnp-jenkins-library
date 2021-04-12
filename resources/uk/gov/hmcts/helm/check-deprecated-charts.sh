371b8c8f5e61f50f95fe9d771f6a70f81ab05b99
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2021-04-12 12:23:38 +0100

    Update check-deprecated-charts.sh

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -2,11 +2,13 @@
 set -x
 
 CHART_DIRECTORY=${1}-${2}
-MIN_JAVA_VERSION="3.4.0"
-MIN_NODEJS_VERSION="2.3.0"
+MIN_JAVA_VERSION="3.4.5"
+MIN_NODEJS_VERSION="2.3.4"
+MIN_JOB_VERSION="0.7.1"
 
 JAVA_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "java" |awk '{ print $2}' | sed "s/~//g")
 NODEJS_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "nodejs" |awk '{ print $2}' | sed "s/~//g")
+JOB_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "job" |awk '{ print $2}' | sed "s/~//g")
 
 if [[ -n $JAVA_VERSION ]] &&  [[ $JAVA_VERSION < $MIN_JAVA_VERSION ]]; then
     echo "Java chart version $JAVA_VERSION is deprecated, please upgrade"
@@ -15,5 +17,10 @@ fi
 
 if [[ -n $NODEJS_VERSION ]] && [[ $NODEJS_VERSION < $MIN_NODEJS_VERSION ]]; then
     echo "Nodejs chart version $NODEJS_VERSION is deprecated, please upgrade"
+    exit 1
+fi
+
+if [[ -n $JOB_VERSION ]] && [[ $JOB_VERSION < $MIN_JOB_VERSION ]]; then
+    echo "Job chart version $MIN_JOB_VERSION is deprecated, please upgrade"
     exit 1
 fi
