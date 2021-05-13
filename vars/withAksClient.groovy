b864638a43ecaa9ba940856972ef9af37310250f
nikola-naydenov-hmcts <nikola.naydenov@hmcts.net>
2021-05-13 06:10:47 +0100

    Update vars/withAksClient.groovy
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- vars/withAksClient.groovy
+++ vars/withAksClient.groovy
@@ -2,6 +2,7 @@ import uk.gov.hmcts.contino.Environment
 
 def call(String subscription, String environment, String product, Closure block) {
   withAcrClient(subscription) {
+    // replacing idam is a hack to workaround incorrect idam environment value
     def envName = environment.replace('idam-', '').toUpperCase()
     env.AKS_CLUSTER_NAME = env."${envName}_AKS_CLUSTER_NAME" ?: "cnp-${environment}-cluster"
     env.AKS_RESOURCE_GROUP = env."${envName}_AKS_RESOURCE_GROUP" ?: "cnp-${environment}-rg"
@@ -13,4 +14,3 @@ def call(String subscription, String product, Closure block) {
   String environment = new Environment(env).previewName
   call(subscription, environment, product, block)
 }
-
