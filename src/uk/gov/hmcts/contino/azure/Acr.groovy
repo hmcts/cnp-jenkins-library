02e9da58d547799553be028adcc1551d20ae8996
Nikola Naydenov <nikolanaydenov@nikolas-mbp.home>
2021-05-14 15:50:59 +0100

    Allow for preview to be retagged for staging

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -128,6 +128,7 @@ class Acr extends Az {
     def additionalTag = dockerImage.getShortName(stage)
     def baseTag = (stage == DockerImage.DeploymentStage.PR  || dockerImage.imageTag == 'staging') ? dockerImage.getBaseTaggedName() : dockerImage.getTaggedName()
     if (stage == DockerImage.DeploymentStage.PREVIEW) {
+      // Non-master builds will only have the base branch name as the tag
       baseTag = dockerImage.getBaseShortName();
     }
     this.az "acr import --force -n ${registryName} -g ${resourceGroup} --subscription ${registrySubscription} --source ${baseTag} -t ${additionalTag}"?.trim()
