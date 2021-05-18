0be37cbea00aba54baaa2f5d8924ede6f8872219
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-05-18 14:48:47 +0100

    Timeout pact tests that get stuck (#744)

--- vars/sectionBuildAndTest.groovy
+++ vars/sectionBuildAndTest.groovy
@@ -146,17 +146,19 @@ def call(params) {
 
     if (config.pactBrokerEnabled && config.pactConsumerTestsEnabled && noSkipImgBuild) {
       stageWithAgent("Pact Consumer Verification", product) {
-        def version = env.GIT_COMMIT.length() > 7 ? env.GIT_COMMIT.substring(0, 7) : env.GIT_COMMIT
-        def isOnMaster = new ProjectBranch(env.BRANCH_NAME).isMaster()
+        timeoutWithMsg(time: 20, unit: 'MINUTES', action: 'Pact Consumer Verification') {
+          def version = env.GIT_COMMIT.length() > 7 ? env.GIT_COMMIT.substring(0, 7) : env.GIT_COMMIT
+          def isOnMaster = new ProjectBranch(env.BRANCH_NAME).isMaster()
 
-        env.PACT_BRANCH_NAME = isOnMaster ? env.BRANCH_NAME : env.CHANGE_BRANCH
-        env.PACT_BROKER_URL = pactBrokerUrl
+          env.PACT_BRANCH_NAME = isOnMaster ? env.BRANCH_NAME : env.CHANGE_BRANCH
+          env.PACT_BROKER_URL = pactBrokerUrl
 
-        /*
-       * These instructions have to be kept in order
-       */
-        pcr.callAround('pact-consumer-tests') {
-          builder.runConsumerTests(pactBrokerUrl, version)
+          /*
+         * These instructions have to be kept in order
+         */
+          pcr.callAround('pact-consumer-tests') {
+            builder.runConsumerTests(pactBrokerUrl, version)
+          }
         }
       }
     }
