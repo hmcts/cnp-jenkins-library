b944f1a9608e3762e8e76d57770846a03f8d6ff1
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-06-11 13:12:48 +0100

    Add null check for AKS Subscription, (idam workaround) (#751)
    * Add null check for AKS Subscription, (idam workaround)
    
    * Update vars/spinInfra.groovy
    
    * Update spinInfra.groovy

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -83,14 +83,12 @@ def call(product, component, environment, tfPlanOnly, subscription, deploymentTa
         env.TF_VAR_deployment_namespace = deploymentNamespace
         env.TF_VAR_subscription = subscription
         env.TF_VAR_component = component
-        if (environment ==~ /^idam-packer-.*/ ) {
-          environmentName = environment.replace("idam-packer-","")
-        } else if (environment ==~ /^idam-.*/ ) {
-          environmentName = environment.replace("idam-","")
-        } else {
-          environmentName = environment
+
+        def aksSubscription = new AKSSubscriptions(this).getAKSSubscriptionByEnvName(environment)
+        
+        if (aksSubscription != null) {
+          env.TF_VAR_aks_subscription_id = aksSubscription.id
         }
-        env.TF_VAR_aks_subscription_id = new AKSSubscriptions(this).getAKSSubscriptionByEnvName(environmentName).id
 
         sh 'env|grep "TF_VAR\\|AZURE\\|ARM\\|STORE" | grep -v ARM_ACCESS_KEY'
 
