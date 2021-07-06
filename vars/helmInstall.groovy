6364861d9a2872ec40057131fafb68d404af88d0
Darren Bromwich <d.bromwich@kainos.com>
2021-07-06 12:51:02 +0100

    added pr section

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -75,7 +75,7 @@ def call(DockerImage dockerImage, Map params) {
     }
 
     onPR
-      for (String label: cnp){
+      for (String label: pr-values:cnp){
         if (fileExists(values.label.{environment.template.yaml})) {
           sh "envsubst < ${valuesEnvTemplate} > ${valuesEnv}"
           values << valuesEnv
