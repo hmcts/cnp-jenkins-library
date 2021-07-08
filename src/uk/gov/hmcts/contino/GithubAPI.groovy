a3d979c4ca38422fd93e9c07b508f727a3797d1e
Darren Bromwich <d.bromwich@kainos.com>
2021-07-08 09:26:42 +0100

    update to getLabelsbyKey

--- src/uk/gov/hmcts/contino/GithubAPI.groovy
+++ src/uk/gov/hmcts/contino/GithubAPI.groovy
@@ -54,7 +54,7 @@ class GithubAPI {
   /**
    * Check Pull Request for dependencies label.
    */
-  def getLabelsbyKey(String branch_name, String cnp) {
+  def getLabelsbyKey(String branch_name, String label: cnp) {
 
     if (new ProjectBranch(branch_name).isPR() == true) {
       def project = currentProject()
