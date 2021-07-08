a622a760c7f00d8762e9203e562179b06385d3a1
Darren Bromwich <d.bromwich@kainos.com>
2021-07-08 12:15:52 +0100

    update to onPR

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -76,10 +76,7 @@ def call(DockerImage dockerImage, Map params) {
 
     onPR ( 
       def githubApi = new GithubAPI(this)
-      def trim-pr-values = { 
-        it.startsWith('pr-values:') ? it - 'pr-values:' : it
-      }
-      for; (String labels, githubApi.getLabelsbyKey(env.BRANCH_NAME, "pr-values")){
+      (String labels, githubApi.getLabelsbyKey(env.BRANCH_NAME, "pr-values")){
         if (fileExists(values.labels.${environment}.template.yaml)) {
           sh "envsubst < ${valuesEnvTemplate} > ${valuesEnv}"
           values << valuesEnv
