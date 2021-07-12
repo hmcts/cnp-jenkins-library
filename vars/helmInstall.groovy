b399d9c0c857d3fcf2a8abfa8c5b226b35902373
Darren Bromwich <d.bromwich@kainos.com>
2021-07-12 10:21:38 +0100

    added minus section

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -74,6 +74,13 @@ def call(DockerImage dockerImage, Map params) {
       values << valuesEnv
     }
 
+    class pr-values { 
+      static void main(String[] args) { 
+          String a = "pr-values:ccd";
+          println(a.minus("pr-values:")); 
+      }
+    }
+
     onPR {
       def githubApi = new GithubAPI(this)
       for (labels in githubApi.getLabelsbyKey(env.BRANCH_NAME, "pr-values") ) {
