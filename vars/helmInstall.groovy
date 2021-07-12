05fd75060c14cd01c587876d04eab5032ee95977
Darren Bromwich <d.bromwich@kainos.com>
2021-07-12 13:16:39 +0100

    minus section updated

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -74,7 +74,7 @@ def call(DockerImage dockerImage, Map params) {
       values << valuesEnv
     }
 
-    class prvalues {
+class prvalues {
           static void main(String[] args) {
             String a = "pr-values:ccd";
             
