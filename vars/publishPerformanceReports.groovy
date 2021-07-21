05e495f793edc9a28f83092d5823025b551297db
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-07-21 11:49:20 +0100

    Update vars/publishPerformanceReports.groovy

--- vars/publishPerformanceReports.groovy
+++ vars/publishPerformanceReports.groovy
@@ -2,7 +2,7 @@ import uk.gov.hmcts.contino.Gatling
 
 def call(steps, params)  {
   try {
-        azureBlobUpload('buildlog-storage-account', steps.env.GATLING_REPORTS_DIR, "performance/${params.product}-${params.component}/${params.environment}")
+      azureBlobUpload('buildlog-storage-account', steps.env.GATLING_REPORTS_DIR, "performance/${params.product}-${params.component}/${params.environment}")
   }
   catch (Exception ex) {
     echo "ERROR: Failed to upload performance reports to blob storage destination performance/${params.product}-${params.component}/${params.environment} " +
