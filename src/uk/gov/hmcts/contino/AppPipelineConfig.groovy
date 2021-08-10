51ddae349f510f46e7cd9677e36aa10934d59103
Enda Kelly <enda.kelly@hmcts.net>
2021-08-10 10:49:46 +0100

    DTSPO-2880 - adding stg:dev mapping to vault env overrides

--- src/uk/gov/hmcts/contino/AppPipelineConfig.groovy
+++ src/uk/gov/hmcts/contino/AppPipelineConfig.groovy
@@ -2,7 +2,7 @@ package uk.gov.hmcts.contino
 
 class AppPipelineConfig extends CommonPipelineConfig implements Serializable {
   Map<String, List<Map<String, Object>>> vaultSecrets = [:]
-  Map<String, String> vaultEnvironmentOverrides = ['preview':'aat']
+  Map<String, String> vaultEnvironmentOverrides = ['preview':'aat', 'stg':'dev']
   String vaultName
   boolean migrateDb = false
   String dbMigrationVaultName
