ac4437a40f7e82f804fa97a4a1c230a597aa0884
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-10-28 15:03:34 +0100

    Add vars in refresh

--- vars/importTerraformModules.groovy
+++ vars/importTerraformModules.groovy
@@ -9,7 +9,9 @@ def call(String subscription, String environment, String product, tags) {
 
     def jsonSlurper = new JsonSlurper()
 
-    sh "terraform refresh"
+    sh "terraform refresh -var 'common_tags=${tags}' -var 'env=${environment}' -var 'product=${product}'" +
+          (fileExists("${environment}.tfvars") ? " -var-file=${environment}.tfvars" : "")
+    
     String stateJsonString =  sh(script: "terraform show -json", returnStdout: true).trim()
     def stateJsonObj = jsonSlurper.parseText(stateJsonString)
 
