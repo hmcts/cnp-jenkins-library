a0eba9bccd10815bc1e21705ba40477081466fa3
Enda Kelly <enda.kelly@hmcts.net>
2022-04-04 11:12:09 +0100

    Remove appInsightsInstrumentationKey env variable

--- vars/collectAdditionalInfrastructureVariablesFor.groovy
+++ vars/collectAdditionalInfrastructureVariablesFor.groovy
@@ -6,10 +6,5 @@ def call(subscription, product, environment) {
   KeyVault keyVault = new KeyVault(this, subscription, "${product}-${environment}")
   def environmentVariables = []
 
-  def appInsightsInstrumentationKey = keyVault.find(ProductVaultEntries.APP_INSIGHTS_INSTRUMENTATION_KEY)
-  if (appInsightsInstrumentationKey) {
-    environmentVariables.add("TF_VAR_appinsights_instrumentation_key=${appInsightsInstrumentationKey}")
-  }
-
   return environmentVariables
 }
