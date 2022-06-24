dbd7f0e36bf7dfca8284eb36511df099f406ff00
Darren Bromwich <darrenb@kainos.com>
2022-06-24 10:08:18 +0100

    updated min versions for blobstorage and servicebus

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -5,8 +5,8 @@ CHART_DIRECTORY=${1}-${2}
 MIN_JAVA_VERSION="3.7.2"
 MIN_NODEJS_VERSION="2.4.1"
 MIN_JOB_VERSION="0.7.3"
-MIN_BLOBSTORAGE_VERSION="0.2.2"
-MIN_SERVICEBUS_VERSION="0.3.3"
+MIN_BLOBSTORAGE_VERSION="0.3.0"
+MIN_SERVICEBUS_VERSION="0.4.0"
 
 JAVA_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "java" |awk '{ print $2}' | sed "s/~//g")
 NODEJS_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "nodejs" |awk '{ print $2}' | sed "s/~//g")
