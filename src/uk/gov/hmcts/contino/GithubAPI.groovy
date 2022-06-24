bd61d88aea8737cb64b52c44596276c16df8b68d
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-24 16:06:19 +0100

    Trace null pointer exception

--- src/uk/gov/hmcts/contino/GithubAPI.groovy
+++ src/uk/gov/hmcts/contino/GithubAPI.groovy
@@ -57,19 +57,19 @@ class GithubAPI {
 
     def statusCode = response.getStatus()
     this.steps.echo "Response Status Code: ${statusCode}"
-    if (statusCode == 200) {
-      this.steps.echo "Response Ok."
-      if (this.cachedLabelList.isValid) {
-        this.steps.echo "Cache is Valid.  Adding new labels to cache."
-        this.cachedLabelList.cache.addAll(labels)
-        this.steps.echo "Updated Cache Contents: ${this.cachedLabelList.cache}"
-      } else {
-        this.steps.echo "Cache is Invalid.  Refreshing Cache."
-        this.refreshLabelCache()
-      }
-    } else {
-      this.steps.echo "Failed to Add Labels."
-    }
+//    if (statusCode == 200) {
+//      this.steps.echo "Response Ok."
+//      if (this.cachedLabelList.isValid) {
+//        this.steps.echo "Cache is Valid.  Adding new labels to cache."
+//        this.cachedLabelList.cache.addAll(labels)
+//        this.steps.echo "Updated Cache Contents: ${this.cachedLabelList.cache}"
+//      } else {
+//        this.steps.echo "Cache is Invalid.  Refreshing Cache."
+//        this.refreshLabelCache()
+//      }
+//    } else {
+//      this.steps.echo "Failed to Add Labels."
+//    }
   }
 
   /**
