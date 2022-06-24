1034c3c01e7618dab04d14d59fe088f9a0758d82
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-24 16:56:10 +0100

    Get properties of response object

--- src/uk/gov/hmcts/contino/GithubAPI.groovy
+++ src/uk/gov/hmcts/contino/GithubAPI.groovy
@@ -55,7 +55,7 @@ class GithubAPI {
       consoleLogResponseBody: true,
       validResponseCodes: '200')
 
-    def statCode = response
+    def statCode = response.getProperties().toString()
     this.steps.echo "Response Status Code: ${statusCode}"
 //    if (statusCode == 200) {
 //      this.steps.echo "Response Ok."
