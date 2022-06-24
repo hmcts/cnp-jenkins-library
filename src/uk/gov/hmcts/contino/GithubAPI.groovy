9018eafa5087fb079afeda1869906e2144b5c67b
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-24 16:51:32 +0100

    Update GithubAPI.groovy

--- src/uk/gov/hmcts/contino/GithubAPI.groovy
+++ src/uk/gov/hmcts/contino/GithubAPI.groovy
@@ -55,7 +55,7 @@ class GithubAPI {
       consoleLogResponseBody: true,
       validResponseCodes: '200')
 
-    def statCode = new JsonSlurper().parseText(response)
+    def statCode = response.status
 //    this.steps.echo "Response Status Code: ${statusCode}"
 //    if (statusCode == 200) {
 //      this.steps.echo "Response Ok."
