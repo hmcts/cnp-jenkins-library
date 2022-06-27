57f1d2316786f0b4e528e41e714208ea3461a9d9
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-27 16:47:12 +0100

    Alter stub attempt

--- test/uk/gov/hmcts/contino/GithubAPITest.groovy
+++ test/uk/gov/hmcts/contino/GithubAPITest.groovy
@@ -76,8 +76,7 @@ class GithubAPITest extends Specification {
     def expectedUrl = 'https://api.github.com/repos/hmcts/some-project/issues/68/labels'
 
     given:
-      response = Stub()
-      response.status = 200
+      response.status >> 200
 
     when:
       githubApi.addLabelsToCurrentPR(labels)
