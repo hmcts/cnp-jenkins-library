0e4ed45a31dad8ce5fc40cfeabb33b6f434d6f55
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-27 17:36:34 +0100

    Test stub response.getStatus()

--- test/uk/gov/hmcts/contino/GithubAPITest.groovy
+++ test/uk/gov/hmcts/contino/GithubAPITest.groovy
@@ -75,6 +75,9 @@ class GithubAPITest extends Specification {
 
     def expectedUrl = 'https://api.github.com/repos/hmcts/some-project/issues/68/labels'
 
+    given:
+      response.getStatus() >> 200
+
     when:
       githubApi.addLabelsToCurrentPR(labels)
 
