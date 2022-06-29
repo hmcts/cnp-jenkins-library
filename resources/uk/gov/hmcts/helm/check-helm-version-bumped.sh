8eea737975ef9b7feb710ce17841f7d2d9493249
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2022-06-29 13:09:46 +0100

    override hmctspublic alias as not supported in flux v2 (#862)

--- resources/uk/gov/hmcts/helm/check-helm-version-bumped.sh
+++ resources/uk/gov/hmcts/helm/check-helm-version-bumped.sh
@@ -27,17 +27,17 @@ else
   DIFF_IN_CHART=false
 fi
 
-DIFF_IN_REQUIREMENTS=false
-if [[ -f charts/"${CHART_DIRECTORY}"/requirements.yaml ]]
-then
-  git diff -s --exit-code origin/master charts/"${CHART_DIRECTORY}"/requirements.yaml
-  if [ $? -eq 1 ]; then
-     echo "Diff in requirements.yaml detected"
-     DIFF_IN_REQUIREMENTS=true
-   fi
+sed -i -e 's/@hmctspublic/https\:\/\/hmctspublic.azurecr.io\/helm\/v1\/repo\//g' charts/"${CHART_DIRECTORY}"/Chart.yaml
+git status charts/"${CHART_DIRECTORY}"/Chart.yaml | grep --quiet charts/"${CHART_DIRECTORY}"/Chart.yaml
+if [ $? -eq 0 ]; then
+  echo "Updated hmctspublic alias in Chart.yaml"
+  ALIAS_UPDATED=true
+else
+  ALIAS_UPDATED=false
 fi
 
-if [[ ${DIFF_IN_VALUES} = 'false' ]] && [[ ${DIFF_IN_REQUIREMENTS} = 'false' ]] && [[ ${DIFF_IN_CHART} = 'false' ]] ; then
+
+if [[ ${DIFF_IN_VALUES} = 'false' ]]  && [[ ${DIFF_IN_CHART} = 'false' ]]  && [[ ${ALIAS_UPDATED} = 'false' ]] ; then
   echo 'No differences requiring chart version bump detected'
   exit 0
 fi
@@ -46,7 +46,9 @@ git diff origin/master charts/"${CHART_DIRECTORY}"/Chart.yaml | grep --quiet '+v
 
 if [ $? -eq 0 ]; then
   echo "Chart.yaml version has been bumped :)"
+  CHART_BUMPED=false
 else
+  CHART_BUMPED=true
   echo "==================================================================="
   echo "=====  Version is not bumped in Chart.yaml file    ====="
   echo "=====  This is required as you changed something in           ====="
@@ -54,17 +56,20 @@ else
   echo "=====  Jenkins will bump the version and commit for you.      ====="
   echo "==================================================================="
 
+  CHART_VERSION=$(cat charts/"${CHART_DIRECTORY}"/Chart.yaml | grep ^version | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')
+  NEW_VERSION=$(echo $CHART_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g' )
+  sed -i -e "s/^version: $CHART_VERSION/version: $NEW_VERSION/" charts/"${CHART_DIRECTORY}"/Chart.yaml
+
+fi
+
+if [[ ${CHART_BUMPED} = 'true' ]]  || [[ ${ALIAS_UPDATED} = 'true' ]] ; then
   git fetch origin $BRANCH:$BRANCH
   git remote set-url origin $(git config remote.origin.url | sed "s/github.com/${USER_NAME}:${BEARER_TOKEN}@github.com/g")
   git config --global user.name ${USER_NAME}
   git config --global user.email ${GIT_APP_EMAIL_ID}
 
-  CHART_VERSION=$(cat charts/"${CHART_DIRECTORY}"/Chart.yaml | grep ^version | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')
-  NEW_VERSION=$(echo $CHART_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g' )
-  sed -i -e "s/^version: $CHART_VERSION/version: $NEW_VERSION/" charts/"${CHART_DIRECTORY}"/Chart.yaml
-
   git add charts/"${CHART_DIRECTORY}"/Chart.yaml
-  git commit -m "Bumping chart version"
+  git commit -m "Bumping chart version/ fixing aliases"
   git push origin HEAD:$BRANCH
   exit 1
 fi
