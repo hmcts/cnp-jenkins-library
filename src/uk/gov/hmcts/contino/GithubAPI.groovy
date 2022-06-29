b41cd6bf05a0e53847b869f297f1c64af7d3af89
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-29 12:49:39 +0100

    Revert "Refactor check to determine if branch is a PR"
    This reverts commit d5cab6f64868aeb5a9041b97caf5d3caff966ef4.

--- src/uk/gov/hmcts/contino/GithubAPI.groovy
+++ src/uk/gov/hmcts/contino/GithubAPI.groovy
@@ -129,20 +129,13 @@ class GithubAPI {
   }
 
   /**
-   * Check if we are working with a PR
-   */
-  def checkPR(String branch_name) {
-    def isPR = new ProjectBranch(branch_name).isPR()
-    this.steps.echo "Is ${branch_name} a PR?: ${isPR}"
-    return isPR
-  }
-
-  /**
    * Check Pull Request for label by a pattern in name.
    */
   def getLabelsbyPattern(String branch_name, String key) {
     this.steps.echo "Getting Labels for Branch: ${branch_name} by Pattern: ${key}"
-    if (checkPR(branch_name)) {
+    def isPR = new ProjectBranch(branch_name).isPR()
+    this.steps.echo "Is this a PR?: ${isPR}"
+    if (isPR) {
       this.steps.echo "PR Confirmed.  Calling getLabels()."
       def foundLabels = getLabels().findAll{it.contains(key)}
       this.steps.echo "Returning Labels: ${foundLabels}"
