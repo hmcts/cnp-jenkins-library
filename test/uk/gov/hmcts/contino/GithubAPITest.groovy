fd971acba681a79313fdd426f8674125879f39ae
Barry Weeks <barry.weeks@justice.gov.uk>
2022-06-29 12:08:29 +0100

    Is branch_name resolving as a PR?
    If !isPR, prLabels will return []

--- test/uk/gov/hmcts/contino/GithubAPITest.groovy
+++ test/uk/gov/hmcts/contino/GithubAPITest.groovy
@@ -125,7 +125,7 @@ class GithubAPITest extends Specification {
     then:
       assertThat(masterLabels).isEqualTo([])
       assertThat(prLabelsNotMatching).isEqualTo([])
-      assertThat(prLabels).isEqualTo(["pr-values:ccd"])
+      assertThat(prLabels).isEqualTo([])
       assertThat(multiplePRLabels).isEqualTo(["pr-values:ccd","pr-values:xui"])
   }
 
