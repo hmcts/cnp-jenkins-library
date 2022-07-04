0e13b3de3e069cb95958b7c03725389401ffc450
Barry Weeks <barry.weeks@justice.gov.uk>
2022-07-04 16:01:03 +0100

    Update sectionDeployToAKS.groovy

--- vars/sectionDeployToAKS.groovy
+++ vars/sectionDeployToAKS.groovy
@@ -134,6 +134,7 @@ def call(params) {
 
           onMaster {
             if (testLabels.contains('enable_fortify_scan')) {
+              enableFortifyScan()
               fortifyScan(
                 pipelineCallbacksRunner: pcr,
                 fortifyVaultName: config.fortifyVaultName ?: "${product}-${environment.nonProdName}",
