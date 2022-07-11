4e3f4c8b3a8157dc987675b55a0e1dccbc4bbf3a
Rees Pozzi <>
2022-07-11 09:50:11 +0100

    Revert var change

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -111,7 +111,7 @@ def call(DockerImage dockerImage, Map params) {
       "--namespace ${namespace}"
     ]
 
-    if (${this.env.DISABLE_TRAEFIK_TLS} == true) {
+    if (this.env.DISABLE_TRAEFIK_TLS == true) {
       //Add global disableTraefikTls flag as true if DISABLE_TRAEFIK_TLS is set to true in jenkins
         options.add("--set global.disableTraefikTls=true")
     }else{
