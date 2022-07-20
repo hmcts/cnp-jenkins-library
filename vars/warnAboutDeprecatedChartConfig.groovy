8e43b41aebeb638d5c3447bb531ff62c80e00af4
Rees Pozzi <rees.pozzi@hmcts.net>
2022-07-20 14:24:54 +0100

    Update warnAboutDeprecatedChartConfig.groovy
    Add help message for failing java upgrades

--- vars/warnAboutDeprecatedChartConfig.groovy
+++ vars/warnAboutDeprecatedChartConfig.groovy
@@ -25,7 +25,7 @@ def call(Map<String, String> params) {
     ./check-deprecated-charts.sh $product $component
     """
   } catch(ignored) {
-    WarningCollector.addPipelineWarning("deprecated_helmcharts", "Please upgrade base helm charts to latest. See https://github.com/hmcts/chart-java/releases, https://github.com/hmcts/chart-nodejs/releases, https://github.com/hmcts/chart-job/releases, https://github.com/hmcts/chart-blobstorage/releases, https://github.com/hmcts/chart-servicebus/releases ", LocalDate.of(2022, 7, 21))
+    WarningCollector.addPipelineWarning("deprecated_helmcharts", "Please upgrade base helm charts to latest. See https://github.com/hmcts/chart-java/releases, https://github.com/hmcts/chart-nodejs/releases, https://github.com/hmcts/chart-job/releases, https://github.com/hmcts/chart-blobstorage/releases, https://github.com/hmcts/chart-servicebus/releases - For Java upgrades, you may encounter postgres issues in your preview (PR) pods, which a commit like https://github.com/hmcts/send-letter-service/pull/1633/commits/51b8f2367c5779951d17bafa9ae1705fd3c90a7e will fix ", LocalDate.of(2022, 7, 21))
   } finally {
     sh 'rm -f check-deprecated-charts.sh'
   }
