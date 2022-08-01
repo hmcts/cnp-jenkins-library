a1ef7ee7452dde4f2273e195cbb49832635a6d3f
Enda Kelly <enda.kelly@hmcts.net>
2022-08-01 15:50:04 +0100

    update buildandtest

--- vars/sectionBuildAndTest.groovy
+++ vars/sectionBuildAndTest.groovy
@@ -108,8 +108,10 @@ def call(params) {
             def buildArgs = projectBranch.isPR() ? " --build-arg DEV_MODE=true" : ""
             if (fileExists(acbTemplateFilePath)) {
               acr.runWithTemplate(acbTemplateFilePath, dockerImage)
+              acr.reconcile
             } else {
               acr.build(dockerImage, buildArgs)
+              acr.reconcile
             }
           }
         }
