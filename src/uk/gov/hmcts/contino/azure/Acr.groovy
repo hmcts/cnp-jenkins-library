fbbce1467e4e0cdfcf86e26200d57fec3ae385e3
Enda Kelly <enda.kelly@hmcts.net>
2022-08-01 16:51:21 +0100

    use second var

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -91,7 +91,7 @@ class Acr extends Az {
   }
 
   def reconcile(DockerImage dockerImage) {
-    repository = ${dockerImage.getRepositoryName()}
+    String repository = dockerImage.getRepositoryName()
     steps.echo "Repository is ${repository}"
   }
 
