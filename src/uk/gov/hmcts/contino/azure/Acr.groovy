db4696dfdd55a337e26164dff49c63d6ad9a3cad
Enda Kelly <enda.kelly@hmcts.net>
2022-08-04 16:06:53 +0100

    more updates

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -100,10 +100,9 @@ class Acr extends Az {
   }
 
   def reconcile(DockerImage dockerImage) {
-    def azJenkins = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az $cmd") }
-    azJenkins 'login --identity'
-    azJenkins 'account show'
-    azJenkins "aks get-credentials --resource-group ss-ptl-00-rg --name ss-ptl-00-aks --subscription DTS-SHAREDSERVICESPTL -a"
+    sh "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins azlogin --identity"
+    sh "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins azaccount show"
+    sh "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az aks get-credentials --resource-group ss-ptl-00-rg --name ss-ptl-00-aks --subscription DTS-SHAREDSERVICESPTL -a"
     String repository = dockerImage.getRepositoryName().replace("/", "-")
     steps.echo "Flux will attempt to get info about image repository ${repository}"
     steps.sh(
