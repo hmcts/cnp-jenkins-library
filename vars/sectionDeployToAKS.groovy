bee27aded10f45a32b921c8922d1f2fe2ec32954
Enda Kelly <enda.kelly@hmcts.net>
2022-08-04 09:03:22 +0100

    use acr.reconcile

--- vars/sectionDeployToAKS.groovy
+++ vars/sectionDeployToAKS.groovy
@@ -43,7 +43,7 @@ def call(params) {
     dockerImage = new DockerImage(product, component, acr, projectBranch.imageTag(), env.GIT_COMMIT, env.LAST_COMMIT_TIMESTAMP)
     onPR {
       acr.retagForStage(DockerImage.DeploymentStage.PR, dockerImage)
-      steps.sh(['script': 'ls /usr/local/bin', 'returnStatus': true])
+      acr.reconcile(dockerImage)
     }
   }
 
