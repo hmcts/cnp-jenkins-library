7b551e23da2f24fbee3a2a96fd88666a206bd743
Enda Kelly <enda.kelly@hmcts.net>
2022-08-05 09:12:37 +0100

    re-add echo

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -102,7 +102,7 @@ class Acr extends Az {
   def reconcile(DockerImage dockerImage) {
     String repository = dockerImage.getRepositoryName().replace("/", "-")
     steps.echo "Flux will attempt to get info about image repository ${repository}"
-    KUSTOMIZATION_OUTPUT = steps.sh (
+    String KUSTOMIZATION_OUTPUT = steps.sh (
       // script: "flux get image repository ${repository}",
       script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az login --identity\n" +
               "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az account show\n" +
@@ -110,7 +110,7 @@ class Acr extends Az {
               "flux get kustomization",
       returnStdout: true
     ).trim()
-    // steps.echo "Here is the output ${KUSTOMIZATION_OUTPUT}"
+    steps.echo "Here is the output ${KUSTOMIZATION_OUTPUT}"
   }
 
   /**
