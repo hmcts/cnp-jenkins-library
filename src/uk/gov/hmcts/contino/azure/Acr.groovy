6144025aa2b5d0b0d47f82ae1a1a5b2197a98704
Enda Kelly <enda.kelly@hmcts.net>
2022-08-05 09:45:02 +0100

    update property values

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -8,8 +8,6 @@ class Acr extends Az {
   def registryName
   def resourceGroup
   def registrySubscription
-  def subscription = params.subscription
-  def namespace = env.TEAM_NAMESPACE
 
   /**
    * Create a new instance of Acr with the given pipeline script, subscription and registry name
@@ -105,7 +103,7 @@ class Acr extends Az {
   def reconcile(DockerImage dockerImage) {
     String repository = dockerImage.getRepositoryName().replace("/", "-")
     steps.echo "Flux will attempt to get info about image repository ${repository}"
-    def kubectl = new Kubectl(this, subscription, namespace, params.aksSubscription.name)
+    def kubectl = new Kubectl(this, subscription == "jenkins", namespace == "toffee", aksSubscription == "DTS-SHAREDSERVICESPTL")
     steps.sh (
       script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az login --identity\n" +
               "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az account show\n",
