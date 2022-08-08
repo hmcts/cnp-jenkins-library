bfb095a9ea658dd758235c2478cbf4bba474fcbf
Enda Kelly <enda.kelly@hmcts.net>
2022-08-08 13:36:21 +0100

    update script

--- resources/uk/gov/hmcts/flux/reconcile-flux-image-repository.sh
+++ resources/uk/gov/hmcts/flux/reconcile-flux-image-repository.sh
@@ -5,6 +5,8 @@ CHART_DIRECTORY=${1}-${2}
 
 echo "Checking for flux image repository"
 
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az login --identity
+
 az aks get-credentials --resource-group ${3} --name ${4} --subscription ${5} -a 
 
 kubectl config get-contexts
