e7506e8625b033b01f99c19401f3e1d585cc2945
Enda Kelly <enda.kelly@hmcts.net>
2022-08-08 12:55:34 +0100

    update env vars

--- vars/sectionPromoteBuildToStage.groovy
+++ vars/sectionPromoteBuildToStage.groovy
@@ -45,10 +45,7 @@ def call(params) {
 
         pcr.callAround("${deploymentStage.label}:promotion") {
           acr.retagForStage(deploymentStage, dockerImage)
-          def resourceGroup = env.PTL_AKS_RESOURCE_GROUP
-          def clusterName = env.PTL_AKS_CLUSTER_NAME
-          def aksSubscription = env.AKS_PTL_SUBSCRIPTION_NAME
-          reconcileFluxImageRepository product: product, component: component, resourceGroup: resourceGroup, clusterName: clusterName, aksSubscription: aksSubscription
+          reconcileFluxImageRepository product: product, component: component, resourceGroup: env.PTL_AKS_RESOURCE_GROUP, clusterName: env.PTL_AKS_CLUSTER_NAME, aksSubscription: env.AKS_PTL_SUBSCRIPTION_NAME
           if (DockerImage.DeploymentStage.PROD == deploymentStage) {
             acr.retagForStage(DockerImage.DeploymentStage.LATEST, dockerImage)
             if (projectBranch.isMaster() && fileExists('build.gradle')) {
