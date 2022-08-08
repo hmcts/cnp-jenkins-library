93df8c040c0a1b37d31d925c84f0941dab65506c
Enda Kelly <enda.kelly@hmcts.net>
2022-08-08 14:23:17 +0100

    update script

--- resources/uk/gov/hmcts/flux/reconcile-flux-image-repository.sh
+++ resources/uk/gov/hmcts/flux/reconcile-flux-image-repository.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -x
+# set -x
 
 CHART_DIRECTORY=${1}-${2}
+# CHART_DIRECTORY="cnp-plum-frontend"
 
 echo "Checking for flux image repository"
 
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az login --identity
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az login --identity > /dev/null
 
 env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az aks get-credentials --resource-group ${3} --name ${4} --subscription ${5} -a 
 
-kubectl config get-contexts
+GET_FLUX_IMAGE_REPOSITORY=$(flux get image repository ${CHART_DIRECTORY})
 
-flux get image repository $CHART_DIRECTORY
+if [[ -z $GET_FLUX_IMAGE_REPOSITORY ]]; then
+    echo "No image repository found called ${CHART_DIRECTORY}. Are you sure this is the correct image repository name?"
+    else
+    echo "flux will now reconcile the image repository ${CHART_DIRECTORY}"
+fi
