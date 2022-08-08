d6b2c65b70ccdaa3b931834c19473aecd8d303bc
Enda Kelly <enda.kelly@hmcts.net>
2022-08-08 14:26:50 +0100

    update script

--- resources/uk/gov/hmcts/flux/reconcile-flux-image-repository.sh
+++ resources/uk/gov/hmcts/flux/reconcile-flux-image-repository.sh
@@ -2,13 +2,17 @@
 # set -x
 
 CHART_DIRECTORY=${1}-${2}
-# CHART_DIRECTORY="cnp-plum-frontend"
+# CHART_DIRECTORY="plum-frontend"
+
+if [[ $CHART_DIRECTORY = "plum-frontend" ]]; then
+    CHART_DIRECTORY="cnp-plum-frontend"
+fi
 
 echo "Checking for flux image repository"
 
 env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az login --identity > /dev/null
 
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az aks get-credentials --resource-group ${3} --name ${4} --subscription ${5} -a 
+env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az aks get-credentials --resource-group ${3} --name ${4} --subscription ${5} -a > /dev/null 
 
 GET_FLUX_IMAGE_REPOSITORY=$(flux get image repository ${CHART_DIRECTORY})
 
