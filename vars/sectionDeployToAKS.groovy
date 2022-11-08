5f0cda26a606eeaf2dee0292bfd5e95160ea7f07
praveenadusumilli <adusumillipraveenkumar@gmail.com>
2022-11-08 09:36:40 +0000

    try

--- vars/sectionDeployToAKS.groovy
+++ vars/sectionDeployToAKS.groovy
@@ -41,8 +41,11 @@ def call(params) {
     imageRegistry = env.TEAM_CONTAINER_REGISTRY ?: env.REGISTRY_NAME
     acr = new Acr(this, subscription, imageRegistry, env.REGISTRY_RESOURCE_GROUP, env.REGISTRY_SUBSCRIPTION)
     dockerImage = new DockerImage(product, component, acr, projectBranch.imageTag(), env.GIT_COMMIT, env.LAST_COMMIT_TIMESTAMP)
-    onPR {
-      acr.retagForStage(DockerImage.DeploymentStage.PR, dockerImage)
+    fileExists('Dockerfile'){
+      onPR {
+        acr.retagForStage(DockerImage.DeploymentStage.PR, dockerImage)
+      }
+    }
     }
   }
 
