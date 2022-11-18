6b21e341fde97d0188678f3940eb956757a25d9f
praveenadusumilli <adusumillipraveenkumar@gmail.com>
2022-11-18 12:11:50 +0000

    remove debug

--- resources/uk/gov/hmcts/helm/aks-debug-info.sh
+++ resources/uk/gov/hmcts/helm/aks-debug-info.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -x
 
 RELEASE_NAME=${1}
 NAMESPACE=${2}
 echo "Helm release has failed, below are the events from cluster:"
-kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp'+  -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
+kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
 echo "Status of pods:"
 kubectl get pods -n "${NAMESPACE}"  | grep  "${RELEASE_NAME}"
 exit 1
