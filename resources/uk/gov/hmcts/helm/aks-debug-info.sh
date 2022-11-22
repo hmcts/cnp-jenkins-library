2914226d4f268295c17d560c527cae14d5a62178
praveenadusumilli <adusumillipraveenkumar@gmail.com>
2022-11-22 15:46:05 +0000

    fix

--- resources/uk/gov/hmcts/helm/aks-debug-info.sh
+++ resources/uk/gov/hmcts/helm/aks-debug-info.sh
@@ -3,11 +3,11 @@ RELEASE_NAME=${1}
 NAMESPACE=${2}
 export TERM=xterm-256color
 
-$(tput setaf 3); echo -e "See below debug information to help troubleshooting the issue.\n ====================================================\n kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n ${NAMESPACE} | grep  ${RELEASE_NAME}"
+tput setaf 3; echo -e "See below debug information to help troubleshooting the issue.\n ====================================================\n kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n ${NAMESPACE} | grep  ${RELEASE_NAME}"
 
 kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
 
-$(tput setaf 3); echo -e "====================================================\n kubectl get pods -n ${NAMESPACE}  -l app.kubernetes.io/instance=${RELEASE_NAME}"
+tput setaf 3; echo -e "====================================================\n kubectl get pods -n ${NAMESPACE}  -l app.kubernetes.io/instance=${RELEASE_NAME}"
 
 kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAME}"
 
@@ -20,7 +20,7 @@ kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAM
 #kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" --field-selector status.phase=Pending -o json | jq '.items[] |  .metadata.name' | xargs kubectl describe pods -n "${NAMESPACE}"
 
 for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" -o json | jq  -r '.items[] | select(.status.containerStatuses[] | ((.ready|not) and .state.waiting.reason=="CrashLoopBackOff")) |  .metadata.name'); do
-$(tput setaf 3); echo -e "====================================================\n Logs for crashing pod $podName: \n kubectl logs  -n ${NAMESPACE} ${podName} -p"
+tput setaf 3; echo -e "====================================================\n Logs for crashing pod $podName: \n kubectl logs  -n ${NAMESPACE} ${podName} -p"
 
 kubectl logs  -n "${NAMESPACE}" ${podName} -p
 
