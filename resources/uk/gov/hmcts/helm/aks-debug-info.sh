0eff2915c74fdcc051815245721f15824da49bea
praveenadusumilli <adusumillipraveenkumar@gmail.com>
2022-11-22 14:11:25 +0000

    fix

--- resources/uk/gov/hmcts/helm/aks-debug-info.sh
+++ resources/uk/gov/hmcts/helm/aks-debug-info.sh
@@ -2,24 +2,26 @@
 RELEASE_NAME=${1}
 NAMESPACE=${2}
 export TERM=xterm-256color
-
+tput setaf 3
 echo "
 
 See below debug information to help troubleshooting the issue.
 ================================================================================
 kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
 "
-tput setaf 3
-kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
 tput sgr0
+kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
+
+tput setaf 3
 echo "
 ================================================================================
 kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAME}"
 
 "
-tput setaf 3
-kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAME}"
 tput sgr0
+
+kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAME}"
+
 # Commenting describe output as that is already covered by events on the namespace
 # echo "
 #================================================================================
@@ -29,16 +31,14 @@ tput sgr0
 #kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" --field-selector status.phase=Pending -o json | jq '.items[] |  .metadata.name' | xargs kubectl describe pods -n "${NAMESPACE}"
 
 for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" -o json | jq  -r '.items[] | select(.status.containerStatuses[] | ((.ready|not) and .state.waiting.reason=="CrashLoopBackOff")) |  .metadata.name'); do
-
+tput setaf 3
 echo "
 ================================================================================
 Logs for crashing pod $podName:
 kubectl logs  -n "${NAMESPACE}" ${podName} -p
 "
-
-tput setaf 3
-kubectl logs  -n "${NAMESPACE}" ${podName} -p
 tput sgr0
+kubectl logs  -n "${NAMESPACE}" ${podName} -p
 
 done
 
