959e24510b0b79e73f2d605a448ebaa755616037
praveenadusumilli <adusumillipraveenkumar@gmail.com>
2022-11-22 16:14:46 +0000

    fix

--- resources/uk/gov/hmcts/helm/aks-debug-info.sh
+++ resources/uk/gov/hmcts/helm/aks-debug-info.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+
 RELEASE_NAME=${1}
 NAMESPACE=${2}
-export TERM=xterm-256color
 
-tput setaf 3; echo -e "See below debug information to help troubleshooting the issue.\n ====================================================\n kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n ${NAMESPACE} | grep  ${RELEASE_NAME}"
+function execute_command () {
+tput setaf 3;
+"$@"
+tput sgr0
+}
 
+echo "
+
+See below debug information to help troubleshooting the issue.
+================================================================================
 kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" | grep  "${RELEASE_NAME}"
-
-tput setaf 3; echo -e "====================================================\n kubectl get pods -n ${NAMESPACE}  -l app.kubernetes.io/instance=${RELEASE_NAME}"
-
+"
+kubectl get events '--field-selector=type!=Normal' '--sort-by=.metadata.creationTimestamp' -n "${NAMESPACE}" |execute_command grep  "${RELEASE_NAME}"
+echo "
+================================================================================
 kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAME}"
 
+"
+execute_command kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAME}"
 # Commenting describe output as that is already covered by events on the namespace
 # echo "
 #================================================================================
@@ -20,9 +31,14 @@ kubectl get pods -n "${NAMESPACE}"  -l app.kubernetes.io/instance="${RELEASE_NAM
 #kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" --field-selector status.phase=Pending -o json | jq '.items[] |  .metadata.name' | xargs kubectl describe pods -n "${NAMESPACE}"
 
 for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" -o json | jq  -r '.items[] | select(.status.containerStatuses[] | ((.ready|not) and .state.waiting.reason=="CrashLoopBackOff")) |  .metadata.name'); do
-tput setaf 3; echo -e "====================================================\n Logs for crashing pod $podName: \n kubectl logs  -n ${NAMESPACE} ${podName} -p"
 
+echo "
+================================================================================
+Logs for crashing pod $podName:
 kubectl logs  -n "${NAMESPACE}" ${podName} -p
+"
+
+execute_command kubectl logs  -n "${NAMESPACE}" ${podName} -p
 
 done
 
