1e28fbbd589e7f43929f825399f7fe40607b102c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-12-20 09:31:49 +0000

    remove expiresAfter

--- vars/withInfraPipeline.groovy
+++ vars/withInfraPipeline.groovy
@@ -18,7 +18,6 @@ def call(String product, String component = null, String expiresAfter, Closure b
   def callbacks = new PipelineCallbacksConfig()
   def callbacksRunner = new PipelineCallbacksRunner(callbacks)
   def nextMonth = LocalDate.now().plusDays(30)
-  def expiresAfter = params.expiresAfter
 
   callbacks.registerAfterAll { stage ->
     metricsPublisher.publish(stage)
