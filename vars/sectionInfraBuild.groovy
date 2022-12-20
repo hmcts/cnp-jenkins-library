7f4b678b775debdcd2b879a29da58fcfd5069854
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-12-20 14:12:19 +0000

    remove var

--- vars/sectionInfraBuild.groovy
+++ vars/sectionInfraBuild.groovy
@@ -16,7 +16,7 @@ def call(params) {
       pcr.callAround("buildinfra:${environment}") {
         timeoutWithMsg(time: 150, unit: 'MINUTES', action: "buildinfra:${environment}") {
           // build environment infrastructure once
-          return spinInfra(product, component, expiresAfter, environment, planOnly, subscription)
+          return spinInfra(product, component, environment, planOnly, subscription)
         }
       }
     }
