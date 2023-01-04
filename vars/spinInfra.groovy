88621697d6824f6fb96fefe53e2df6bdc93d346a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-04 16:00:14 +0000

    update condition

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -51,7 +51,7 @@ def call(params) {
         if (environment != 'sandbox' && environment != 'sbox') {} 
         
         else {
-          pipelineTags.put("expiresAfter", "$expires").toString()
+          pipelineTags.putAt("expiresAfter", "$expires").toString()
         }
 
         log.info "Building with following input parameters: common_tags='$pipelineTags'; product='$product'; component='$component'; deploymentNamespace='$deploymentNamespace'; environment='$environment'; subscription='$subscription'; tfPlanOnly='$tfPlanOnly'"
