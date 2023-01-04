48772791b2e86164a3086d1a4adf1f9163648207
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-04 14:45:23 +0000

    try put operation

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -51,8 +51,7 @@ def call(params) {
         if (environment != 'sandbox' && environment != 'sbox') {} 
         
         else {
-          expiresTag = new TerraformTagMap([expiresAfter: expires]).toString()
-          pipelineTags << expiresTag
+          pipelineTags.put("expiresAfter": expires)
         }
 
         log.info "Building with following input parameters: common_tags='$pipelineTags'; product='$product'; component='$component'; deploymentNamespace='$deploymentNamespace'; environment='$environment'; subscription='$subscription'; tfPlanOnly='$tfPlanOnly'"
