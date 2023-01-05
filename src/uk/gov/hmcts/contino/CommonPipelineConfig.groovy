3ae703bd752b66837d2b31bfb74cecd76ac291b1
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-05 12:30:27 +0000

    try toString

--- src/uk/gov/hmcts/contino/CommonPipelineConfig.groovy
+++ src/uk/gov/hmcts/contino/CommonPipelineConfig.groovy
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.contino
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class CommonPipelineConfig implements Serializable {
   String slackChannel
 
   Set<String> branchesToSyncWithMaster = []
 
-  String expiryDate
+  String expiryDate = LocalDate.now().plusDays(30).toString()
 
 }
