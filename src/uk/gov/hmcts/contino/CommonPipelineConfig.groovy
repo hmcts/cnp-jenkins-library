8f7f63dc4179e4b8612dcb312c866eaa889c5146
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-05 10:28:06 +0000

    remove set string

--- src/uk/gov/hmcts/contino/CommonPipelineConfig.groovy
+++ src/uk/gov/hmcts/contino/CommonPipelineConfig.groovy
@@ -1,14 +1,10 @@
 package uk.gov.hmcts.contino
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 class CommonPipelineConfig implements Serializable {
   String slackChannel
 
   Set<String> branchesToSyncWithMaster = []
 
-  LocalDate nextMonth = LocalDate.now().plusDays(30)
-
-  Set<String> expiryDate = nextMonth.format("yyyy-MM-dd")
+  String expiryDate
 
 }
