38d8ad1fbabc9cd50c4aee97b1694aa85c3a1b32
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-06 14:58:04 +0000

    update var

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -72,7 +72,7 @@ def call(params) {
 
         def tags = [environment: Environment.toTagName(environment), changeUrl: changeUrl, managedBy: teamName, BuiltFrom: builtFrom, contactSlackChannel: contactSlackChannel, application: env.TEAM_APPLICATION_TAG, businessArea: env.BUSINESS_AREA_TAG ]
 
-        def test = new Environment(env).sandbox
+        def test = new Environment(env).sandbox ?: "unknown"
 
         println "test var is " test
 
