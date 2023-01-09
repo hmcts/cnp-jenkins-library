de711e27161e0e8a27d8e7075cf8dd6edc2fac82
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-09 09:55:04 +0000

    update condition

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -72,7 +72,7 @@ def call(params) {
 
         def tags = [environment: Environment.toTagName(environment), changeUrl: changeUrl, managedBy: teamName, BuiltFrom: builtFrom, contactSlackChannel: contactSlackChannel, application: env.TEAM_APPLICATION_TAG, businessArea: env.BUSINESS_AREA_TAG ]
 
-        if (new Environment(this).sandbox == environment) { 
+        if (new Environment(env).sandbox == environment) { 
           pipelineTags = tags + [expiresAfter: expires]
         }
         
