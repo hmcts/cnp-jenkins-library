1e35224c5b5e06cbd987b8edbc1cf890c7fbf3af
Filippo Possenti <filippo.possenti@version1.com>
2023-01-12 16:43:22 +0000

    Read pods logs and save in artifacts

--- resources/uk/gov/hmcts/helm/save-pods-logs.sh
+++ resources/uk/gov/hmcts/helm/save-pods-logs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 RELEASE_NAME=${1}
 NAMESPACE=${2}
@@ -6,16 +6,12 @@ PODS_LOGS_DIR=${3}
 
 mkdir ${PODS_LOGS_DIR}
 
-echo "
-Check Jenkins artifacts for the pods logs.
-================================================================================
-"
+echo "Saving pods logs for release ${RELEASE_NAME} under Jenkins artifacts directory ${PODS_LOGS_DIR}..."
 
-kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" | awk '{print $1}'
-
-for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" | awk '{print $1}'); do
+for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" -o json | jq -r '.items[] | select(.status.containerStatuses[] | ((.ready|not) and .state.waiting.reason=="CrashLoopBackOff")) |  .metadata.name'); do
 
   kubectl logs -n "${NAMESPACE}" ${podName} > ${PODS_LOGS_DIR}/${podName}.log
 
 done
 
+echo "... done."
--- vars/savePodsLogs.groovy
+++ vars/savePodsLogs.groovy
@@ -1,20 +1,24 @@
 import uk.gov.hmcts.contino.DockerImage
+import uk.gov.hmcts.contino.Kubectl
 
 
 def call(DockerImage dockerImage, Map params, String scope) {
-  onPR {
-    stageWithAgent("Save pods logs", params.product) {
-      def chartName = "${params.product}-${params.component}"
-      def imageTag = dockerImage.getTag()
-      def releaseName = "${chartName}-${imageTag}"
+  def chartName = "${params.product}-${params.component}"
+  def imageTag = dockerImage.getImageTag()
+  def releaseName = "${chartName}-${imageTag}"
+  def namespace = env.TEAM_NAMESPACE
 
-      steps.writeFile(file: 'save-pods-logs.sh', text: steps.libraryResource('uk/gov/hmcts/helm/save-pods-logs.sh'))
-      steps.sh """
-        chmod +x save-pods-logs.sh
-        ./save-pods-logs.sh ${releaseName} ${env.TEAM_NAMESPACE} pods-logs-${scope}
-        rm -f save-pods-logs.sh
-      """
-      steps.archiveArtifacts allowEmptyArchive: true, artifacts: "pods-logs-${scope}/**"
-    }
+  withAksClient(params.subscription, params.environment, params.product) {
+    def kubectl = new Kubectl(this, params.subscription, namespace, params.aksSubscription.name)
+    kubectl.login()
+
+    steps.writeFile(file: 'save-pods-logs.sh', text: steps.libraryResource('uk/gov/hmcts/helm/save-pods-logs.sh'))
+    steps.sh(label: "Save pods logs in artifacts, under pods-logs-${scope}", script: """
+    chmod +x save-pods-logs.sh
+    ./save-pods-logs.sh ${releaseName} ${namespace} pods-logs-${scope}
+    rm -f save-pods-logs.sh
+    """)
+
+    steps.archiveArtifacts(allowEmptyArchive: true, artifacts: "pods-logs-${scope}/**")
   }
 }
