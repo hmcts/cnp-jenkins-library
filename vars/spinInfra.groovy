1bb9ed580820eae0fdb936d94243ee1c06533f79
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-01-13 12:24:25 +0000

    fix typo

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -29,7 +29,7 @@ def call(product, component, environment, tfPlanOnly, subscription, deploymentTa
 def call(Map<String, ?> params) {
   def config = [
         productName     : params.component ? "$params.product-$params.component" : params.product,
-        deploymentNamespace : new ProjectBranch(env.BRANCH_NAME).deploymentNamespace()
+        deploymentNamespace : new ProjectBranch(env.BRANCH_NAME).deploymentNamespace(),
         planOnly: params.planOnly ?: true
   ] << params
 
