4c172069a91b795eb40e3466618bc949fbae7c6d
mokainos <72137340+mokainos@users.noreply.github.com>
2023-01-17 14:33:02 +0000

    versioncompare (#968)

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -3,6 +3,7 @@ set -x
 
 CHART_DIRECTORY=${1}-${2}
 declare -A deprecationMap
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); } 
 
 deprecationMap["java"]="4.0.4"
 deprecationMap["nodejs"]="2.4.8"
@@ -15,7 +16,7 @@ deprecationMap["elasticsearch"]="7.8.2"
 for deprecation in "${!deprecationMap[@]}"
 do
   CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^$deprecation " |awk '{ print $2}' | sed "s/~//g")
-  if [[ -n $CURRENT_VERSION ]] &&  [[ $CURRENT_VERSION < ${deprecationMap[$deprecation]} ]]; then
+  if [[ -n $CURRENT_VERSION ]] &&  [ $(ver $CURRENT_VERSION) -lt $(ver ${deprecationMap[$deprecation]}) ]; then
       echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${deprecationMap[$deprecation]}"
       exit 1
   fi
