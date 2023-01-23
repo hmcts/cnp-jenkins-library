e3ae6c790fb9c9bde79cdecb615c190039b09883
Filippo Possenti <filippo.possenti@version1.com>
2023-01-23 14:06:55 +0000

    Try/catch to prevent errors in pods logs retrieval from stopping the build

--- vars/savePodsLogs.groovy
+++ vars/savePodsLogs.groovy
@@ -3,22 +3,26 @@ import uk.gov.hmcts.contino.Kubectl
 
 
 def call(DockerImage dockerImage, Map params, String scope) {
-  def chartName = "${params.product}-${params.component}"
-  def imageTag = dockerImage.getImageTag()
-  def releaseName = "${chartName}-${imageTag}"
-  def namespace = env.TEAM_NAMESPACE
+  try {
+    def chartName = "${params.product}-${params.component}"
+    def imageTag = dockerImage.getImageTag()
+    def releaseName = "${chartName}-${imageTag}"
+    def namespace = env.TEAM_NAMESPACE
 
-  withAksClient(params.subscription, params.environment, params.product) {
-    def kubectl = new Kubectl(this, params.subscription, namespace, params.aksSubscription.name)
-    kubectl.login()
+    withAksClient(params.subscription, params.environment, params.product) {
+      def kubectl = new Kubectl(this, params.subscription, namespace, params.aksSubscription.name)
+      kubectl.login()
 
-    steps.writeFile(file: 'save-pods-logs.sh', text: steps.libraryResource('uk/gov/hmcts/helm/save-pods-logs.sh'))
-    steps.sh(label: "Save pods logs in artifacts, under pods-logs-${scope}", script: """
+      steps.writeFile(file: 'save-pods-logs.sh', text: steps.libraryResource('uk/gov/hmcts/helm/save-pods-logs.sh'))
+      steps.sh(label: "Save pods logs in artifacts, under pods-logs-${scope}", script: """
     chmod +x save-pods-logs.sh
     ./save-pods-logs.sh ${releaseName} ${namespace} pods-logs-${scope}
     rm -f save-pods-logs.sh
     """)
 
-    steps.archiveArtifacts(allowEmptyArchive: true, artifacts: "pods-logs-${scope}/**")
+      steps.archiveArtifacts(allowEmptyArchive: true, artifacts: "pods-logs-${scope}/**")
+    }
+  } catch(err) {
+    steps.echo "Unable to complete saving pods logs in Jenkins artifacts"
   }
 }
