4fc2ce9698856f13e9990d0b5c4039d0135306ee
Filippo Possenti <filippo.possenti@version1.com>
2023-01-23 14:11:33 +0000

    Fixing filtering of pods

--- resources/uk/gov/hmcts/helm/save-pods-logs.sh
+++ resources/uk/gov/hmcts/helm/save-pods-logs.sh
@@ -8,7 +8,7 @@ mkdir ${PODS_LOGS_DIR}
 
 echo "Saving pods logs for release ${RELEASE_NAME} under Jenkins artifacts directory ${PODS_LOGS_DIR}..."
 
-for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" -o json | jq -r '.items[] | select(.status.containerStatuses[] | ((.ready|not) and .state.waiting.reason=="CrashLoopBackOff")) |  .metadata.name'); do
+for podName in $(kubectl get pods -n "${NAMESPACE}" -l app.kubernetes.io/instance="${RELEASE_NAME}" -o json | jq -r '.items[] | .metadata.name'); do
 
   kubectl logs -n "${NAMESPACE}" ${podName} > ${PODS_LOGS_DIR}/${podName}.log
 
