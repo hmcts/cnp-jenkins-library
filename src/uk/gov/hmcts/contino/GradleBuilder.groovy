b0a69ffbf4ae240ea5f41cbbdeba13c92d36bc55
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-04 16:47:29 +0000

    set +x to prevent command echoing

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -138,8 +138,9 @@ class GradleBuilder extends AbstractBuilder {
       try {
           localSteps.sh '''set +x'''
           gradle("--stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name='org.postgresql.Driver' -Ddata.connection_string='jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck' -Ddata.user='${localSteps.env.OWASPDB_V6_ACCOUNT}' -Ddata.password='${localSteps.env.OWASPDB_V6_PASSWORD}'  -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate")
-          localSteps.sh '''set -x'''
+
       } finally {
+        localSteps.sh '''set -x'''
         localSteps.archiveArtifacts 'build/reports/dependency-check-report.html'
         String dependencyReport = localSteps.readFile('build/reports/dependency-check-report.json')
 
