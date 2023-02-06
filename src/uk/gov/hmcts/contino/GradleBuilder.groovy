8109c049b21d79659cd1429e11320cc65674f44a
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-06 10:27:16 +0000

    new attempt

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -136,7 +136,8 @@ class GradleBuilder extends AbstractBuilder {
     ]
     localSteps.withAzureKeyvault(secrets) {
       try {
-        localSteps.sh './gradlew --no-daemon --init-script init.gradle --stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name=org.postgresql.Driver -Ddata.connection_string=jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck -Ddata.user=$localSteps.env.OWASPDB_V6_ACCOUNT -Ddata.password=$localSteps.env.OWASPDB_V6_PASSWORD  -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate'
+        localSteps.sh.withEnv('./gradlew --no-daemon --init-script init.gradle --stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name=org.postgresql.Driver -Ddata.connection_string=jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck -Ddata.user=$localSteps.env.OWASPDB_V6_ACCOUNT -Ddata.password=$localSteps.env.OWASPDB_V6_PASSWORD  -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate')
+
 
       } finally {
         localSteps.archiveArtifacts 'build/reports/dependency-check-report.html'
