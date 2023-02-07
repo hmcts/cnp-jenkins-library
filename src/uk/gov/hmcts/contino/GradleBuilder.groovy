bb75a54cea279acfbd0edb689dfcfc6c7fc37faf
Daniel Furnivall <danielfurnivall@gmail.com>
2023-02-07 11:10:23 +0000

    test currently passing, trying something new v2

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -135,9 +135,11 @@ class GradleBuilder extends AbstractBuilder {
       [ secretType: 'Secret', name: 'OWASPPostgresDb-v6-Account', version: '', envVariable: 'OWASPDB_V6_ACCOUNT' ],
       [ secretType: 'Secret', name: 'OWASPPostgresDb-v6-Password', version: '', envVariable: 'OWASPDB_V6_PASSWORD' ]
     ]
-    localSteps.azureKeyVault(secrets) {
-        // using sh directly so that secrets don't get interpolated in the gradle function
-        steps.sh('./gradlew --no-daemon --init-script init.gradle --stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name="org.postgresql.Driver" -Ddata.connection_string="jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck" -Ddata.user=$OWASPDB_V6_ACCOUNT -Ddata.password=$OWASPDB_V6_PASSWORD -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate')
+
+    steps.azureKeyVault(secrets) {
+      // using sh directly so that secrets don't get interpolated in the gradle function
+      steps.sh('./gradlew --no-daemon --init-script init.gradle --stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name="org.postgresql.Driver" -Ddata.connection_string="jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck" -Ddata.user=$OWASPDB_V6_ACCOUNT -Ddata.password=$OWASPDB_V6_PASSWORD -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate')
+    }
     localSteps.archiveArtifacts 'build/reports/dependency-check-report.html'
     String dependencyReport = localSteps.readFile('build/reports/dependency-check-report.json')
 
@@ -145,7 +147,6 @@ class GradleBuilder extends AbstractBuilder {
 
     new CVEPublisher(localSteps)
       .publishCVEReport('java', cveReport)
-      }
     }
 
   def prepareCVEReport(String owaspReportJSON) {
