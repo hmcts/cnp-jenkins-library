2116af8df4d862745e78bd96d69a1717bcb3599f
Tim Jacomb <timjacomb1@gmail.com>
2023-02-07 09:39:19 +0000

    Remove suppressions

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -137,11 +137,9 @@ class GradleBuilder extends AbstractBuilder {
     ]
     localSteps.withAzureKeyvault(secrets) {
       try {
-        steps.sh('''
-set +x
-./gradlew --no-daemon --init-script init.gradle --stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name="org.postgresql.Driver" -Ddata.connection_string="jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck" -Ddata.user=$OWASPDB_V6_ACCOUNT -Ddata.password=$OWASPDB_V6_PASSWORD -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate
-set -x
-''')
+        steps.sh(
+          './gradlew --no-daemon --init-script init.gradle --stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name="org.postgresql.Driver" -Ddata.connection_string="jdbc:postgresql://owaspdependency-v6-prod.postgres.database.azure.com/owaspdependencycheck" -Ddata.user=$OWASPDB_V6_ACCOUNT -Ddata.password=$OWASPDB_V6_PASSWORD -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate'
+        )
       } catch (Exception e){
         Functions.printThrowable(e)
       } finally {
