d458c950a88a9c93bc9d5676849dcf9e8ccc0481
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-14 11:09:52 +0000

    update deploy to env

--- vars/sectionDeployToEnvironment.groovy
+++ vars/sectionDeployToEnvironment.groovy
@@ -41,7 +41,7 @@ def call(params) {
                   component: component,
                   pipelineCallbacksRunner: pcr,
                   planOnly: tfPlanOnly,
-                  expires: expires
+                  expires: config.expiryDate
                 )
             }
 
