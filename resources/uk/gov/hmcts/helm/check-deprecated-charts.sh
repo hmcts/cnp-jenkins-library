3aee9d4a6f649fcfeeda974ea9cf26d5984a945c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-24 15:24:55 +0000

    add purposeful deprecation

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -6,7 +6,7 @@ declare -A deprecationMap
 function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
 deprecationMap["java"]="4.0.12"
-deprecationMap["nodejs"]="2.4.14"
+deprecationMap["nodejs"]="2.4.12"
 deprecationMap["job"]="0.7.10"
 deprecationMap["blobstorage"]="0.3.0"
 deprecationMap["servicebus"]="0.4.0"
