5295b7a2fbc23df09850bea5b671e9fb0e731846
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-27 14:28:16 +0000

    undo

--- vars/warnAboutDeprecatedChartConfig.groovy
+++ vars/warnAboutDeprecatedChartConfig.groovy
@@ -1,5 +1,4 @@
 import uk.gov.hmcts.pipeline.deprecation.WarningCollector
-import uk.gov.hmcts.contino.MetricsPublisher
 
 import java.time.LocalDate
 
@@ -7,8 +6,6 @@ def call(Map<String, String> params) {
 
   def product = params.product
   def component = params.component
-  def slackChannel = env.BUILD_NOTICES_SLACK_CHANNEL
-  MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild, product, component)
 
   writeFile file: 'check-helm-api-version.sh', text: libraryResource('uk/gov/hmcts/helm/check-helm-api-version.sh')
   writeFile file: 'check-deprecated-charts.sh', text: libraryResource('uk/gov/hmcts/helm/check-deprecated-charts.sh')
@@ -29,7 +26,6 @@ def call(Map<String, String> params) {
     """
   } catch(ignored) {
     WarningCollector.addPipelineWarning("deprecated_helmcharts", "Please upgrade base helm charts to latest. See releases on the chart repo for latest updates, example: https://github.com/hmcts/chart-java/releases", LocalDate.of(2023, 02, 14))
-    notifyPipelineDeprecations(slackChannel, metricsPublisher)
   } finally {
     sh 'rm -f check-deprecated-charts.sh'
   }
