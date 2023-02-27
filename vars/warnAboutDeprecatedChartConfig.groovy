a36d945286279c93e8558e032ba36bb2d5b3513a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-27 14:09:52 +0000

    try updating warn chart config

--- vars/warnAboutDeprecatedChartConfig.groovy
+++ vars/warnAboutDeprecatedChartConfig.groovy
@@ -6,6 +6,8 @@ def call(Map<String, String> params) {
 
   def product = params.product
   def component = params.component
+  def slackChannel = env.BUILD_NOTICES_SLACK_CHANNEL
+  MetricsPublisher metricsPublisher = new MetricsPublisher(this, currentBuild, product, component)
 
   writeFile file: 'check-helm-api-version.sh', text: libraryResource('uk/gov/hmcts/helm/check-helm-api-version.sh')
   writeFile file: 'check-deprecated-charts.sh', text: libraryResource('uk/gov/hmcts/helm/check-deprecated-charts.sh')
@@ -26,6 +28,7 @@ def call(Map<String, String> params) {
     """
   } catch(ignored) {
     WarningCollector.addPipelineWarning("deprecated_helmcharts", "Please upgrade base helm charts to latest. See releases on the chart repo for latest updates, example: https://github.com/hmcts/chart-java/releases", LocalDate.of(2023, 02, 14))
+    notifyPipelineDeprecations(slackChannel, metricsPublisher)
   } finally {
     sh 'rm -f check-deprecated-charts.sh'
   }
