9eba1c2597233b836a8df44c663a74238ddbc922
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-27 14:20:29 +0000

    uncomment

--- vars/notifyPipelineDeprecations.groovy
+++ vars/notifyPipelineDeprecations.groovy
@@ -33,9 +33,9 @@ def call(String teamSlackChannel, MetricsPublisher metricsPublisher ) {
         slackWarningMessage = slackWarningMessage.concat("@channel , this is sent here as ${changeAuthor} github user doesn't have a slack mapping in https://github.com/hmcts/github-slack-user-mappings \n\n ")
       }
     }
-    // if(channel == null ) {
-    //   channel = teamSlackChannel
-    // }
+    if(channel == null ) {
+      channel = teamSlackChannel
+    }
     slackWarningMessage = slackWarningMessage.concat("We have noticed deprecated configuration in ${env.JOB_NAME}: <${env.RUN_DISPLAY_URL}|Build ${env.BUILD_DISPLAY_NAME}> \n\n ")
       .concat(warningMessage)
 
