d8ae384910bb6d3df7d667e66f79cce7da8dea2a
Louise Huyton <louise.huyton@justice.gov.uk>
2023-02-27 11:36:44 +0000

    DTSPO-7198 - Add frontend and backend security.sh scripts

---
+++ resources/uk/gov/hmcts/pipeline/security/backend-security.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo ${TEST_URL}
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
+cp /zap/api-report.html functional-output/
+cp *.* functional-output/
