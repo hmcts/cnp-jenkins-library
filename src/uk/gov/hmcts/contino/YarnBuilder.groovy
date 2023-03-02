b689cb96fbd479f1cb38365fc2180266c25bd895
Daniel Furnivall <danielfurnivall@gmail.com>
2023-03-02 10:14:59 +0000

    fixed indents

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -312,11 +312,11 @@ EOF
   }
 
   private nagAboutYarnAuditChange() {
-    if (auditDisparity()){
+    if (auditDisparity()) {
       WarningCollector.addPipelineWarning("transitive_dependency_audit_incoming",
         "We will be adding the --recursive flag to CVE scanning to check your transitive dependencies, your security scan is currently failing with these dependencies added to the scan. Run `yarn npm audit --recursive --environment production` ahead of time to catch and fix issues so you won't be blocked on the day of the change.", LocalDate.of(2023, 03, 15))
     }
-    }
+  }
 
   private isYarnV2OrNewer() {
     def status = steps.sh label: "Determine if is yarn v1", script: '''
@@ -326,8 +326,8 @@ EOF
   }
 
   private nagAboutOldYarnVersions() {
-     if (!isYarnV2OrNewer()){
-       WarningCollector.addPipelineWarning("old_yarn_version", "Please upgrade to Yarn V3, see https://moj.enterprise.slack.com/files/T1L0WSW9F/F04784SLAJC?origin_team=T1L0WSW9F", LocalDate.of(2023, 04, 26))
+    if (!isYarnV2OrNewer()) {
+      WarningCollector.addPipelineWarning("old_yarn_version", "Please upgrade to Yarn V3, see https://moj.enterprise.slack.com/files/T1L0WSW9F/F04784SLAJC?origin_team=T1L0WSW9F", LocalDate.of(2023, 04, 26))
     }
   }
 
