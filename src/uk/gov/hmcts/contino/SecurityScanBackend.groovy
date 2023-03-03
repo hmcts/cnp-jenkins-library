477ccb8c0036f528262e3633ceb2feb6f96d2d9b
Louise Huyton <louise.huyton@justice.gov.uk>
2023-03-03 10:22:54 +0000

    Update SecurityScanBackend.groovy

--- src/uk/gov/hmcts/contino/SecurityScanBackend.groovy
+++ src/uk/gov/hmcts/contino/SecurityScanBackend.groovy
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.contino
 
 
-class SecurityScan implements Serializable {
+class SecurityScanBackend implements Serializable {
     public static final String OWASP_ZAP_IMAGE = 'owasp/zap2docker-stable:2.11.1'
     public static final String OWASP_ZAP_ARGS = '-u 0:0 --name zap -p 1001:1001 -v $WORKSPACE:/zap/wrk/:rw'
     public static final String GLUEIMAGE = 'hmcts/zap-glue:latest'
     public static final String GLUE_ARGS = '-u 0:0 --name=Glue -w $(pwd):/tmp'
     def steps
 
-    SecurityScan(steps) {
+    SecurityScanBackend(steps) {
         this.steps = steps
     }
 
@@ -16,8 +16,8 @@ class SecurityScan implements Serializable {
         try {
             this.steps.withDocker(OWASP_ZAP_IMAGE, OWASP_ZAP_ARGS) {
                 this.steps.sh '''
-                    chmod +x security-backend.sh
-                    ./security-backend.sh
+                    chmod +x security.sh
+                    ./security.sh
                     '''
             }
             this.steps.withDocker(GLUEIMAGE, GLUE_ARGS) {
