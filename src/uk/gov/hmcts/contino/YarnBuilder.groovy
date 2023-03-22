ae69d6c5f610eef4106df874a88a5f354f2ee3bc
Louise Huyton <louise.huyton@justice.gov.uk>
2023-03-22 14:59:08 +0000

    try use SecurityScan class in YarnBuilder

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -344,7 +344,7 @@ EOF
   @Override
   def securityScan() {
     steps.writeFile(file: 'security.sh', text: steps.libraryResource('uk/gov/hmcts/pipeline/security/frontend/security.sh'))
-    this.securitytest.execute()
+    this.securityScan = new SecurityScan(steps)
     WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 03, 29))
 
   }
