a3c639c81c5a82e739b3d3cba1b7702e3ab3a63a
Louise Huyton <louise.huyton@justice.gov.uk>
2023-03-22 16:51:25 +0000

    add echo

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -343,8 +343,8 @@ EOF
 
   @Override
   def securityScan() {
-    
     if (steps.fileExists(".security.sh")) {
+      echo "file exists"
       WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 03, 29))
     }
     steps.writeFile(file: 'security.sh', text: steps.libraryResource('uk/gov/hmcts/pipeline/security/frontend/security.sh'))
