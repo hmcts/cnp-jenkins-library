9712fdfab658eda339c42786121fd9933ff0bc58
Louise Huyton <louise.huyton@justice.gov.uk>
2023-03-22 15:38:07 +0000

    try this

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -344,7 +344,7 @@ EOF
   @Override
   def securityScan() {
     steps.writeFile(file: 'security.sh', text: steps.libraryResource('uk/gov/hmcts/pipeline/security/frontend/security.sh'))
-    this.securityScan = new SecurityScan(this.steps)
+    this.securitytest = new SecurityScan(this.steps)
     WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 03, 29))
 
   }
