bac8678e82538f2ea0025a315349d43e7f346db4
Louise Huyton <louise.huyton@justice.gov.uk>
2023-03-23 14:51:52 +0000

    try this

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -18,9 +18,8 @@ class YarnBuilder extends AbstractBuilder {
 
   def securityScan
 
-  def SecurityScan(steps, securityScan) {
+  def SecurityScan(steps) {
     super(steps)
-    this.securityScan = new SecurityScan(steps)
   }
 
   def build() {
