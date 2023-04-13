7c132450188195bd204f02308388432c0a344368
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-04-13 10:44:54 +0100

    fix

--- src/uk/gov/hmcts/pipeline/DeprecationConfig.groovy
+++ src/uk/gov/hmcts/pipeline/DeprecationConfig.groovy
@@ -14,7 +14,7 @@ class DeprecationConfig {
       def response = steps.httpRequest(
         consoleLogResponseBody: true,
         timeout: 10,
-        url: "https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/adddeprecatedconfig/deprecation-config.yml",
+        url: "https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/deprecation-config.yml",
         validResponseCodes: '200'
       )
       deprecationConfig = steps.readYaml (text: response.content)
