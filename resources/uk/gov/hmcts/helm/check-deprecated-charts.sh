48057c8e7903d755bc4a82ee0c74363483fdd98c
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-04-13 11:37:26 +0100

    fix

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -2,14 +2,14 @@
 set -x
 
 CHART_DIRECTORY=${1}-${2}
-CHART_NAME=${3}
-CHART_VERSION=${4}
+DEPRECATED_CHART_NAME=${3}
+DEPRECATED_CHART_VERSION=${4}
 
 function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
 
-CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^${CHART_NAME}" |awk '{ print $2}' | sed "s/~//g")
-if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${CHART_VERSION}) ]; then
-    echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${CHART_VERSION}"
+CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^${DEPRECATED_CHART_NAME}" |awk '{ print $2}' | sed "s/~//g")
+if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_CHART_VERSION}) ]; then
+    echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${DEPRECATED_CHART_VERSION}"
     exit 1
 fi
