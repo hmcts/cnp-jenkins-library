c047eb4e2d09197efa676936c8ab09174b195d31
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2023-04-13 13:44:22 +0100

    Move deprecation config to external file (#1041)

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -2,22 +2,14 @@
 set -x
 
 CHART_DIRECTORY=${1}-${2}
-declare -A deprecationMap
+DEPRECATED_CHART_NAME=${3}
+DEPRECATED_CHART_VERSION=${4}
+
 function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
-deprecationMap["java"]="4.0.13"
-deprecationMap["nodejs"]="2.4.14"
-deprecationMap["job"]="0.7.11"
-deprecationMap["blobstorage"]="0.3.0"
-deprecationMap["servicebus"]="0.4.0"
-deprecationMap["ccd"]="8.0.27"
-deprecationMap["elasticsearch"]="7.17.3"
 
-for deprecation in "${!deprecationMap[@]}"
-do
-  CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^$deprecation" |awk '{ print $2}' | sed "s/~//g")
-  if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${deprecationMap[$deprecation]}) ]; then
-      echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${deprecationMap[$deprecation]}"
-      exit 1
-  fi
-done
+CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^${DEPRECATED_CHART_NAME}" |awk '{ print $2}' | sed "s/~//g")
+if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_CHART_VERSION}) ]; then
+    echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${DEPRECATED_CHART_VERSION}"
+    exit 1
+fi
