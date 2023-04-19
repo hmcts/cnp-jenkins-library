f7f0ec078c20a56855d34dae4cfaaf7fa9b367dd
Matt Slater <72858053+msl8r@users.noreply.github.com>
2023-04-19 15:09:47 +0100

    Exit if SecurityRules var is not set. (#1049)
    Command continues but breaks the build as the port number isn't processed

--- resources/uk/gov/hmcts/pipeline/security/backend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/backend/security.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+
+if [[ -z ${SecurityRules} ]]; then
+  echo "SecurityRules variable not set. Please set it as a parameter in your Jenkinsfile_nightly file."
+  exit 1
+fi
+
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -z "-config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt" -l FAIL
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 export LC_ALL=C.UTF-8
