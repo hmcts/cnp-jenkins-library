494251947e4e2d52a9dde59dcbed25b2b5e56ba8
Louise Huyton <louise.huyton@justice.gov.uk>
2023-04-19 16:53:27 +0100

    see if works for backend also

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -269,15 +269,15 @@ EOF
 
   @Override
   def securityScan(){
-    if (localSteps.fileExists(".ci/security.sh")) {
-      this.securitytest.execute()
+    if (steps.fileExists(".ci/security.sh")) {
+      steps.writeFile(file: 'security.sh', text: steps.readFile('.ci/security.sh'))
     }
-    // else if (localSteps.fileExists("security.sh")) {
-    //   WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 04, 17))
-    // } else {
-    //   localSteps.writeFile(file: 'security.sh', text: localSteps.libraryResource('uk/gov/hmcts/pipeline/security/backend/security.sh'))
-    //   this.securitytest.execute()
-    // }
+    else if (localSteps.fileExists("security.sh")) {
+      WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 04, 17))
+    } else {
+      localSteps.writeFile(file: 'security.sh', text: localSteps.libraryResource('uk/gov/hmcts/pipeline/security/backend/security.sh'))
+    }
+    this.securitytest.execute()
   }
 
   @Override
