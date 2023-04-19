20494c599e668ca1dc3686f7d5675c4c7e5beade
Louise Huyton <louise.huyton@justice.gov.uk>
2023-04-19 16:07:26 +0100

    change if statement

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -270,14 +270,14 @@ EOF
   @Override
   def securityScan(){
     if (localSteps.fileExists(".ci/security.sh")) {
-      echo "Override: use local security.sh script"
+      this.securitytest.execute()
     }
     else if (localSteps.fileExists("security.sh")) {
       WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 04, 17))
     } else {
       localSteps.writeFile(file: 'security.sh', text: localSteps.libraryResource('uk/gov/hmcts/pipeline/security/backend/security.sh'))
+      this.securitytest.execute()
     }
-    this.securitytest.execute()
   }
 
   @Override
