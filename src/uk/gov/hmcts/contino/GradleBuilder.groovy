24d181f366b0eab0c6084f57c6b9c2150d807380
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-04-28 16:04:58 +0100

    use WarningCollector

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -11,9 +11,7 @@ class GradleBuilder extends AbstractBuilder {
 
   def nonProdName = System.getenv("SUBSCRIPTION_NAME")
 
-  sh '''
-    echo "env is ${nonProdName}"
-  '''
+  WarningCollector.addPipelineWarning("env is ${nonProdName}", LocalDate.of(2022, 6, 30))
 
   def owaspenv = nonProdName == "nonprod" || nonProdName == "prod" ? 'prod' : 'sandbox'
 
