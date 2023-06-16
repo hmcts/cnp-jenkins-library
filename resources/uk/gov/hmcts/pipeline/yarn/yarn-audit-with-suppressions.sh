0343ddf51269324ceb9ee25e9349741007295604
Tim Jacomb <timjacomb1@gmail.com>
2023-06-16 10:29:19 +0100

    Only yarn v3

--- resources/uk/gov/hmcts/pipeline/yarn/yarn-audit-with-suppressions.sh
+++ resources/uk/gov/hmcts/pipeline/yarn/yarn-audit-with-suppressions.sh
@@ -1,44 +1,49 @@
 #!/usr/bin/env bash
-
 # This is a wrapper script for yarn audit that allows suppressing
 # vulnerabilities without a fix
-# Source: https://github.com/yarnpkg/yarn/issues/6669#issuecomment-463684767
-# Note: There's an upstream PR for adding this functionality: https://github.com/yarnpkg/yarn/pull/8223
 
 set +e
-yarn audit --groups dependencies
-result=$?
+today=$(date +%Y-%m-%d)
+enddate="2023-06-15"
 
-yarn audit --groups dependencies --json > yarn-audit-issues-result
+if [[ "$today" > "$enddate" ]]; then
+yarn npm audit --recursive --environment production
+result=$?
+yarn npm audit --recursive --environment production --json > yarn-audit-result
+else
+ yarn npm audit --environment production
+ result=$?
+ yarn npm audit --environment production --json > yarn-audit-result
+fi
 
 set -e
 
 if [ "$result" != 0 ]; then
   if [ -f yarn-audit-known-issues ]; then
     set +e
-    grep auditAdvisory yarn-audit-issues-result > yarn-audit-issues
+    cat yarn-audit-result | jq -cr '.advisories| to_entries[] | {"type": "auditAdvisory", "data": { "advisory": .value }}' > yarn-audit-issues
     set -e
 
     if diff -q yarn-audit-known-issues yarn-audit-issues > /dev/null 2>&1; then
       rm -f yarn-audit-issues
       echo
-      echo Ignorning known vulnerabilities
+      echo Ignoring known vulnerabilities
       exit 0
     fi
   fi
+  cat <<'EOF'
+    Security vulnerabilities were found that were not ignored
 
-  echo
-  echo Security vulnerabilities were found that were not ignored
-  echo
-  echo Check to see if these vulnerabilities apply to production
-  echo and/or if they have fixes available. If they do not have
-  echo fixes and they do not apply to production, you may ignore them
-  echo
-  echo To ignore these vulnerabilities, run:
-  echo
-  echo "yarn audit --groups dependencies --json | grep auditAdvisory > yarn-audit-known-issues"
-  echo
-  echo and commit the yarn-audit-known-issues file
+    Check to see if these vulnerabilities apply to production
+    and/or if they have fixes available. If they do not have
+    fixes and they do not apply to production, you may ignore them
+
+    To ignore these vulnerabilities, run:
+
+    `yarn npm audit --environment production --recursive --json | jq -cr '.advisories| to_entries[] | {"type": "auditAdvisory", "data": { "advisory": .value }}' > yarn-audit-known-issues`
+
+    and commit the yarn-audit-known-issues file
+EOF
 
   rm -f yarn-audit-issues
 
