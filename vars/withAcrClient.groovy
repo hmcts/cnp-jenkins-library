1153fe6579883b524e7292721f689ad57a5c562a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-26 15:51:38 +0100

    remove condition

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -1,8 +1,6 @@
 
 def call(String subscription, Closure block) {
-  env.IS_DOCKER_BUILD_AGENT && env.IS_DOCKER_BUILD_AGENT.toBoolean() {
     doAcrOrKubectlTask(subscription, false, block)
-  }
 }
 
 def doAcrOrKubectlTask(String subscription, boolean alwaysLogin, Closure block) {
