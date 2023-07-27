65d570f595e84fe2459e449740aa880b0abd917c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-27 10:26:17 +0100

    remove suggested block

--- vars/withSubscriptionLogin.groovy
+++ vars/withSubscriptionLogin.groovy
@@ -5,28 +5,19 @@
  * @param body the body to execute after logged in
  */
 def call(String subscription, boolean alwaysExecute, Closure body) {
-  if (!alwaysExecute && env.SUBSCRIPTION_NAME == subscription && fileExists("/opt/jenkins/.azure-${subscription}")) {
-    echo "Using existing az login: /opt/jenkins/.azure-${subscription}"
-    withAzureKeyvault([
-      [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: "${subscription}-subscription-id", version: '', envVariable: 'ARM_SUBSCRIPTION_ID']
-    ]) {
-      body.call()
-    }
-  } else {
-    echo "New az login: /opt/jenkins/.azure-${subscription}"
-    Closure az = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az $cmd", returnStdout: true).trim() }
-    az 'login --identity'
+  echo "New az login: /opt/jenkins/.azure-${subscription}"
+  Closure az = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az $cmd", returnStdout: true).trim() }
+  az 'login --identity'
 
-    withAzureKeyvault([
-      [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: "${subscription}-subscription-id", version: '', envVariable: 'ARM_SUBSCRIPTION_ID']
-    ]) {
-      az "account set --subscription ${env.ARM_SUBSCRIPTION_ID}"
-      def tenantId = az "account show --query tenantId -o tsv"
-      env.SUBSCRIPTION_NAME = subscription
-      env.ARM_TENANT_ID = tenantId
-      env.CURRENT_ARM_SUBSCRIPTION_ID = env.ARM_SUBSCRIPTION_ID
-      body.call()
-    }
+  withAzureKeyvault([
+    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: "${subscription}-subscription-id", version: '', envVariable: 'ARM_SUBSCRIPTION_ID']
+  ]) {
+    az "account set --subscription ${env.ARM_SUBSCRIPTION_ID}"
+    def tenantId = az "account show --query tenantId -o tsv"
+    env.SUBSCRIPTION_NAME = subscription
+    env.ARM_TENANT_ID = tenantId
+    env.CURRENT_ARM_SUBSCRIPTION_ID = env.ARM_SUBSCRIPTION_ID
+    body.call()
   }
 }
 
