3a23c93888c5f85ef2abbcc3c10eec1b80348553
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-27 09:52:03 +0100

    remove doAcrOrKubectlTask

--- vars/withAcrClient.groovy
+++ vars/withAcrClient.groovy
@@ -1,13 +1,5 @@
 
 def call(String subscription, Closure block) {
-  if (env.IS_DOCKER_BUILD_AGENT && env.IS_DOCKER_BUILD_AGENT.toBoolean()) {
-    doAcrOrKubectlTask(subscription, false, block)
-  } else {
-      doAcrOrKubectlTask(subscription, true, block)
-  }
-}
-
-def doAcrOrKubectlTask(String subscription, boolean alwaysLogin, Closure block) {
   withSubscriptionLogin(subscription, alwaysLogin) {
     withRegistrySecrets {
       block.call()
