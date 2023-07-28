f80370baf842976c63c66b4100c0a80a17c0395e
Narinder.Doal <narinder.doal@justice.gov.uk>
2023-07-28 14:14:28 +0100

    update nagger warning typo

--- src/uk/gov/hmcts/contino/AppPipelineDsl.groovy
+++ src/uk/gov/hmcts/contino/AppPipelineDsl.groovy
@@ -81,7 +81,7 @@ class AppPipelineDsl extends CommonPipelineDsl implements Serializable {
   void enableCleanupOfHelmReleaseOnSuccess() {
     config.clearHelmReleaseOnSuccess = true;
 
-        WarningCollector.addPipelineWarning("Helm-ReleaseonSuccess-deprication", "`enableCleanupOfHelmReleaseOnSuccess` is now the default, please remove it from your `Jenkinsfile`.", LocalDate.of(2023, 8, 31));
+        WarningCollector.addPipelineWarning("Helm-ReleaseonSuccess-deprecation", "`enableCleanupOfHelmReleaseOnSuccess` is now the default, please remove it from your `Jenkinsfile`.", LocalDate.of(2023, 8, 31));
     
   }
 
@@ -93,7 +93,7 @@ class AppPipelineDsl extends CommonPipelineDsl implements Serializable {
     config.clearHelmReleaseOnSuccess = true;
     config.clearHelmReleaseOnFailure = true;
 
-        WarningCollector.addPipelineWarning("Helm-ReleaseAlways-deprication", "`enableCleanupOfHelmReleaseAlways` is now the default, please remove it from your `Jenkinsfile`.", LocalDate.of(2023, 8, 31));
+        WarningCollector.addPipelineWarning("Helm-ReleaseAlways-deprecation", "`enableCleanupOfHelmReleaseAlways` is now the default, please remove it from your `Jenkinsfile`.", LocalDate.of(2023, 8, 31));
   
   }
 
