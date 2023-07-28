f47f493bdc675b2706c52cb9dd4ddc2ed6f324b0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-07-28 08:36:33 +0100

    Update vars/withSubscriptionLogin.groovy
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- vars/withSubscriptionLogin.groovy
+++ vars/withSubscriptionLogin.groovy
@@ -4,7 +4,7 @@
  * @param subscription the subscription short name, i.e. sandbox, qa, nonprod, prod
  * @param body the body to execute after logged in
  */
-def call(String subscription, boolean alwaysExecute, Closure body) {
+def call(String subscription, Closure body) {
   echo "New az login: /opt/jenkins/.azure-${subscription}"
   Closure az = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az $cmd", returnStdout: true).trim() }
   az 'login --identity'
