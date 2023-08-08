7eb6797abb8b391d42719ea5d4bc20ca45b73136
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-08 14:59:22 +0100

    Update src/uk/gov/hmcts/contino/GradleBuilder.groovy
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -286,7 +286,7 @@ EOF
     } else {
       WarningCollector.addPipelineWarning("gatling_docker_deprecated",
         "Please use the gatling plugin instead of the docker image " +
-          "See <https://github.com/hmcts/cnp-plum-recipes-service/pull/817/files|example>", LocalDate.of(2023, 8, 1)
+          "See <https://github.com/hmcts/cnp-plum-recipes-service/pull/817/files|example>", LocalDate.of(2023, 9, 1)
       )
       super.executeGatling()
     }
