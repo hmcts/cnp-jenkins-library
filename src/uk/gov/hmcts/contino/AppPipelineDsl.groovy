197c2cd3a7ce3f1ef53dc5c9a12ec4c6fcfb0f3e
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2023-08-11 11:13:52 +0100

    Apply suggestions from code review
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- src/uk/gov/hmcts/contino/AppPipelineDsl.groovy
+++ src/uk/gov/hmcts/contino/AppPipelineDsl.groovy
@@ -79,7 +79,6 @@ class AppPipelineDsl extends CommonPipelineDsl implements Serializable {
   }
 
   void enableCleanupOfHelmReleaseOnSuccess() {
-    config.clearHelmReleaseOnSuccess = true;
 
         WarningCollector.addPipelineWarning("Helm-ReleaseonSuccess-deprecation", "`enableCleanupOfHelmReleaseOnSuccess` is now the default, please remove it from your `Jenkinsfile`.", LocalDate.of(2023, 8, 31));
     
