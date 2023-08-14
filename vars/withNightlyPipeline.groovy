11e467b037b4b105968dd1281a9d247ad9f6e087
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-14 10:19:22 +0100

    use else if condition

--- vars/withNightlyPipeline.groovy
+++ vars/withNightlyPipeline.groovy
@@ -46,7 +46,16 @@ def call(type,product,component,Closure body) {
 
   // def teamConfig = new TeamConfig(this).setTeamConfigEnv(product)
   String agentType = env.BUILD_AGENT_TYPE
-  String nodeSelector = agentType == "" ? "daily" : agentType + ' && daily'
+
+  def nodeSelector {
+    if (agentType = "") {
+        def nodeSelector = "daily" 
+      } else if (agentType = "arm") {
+        def nodeSelector = "arm"
+      } else {
+        def nodeSelector = agentType + ' && daily'
+      }
+  }
 
   node(nodeSelector) {
     timeoutWithMsg(time: 300, unit: 'MINUTES', action: 'pipeline') {
