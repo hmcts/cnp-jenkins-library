0a8238f121bf834ff2d681b4fda51429afdf8d91
Rees Pozzi <reespozzi@icloud.com>
2023-08-15 14:55:10 +0100

    Remove startswith

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -10,7 +10,7 @@ function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
 CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^${DEPRECATED_CHART_NAME}" |awk '{ print $2}' | sed "s/~//g")
 # Only run deprecation checks on non beta/alpha chart versions
-if [[ ! $CURRENT_VERSION =~ ^(alpha|beta).* ]]; then
+if [[ ! $CURRENT_VERSION =~ (alpha|beta).* ]]; then
     if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_CHART_VERSION}) ]; then
         echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${DEPRECATED_CHART_VERSION}"
         exit 1
