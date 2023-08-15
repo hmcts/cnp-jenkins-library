e6a0053fc2c7f735491ef552ef432fc897248540
Rees Pozzi <reespozzi@icloud.com>
2023-08-15 14:33:04 +0100

    Update script to allow beta/alpha versions

--- resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
+++ resources/uk/gov/hmcts/helm/check-deprecated-charts.sh
@@ -9,7 +9,10 @@ function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
 
 CURRENT_VERSION=$(helm dependency ls charts/${CHART_DIRECTORY}/ | grep "^${DEPRECATED_CHART_NAME}" |awk '{ print $2}' | sed "s/~//g")
-if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_CHART_VERSION}) ]; then
-    echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${DEPRECATED_CHART_VERSION}"
-    exit 1
+# Only run deprecation checks on non beta/alpha chart versions
+if [[ ! $CURRENT_VERSION == *"beta"* ]] && [[ ! $CURRENT_VERSION == *"alpha"* ]]; then
+    if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_CHART_VERSION}) ]; then
+        echo "$deprecation chart $CURRENT_VERSION is deprecated, please upgrade to at least ${DEPRECATED_CHART_VERSION}"
+        exit 1
+    fi
 fi
