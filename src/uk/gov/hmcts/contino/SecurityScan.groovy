fe5b288090d26cf00581b9b5764471e652ec590c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-17 15:01:03 +0100

    revert to zap user

--- src/uk/gov/hmcts/contino/SecurityScan.groovy
+++ src/uk/gov/hmcts/contino/SecurityScan.groovy
@@ -3,7 +3,7 @@ package uk.gov.hmcts.contino
 
 class SecurityScan implements Serializable {
     public static final String OWASP_ZAP_IMAGE = 'owasp/zap2docker-stable:2.12.0'
-    public static final String OWASP_ZAP_ARGS = '-u jenkins --name zap -p 1001:1001 -v $WORKSPACE:/zap/wrk/:rw'
+    public static final String OWASP_ZAP_ARGS = '-u zap --name zap -p 1001:1001 -v $WORKSPACE:/zap/wrk/:rw'
     public static final String GLUEIMAGE = 'hmcts/zap-glue:latest'
     public static final String GLUE_ARGS = '-u 0:0 --name=Glue -w $(pwd):/tmp'
     def steps
@@ -14,12 +14,6 @@ class SecurityScan implements Serializable {
 
     def execute() {
         try {
-            this.steps.sh '''
-                containerId=$(docker ps -q -f name=zap)
-                if [[ -n ${containerId} ]]; then
-                    docker container rm $containerId
-                fi
-                '''
             this.steps.withDocker(OWASP_ZAP_IMAGE, OWASP_ZAP_ARGS) {
                 this.steps.sh '''
                     chmod +x security.sh
