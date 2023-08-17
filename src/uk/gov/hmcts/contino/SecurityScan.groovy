8bfd705fa7122c2a5cc773d0f3b4692c20738a04
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-17 10:18:45 +0100

    Revert "DTSPO-14034 - Update owasp2docker image" (#1136)

--- src/uk/gov/hmcts/contino/SecurityScan.groovy
+++ src/uk/gov/hmcts/contino/SecurityScan.groovy
@@ -2,7 +2,7 @@ package uk.gov.hmcts.contino
 
 
 class SecurityScan implements Serializable {
-    public static final String OWASP_ZAP_IMAGE = 'owasp/zap2docker-stable:2.12.0'
+    public static final String OWASP_ZAP_IMAGE = 'owasp/zap2docker-stable:2.11.1'
     public static final String OWASP_ZAP_ARGS = '-u 0:0 --name zap -p 1001:1001 -v $WORKSPACE:/zap/wrk/:rw'
     public static final String GLUEIMAGE = 'hmcts/zap-glue:latest'
     public static final String GLUE_ARGS = '-u 0:0 --name=Glue -w $(pwd):/tmp'
