cbb7e2e8df348836a738acd913d0aaded3c09fae
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-17 12:01:35 +0100

    update security scripts and remove daily label temporarily

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -10,13 +10,9 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
     sleep .1
   done
   echo "ZAP has successfully started"
-  zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
-  zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
-  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
-  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
-  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
+  zap-full-scan.py -t ${TEST_URL} -d -u ${SecurityRules} -P 1001 -l FAIL -r activescan.html
+
   echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
   chown -R $(id -u):$(id -u) activescan.html
   curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.html functional-output/
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
