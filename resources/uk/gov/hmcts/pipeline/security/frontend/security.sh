6288e3cbaddb9e07949204a44b4d4bc4c0e620fb
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-18 15:49:58 +0100

    add -cmd switch

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -7,7 +7,7 @@ echo ${TEST_URL}
 /opt/zap/zap.sh -daemon -host 0.0.0.0 -port 1001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
 -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' \
 -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* \
--config api.addrs.addr.regex=true -quickurl ${TEST_URL} -quickout activescan.html
+-config api.addrs.addr.regex=true -cmd -quickurl ${TEST_URL} -quickout activescan.html
 
 echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
 chown -R $(id -u):$(id -u) activescan.html
