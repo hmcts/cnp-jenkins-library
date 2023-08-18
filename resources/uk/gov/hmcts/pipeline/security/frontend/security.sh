d0c2e7eea401b17a017e9bcbe937b63d9a9e3f0a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-18 14:44:46 +0100

    update port

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -6,12 +6,12 @@ echo ${TEST_URL}
 
 whoami
 
-/opt/zap/zap.sh -daemon -host 0.0.0.0 -port 8080 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
+/opt/zap/zap.sh -daemon -host 0.0.0.0 -port 10001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
 -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' \
 -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* \
 -config api.addrs.addr.regex=true -quickurl ${TEST_URL} -quickout activescan.html
 
 echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
 chown -R $(id -u):$(id -u) activescan.html
-curl --fail http://0.0.0.0:8080/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+curl --fail http://0.0.0.0:10001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp *.html functional-output/
