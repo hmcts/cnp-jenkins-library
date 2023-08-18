5a680399ae2c51bbce3675705ca3c29d78251743
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-18 13:31:47 +0100

    update security.sh frontend

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -4,19 +4,12 @@ export LANG=C.UTF-8
 
 echo ${TEST_URL}
 
-whoami
+/opt/zap/zap.sh -daemon -host 0.0.0.0 -port 1001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
+-config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' \
+-config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* \
+-config api.addrs.addr.regex=true -quickurl ${TEST_URL} -quickout activescan.html
 
-zap-x.sh -daemon -host 0.0.0.0 -port 1001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true &
-i=0
-while !(curl -s http://0.0.0.0:1001) > /dev/null
-  do
-    i=$(( (i+1) %4 ))
-    sleep .1
-  done
-  echo "ZAP has successfully started"
-  zap-full-scan.py -t ${TEST_URL} -P 1001 -l FAIL -r activescan.html -d
-
-  echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-  chown -R $(id -u):$(id -u) activescan.html
-  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-  cp *.html functional-output/
+echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
+chown -R $(id -u):$(id -u) activescan.html
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+cp *.html functional-output/
