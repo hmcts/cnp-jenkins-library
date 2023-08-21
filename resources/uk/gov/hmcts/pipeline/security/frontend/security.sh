2573c326eb8c40cf5dddd9232634735421874c90
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-21 09:29:55 +0100

    update directory

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -12,9 +12,9 @@ fi
 sudo /opt/zap/zap.sh -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
 -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' \
 -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* \
--config api.addrs.addr.regex=true -cmd -quickurl ${TEST_URL} -quickout activescan.html
+-config api.addrs.addr.regex=true -cmd -quickurl ${TEST_URL} -quickout ${WORKSPACE}/activescan.html
 
 echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-chown -R $(id -u):$(id -u) activescan.html
+chown -R $(id -u):$(id -u) ${WORKSPACE}/activescan.html
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp *.html functional-output/
