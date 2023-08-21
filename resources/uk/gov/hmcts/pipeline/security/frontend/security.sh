ae574a9cfc65d1c4fc26984d7118ecf653be2d0e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-21 09:19:07 +0100

    revert earlier change

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -9,7 +9,7 @@ else
   echo ${TEST_URL}
 fi
 
-/opt/zap/zap.sh -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
+sudo /opt/zap/zap.sh -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
 -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' \
 -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* \
 -config api.addrs.addr.regex=true -cmd -quickurl ${TEST_URL} -quickout activescan.html
