f243511e31e0fd9298a8147d84f0b81c8a7d952d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-22 08:42:11 +0100

    use daemon

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -9,14 +9,23 @@ else
   echo ${TEST_URL}
 fi
 
-sudo /opt/zap/zap.sh -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
+sudo /opt/zap/zap.sh -host 0.0.0.0 -port 1001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true \
 -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' \
 -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* \
--config api.addrs.addr.regex=true -cmd -quickurl ${TEST_URL} -quickout ${WORKSPACE}/activescan.html
+-config api.addrs.addr.regex=true &
+
+i=0
+while !(curl -s http://0.0.0.0:1001) > /dev/null
+  do
+    i=$(( (i+1) %4 ))
+    sleep .1
+  done
+
+sudo /opt/zap/zap.sh -cmd -quickurl ${TEST_URL} -quickout ${WORKSPACE}/activescan.html
 
 echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
 sudo chown -R $(id -u):$(id -u) ${WORKSPACE}/activescan.html
-# curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output ${WORKSPACE}/report.json
 mkdir -p ${WORKSPACE}/functional-output
 sudo chmod a+wx ${WORKSPACE}/functional-output
 cp ${WORKSPACE}/*.html ${WORKSPACE}/functional-output/
