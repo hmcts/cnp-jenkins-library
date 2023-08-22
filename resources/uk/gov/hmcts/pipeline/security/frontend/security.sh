020a22be9eac1d5b92ae92424e6522086eaa3c4a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-22 10:09:06 +0100

    initial commit

--- resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/frontend/security.sh
@@ -2,6 +2,10 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+echo ${TEST_URL}
+
+whoami
+
 zap-x.sh -daemon -host 0.0.0.0 -port 1001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config globalexcludeurl.url_list.url.regex='^https?:\/\/.*\/(?:.*login.*)+$' -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true &
 i=0
 while !(curl -s http://0.0.0.0:1001) > /dev/null
@@ -10,13 +14,11 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
     sleep .1
   done
   echo "ZAP has successfully started"
-  zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
-  zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
-  zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
-  zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
-  zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
+  zap-full-scan.py -t ${TEST_URL} -P 1001 -l FAIL -r /zap/wrk/activescan.html -d
+
   echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-  chown -R $(id -u):$(id -u) activescan.html
-  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-  cp *.html functional-output/
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
+  chown -R $(id -u):$(id -u) /zap/wrk/activescan.html
+  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output /zap/wrk/report.json
+  mkdir -p /zap/wrk/functional-output
+  chmod a+wx /zap/wrk/functional-output
+  cp /zap/wrk/*.html /zap/wrk//functional-output/
