b30f744842c356e7a9bb2547ed8b9490c0c8af53
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-24 11:23:19 +0100

    remove docker image azcopy

--- vars/azureBlobUpload.groovy
+++ vars/azureBlobUpload.groovy
@@ -1,5 +1,4 @@
 def call(String credentialsId, String source, String destination) {
-  withDocker('hmcts/moj-azcopy-image:7.2.0-netcore-1.0', '-u root') {
     withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'STORAGE_ACCOUNT_KEY', usernameVariable: 'STORAGE_ACCOUNT_NAME')]) {
       withEnv(["SOURCE=${source}", "DESTINATION=${destination}"]) {
         sh 'azcopy --source ${SOURCE} \
@@ -8,5 +7,4 @@ def call(String credentialsId, String source, String destination) {
           --set-content-type --recursive'
       }
     }
-  }
 }
