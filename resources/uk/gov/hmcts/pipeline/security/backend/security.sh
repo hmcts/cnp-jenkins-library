113b65a658520075b5a06ec17d6865937c3f4bb2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-24 15:21:33 +0100

    update backend script

--- resources/uk/gov/hmcts/pipeline/security/backend/security.sh
+++ resources/uk/gov/hmcts/pipeline/security/backend/security.sh
@@ -6,11 +6,19 @@ if [[ -z ${SecurityRules} ]]; then
   exit 1
 fi
 
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -z "-config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt" -l FAIL -r /zap/wrk/api-report.html
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-mkdir -p functional-output
-chmod a+wx functional-output
-cp /zap/wrk/api-report.html functional-output/
-cp *.* functional-output/
+zap-x.sh -daemon -host 0.0.0.0 -port 1001 -config rules.cookie.ignorelist=_ga,_gid,_gat,dtCookie,dtLatC,dtPC,dtSa,rxVisitor,rxvt &
+i=0
+while !(curl -s http://0.0.0.0:1001) > /dev/null
+  do
+    i=$(( (i+1) %4 ))
+    sleep .1
+  done
+  echo "ZAP has successfully started"
+  zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL -r /zap/wrk/api-report.html
+  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+  export LC_ALL=C.UTF-8
+  export LANG=C.UTF-8
+  mkdir -p functional-output
+  chmod a+wx functional-output
+  cp /zap/wrk/api-report.html functional-output/
+  cp *.* functional-output/
