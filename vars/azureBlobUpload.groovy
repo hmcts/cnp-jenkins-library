9a0fe51f8d99d005f21e17b0bef6f37bafecabb0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-24 12:10:45 +0100

    fix typo

--- vars/azureBlobUpload.groovy
+++ vars/azureBlobUpload.groovy
@@ -1,7 +1,7 @@
 def call(String credentialsId, String source, String destination) {
     withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'STORAGE_ACCOUNT_KEY', usernameVariable: 'STORAGE_ACCOUNT_NAME')]) {
       withEnv(["SOURCE=${source}", "DESTINATION=${destination}"]) {
-        sh 'chmod +x /usr/bin/azcopy && \
+        sh 'sudo chmod +x /usr/bin/azcopy && \
         azcopy --source ${SOURCE} \
           --destination https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${DESTINATION} \
           --dest-key ${STORAGE_ACCOUNT_KEY} \
