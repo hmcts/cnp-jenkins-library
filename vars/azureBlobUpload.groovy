e613b17807b724d5a34073ea9e8e879b7f04ca6a
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-08-25 15:00:12 +0100

    test without azcopy login

--- vars/azureBlobUpload.groovy
+++ vars/azureBlobUpload.groovy
@@ -2,7 +2,7 @@ def call(String subscription, String credentialsId, String source, String destin
     withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'STORAGE_ACCOUNT_KEY', usernameVariable: 'STORAGE_ACCOUNT_NAME')]) {
       withSubscriptionLogin(subscription) {
         withEnv(["SOURCE=${source}", "DESTINATION=${destination}"]) {
-          sh 'azcopy login --identity && \
+          sh 'export AZCOPY_AUTO_LOGIN_TYPE=MSI && \
           azcopy cp ${SOURCE} \
             https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${DESTINATION} \
             --recursive=true'
