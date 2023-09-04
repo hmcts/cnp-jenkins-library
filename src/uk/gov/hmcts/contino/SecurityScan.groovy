31b65726f6ab844c97e7e3b933aabcb09113619c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-09-04 12:24:16 +0100

    Update SecurityScan.groovy

--- src/uk/gov/hmcts/contino/SecurityScan.groovy
+++ src/uk/gov/hmcts/contino/SecurityScan.groovy
@@ -4,8 +4,8 @@ package uk.gov.hmcts.contino
 class SecurityScan implements Serializable {
     public static final String OWASP_ZAP_IMAGE = 'owasp/zap2docker-stable:2.11.1'
     public static final String OWASP_ZAP_ARGS = '-u 0:0 --name zap -p 1001:1001 -v $WORKSPACE:/zap/wrk/:rw'
-    public static final String GLUEIMAGE = 'hmctspublic.azurecr.io/zap-glue:c14eff2a-1692784552'
-    public static final String GLUE_ARGS = '-u 0:0 --name=Glue -v ${WORKSPACE}:/tmp -w /tmp'
+    public static final String GLUEIMAGE = 'hmcts/zap-glue:latest'
+    public static final String GLUE_ARGS = '-u 0:0 --name=Glue -w $(pwd):/tmp'
     def steps
 
     SecurityScan(steps) {
