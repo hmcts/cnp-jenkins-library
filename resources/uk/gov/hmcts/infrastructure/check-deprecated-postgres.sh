eade1881e57e0d24b65825ebca7fd76f51901c81
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-09-06 15:32:05 +0100

    update script

--- resources/uk/gov/hmcts/infrastructure/check-deprecated-postgres.sh
+++ resources/uk/gov/hmcts/infrastructure/check-deprecated-postgres.sh
@@ -2,7 +2,7 @@
 
 grep -R --quiet "hmcts/cnp-module-postgres" *
 
-if grep -R --quiet "hmcts/cnp-module-postgres" *; then
+if grep -R --quiet "hmcts/cnp-module-postgres"; then
   echo "====================================================================================================="
   echo "=== You appear to be using the cnp-module-postgres terraform module ==="
   echo "=== This module has been deprecated as Postgres v11 is going end of life in November 2023 ===="
