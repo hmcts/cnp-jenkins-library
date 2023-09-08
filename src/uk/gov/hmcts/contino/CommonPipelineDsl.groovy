07bb274ace60709645db5c9bf10578912c58d079
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-09-08 10:56:49 +0100

    Discard changes to src/uk/gov/hmcts/contino/CommonPipelineDsl.groovy

--- src/uk/gov/hmcts/contino/CommonPipelineDsl.groovy
+++ src/uk/gov/hmcts/contino/CommonPipelineDsl.groovy
@@ -59,7 +59,4 @@ abstract class CommonPipelineDsl implements Serializable {
   void expires(String expiresAfter) {
     config.expiryDate = expiresAfter
   }
-  void autoShutdown(Boolean autoShutdown) {
-    config.autoShutdown = autoShutdown
-  }
 }
