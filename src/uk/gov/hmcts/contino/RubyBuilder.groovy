2914b3309a37725f5299b8bfb202dde39e57ceeb
Ben Neill <benjaminrneill@gmail.com>
2023-09-08 12:20:41 +0100

    Update RubyBuilder.groovy

--- src/uk/gov/hmcts/contino/RubyBuilder.groovy
+++ src/uk/gov/hmcts/contino/RubyBuilder.groovy
@@ -99,12 +99,10 @@ class RubyBuilder extends AbstractBuilder {
     steps.echo("here")
     steps.sh(script: """#!/bin/bash -l
       source /usr/local/rvm/scripts/rvm
-      rvm install $(< .ruby-version)
       rvm use
       """, label: "rvm use")
     steps.sh(script: """#!/bin/bash -l
       source /usr/local/rvm/scripts/rvm
-      rvm install $(< .ruby-version)
       rvm use
 
       bundle ${task}
