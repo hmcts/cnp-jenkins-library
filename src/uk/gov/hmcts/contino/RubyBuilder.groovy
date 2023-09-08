cbdfa88211f3f3b7281520c6805739ae78224588
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-09-08 12:31:24 +0100

    try

--- src/uk/gov/hmcts/contino/RubyBuilder.groovy
+++ src/uk/gov/hmcts/contino/RubyBuilder.groovy
@@ -99,7 +99,7 @@ class RubyBuilder extends AbstractBuilder {
     steps.echo("here")
     steps.sh(script: """#!/bin/bash -l
       source /usr/local/rvm/scripts/rvm
-      rvm install
+      rvm install \$(cat .ruby-version)
       rvm use
       """, label: "rvm use")
     steps.sh(script: """#!/bin/bash -l
