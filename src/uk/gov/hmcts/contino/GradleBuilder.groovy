1e07a5fc9504c04e09ed19f13a4724b3b73676b3
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-09-14 08:59:32 +0100

    add new logic for gradle frontend

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -264,7 +264,11 @@ EOF
     } else if (localSteps.fileExists("security.sh")) {
       WarningCollector.addPipelineWarning("security.sh_moved", "Please remove security.sh from root of repository, no longer needed as it has been moved to the Jenkins library", LocalDate.of(2023, 04, 17))
     } else {
-      localSteps.writeFile(file: 'security.sh', text: localSteps.libraryResource('uk/gov/hmcts/pipeline/security/backend/security.sh'))
+      if (localSteps.env.IS_FRONTEND = "true") {
+        localSteps.writeFile(file: 'security.sh', text: localSteps.libraryResource('uk/gov/hmcts/pipeline/security/frontend/security.sh'))
+      } else {
+        localSteps.writeFile(file: 'security.sh', text: localSteps.libraryResource('uk/gov/hmcts/pipeline/security/backend/security.sh'))
+      }
     }
     this.securitytest.execute()
   }
