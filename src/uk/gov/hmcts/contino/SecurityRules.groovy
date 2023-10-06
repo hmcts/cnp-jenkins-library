d17385677f81614417f7fdd5c6b00cde036ade02
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-06 14:39:41 +0100

    update class

--- src/uk/gov/hmcts/contino/SecurityRules.groovy
+++ src/uk/gov/hmcts/contino/SecurityRules.groovy
@@ -13,7 +13,7 @@ class SecurityRules {
 
   def getSecurityRules() {
     def response = steps.httpRequest(httpMode: 'GET',
-      acceptType: 'APPLICATION_JSON'
+      acceptType: 'APPLICATION_JSON',
       url: "https://raw.githubusercontent.com/hmcts/security-test-rules/master/conf/security-rules.conf")
     return response
   }
