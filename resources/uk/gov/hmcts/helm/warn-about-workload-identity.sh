0ee484fbbb2e436549379c6953c7b4c0078886ff
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-10-06 15:59:03 +0100

    Remove search for workload flag
    This is now enabled by default in base charts, not in each apps values.yaml file

--- resources/uk/gov/hmcts/helm/warn-about-workload-identity.sh
+++ resources/uk/gov/hmcts/helm/warn-about-workload-identity.sh
@@ -2,21 +2,12 @@
 
 CHART_DIRECTORY=${1}-${2}
 cd charts/"${CHART_DIRECTORY}" || exit 10
-
-grep --quiet "useWorkloadIdentity: true" values.yaml
-
+# Check if app still has aad config
+grep --quiet  "aadIdentityName:" values.yaml
 if [ $? -gt 0 ]; then
-  echo "==========================================================================================================="
-  echo "=====  Please migrate your application to use workload identity, missing useWorkloadIdentity: true.  ======"
-  echo "==========================================================================================================="
-  exit 1
+  echo "Application has removed aad pod binding references."
 else
-  grep --quiet  "aadIdentityName:" values.yaml
-  if [ $? -gt 0 ]; then
-    echo "Application has workload identity enabled."
-  else
-    echo "===================================================================================================================="
-    echo "========  Please migrate your application to use workload identity, app is still using aadIdentityName flag.  ======"
-    echo "===================================================================================================================="
-  fi
+  echo "===================================================================================================================="
+  echo "========  Please migrate your application to use workload identity, app is still using aadIdentityName flag.  ======"
+  echo "===================================================================================================================="
 fi
