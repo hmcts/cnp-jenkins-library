3451853ebcbcda3cce88c1064032d9a16f56919a
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-10-09 11:20:03 +0100

    Update to search all yaml files for flag

--- resources/uk/gov/hmcts/helm/warn-about-workload-identity.sh
+++ resources/uk/gov/hmcts/helm/warn-about-workload-identity.sh
@@ -2,8 +2,8 @@
 
 CHART_DIRECTORY=${1}-${2}
 cd charts/"${CHART_DIRECTORY}" || exit 10
-# Check if app still has aad config
-grep --quiet  "aadIdentityName:" values.yaml
+# Check if app still has aad config in any of it's yaml files
+grep -r --quiet  "aadIdentityName:" . --include=*.yaml
 if [ $? -gt 0 ]; then
   echo "Application has removed aad pod binding references."
 else
