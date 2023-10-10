7ca9b82dea065b444a5a2dddded529dbc8740c5f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-10 16:40:36 +0100

    update nightly tests

--- vars/sectionNightlyTests.groovy
+++ vars/sectionNightlyTests.groovy
@@ -99,7 +99,7 @@ def call(PipelineCallbacksRunner pcr, AppPipelineConfig config, PipelineType pip
         warnError('Failure in securityScan') {
           pcr.callAround('securityScan') {
             timeout(time: config.securityScanTimeout, unit: 'MINUTES') {
-              builder.securityScan()
+              builder.securityScan(urlExclusions, isFrontend)
             }
           }
         }
