22dfe3485bb86fcb5c6bcfa751093880061d8e81
Rees Pozzi <rees.pozzi@justice.gov.uk>
2023-10-10 14:17:44 +0100

    Revert "DTSPO-12678 Add workload identity nagger to pipeline (#1056)" (#1163)
    This reverts commit ec69979bf2b1df02f06f0d7f5d78dd43a42e0e99.

---
+++ vars/warnAboutAADIdentityPreviewHack.groovy
@@ -0,0 +1,15 @@
+import uk.gov.hmcts.pipeline.deprecation.WarningCollector
+
+def call(Map<String, String> params) {
+  def product = params.product
+  def component = params.component
+
+  writeFile file: 'warn-about-aad-identity-preview-hack.sh', text: libraryResource('uk/gov/hmcts/helm/warn-about-aad-identity-preview-hack.sh')
+
+  sh """
+    chmod +x warn-about-aad-identity-preview-hack.sh
+    ./warn-about-aad-identity-preview-hack.sh $product $component
+  """
+
+  sh 'rm warn-about-aad-identity-preview-hack.sh'
+}
