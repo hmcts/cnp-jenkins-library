8ab24738c4c6db877dc30f70cf4fadd13f25febe
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-11 15:49:49 +0100

    update gradleBuilder

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -269,7 +269,7 @@ EOF
   //   this.securitytest.execute()
   // }
 
-  def securityScan(){
+  def securityScan(args){
     if (steps.fileExists(".ci/security.sh")) {
       // hook to allow teams to override the default `security.sh` that we provide
       steps.writeFile(file: 'security.sh', text: steps.readFile('.ci/security.sh'))
