6650458f83469f6dd4f1dff673493da0b4d26a47
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-11 15:02:50 +0100

    test

--- vars/sectionNightlyTests.groovy
+++ vars/sectionNightlyTests.groovy
@@ -94,7 +94,7 @@ def call(PipelineCallbacksRunner pcr, AppPipelineConfig config, PipelineType pip
       }
     }
 
-    if (config.securityScan) {
+    if (!config.securityScan) {
       stageWithAgent('Security scan', product) {
         warnError('Failure in securityScan') {
           pcr.callAround('securityScan') {
