a84fc4fb79a51ba3134a7b850da6a9468255be2a
Jason Paige <j@jasonpaige.co.uk>
2023-10-11 10:20:47 +0100

    Test Config changes in branch

--- src/uk/gov/hmcts/pipeline/TeamConfig.groovy
+++ src/uk/gov/hmcts/pipeline/TeamConfig.groovy
@@ -45,7 +45,7 @@ class TeamConfig {
       def response = steps.httpRequest(
         consoleLogResponseBody: true,
         timeout: 10,
-        url: "https://raw.githubusercontent.com/hmcts/${repo}/master/team-config.yml",
+        url: "https://raw.githubusercontent.com/hmcts/${repo}/DTSSE-3586/onboard-teams-to-ardoq/team-config.yml",
         validResponseCodes: '200'
       )
       teamConfigMap = steps.readYaml (text: response.content)
