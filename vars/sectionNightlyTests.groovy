9cc719fde4a8a0bce5fc7c7f4795f192aa330854
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-12 09:27:04 +0100

    add another function

--- vars/sectionNightlyTests.groovy
+++ vars/sectionNightlyTests.groovy
@@ -94,7 +94,19 @@ def call(PipelineCallbacksRunner pcr, AppPipelineConfig config, PipelineType pip
       }
     }
 
-    if (config.securityScan) {
+    if (config.securityScan && config.isFrontend == false) {
+      stageWithAgent('Security scan', product) {
+        warnError('Failure in securityScan') {
+          pcr.callAround('securityScan') {
+            timeout(time: config.securityScanTimeout, unit: 'MINUTES') {
+              builder.securityScan()
+            }
+          }
+        }
+      }
+    }
+
+    if (config.securityScan && config.isFrontend == true) {
       stageWithAgent('Security scan', product) {
         warnError('Failure in securityScan') {
           pcr.callAround('securityScan') {
