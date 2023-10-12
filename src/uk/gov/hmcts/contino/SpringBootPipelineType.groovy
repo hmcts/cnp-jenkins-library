e3a4252111bf27074d70ba1e0f12500b2650452b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-12 15:03:16 +0100

    add new function

--- src/uk/gov/hmcts/contino/SpringBootPipelineType.groovy
+++ src/uk/gov/hmcts/contino/SpringBootPipelineType.groovy
@@ -4,18 +4,16 @@ class SpringBootPipelineType implements PipelineType, Serializable {
   def steps
   def product
   def app
-  def isFrontend
 
   Builder builder
 
-  SpringBootPipelineType(steps, product, app, isFrontend = null) {
+  SpringBootPipelineType(steps, product, app) {
     this.steps = steps
     this.product = product
     this.app = app
     this.isFrontend = isFrontend
 
     this.steps.env.PRODUCT = product
-    this.steps.env.IS_FRONTEND = isFrontend
 
     builder = new GradleBuilder(steps, product)
   }
