5c8b5c8a738d6a1f5a3664608c0e45e7ea29db66
David Jones <dave.jones@hmcts.net>
2023-10-19 10:29:22 +0100

    Fee and Pay Node Extension deadline date.

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -325,7 +325,7 @@ EOF
       case "bar":
       case "fees-register":
       case "ccpay":
-        date = LocalDate.of(2023, 10, 21)
+        date = LocalDate.of(2023, 10, 31)
         break
       default:
         date = NODEJS_EXPIRATION
