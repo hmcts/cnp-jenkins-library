67e74acdffe952d62d360b922431b0d841bd62c3
Dave Jones <38425925+davejones74@users.noreply.github.com>
2023-11-03 14:38:08 +0000

    Fee and Pay NodeJS 18 Ext 18 Nov (#1176)
    Co-authored-by: Thor-tech-of-metal <it.consulting@tutanota.com>

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -329,7 +329,7 @@ EOF
       case "bar":
       case "fees-register":
       case "ccpay":
-        date = LocalDate.of(2023, 10, 31)
+        date = LocalDate.of(2023, 11, 18)
         break
       default:
         date = NODEJS_EXPIRATION
