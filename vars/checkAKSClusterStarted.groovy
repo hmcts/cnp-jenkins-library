de731a31a56f1c1399598973045e25a11a20fc00
Rees Pozzi <reespozzi@icloud.com>
2023-11-06 15:32:11 +0000

    DSTPO-15108: Wait for workflow to run

--- vars/checkAKSClusterStarted.groovy
+++ vars/checkAKSClusterStarted.groovy
@@ -5,19 +5,20 @@ def call(Map params) {
   def environment = params.environment
   def subscription = params.subscription  
   def subscription_name = params.aksSubscription.name
-
-  def azCommand = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az $cmd", returnStdout: true).trim() }
   def business_area = env.BUSINESS_AREA_TAG
   def cluster_rg = env.AKS_RESOURCE_GROUP
   def cluster_name = env.AKS_CLUSTER_NAME
 
+
   log.info("Checking AKS Environment: $environment, Subscription is $subscription_name, in business area: $business_area")
+  def azCommand = { cmd -> return sh(script: "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-jenkins az $cmd", returnStdout: true).trim() }
   azCommand 'login --identity > /dev/null'
   azCommand "account set -s $subscription_name"
 
   // Initial check to see if env is up by checking cluster status
   def cluster_data = azCommand "aks show -n ${cluster_name} -g ${cluster_rg} -o json"
   def cluster_status = new JsonSlurper().parseText(cluster_data).powerState.code
+
   // Extract cluster number 
   def cluster_number = cluster_name[-6..-5]
   // Workflow only accepts sbox as a parameter
@@ -25,9 +26,11 @@ def call(Map params) {
 
   if( cluster_status == "Running" ){
     println "Cluster is running, continue pipeline"
+  } else {
+    println "AKS Cluster in stopped state - starting environment"
     GithubAPI gitHubAPI = new GithubAPI(this)
     gitHubAPI.startAksEnvironmentWorkflow("manual-start.yaml", "${business_area}", "${cluster_number}", "${environment}")
-  } else {
-    println "AKS Cluster in stopped state"
+    // Wait 5 minutes env to start up
+    sleep(5 * 60000)
   }
 }
