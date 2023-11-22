7eea73530e76ab2ae21c5a3570979c75133b3ed7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-11-22 08:30:14 +0000

    remove dollar

--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -30,7 +30,7 @@ def call(DockerImage dockerImage, Map params) {
   EnvironmentDnsConfigEntry dnsConfigEntry = new EnvironmentDnsConfig(this).getEntry(params.environment, product, component)
   AzPrivateDns azPrivateDns = new AzPrivateDns(this, params.environment, dnsConfigEntry)
   String serviceFqdn = azPrivateDns.getHostName(aksServiceName)
-  // def hasCname = azPrivateDns.checkForCname()
+  def hasCname = azPrivateDns.checkForCname()
 
   if (serviceFqdn.endsWith(".internal")) {
     WarningCollector.addPipelineWarning("internal_domain", "Usage of `.internal` URLs for preview and AAT Staging is deprecated, please see https://hmcts-reform.slack.com/archives/CA4F2MAFR/p1675868743958669", LocalDate.of(2023, 04, 26))
@@ -108,7 +108,7 @@ def call(DockerImage dockerImage, Map params) {
     
     def disableTraefikTls
 
-    if(${hasCname} == true) {
+    if hasCname == true {
       disableTraefikTls = true
     } else disableTraefikTls = false
     
