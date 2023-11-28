10c0d29884ae1f17d4bb8f4ff444cdf30979c6cf
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-11-28 12:16:52 +0000

    update commands

--- src/uk/gov/hmcts/contino/AzPrivateDns.groovy
+++ src/uk/gov/hmcts/contino/AzPrivateDns.groovy
@@ -60,6 +60,8 @@ class AzPrivateDns {
       return zone
     }
 
+  def ttl = ttlIsSet.ttl
+
    def checkForCname(recordName) {
 
     try {
@@ -92,8 +94,8 @@ class AzPrivateDns {
       // if no A record or CNAME already exists, create an A record pointing to the private IP
       if (!aRecordSet) {
         if (cnameExists == false) {
-          this.steps.echo "Registering DNS for ${recordName} to ${serviceIP} with ttl = ${ttlIsSet}"
-          this.az.az "network private-dns record-set a create -g ${resourceGroup} -z ${zone} -n ${recordName} --ttl ${ttlIsSet} --subscription ${subscription}"
+          this.steps.echo "Registering DNS for ${recordName} to ${serviceIP} with ttl = ${ttl}"
+          this.az.az "network private-dns record-set a create -g ${resourceGroup} -z ${zone} -n ${recordName} --ttl ${ttl} --subscription ${subscription}"
           this.az.az "network private-dns record-set a add-record -g ${resourceGroup} -z ${zone} -n ${recordName} -a ${serviceIP} --subscription ${subscription}"
         } else {
           this.steps.echo "CNAME already exists for ${recordName}"
