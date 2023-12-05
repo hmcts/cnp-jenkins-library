38a4d8d2190e767decfbf479c76f624e28f42988
Rees Pozzi <reespozzi@icloud.com>
2023-12-05 16:13:52 +0000

    Add catch block for logging

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -139,6 +139,10 @@ class GradleBuilder extends AbstractBuilder {
           WarningCollector.addPipelineWarning("deprecate_owasp_8", "Older versions of Owasp dependency check  plugin are not supported, please move to latest 9.x.", new Date().parse("dd.MM.yyyy", "15.12.2023"))
           gradle("--stacktrace -DdependencyCheck.failBuild=true -Dcve.check.validforhours=24 -Danalyzer.central.enabled=false -Ddata.driver_name='org.postgresql.Driver' -Ddata.connection_string='${localSteps.env.OWASPDB_V14_CONNECTION_STRING}' -Ddata.user='${localSteps.env.OWASPDB_V14_ACCOUNT}' -Ddata.password='${localSteps.env.OWASPDB_V14_PASSWORD}'  -Danalyzer.retirejs.enabled=false -Danalyzer.ossindex.enabled=false dependencyCheckAggregate")
         }
+      } catch( Exception ex) {
+         println(ex.toString());
+         println(ex.getMessage());
+         println(ex.getStackTrace());
       } finally {
         localSteps.archiveArtifacts 'build/reports/dependency-check-report.html'
         String dependencyReport = localSteps.readFile('build/reports/dependency-check-report.json')
