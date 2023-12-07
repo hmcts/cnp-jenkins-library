cef929f0cad81a29b361c031719bfc94fb6b60eb
Rees Pozzi <reespozzi@icloud.com>
2023-12-07 12:14:56 +0000

    Change print statement

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -132,8 +132,8 @@ class GradleBuilder extends AbstractBuilder {
   }
 
   def hasPlugin(String pluginName) {
-    println("print buildenv")
-    println(gradleWithOutput("buildEnvironment").contains(pluginName))
+    println "print buildenv"
+    println gradleWithOutput("buildEnvironment").contains(pluginName)
     return gradleWithOutput("buildEnvironment").contains(pluginName)
   }
 
