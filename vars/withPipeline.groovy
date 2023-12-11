56355e7bebef6997967f7aa23617565c63d324e9
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-12-11 09:45:55 +0000

    restore file

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -63,8 +63,7 @@ def call(type, String product, String component, Closure body) {
   Environment environment = new Environment(env)
 
   def teamConfig = new TeamConfig(this).setTeamConfigEnv(product)
-  // String agentType = env.BUILD_AGENT_TYPE
-  String agentType = "arm"
+  String agentType = env.BUILD_AGENT_TYPE
 
   node(agentType) {
     timeoutWithMsg(time: 180, unit: 'MINUTES', action: 'pipeline') {
