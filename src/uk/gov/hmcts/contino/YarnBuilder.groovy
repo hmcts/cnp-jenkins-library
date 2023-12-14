9d9c24e41268ae2e302d0ef69b2d5d649cb21225
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-12-14 12:40:08 +0000

    update cache value

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -128,7 +128,8 @@ class YarnBuilder extends AbstractBuilder {
       steps.sh """
         set +ex
         export NVM_DIR='/home/jenkinsssh/.nvm' # TODO get home from variable
-        export YARN_ENABLE_GLOBAL_CACHE=true
+        export YARN_GLOBAL_FOLDER=/opt/.yarn
+        export YARN_ENABLE_GLOBAL_CACHE=false
         . /opt/nvm/nvm.sh || true
         nvm install
         set -ex
@@ -280,7 +281,8 @@ EOF
         set -ex
 
         export PATH=\$HOME/.local/bin:\$PATH
-        export YARN_ENABLE_GLOBAL_CACHE=true
+        export YARN_GLOBAL_FOLDER=/opt/.yarn
+        export YARN_ENABLE_GLOBAL_CACHE=false
 
         if ${prepend.toBoolean()}; then
           ${prepend}yarn ${task}
@@ -291,7 +293,8 @@ EOF
     } else {
       steps.sh("""
         export PATH=\$HOME/.local/bin:\$PATH
-        export YARN_ENABLE_GLOBAL_CACHE=true
+        export YARN_GLOBAL_FOLDER=/opt/.yarn
+        export YARN_ENABLE_GLOBAL_CACHE=false
 
         if ${prepend.toBoolean()}; then
           ${prepend}yarn ${task}
@@ -308,7 +311,8 @@ EOF
     }
     def status = steps.sh(script: """
       export PATH=\$HOME/.local/bin:\$PATH
-      export YARN_ENABLE_GLOBAL_CACHE=true
+      export YARN_GLOBAL_FOLDER=/opt/.yarn
+      export YARN_ENABLE_GLOBAL_CACHE=false
 
       if ${prepend.toBoolean()}; then
         ${prepend}yarn ${task} 1> /dev/null 2> /dev/null
