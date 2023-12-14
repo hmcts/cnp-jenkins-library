ae5ec898781cdc98ecef4e191cbb3d86ab0eae39
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-12-14 11:54:27 +0000

    unset vars

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -27,6 +27,14 @@ class Acr extends Az {
     this.registrySubscription = registrySubscription
   }
 
+  def unsetYarnVariables() {
+    steps.sh(
+      script: "unset YARN_GLOBALFOLDER && unset YARN_ENABLE_GLOBAL_CACHE",
+      returnStdout: true
+    )
+    this.run()
+  }
+
   /**
    * Log into ACR.  Can be used instead of 'docker login'.  You need to be logged into a subscription first.
    *
