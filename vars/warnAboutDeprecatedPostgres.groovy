1cb0028d6bde60a126e74b98d95212f286d7e892
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-01-31 15:15:47 +0000

    adding new swtich logic for extending postgres migration deadline for specific repos

--- vars/warnAboutDeprecatedPostgres.groovy
+++ vars/warnAboutDeprecatedPostgres.groovy
@@ -3,6 +3,19 @@ import uk.gov.hmcts.pipeline.deprecation.WarningCollector
 import java.time.LocalDate
 
 def call() {
+  switch (env.GIT_URL) {
+      case "https://github.com/HMCTS/am-org-role-mapping-service.git":
+      case "https://github.com/hmcts/am-role-assignment-service.git":
+      case "https://github.com/hmcts/am-judicial-booking-service.git":
+        expiryDate = LocalDate.of(2024, 03, 31)
+        break
+      case "https://github.com/hmcts/wa-case-event-handler.git":
+        expiryDate = LocalDate.of(2024, 02, 9)
+        break
+      default:
+        expiryDate = LocalDate.of(2024, 01, 31)
+        break
+    }
 
   writeFile file: 'check-deprecated-postgres.sh', text: libraryResource('uk/gov/hmcts/infrastructure/check-deprecated-postgres.sh')
 
@@ -12,7 +25,7 @@ def call() {
     ./check-deprecated-postgres.sh
     """
   } catch(ignored) {
-    WarningCollector.addPipelineWarning("deprecated_postgres", "Please migrate to the flexible server postgres module. See this Slack announcement for more info https://hmcts-reform.slack.com/archives/CA4F2MAFR/p1692714862133249", LocalDate.of(2024, 01, 31))
+    WarningCollector.addPipelineWarning("deprecated_postgres", "Please migrate to the flexible server postgres module. See this Slack announcement for more info https://hmcts-reform.slack.com/archives/CA4F2MAFR/p1692714862133249", expiryDate)
   } finally {
     sh 'rm -f check-deprecated-postgres.sh'
   }
