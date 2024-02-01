fcbfcaeae451e0a79e66398e883a5b0ca214f882
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-02-01 15:16:52 +0000

    Update postgres (#1202)
    * Update postgres
    
    * Update warnAboutDeprecatedPostgres.groovy

--- vars/warnAboutDeprecatedPostgres.groovy
+++ vars/warnAboutDeprecatedPostgres.groovy
@@ -18,6 +18,12 @@ switch (gitUrl.toLowerCase()) {
     case "https://github.com/hmcts/ts-translation-service.git":
         expiryDate = LocalDate.of(2024, 3, 31);
         break;
+    case "https://github.com/hmcts/ethos-repl-docmosis-service.git":
+    case "https://github.com/hmcts/ecm-shared-infrastructure.git":
+    case "https://github.com/hmcts/et-ccd-callbacks.git":
+    case "https://github.com/hmcts/et-message-handler.git":
+        expiryDate = LocalDate.of(2024, 2, 29);
+        break;
     case "https://github.com/hmcts/wa-case-event-handler.git":
         expiryDate = LocalDate.of(2024, 2, 12);
         break;
