ceb0d1874043ec8ff541fdbc1f2f409a144aea90
Jordan Hoey <125922012+JordanHoey96@users.noreply.github.com>
2024-02-01 10:53:14 +0000

    DTSPO-16606 - Postgres migration deadline extension (#1199)
    * adding new swtich logic for extending postgres migration deadline for specific repos
    
    * Adding output var
    
    * adding var def
    
    * adjusting date
    
    * testing outputs
    
    * removing debugging
    
    * testing
    
    * testing
    
    * adding toLowerCase()
    
    * correcting

--- vars/warnAboutDeprecatedPostgres.groovy
+++ vars/warnAboutDeprecatedPostgres.groovy
@@ -3,6 +3,22 @@ import uk.gov.hmcts.pipeline.deprecation.WarningCollector
 import java.time.LocalDate
 
 def call() {
+String gitUrl = env.GIT_URL;
+LocalDate expiryDate;
+ 
+switch (gitUrl.toLowerCase()) {
+    case "https://github.com/hmcts/am-org-role-mapping-service.git":
+    case "https://github.com/hmcts/am-role-assignment-service.git":
+    case "https://github.com/hmcts/am-judicial-booking-service.git":
+        expiryDate = LocalDate.of(2024, 3, 31);
+        break;
+    case "https://github.com/hmcts/wa-case-event-handler.git":
+        expiryDate = LocalDate.of(2024, 2, 12);
+        break;
+    default:
+        expiryDate = LocalDate.of(2024, 1, 31);
+        break;
+}
 
   writeFile file: 'check-deprecated-postgres.sh', text: libraryResource('uk/gov/hmcts/infrastructure/check-deprecated-postgres.sh')
 
@@ -12,7 +28,7 @@ def call() {
     ./check-deprecated-postgres.sh
     """
   } catch(ignored) {
-    WarningCollector.addPipelineWarning("deprecated_postgres", "Please migrate to the flexible server postgres module. See this Slack announcement for more info https://hmcts-reform.slack.com/archives/CA4F2MAFR/p1692714862133249", LocalDate.of(2024, 02, 6))
+    WarningCollector.addPipelineWarning("deprecated_postgres", "Please migrate to the flexible server postgres module. See this Slack announcement for more info https://hmcts-reform.slack.com/archives/CA4F2MAFR/p1692714862133249", expiryDate)
   } finally {
     sh 'rm -f check-deprecated-postgres.sh'
   }
