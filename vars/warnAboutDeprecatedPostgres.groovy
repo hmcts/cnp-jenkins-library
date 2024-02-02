6f75d1af8beaed919bb6f26d47bfc7092f18d638
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2024-02-02 16:11:40 +0000

    extending date for postgres deprecated checkign on jenkins builds for Ref data applications (#1204)
    Co-authored-by: Jordan Hoey <125922012+JordanHoey96@users.noreply.github.com>

--- vars/warnAboutDeprecatedPostgres.groovy
+++ vars/warnAboutDeprecatedPostgres.groovy
@@ -5,7 +5,7 @@ import java.time.LocalDate
 def call() {
 String gitUrl = env.GIT_URL;
 LocalDate expiryDate;
- 
+
 switch (gitUrl.toLowerCase()) {
     case "https://github.com/hmcts/am-org-role-mapping-service.git":
     case "https://github.com/hmcts/am-role-assignment-service.git":
@@ -16,6 +16,13 @@ switch (gitUrl.toLowerCase()) {
     case "https://github.com/hmcts/hmc-cft-hearing-service.git":
     case "https://github.com/hmcts/cpo-case-payment-orders-api.git":
     case "https://github.com/hmcts/ts-translation-service.git":
+    case "https://github.com/hmcts/rd-commondata-api.git":
+    case "https://github.com/hmcts/rd-professional-api.git":
+    case "https://github.com/hmcts/rd-location-ref-api.git":
+    case "https://github.com/hmcts/rd-judicial-api.git":
+    case "https://github.com/hmcts/rd-user-profile-api.git":
+    case "https://github.com/hmcts/rd-profile-sync.git":
+    case "https://github.com/hmcts/rd-caseworker-ref-api.git":
     case "https://github.com/hmcts/bulk-scan-processor.git":
     case "https://github.com/hmcts/bulk-scan-orchestrator.git":
     case "https://github.com/hmcts/bulk-scan-payment-processor.git":
