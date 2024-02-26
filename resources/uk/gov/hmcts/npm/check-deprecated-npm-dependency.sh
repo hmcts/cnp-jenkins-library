9bf21138ba86eaea859abceb6ea1818b0b97962c
Rees Pozzi <reespozzi@icloud.com>
2024-02-26 11:56:47 +0000

    Test checking all package.json files

--- resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
+++ resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
@@ -5,10 +5,21 @@ DEPENDENCY=${1}
 REQUIRED_VERSION=${2}
 
 # Fetches first package.json file and trims version of angular/core to get the major version value 
-find . -type f -name "package.json" | head -1 | xargs cat
+# find . -type f -name "package.json" | head -1 | xargs cat
 
-CURRENT_VERSION=$(find . -type f -name "package.json" | head -1 | xargs cat | grep $DEPENDENCY | sed 's/.*"\^\(.*\)".*/\1/' | cut -d '.' -f 1)
+# CURRENT_VERSION=$(find . -type f -name "package.json" | head -1 | xargs cat | grep $DEPENDENCY | sed 's/.*"\^\(.*\)".*/\1/' | cut -d '.' -f 1)
 
+
+CURRENT_VERSION=""
+for file in $(find . -type f -name "package.json"); do
+    version=$(cat "$file" | grep "$DEPENDENCY" | sed 's/.*"\^\(.*\)".*/\1/' | cut -d '.' -f 1)
+    if [[ -n "$version" ]]; then
+        CURRENT_VERSION="$version"
+        break
+    fi
+done
+
+echo "Current version: $CURRENT_VERSION"
 # Only exit with 1 if there is a deprecation spotted
 if [[ -n $CURRENT_VERSION ]] && [ ${CURRENT_VERSION} -lt ${REQUIRED_VERSION} ]; then
     echo "${DEPENDENCY} version ${CURRENT_VERSION} is deprecated... Please upgrade to ${REQUIRED_VERSION}"
