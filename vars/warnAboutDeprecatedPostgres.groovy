0b6bdf7e997fef0a6afcf77dab15c244b09553cc
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-03-01 09:35:25 +0000

    Update warnAboutDeprecatedPostgres.groovy (#1209)
    Extending due to MS issue whilst attempting prod migration. Has been resolved but need to reschedule with release management

--- vars/warnAboutDeprecatedPostgres.groovy
+++ vars/warnAboutDeprecatedPostgres.groovy
@@ -29,13 +29,11 @@ switch (gitUrl.toLowerCase()) {
     case "https://github.com/hmcts/blob-router-service.git":
     case "https://github.com/hmcts/reform-scan-notification-service.git":
     case "https://github.com/hmcts/send-letter-service.git":
-        expiryDate = LocalDate.of(2024, 3, 31);
-        break;
     case "https://github.com/hmcts/ethos-repl-docmosis-service.git":
     case "https://github.com/hmcts/ecm-shared-infrastructure.git":
     case "https://github.com/hmcts/et-ccd-callbacks.git":
     case "https://github.com/hmcts/et-message-handler.git":
-        expiryDate = LocalDate.of(2024, 2, 29);
+        expiryDate = LocalDate.of(2024, 3, 31);
         break;
     case "https://github.com/hmcts/wa-case-event-handler.git":
         expiryDate = LocalDate.of(2024, 2, 12);
