8ccb2bd16b75e7f86e4f4ec7f173e3743e84a507
Louise Huyton <louise.huyton@justice.gov.uk>
2024-03-11 15:49:31 +0000

    initiate githubApi

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -118,7 +118,8 @@ def call(type, String product, String component, Closure body) {
               component: component,
               tfPlanOnly: true
             )
-
+          def githubApi = new GithubAPI(this)
+            // if (githubApi.getTopicsbyPattern(${project}, "plan-on-prod")) {
             sectionDeployToEnvironment(
               appPipelineConfig: pipelineConfig,
               pipelineCallbacksRunner: callbacksRunner,
