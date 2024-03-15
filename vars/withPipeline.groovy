368e7b32e7d3e2d020340aa51ea7364af19de238
Louise Huyton <louise.huyton@justice.gov.uk>
2024-03-15 14:52:37 +0000

    use array instead of string

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -123,10 +123,10 @@ def call(type, String product, String component, Closure body) {
             def githubApi = new GithubAPI(this)
             if (githubApi.checkForTopic("plan-on-prod")) {
               if (!githubApi.checkForLabel("PR-123", "plan-on-prod")) {
-                githubApi.addLabelsToCurrentPR("plan-on-prod")
+                githubApi.addLabelsToCurrentPR(["plan-on-prod"])
               }
             }
-            
+
             if (githubApi.checkForLabel("PR-123", "plan-on-prod")) {
             sectionDeployToEnvironment(
               appPipelineConfig: pipelineConfig,
