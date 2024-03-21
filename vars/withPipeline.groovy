63ca0f8caab2351a2bff2baedc7683b4d73664a1
Louise Huyton <louise.huyton@justice.gov.uk>
2024-03-21 16:48:00 +0000

    try echo base_env_name

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -126,6 +126,7 @@ def call(type, String product, String component, Closure body) {
             def base_env_name
             if (githubApi.checkForTopic("plan-on-prod")) {
               base_env_name = githubApi.getPRs()
+              echo base_env_name
               // for(item in base_envs) {
                 // if (githubApi.getPRs("demo")) {
                 //   base_env_name = "demo"
