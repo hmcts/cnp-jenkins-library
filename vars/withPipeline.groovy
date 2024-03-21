97cf326d5fdb4fc7ae8ae17f0022cd97ed7e3a60
Louise Huyton <louise.huyton@justice.gov.uk>
2024-03-21 17:29:06 +0000

    try use println instead of echo

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -126,7 +126,7 @@ def call(type, String product, String component, Closure body) {
             def base_env_name
             if (githubApi.checkForTopic("plan-on-prod")) {
               base_env_name = githubApi.checkForBase("demo")
-              echo base_env_name
+              println base_env_name
               // for(item in base_envs) {
                 // if (githubApi.getPRs("demo")) {
                 //   base_env_name = "demo"
