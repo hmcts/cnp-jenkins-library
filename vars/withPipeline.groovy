8906cfe5948e47436839a6240c26bea7908af3d6
Louise Huyton <louise.huyton@justice.gov.uk>
2024-03-21 17:52:47 +0000

    print json response

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -126,6 +126,7 @@ def call(type, String product, String component, Closure body) {
             def base_env_name
             if (githubApi.checkForTopic("plan-on-prod")) {
               base_env_name = githubApi.checkForBase("ref=demo,")
+              println githubApi.refreshPRCache()
               println base_env_name
               // for(item in base_envs) {
                 // if (githubApi.getPRs("demo")) {
