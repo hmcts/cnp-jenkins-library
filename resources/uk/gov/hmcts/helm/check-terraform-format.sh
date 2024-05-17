d696265269fb5e562ddc81793aee64e29f73c15f
Narinder.Doal <narinder.doal@justice.gov.uk>
2024-05-17 17:22:10 +0100

    adding master updates

--- resources/uk/gov/hmcts/helm/check-terraform-format.sh
+++ resources/uk/gov/hmcts/helm/check-terraform-format.sh
@@ -1,41 +1,35 @@
-#!/bin/bash
-
-version_output=$(terraform --version)
-
-terraform_version="$(echo "$version_output" | grep "Terraform v" | cut -d'v' -f 2)"
-azurerm_version="$(echo "$version_output" | grep "azurerm" | cut -d'v' -f 3)"
-
-terraform_major="$(echo "$terraform_version" | cut -d'.' -f 1)"
-azurerm_major="$(echo "$azurerm_version" | cut -d'.' -f 1)"
-
-if [[ "$azurerm_major" -eq "3" ]] && [[ "$terraform_major" -eq "1" ]] ; then
-  echo "Terraform is at version - v${terraform_version}"
-  echo "The Azurerm provider is at version v${azurerm_version}"
-else
-  echo "====================================================================================================="
-  echo "=====  Please update your .terraform-version file, to the latest stable 1.x release            ======"
-  echo "=====  The contents of this file will just be a Terraform version number, eg.                  ======"
-  echo "=====                                                                                          ======"
-  echo "=====  1.3.7                                                                                   ======"
-  echo "=====                                                                                          ======"
-  echo "=====  Ensure your required_providers block is on the latest 3.x version of azurerm, i.e       ======"
-  echo "=====                                                                                          ======"
-  echo "=====  terraform {                                                                             ======"
-  echo "=====    required_providers {                                                                  ======"
-  echo "=====      azurerm = {                                                                         ======"
-  echo "=====       source  = \"hashicorp/azurerm\"                                                      ======"
-  echo "=====       version = \"3.40\"                                                                   ======"
-  echo "=====      }                                                                                   ======"
-  echo "=====    }                                                                                     ======"
-  echo "=====  }                                                                                       ======"
-  echo "=====                                                                                          ======"
-  echo "=====  Enable terraform in your renovate config, either by extending config:base or enabling   ======"
-  echo "=====  the terraform manager.                                                                  ======"
-  echo "=====                                                                                          ======"
-  echo "=====  See:                                                                                    ======"
-  echo "=====   - https://github.com/hmcts/spring-boot-template/pull/412                               ======"
-  echo "=====   - https://github.com/hmcts/draft-store/pull/1168                                       ======"
-  echo "=====   - https://github.com/hmcts/spring-boot-template/blob/master/.github/renovate.json      ======"
-  echo "====================================================================================================="
-  exit 1
-fi
+def checkTerraformFormatting() {
+    def fmtExitCode = sh(returnStatus: true, script: 'terraform fmt -check=true -recursive')
+    echo "Terraform fmt exit status: ${fmtExitCode}"
+    if (fmtExitCode != 0) {
+        echo 'Terraform code is not formatted correctly'
+        // Format the Terraform code recursively
+        sh 'terraform fmt -recursive'
+        // Commit the formatting changes
+        gitFetch()
+        updateGitRemoteUrl()
+        configureGitUser()
+        commitFormattingChanges()
+        pushChangesToBranch()
+        error("Terraform was not formatted correctly, it has been reformatted and pushed back to your Pull Request.")
+    }
+}
+def gitFetch() {
+    sh "git fetch origin ${env.BRANCH_NAME}:${env.BRANCH_NAME}"
+}
+def updateGitRemoteUrl() {
+    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+        sh "git remote set-url origin $(git config remote.origin.url | sed 's/github.com/${env.GIT_USERNAME}:${GITHUB_TOKEN}@github.com/g')"
+    }
+}
+def configureGitUser() {
+    sh "git config --global user.name ${env.GIT_USERNAME}"
+    sh "git config --global user.email ${env.GIT_EMAIL}"
+}
+def commitFormattingChanges() {
+    sh "git add \$(find . -type f -name '*.tf')"
+    sh "git commit -m 'Updating Terraform Formatting'"
+}
+def pushChangesToBranch() {
+    sh "git push origin HEAD:${env.BRANCH_NAME}"
+}
