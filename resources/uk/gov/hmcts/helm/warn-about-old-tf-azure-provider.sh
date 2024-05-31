7ed8b93884ac1f009c1fd2305261ffa6fe9ea2d0
Rees Pozzi <reespozzi@icloud.com>
2024-05-31 12:20:06 +0100

    DTSPO-17348: Understand current functionality

--- resources/uk/gov/hmcts/helm/warn-about-old-tf-azure-provider.sh
+++ resources/uk/gov/hmcts/helm/warn-about-old-tf-azure-provider.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DEPENDENCY=${1}
+REQUIRED_VERSION=${2}
 
 version_output=$(terraform --version)
 
@@ -6,36 +8,55 @@ terraform_version="$(echo "$version_output" | grep "Terraform v" | cut -d'v' -f 
 azurerm_version="$(echo "$version_output" | grep "azurerm" | cut -d'v' -f 3)"
 
 terraform_major="$(echo "$terraform_version" | cut -d'.' -f 1)"
-azurerm_major="$(echo "$azurerm_version" | cut -d'.' -f 1)"
 
-if [[ "$azurerm_major" -eq "3" ]] && [[ "$terraform_major" -eq "1" ]] ; then
-  echo "Terraform is at version - v${terraform_version}"
-  echo "The Azurerm provider is at version v${azurerm_version}"
-else
-  echo "====================================================================================================="
-  echo "=====  Please update your .terraform-version file, to the latest stable 1.x release            ======"
-  echo "=====  The contents of this file will just be a Terraform version number, eg.                  ======"
-  echo "=====                                                                                          ======"
-  echo "=====  1.3.7                                                                                   ======"
-  echo "=====                                                                                          ======"
-  echo "=====  Ensure your required_providers block is on the latest 3.x version of azurerm, i.e       ======"
-  echo "=====                                                                                          ======"
-  echo "=====  terraform {                                                                             ======"
-  echo "=====    required_providers {                                                                  ======"
-  echo "=====      azurerm = {                                                                         ======"
-  echo "=====       source  = \"hashicorp/azurerm\"                                                      ======"
-  echo "=====       version = \"3.40\"                                                                   ======"
-  echo "=====      }                                                                                   ======"
-  echo "=====    }                                                                                     ======"
-  echo "=====  }                                                                                       ======"
-  echo "=====                                                                                          ======"
-  echo "=====  Enable terraform in your renovate config, either by extending config:base or enabling   ======"
-  echo "=====  the terraform manager.                                                                  ======"
-  echo "=====                                                                                          ======"
-  echo "=====  See:                                                                                    ======"
-  echo "=====   - https://github.com/hmcts/spring-boot-template/pull/412                               ======"
-  echo "=====   - https://github.com/hmcts/draft-store/pull/1168                                       ======"
-  echo "=====   - https://github.com/hmcts/spring-boot-template/blob/master/.github/renovate.json      ======"
-  echo "====================================================================================================="
-  exit 1
+echo "terraform major version is: $terraform_major"
+echo "terraform version is: $terraform_version"
+if [[ "$DEPENDENCY" == "terraform" ]] ; then
+  echo "terraform required version is: $REQUIRED_VERSION"
 fi
+
+# if [[ "$DEPENDENCY" == "terraform" ]] ; then
+  # check major version
+#   if [[ "$terraform_major" -ge "$REQUIRED_VERSION" ]] ; then
+#     echo "Terraform is at acceptable version - v${terraform_version}"
+#   else
+#       echo "====================================================================================================="
+#       echo "=====  Please update your major terraform version to the latest stable release                 ======"
+#       echo "=====  The contents of this file will just be a Terraform version number, eg.                  ======"
+#       echo "=====                                                                                          ======"
+#       echo "=====  1.3.7                                                                                   ======"
+#       echo "=====                                                                                          ======"
+#       echo "=====  Enable terraform in your renovate config, either by extending config:base or enabling   ======"
+#       echo "=====  the terraform manager.                                                                  ======"
+#       echo "=====                                                                                          ======"
+#       echo "=====  See:                                                                                    ======"
+#       echo "=====   - https://github.com/hmcts/spring-boot-template/pull/412                               ======"
+#       echo "=====   - https://github.com/hmcts/draft-store/pull/1168                                       ======"
+#       echo "=====   - https://github.com/hmcts/spring-boot-template/blob/master/.github/renovate.json      ======"
+#       echo "====================================================================================================="
+#       exit 1
+#     fi
+# elif [[ "$DEPENDENCY" == "providers" ]] ; then
+#   echo
+  # if [[ "$terraform_major" -ge "$REQUIRED_VERSION" ]] ; then
+  #   echo "Terraform is at acceptable version - v${terraform_version}"
+  # else
+  #     echo "====================================================================================================="
+  #     echo "=====  Please update your major terraform version to the latest stable release                 ======"
+  #     echo "=====  The contents of this file will just be a Terraform version number, eg.                  ======"
+  #     echo "=====                                                                                          ======"
+  #     echo "=====  1.3.7                                                                                   ======"
+  #     echo "=====                                                                                          ======"
+  #     echo "=====  Enable terraform in your renovate config, either by extending config:base or enabling   ======"
+  #     echo "=====  the terraform manager.                                                                  ======"
+  #     echo "=====                                                                                          ======"
+  #     echo "=====  See:                                                                                    ======"
+  #     echo "=====   - https://github.com/hmcts/spring-boot-template/pull/412                               ======"
+  #     echo "=====   - https://github.com/hmcts/draft-store/pull/1168                                       ======"
+  #     echo "=====   - https://github.com/hmcts/spring-boot-template/blob/master/.github/renovate.json      ======"
+  #     echo "====================================================================================================="
+  #     exit 1
+  #   fi
+# else
+#   echo "Dependency value not recognised, terraform versions not checked"
+# fi
