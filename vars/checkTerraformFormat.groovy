5e874ae7c4952098bd71884baa9c7c405f253518
Narinder.Doal <narinder.doal@justice.gov.uk>
2024-06-03 17:20:10 +0100

    adding speach marks in groovy folder

--- vars/checkTerraformFormat.groovy
+++ vars/checkTerraformFormat.groovy
@@ -5,10 +5,10 @@ def call() {
   writeFile file: 'check-terraform-format.sh', text: libraryResource('uk/gov/hmcts/helm/check-terraform-format.sh')
 
   try {
-    sh ""
+    sh """
     chmod +x check-terraform-format.sh
     ./check-terraform-format.sh
-    ""
+    """
   } catch(ignored) {
     WarningCollector.addPipelineWarning("Terraform was not formatted correctly", "it has been reformatted and pushed back to your Pull Request")
   } finally {
