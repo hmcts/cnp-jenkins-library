fc7d77ed1c2a44f98211058e0eac73bf9cc4ed2e
Narinder.Doal <narinder.doal@justice.gov.uk>
2024-06-07 15:00:05 +0100

    update logic

--- resources/uk/gov/hmcts/helm/check-terraform-format.sh
+++ resources/uk/gov/hmcts/helm/check-terraform-format.sh
@@ -22,33 +22,30 @@ checkTerraformFormat() {
 
     # Format the Terraform code recursively
     terraform fmt -recursive
+  
+  git fetch origin $BRANCH:$BRANCH
+  git remote set-url origin $(git config remote.origin.url | sed "s/github.com/${USER_NAME}:${BEARER_TOKEN}@github.com/g")
+
+  # configureGitUser
+  git config --global user.name ${USER_NAME} 
+  git config --global user.email ${GIT_APP_EMAIL_ID}
+
+
+  # commitFormattingChanges
+  files=$(git ls-files -m) 
+  for file in $files; do 
+  git add $file
+  done 
+  git commit -m "Updating Terraform Formatting"
+
+
+  # PushChangesToBranch    
+  git push origin HEAD:$BRANCH
 
     echo "Terraform was not formatted correctly, it has been reformatted and pushed back to your Pull Request."
   else
     echo 'Terraform code is formatted correctly'
   fi
 }
-
-  if [ -n $fmtOutput ]; then
-
-git fetch origin $BRANCH:$BRANCH
-git remote set-url origin $(git config remote.origin.url | sed "s/github.com/${USER_NAME}:${BEARER_TOKEN}@github.com/g")
-
-# configureGitUser
-git config --global user.name ${USER_NAME} 
-git config --global user.email ${GIT_APP_EMAIL_ID}
-
-
-# commitFormattingChanges
-files=$(git ls-files -m) 
-for file in $files; do 
-git add $file
-done 
-git commit -m "Updating Terraform Formatting"
-
-
-# PushChangesToBranch    
-git push origin HEAD:$BRANCH
-fi
 
 checkTerraformFormat
