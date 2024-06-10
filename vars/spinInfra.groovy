0b832a79a6fccb952b6f161ab9128aaadee2785a
Rees Pozzi <reespozzi@icloud.com>
2024-06-10 15:57:38 +0100

    DTSPO-17348: Pass product through to slack message

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -108,7 +108,7 @@ def call(Map<String, ?> params) {
             -backend-config "key=${config.productName}/${environmentDeploymentTarget}/terraform.tfstate"
         """
 
-        warnAboutOldTfAzureProvider(config.environment)
+        warnAboutOldTfAzureProvider(config.environment, config.product)
         warnAboutDeprecatedPostgres()
 
         env.TF_VAR_subscription = config.subscription
--- vars/warnAboutOldTfAzureProvider.groovy
+++ vars/warnAboutOldTfAzureProvider.groovy
@@ -2,7 +2,7 @@ import uk.gov.hmcts.pipeline.deprecation.WarningCollector
 import uk.gov.hmcts.pipeline.DeprecationConfig
 import java.time.LocalDate
 
-def call(String environment) {
+def call(String environment, String product) {
    
   def tfDeprecationConfig = new DeprecationConfig(this).getDeprecationConfig().terraform
   writeFile file: 'warn-about-old-tf-azure-provider.sh', text: libraryResource('uk/gov/hmcts/helm/warn-about-old-tf-azure-provider.sh')
@@ -28,15 +28,15 @@ def call(String environment) {
   if (slackDeprecationMessage) {
     def formattedMessage = slackDeprecationMessage.collect { deprecation ->
       """\
-      - Dependency: ${deprecation.dependency}
-        Message: ${deprecation.message}
-        Deadline: ${deprecation.deadline}""".stripIndent()
+      Dependency: ${deprecation.dependency}
+      Message: ${deprecation.message}
+      Deadline: ${deprecation.deadline}""".stripIndent()
     }.join("\n\n")
 
     def earliestDeadline = deprecationDeadlines.min()
     WarningCollector.addPipelineWarning(
       "updated_terraform_versions",
-      "\n\nPlease update the following terraform dependencies in ${environment}: \n\n${formattedMessage}\n\n",
+      "\n\nPlease update the following terraform dependencies in ${environment} for ${product}: \n\n${formattedMessage}\n\n",
       LocalDate.parse(earliestDeadline)
     )
   }
