9bef12d8ae4e3b1c7531118ff746f4c9d3c2cf9a
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-06-17 16:35:14 +0100

    Add script to check for deprecated properties-volume-spring-boot-starter

---
+++ resources/uk/gov/hmcts/gradle/check-deprecated-properties-volume-spring-boot-starter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+DEPENDENCY="properties-volume-spring-boot-starter"
+DEPRECATED_IN="org.springframework.boot:spring-boot"
+DEPRECATED_IN_VERSION="2.4.0"
+DEPRECATED_LINK="https://github.com/hmcts/properties-volume-spring-boot-starter?tab=readme-ov-file#hmcts-properties-volume-library-deprecated"
+
+CURRENT_VERSION=$(./gradlew --no-daemon --init-script init.gradle -q dependencyInsight --no-daemon --dependency ${DEPRECATED_IN} | grep "${DEPRECATED_IN}" | head -1 | sed 's/ (selected by rule)//' | awk -F':' '{print $NF}')
+
+if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_IN_VERSION}) ]; then
+		echo "The use of ${DEPENDENCY} was deprecated in ${DEPRECATED_IN} version ${DEPRECATED_IN_VERSION}... Further info can be found at ${DEPRECATED_LINK}"
+		exit 1
+fi
