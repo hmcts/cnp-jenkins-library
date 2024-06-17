f6485ae84a940b38a3f99a4b677b8f573809848a
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-06-17 17:04:22 +0100

    Add ver function

--- resources/uk/gov/hmcts/gradle/check-deprecated-properties-volume-spring-boot-starter.sh
+++ resources/uk/gov/hmcts/gradle/check-deprecated-properties-volume-spring-boot-starter.sh
@@ -8,6 +8,8 @@ DEPRECATED_LINK="https://github.com/hmcts/properties-volume-spring-boot-starter?
 
 CURRENT_VERSION=$(./gradlew --no-daemon --init-script init.gradle -q dependencyInsight --no-daemon --dependency ${DEPRECATED_IN} | grep "${DEPRECATED_IN}" | head -1 | sed 's/ (selected by rule)//' | awk -F':' '{print $NF}')
 
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+
 if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_IN_VERSION}) ]; then
 		echo "The use of ${DEPENDENCY} was deprecated in ${DEPRECATED_IN} version ${DEPRECATED_IN_VERSION}... Further info can be found at ${DEPRECATED_LINK}"
 		exit 1
