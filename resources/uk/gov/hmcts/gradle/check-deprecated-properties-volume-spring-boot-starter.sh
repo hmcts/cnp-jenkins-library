286cd0deab17f06da4348cafbfacbd007ac91496
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-06-18 09:59:14 +0100

    Change condition to less than to test

--- resources/uk/gov/hmcts/gradle/check-deprecated-properties-volume-spring-boot-starter.sh
+++ resources/uk/gov/hmcts/gradle/check-deprecated-properties-volume-spring-boot-starter.sh
@@ -13,7 +13,7 @@ DEPENDENCY_IN_USE=$(./gradlew --no-daemon --init-script init.gradle -q dependenc
 if [[ -n $DEPENDENCY_IN_USE ]]; then
 	CURRENT_VERSION=$(./gradlew --no-daemon --init-script init.gradle -q dependencyInsight --no-daemon --dependency ${DEPRECATED_IN} | grep "${DEPRECATED_IN}" | head -1 | sed 's/ (selected by rule)//' | awk -F':' '{print $NF}')
 
-	if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -gt $(ver ${DEPRECATED_IN_VERSION}) ]; then
+	if [[ -n $CURRENT_VERSION ]] && [ $(ver $CURRENT_VERSION) -lt $(ver ${DEPRECATED_IN_VERSION}) ]; then
 			echo "The use of ${DEPENDENCY} was deprecated in ${DEPRECATED_IN#*:} version ${DEPRECATED_IN_VERSION}... Further info can be found at ${DEPRECATED_LINK}"
 			exit 1
 	fi
