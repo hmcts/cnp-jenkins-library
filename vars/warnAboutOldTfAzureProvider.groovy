a890ed5819e52118be49858e972e9c12f5832380
Rees Pozzi <reespozzi@icloud.com>
2024-07-18 13:10:59 +0100

    Update terraform provider to look at correct item

--- vars/warnAboutOldTfAzureProvider.groovy
+++ vars/warnAboutOldTfAzureProvider.groovy
@@ -17,11 +17,11 @@ def call(String environment, String product) {
       ./warn-about-old-tf-azure-provider.sh $dependency $deprecation.version
       """
     } catch(ignored) {
-      if (deprecation.dependency && deprecation.message && deprecation.deadline) {
+      if (dependency && deprecation.message && deprecation.deadline) {
         println("Added deprecation to WarningCollector")
         WarningCollector.addPipelineWarning(
             "updated_terraform_versions",
-            "`${deprecation.dependency}` - ${deprecation.message}, update by ${deprecation.deadline}",
+            "`${dependency}` - ${deprecation.message}, update by ${deprecation.deadline}",
             LocalDate.parse(deprecation.deadline)
         )
       }
