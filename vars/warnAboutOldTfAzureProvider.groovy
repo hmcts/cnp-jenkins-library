7c907a6a161c691a2f158bb49533a0a994bd0f39
Rees Pozzi <reespozzi@icloud.com>
2024-07-18 16:00:29 +0100

    Try with real message now it worked

--- vars/warnAboutOldTfAzureProvider.groovy
+++ vars/warnAboutOldTfAzureProvider.groovy
@@ -24,7 +24,8 @@ def call(String environment, String product) {
           deadline: deprecation.date_deadline
       ]
       deprecationDeadlines << deprecation.date_deadline
-      WarningCollector.addPipelineWarning("updated_tf_versions" ,"TESTING IT WORKS",  LocalDate.parse(deprecation.date_deadline))
+
+      WarningCollector.addPipelineWarning("updated_tf_versions" ,"${dependency} - minimum required: *${deprecation.version}, by ${deprecation.deadline}",  LocalDate.parse(deprecation.date_deadline))
       // WarningCollector.addPipelineWarning(
       //     "updated_terraform_versions",
       //     "For " + dependency + " - minimum required is *" + deprecation.version +"*, update by " + deprecation.deadline + ".",
