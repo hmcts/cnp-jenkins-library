99a3799196697c4059d1a70034c721dc2ddf2f83
Rees Pozzi <reespozzi@icloud.com>
2024-07-18 16:23:06 +0100

    Update header, update date formatting

--- vars/notifyPipelineDeprecations.groovy
+++ vars/notifyPipelineDeprecations.groovy
@@ -42,7 +42,7 @@ def call(String teamSlackChannel, MetricsPublisher metricsPublisher ) {
        return
     }
 
-    // warningMessage.addHeader("We have noticed deprecated configuration in ${env.JOB_NAME}: <${env.RUN_DISPLAY_URL}|Build ${env.BUILD_DISPLAY_NAME}>")
+    warningMessage.addHeader("Deprecated configuration in ${env.JOB_NAME}: <${env.RUN_DISPLAY_URL}|Build ${env.BUILD_DISPLAY_NAME}>")
     warningMessage.setWarningColor()
     println("Message blocks to be sent: " + warningMessage.blocks)
     
