f8619fd608cb65a1375d821782b5c0339eec7681
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-07-18 11:32:01 +0100

    Update withPipeline.groovy

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -20,6 +20,7 @@ import uk.gov.hmcts.pipeline.DeprecationConfig
 def call(type, String product, String component, Closure body) {
 
     def branch = new ProjectBranch(env.BRANCH_NAME)
+  
     def deploymentNamespace = branch.deploymentNamespace()
     def deploymentProduct = deploymentNamespace ? "$deploymentNamespace-$product" : product
 
