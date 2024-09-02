07d3a0c54f40c249dff54a91c1df35ad05802920
Rees Pozzi <reespozzi@icloud.com>
2024-09-02 13:09:32 +0100

    Try wrapping in withCredentials

--- vars/onTerraformChangeInPR.groovy
+++ vars/onTerraformChangeInPR.groovy
@@ -2,14 +2,16 @@ def call(Closure block) {
   folderExists('infrastructure') {
     writeFile file: 'check-infrastructure-files-changed.sh', text: libraryResource('uk/gov/hmcts/infrastructure/check-infrastructure-files-changed.sh')
 
-    def infraFolderHasChanges = sh(
-      script: "chmod +x check-infrastructure-files-changed.sh\n" +
-        "    ./check-infrastructure-files-changed.sh",
-      returnStatus: true
-    )
-    sh 'rm check-infrastructure-files-changed.sh'
-    if (infraFolderHasChanges == 1) {
-      return block.call()
+    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'BEARER_TOKEN', usernameVariable: 'APP_ID')]) {
+      def infraFolderHasChanges = sh(
+        script: "chmod +x check-infrastructure-files-changed.sh\n" +
+          "    ./check-infrastructure-files-changed.sh",
+        returnStatus: true
+      )
+      sh 'rm check-infrastructure-files-changed.sh'
+      if (infraFolderHasChanges == 1) {
+        return block.call()
+      }
     }
   }
 }
