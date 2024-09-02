456aff5a9f87691bbb86f2ab96c4b6c4ba4503de
Rees Pozzi <reespozzi@icloud.com>
2024-09-02 13:39:50 +0100

    Switch order of git commands?

--- resources/uk/gov/hmcts/infrastructure/check-infrastructure-files-changed.sh
+++ resources/uk/gov/hmcts/infrastructure/check-infrastructure-files-changed.sh
@@ -4,6 +4,6 @@ set -x
 USER_NAME=${1}
 BEARER_TOKEN=${2}
 
-git fetch origin master:master
 git remote set-url origin $(git config remote.origin.url | sed "s/github.com/${USER_NAME}:${BEARER_TOKEN}@github.com/g")
+git fetch origin master:master
 git diff -s --exit-code master infrastructure/
