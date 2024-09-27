949bd4329cb5eb01e95d51b4b95f67a9970a305e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-09-27 09:36:17 +0100

    add quotes

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -156,7 +156,7 @@ class Acr extends Az {
     def tagFound = false
     try {
       def tags = this.az "acr repository show-tags -n ${registryName} --subscription ${registrySubscription} --repository ${repository}"
-      tagFound = tags.contains(pr-5455-8e55004-20240927074254)
+      tagFound = tags.contains("${tag}")
       steps.echo "Current tags: ${tags}. Is ${tag} available? ... ${tagFound}"
     } catch (noTagsError) {
     } // Do nothing -> return false
