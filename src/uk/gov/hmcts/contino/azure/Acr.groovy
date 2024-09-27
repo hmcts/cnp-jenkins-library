e58bf95d16efc6f006c104d8af942fc1a015b89d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-09-27 09:38:48 +0100

    remove variable

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -156,7 +156,6 @@ class Acr extends Az {
     def tagFound = false
     try {
       def tags = this.az "acr repository show-tags -n ${registryName} --subscription ${registrySubscription} --repository ${repository}"
-      def tag = "${tag}"
       steps.echo "Current tag: ${tag}"
       tagFound = tags.contains(tag)
       steps.echo "Current tags: ${tags}. Is ${tag} available? ... ${tagFound}"
