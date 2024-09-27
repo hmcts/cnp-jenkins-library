6c3d869a8f7fbd54e9aa92c07ddd460cb9b5dc85
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-09-27 15:24:06 +0100

    DTSPO-18983 - Remove new line from ACR image (#1270)
    * reverse logic of hasTag
    
    * echo tags
    
    * hardcode to test
    
    * output tag value
    
    * output tag value
    
    * update hastag
    
    * use variable
    
    * add quotes
    
    * update acr command
    
    * escape quotes
    
    * restore boolean
    
    * restore boolean
    
    * change tagsFound
    
    * change tagsFound
    
    * use boolean
    
    * change tagsFound
    
    * output tag value
    
    * output tag value
    
    * output command
    
    * output command
    
    * use jq
    
    * update logic
    
    * move command
    
    * escape quote
    
    * restore original
    
    * test new method
    
    * try quotes
    
    * hard code tag
    
    * remove quotes
    
    * add quotes
    
    * echo
    
    * remove variable
    
    * add quotes
    
    * try remove newline
    
    * remove echo
    
    * remove echo

--- src/uk/gov/hmcts/contino/azure/Acr.groovy
+++ src/uk/gov/hmcts/contino/azure/Acr.groovy
@@ -156,7 +156,7 @@ class Acr extends Az {
     def tagFound = false
     try {
       def tags = this.az "acr repository show-tags -n ${registryName} --subscription ${registrySubscription} --repository ${repository}"
-      tagFound = tags.contains(tag)
+      tagFound = tags.contains(tag.replace("\n", ""))
       // steps.echo "Current tags: ${tags}. Is ${tag} available? ... ${tagFound}"
     } catch (noTagsError) {
     } // Do nothing -> return false
