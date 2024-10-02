8e55d2c82a867fdcc37464c9838385cc8c052fdf
nicholas chang <nicholasc001@hotmail.com>
2024-10-02 12:07:34 +0100

    edit add repo function

--- src/uk/gov/hmcts/contino/Helm.groovy
+++ src/uk/gov/hmcts/contino/Helm.groovy
@@ -49,9 +49,12 @@ class Helm {
     this.steps.sh(label: "helm repo rm ${registryName}", script: "helm repo rm ${registryName} || echo 'Helm repo may not exist on disk, skipping remove'")
   }
 
-  def addRepo() {
-    this.acr.az "acr helm repo add --subscription ${registrySubscription} --name ${registryName}"
-  }
+def addRepo() {
+    withCredentials([azureServicePrincipal(credentialsId: 'azure-sp-credentials')]) {
+        this.steps.sh(script: "az acr login --name ${registryName}")
+        this.steps.sh(script: "helm repo add ${registryName} https://${registryName}.azurecr.io/helm")
+    }
+}
 
   def publishIfNotExists(List<String> values) {
     configureAcr()
