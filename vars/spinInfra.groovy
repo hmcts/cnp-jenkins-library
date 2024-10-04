3b12a2160d4f32960bae8fc3fae5515f21fd3f47
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-10-04 08:46:44 +0100

    remove else

--- vars/spinInfra.groovy
+++ vars/spinInfra.groovy
@@ -72,9 +72,9 @@ def call(Map<String, ?> params) {
         if (Environment.toTagName(config.environment) == "sandbox" &&
             !builtFrom.toLowerCase().contains("cnp-plum")) {
             tags = tags + [startupMode: "onDemand"]
-          } else if (Environment.toTagName(config.environment) == "sandbox" &&
-            !builtFrom.toLowerCase().contains("sds-toffee")) {
-            tags = tags + [startupMode: "onDemand"]
+          // } else if (Environment.toTagName(config.environment) == "sandbox" &&
+          //   !builtFrom.toLowerCase().contains("sds-toffee")) {
+          //   tags = tags + [startupMode: "onDemand"]
         }
 
         if (changeUrl && changeUrl != "null" && changeUrl != "") {
