58fc3e6c1f27551504b0dc92bb9886c84ac7b3ee
Danny Furnivall <danielfurnivall@gmail.com>
2025-02-05 12:25:21 +0000

    Danny - Fix 15 minute timeout on yarn installs with warnings (#1299)
    * Update YarnBuilder.groovy
    
    * Update YarnBuilder.groovy
    
    * Update YarnBuilderTest.groovy
    
    * Update YarnBuilderTest.groovy
    
    * Update YarnBuilderTest.groovy
    
    * Update YarnBuilderTest.groovy
    
    * Update YarnBuilderTest.groovy
    
    * Update YarnBuilderTest.groovy
    
    * Fix indentation
    
    Thanks @reespozzi

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -268,37 +268,43 @@ EOF
   }
 
   private runYarn(String task, String prepend = "") {
-    if (prepend && !prepend.endsWith(' ')) {
-      prepend += ' '
-    }
-
-    if (steps.fileExists(NVMRC)) {
-      steps.sh """
-        set +ex
-        export NVM_DIR='/home/jenkinsssh/.nvm' # TODO get home from variable
-        . /opt/nvm/nvm.sh || true
-        nvm install
-        set -ex
-
-        export PATH=\$HOME/.local/bin:\$PATH
-
-        if ${prepend.toBoolean()}; then
-          ${prepend}yarn ${task}
-        else
-          yarn ${task}
-        fi
-      """
-    } else {
-      steps.sh("""
-        export PATH=\$HOME/.local/bin:\$PATH
-
-        if ${prepend.toBoolean()}; then
-          ${prepend}yarn ${task}
-        else
-          yarn ${task}
-        fi
-      """)
-    }
+      if (prepend && !prepend.endsWith(' ')) {
+        prepend += ' '
+      }
+  
+      if (steps.fileExists(NVMRC)) {
+        def status = steps.sh(script: """
+          set +ex
+          export NVM_DIR='/home/jenkinsssh/.nvm'
+          . /opt/nvm/nvm.sh || true
+          nvm install
+          export PATH=\$HOME/.local/bin:\$PATH
+  
+          if ${prepend.toBoolean()}; then
+            ${prepend}yarn ${task}
+          else
+            yarn ${task}
+          fi
+        """, returnStatus: true)
+  
+        if (status != 0 && !task.contains('install')) {
+          steps.error("Yarn task '${task}' failed with status ${status}")
+        }
+      } else {
+        def status = steps.sh(script: """
+          export PATH=\$HOME/.local/bin:\$PATH
+  
+          if ${prepend.toBoolean()}; then
+            ${prepend}yarn ${task}
+          else
+            yarn ${task}
+          fi
+        """, returnStatus: true)
+  
+        if (status != 0 && !task.contains('install')) {
+          steps.error("Yarn task '${task}' failed with status ${status}")
+        }
+      }
   }
 
   private runYarnQuiet(String task, String prepend = "") {
