0a1bfeabfb9ec2c948d35ab784883f5ba62921b0
Thomas Thornton <>
2025-02-20 16:19:44 +0000

    test if jenkins breaks

--- vars/warnAboutOldTfAzureProvider.groovy
+++ vars/warnAboutOldTfAzureProvider.groovy
@@ -3,7 +3,7 @@ import uk.gov.hmcts.pipeline.DeprecationConfig
 import java.time.LocalDate
 
 def call(String environment, String product) {
-
+  String gitUrl = env.GIT_URL
   def tfDeprecationConfig = new DeprecationConfig(this).getDeprecationConfig().terraform
   writeFile file: 'warn-about-old-tf-azure-provider.sh', text: libraryResource('uk/gov/hmcts/helm/warn-about-old-tf-azure-provider.sh')
 
@@ -17,7 +17,19 @@ def call(String environment, String product) {
       ./warn-about-old-tf-azure-provider.sh $dependency 5.0.0
       """
     } catch(ignored) {
-      WarningCollector.addPipelineWarning("updated_tf_versions" ,"`${dependency}` - minimum required: *5.0.0*.",  LocalDate.of(2027, 2, 1))
+      LocalDate deadlineDate = deprecation.date_deadline
+      
+      if (dependency == "registry.terraform.io/hashicorp/azurerm") {
+        switch (gitUrl?.toLowerCase()) {
+          case "https://github.com/hmcts/cnp-plum-shared-infrastructure.git":
+            deadlineDate = LocalDate.of(2024, 7, 2)
+            break
+          default:
+            deadlineDate = LocalDate.parse(deprecation.date_deadline)
+        }
+      }
+      
+      WarningCollector.addPipelineWarning("updated_tf_versions" ,"`${dependency}` - minimum required: *${deprecation.version}*.", deadlineDate)
     }
   }
   sh 'rm -f warn-about-old-tf-azure-provider.sh'
