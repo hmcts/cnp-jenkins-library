a637f625339f8b21729d353c9836193732ebd6e6
Thomas Thornton <>
2025-02-20 15:33:43 +0000

    reverting back and switching theory

--- vars/warnAboutOldTfAzureProvider.groovy
+++ vars/warnAboutOldTfAzureProvider.groovy
@@ -5,16 +5,6 @@ import java.time.LocalDate
 def call(String environment, String product) {
   String gitUrl = env.GIT_URL
   def tfDeprecationConfig = new DeprecationConfig(this).getDeprecationConfig().terraform
-  LocalDate defaultExpiryDate = null
-
-  // Repository specific expiry dates
-  switch (gitUrl.toLowerCase()) {
-    case "https://github.com/hmcts/cnp-plum-shared-infrastructure.git":
-    case "https://github.com/HMCTS/cnp-jenkins-library.git":
-      defaultExpiryDate = LocalDate.of(2024, 2, 19)
-        break
-  }
-
   writeFile file: 'warn-about-old-tf-azure-provider.sh', text: libraryResource('uk/gov/hmcts/helm/warn-about-old-tf-azure-provider.sh')
 
   def slackDeprecationMessage = []
@@ -23,14 +13,15 @@ def call(String environment, String product) {
   tfDeprecationConfig.each { dependency, deprecation ->
     try {
       sh """
-      echo "Thomas Test"
       chmod +x warn-about-old-tf-azure-provider.sh
       ./warn-about-old-tf-azure-provider.sh $dependency $deprecation.version
       """
     } catch(ignored) {
-      def expiryDate = dependency == 'registry.terraform.io/hashicorp/azurerm' ? defaultExpiryDate : LocalDate.parse(deprecation.date_deadline)
-      echo "Using expiry date: ${expiryDate} for dependency: ${dependency}"
-      WarningCollector.addPipelineWarning("updated_tf_versions" ,"`${dependency}` - minimum required: *${deprecation.version}*.", expiryDate)
+      LocalDate deadlineDate = gitUrl?.toLowerCase() == "https://github.com/hmcts/cnp-plum-shared-infrastructure.git" 
+        ? LocalDate.of(2024, 7, 2)
+        : LocalDate.parse(deprecation.date_deadline)
+      
+      WarningCollector.addPipelineWarning("updated_tf_versions" ,"`${dependency}` - minimum required: *${deprecation.version}*.", deadlineDate)
     }
   }
   sh 'rm -f warn-about-old-tf-azure-provider.sh'
