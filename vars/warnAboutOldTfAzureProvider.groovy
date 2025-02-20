cdf6d4ecb53d5ee8f0454c0e54efaaa20eb33f6f
Thomas Thornton <>
2025-02-20 14:16:17 +0000

    extend terraform provider from default

--- vars/warnAboutOldTfAzureProvider.groovy
+++ vars/warnAboutOldTfAzureProvider.groovy
@@ -3,6 +3,18 @@ import uk.gov.hmcts.pipeline.DeprecationConfig
 import java.time.LocalDate
 
 def call(String environment, String product) {
+  String gitUrl = env.GIT_URL
+  LocalDate defaultExpiryDate = LocalDate.parse(deprecation.date_deadline)
+
+  // Repository specific expiry dates
+  switch (gitUrl.toLowerCase()) {
+    case "https://github.com/hmcts/am-org-role-mapping-service.git":
+        defaultExpiryDate = LocalDate.of(2025, 4, 10)
+        break
+    default:
+        // Will use the date from deprecation config
+        break
+  }
 
   def tfDeprecationConfig = new DeprecationConfig(this).getDeprecationConfig().terraform
   writeFile file: 'warn-about-old-tf-azure-provider.sh', text: libraryResource('uk/gov/hmcts/helm/warn-about-old-tf-azure-provider.sh')
@@ -17,7 +29,7 @@ def call(String environment, String product) {
       ./warn-about-old-tf-azure-provider.sh $dependency $deprecation.version
       """
     } catch(ignored) {
-      WarningCollector.addPipelineWarning("updated_tf_versions" ,"`${dependency}` - minimum required: *${deprecation.version}*.",  LocalDate.parse(deprecation.date_deadline))
+      WarningCollector.addPipelineWarning("updated_tf_versions" ,"`${dependency}` - minimum required: *${deprecation.version}*.", defaultExpiryDate)
     }
   }
   sh 'rm -f warn-about-old-tf-azure-provider.sh'
