c541c2ff2f2c44295460fc020df14616ac34fb3b
ieuanb74 <144245358+ieuanb74@users.noreply.github.com>
2025-02-24 15:50:07 +0000

    Revert script (#1306)
    * Revert script
    
    * Revert script

--- resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
+++ resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 set -x
 
-function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
-
 DEPENDENCY=${1}
 REQUIRED_VERSION=${2}
 
 # Attempt to find dependency version in a package.json file
 CURRENT_VERSION=""
 
-# Handle scoped packages
-if [[ "$DEPENDENCY" == *"/"* ]]; then
-    DEPENDENCY="@$DEPENDENCY"
-fi
-
-version=$(yarn info "$DEPENDENCY" --json | jq -r '.children.Version')
-if [[ $? -eq 0 && -n "$version" ]]; then
-    CURRENT_VERSION=$version
+version=$(yarn info "$DEPENDENCY" --json | jq -r '.children.Version' | cut -d '.' -f 1)
+if [[ -n "$version" ]]; then
+    CURRENT_VERSION="$version"
     echo "Current version: $CURRENT_VERSION"
     # Only exit with 1 if there is a deprecation spotted
-    if [ $(ver $CURRENT_VERSION) -lt $(ver $REQUIRED_VERSION) ]; then
+    if [ ${CURRENT_VERSION} -lt ${REQUIRED_VERSION} ]; then
         echo "${DEPENDENCY} version ${CURRENT_VERSION} is deprecated... Please upgrade to ${REQUIRED_VERSION}"
         exit 1
     fi
