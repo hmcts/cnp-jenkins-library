76eb85018af8391c174892df23067ec725fdf862
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-02-24 15:46:13 +0000

    Revert script

--- resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
+++ resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
@@ -9,13 +9,8 @@ REQUIRED_VERSION=${2}
 # Attempt to find dependency version in a package.json file
 CURRENT_VERSION=""
 
-# Handle scoped packages
-if [[ "$DEPENDENCY" == *"/"* ]]; then
-    DEPENDENCY="@$DEPENDENCY"
-fi
-
 version=$(yarn info "$DEPENDENCY" --json | jq -r '.children.Version')
-if [[ $? -eq 0 && -n "$version" ]]; then
+if [[ -n "$version" ]]; then
     CURRENT_VERSION=$version
     echo "Current version: $CURRENT_VERSION"
     # Only exit with 1 if there is a deprecation spotted
