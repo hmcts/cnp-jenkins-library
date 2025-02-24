e01884a160218af652fb748127f7e5d1ab23c5f8
ieuanb74 <144245358+ieuanb74@users.noreply.github.com>
2025-02-24 13:52:29 +0000

    Update npm script to check each dependecy (#1301)
    * Update npm script to check each dependecy
    
    * Add function to use for version comparison
    
    * Update comment
    
    * Test pa11y nagger
    
    * Revert after testing

--- resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
+++ resources/uk/gov/hmcts/npm/check-deprecated-npm-dependency.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -x
 
+function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+
 DEPENDENCY=${1}
 REQUIRED_VERSION=${2}
 
-# Attempt to find angular version in a package.json file and trims version of angular/core to get the major version value 
+# Attempt to find dependency version in a package.json file
 CURRENT_VERSION=""
-version=$(yarn info "@angular/core" --json | jq -r '.children.Version' | cut -d '.' -f 1 )
+version=$(yarn info "$DEPENDENCY" --json | jq -r '.children.Version')
 if [[ -n "$version" ]]; then
     CURRENT_VERSION="$version"
     echo "Current version: $CURRENT_VERSION"
     # Only exit with 1 if there is a deprecation spotted
-    if [ ${CURRENT_VERSION} -lt ${REQUIRED_VERSION} ]; then
+    if [ $(ver $CURRENT_VERSION) -lt $(ver $REQUIRED_VERSION) ]; then
         echo "${DEPENDENCY} version ${CURRENT_VERSION} is deprecated... Please upgrade to ${REQUIRED_VERSION}"
         exit 1
     fi
