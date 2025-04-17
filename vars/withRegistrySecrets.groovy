a19c398fd2a0b3adbed9eaeccff8a29af71592a8
Marty Fox <martin.fox1@justice.gov.uk>
2025-04-17 16:16:42 +0100

    Adding a docker hub login using existing credentials

--- vars/withRegistrySecrets.groovy
+++ vars/withRegistrySecrets.groovy
@@ -3,6 +3,8 @@ def call(Closure block) {
     [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'public-registry-name', version: '', envVariable: 'REGISTRY_NAME'],
     [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'public-registry-rg', version: '', envVariable: 'REGISTRY_RESOURCE_GROUP'],
     [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'public-registry-sub', version: '', envVariable: 'REGISTRY_SUBSCRIPTION'],
+    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'docker-hub-username', version: '', envVariable: 'DOCKER-HUB-USERNAME'],
+    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'docker-hub-password', version: '', envVariable: 'DOCKER-HUB-PASSWORD'],
   ]
 
   withAzureKeyvault(registrySecrets) {
