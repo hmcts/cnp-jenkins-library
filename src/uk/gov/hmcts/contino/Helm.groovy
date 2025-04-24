c0adcd762d1e549b0132d576d5fe58a6fd2c1195
Marty Fox <martin.fox1@justice.gov.uk>
2025-04-24 10:49:30 +0100

    Attempt to use function

--- src/uk/gov/hmcts/contino/Helm.groovy
+++ src/uk/gov/hmcts/contino/Helm.groovy
@@ -62,9 +62,9 @@ class Helm {
   }
 
   def authenticateDockerHub() {
-    // this.docker.loginDockerHub("${dockerHubUsername}", "${dockerHubPassword}")
     this.steps.echo "Log into Docker Hub"
-    this.steps.sh(label: "docker login", script: "echo ${dockerHubPassword} | docker login --username ${dockerHubUsername} --password-stdin")
+    this.docker.loginDockerHub("${dockerHubUsername}", "${dockerHubPassword}")
+    // this.steps.sh(label: "docker login", script: "echo ${dockerHubPassword} | docker login --username ${dockerHubUsername} --password-stdin")
   }
 
   def publishIfNotExists(List<String> values) {
