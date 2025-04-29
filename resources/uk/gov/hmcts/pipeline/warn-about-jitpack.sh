7b96814a46b207ec54b0caa6af2e3b4237602506
Rees Pozzi <reespozzi@icloud.com>
2025-04-29 14:37:12 +0100

    Explicitly bash

--- resources/uk/gov/hmcts/pipeline/warn-about-jitpack.sh
+++ resources/uk/gov/hmcts/pipeline/warn-about-jitpack.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Check gradle files in project to see if jitpack.io is being referenced
 if [[ -n $(grep -r --include="*.gradle" 'jitpack.io' .) ]]; then
     echo "Jitpack use detected"
