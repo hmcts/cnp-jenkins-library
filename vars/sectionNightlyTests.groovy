8f7d6fbbbfddf55febb7aa8b2b734d6438ddeea5
yogeshruperai <yogesh.ruperai@solirius.com>
2025-07-11 16:34:18 +0100

    adding Alerts into enablePerformanceTest using new parameters with default values

--- vars/sectionNightlyTests.groovy
+++ vars/sectionNightlyTests.groovy
@@ -89,7 +89,7 @@ def call(pcr, config, pipelineType, String product, String component, String sub
 
                 pcr.callAround('PerformanceTest') {
                   timeoutWithMsg(time: config.perfTestTimeout, unit: 'MINUTES', action: 'Performance test') {
-                    if (i == 0) {
+                    if ((i == 0) && (triggeredByTimer == true) && (config.perfRerunOnFail == true)){
                       catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
                         builder.performanceTest()
                       }
