8798e82166caebddb58fd604906f18dc9fff9170
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2025-08-07 10:04:21 +0100

    Register allowed method

--- test/withNightlyPipeline/onMaster/withAngularNightlyPipelineOnMasterTests.groovy
+++ test/withNightlyPipeline/onMaster/withAngularNightlyPipelineOnMasterTests.groovy
@@ -28,15 +28,15 @@ class withAngularNightlyPipelineOnMasterTests extends BaseCnpPipelineTest {
       setupToolVersion(1) {}
       build(1) {}
       securityCheck(1) {}
-      // Handle parameterless crossBrowserTest calls
-      crossBrowserTest(0..5) {}
-      // Handle parameterized crossBrowserTest calls with browser names
-      crossBrowserTest(0..10) { String browserName -> }
       performanceTest(1) {}
       mutationTest(1){}
       fullFunctionalTest(1){}
       asBoolean() { return true }
     }
+
+    // Register crossBrowserTest method variants to avoid MockFor overloading issues
+    helper.registerAllowedMethod("crossBrowserTest", [], {})
+    helper.registerAllowedMethod("crossBrowserTest", [String.class], { String browser -> })
 
     mockBuilder.use {
         runScript("testResources/$jenkinsFile")
