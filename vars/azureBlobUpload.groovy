af1ddd9e423342f103f11098795307408caf39e2
Dean Cullen <Dean_cullen@next.co.uk>
2025-09-03 15:41:38 +0100

    reverted azureBlob change

--- vars/azureBlobUpload.groovy
+++ vars/azureBlobUpload.groovy
@@ -1,10 +1,12 @@
 def call(String subscription, String credentialsId, String source, String destination) {
     withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'STORAGE_ACCOUNT_KEY', usernameVariable: 'STORAGE_ACCOUNT_NAME')]) {
       withSubscriptionLogin(subscription) {
+        {
           sh 'export AZCOPY_AUTO_LOGIN_TYPE=MSI && \
           azcopy cp ${SOURCE} \
             https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${DESTINATION} \
             --recursive=true'
+        }
         }
       }
     }
