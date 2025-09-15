8479dd0cea63f2a599913cf34102a9e222a4cdd0
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2025-09-15 13:55:37 +0100

    Update YarnBuilder.groovy

--- src/uk/gov/hmcts/contino/YarnBuilder.groovy
+++ src/uk/gov/hmcts/contino/YarnBuilder.groovy
@@ -146,9 +146,14 @@ class YarnBuilder extends AbstractBuilder {
       """
     } finally {
       steps.sh """
-        cat yarn-audit-result-formatted | jq -c '. | {type: "auditSummary", data: .metadata}' > yarn-audit-issues-result-summary
-        cat yarn-audit-result-formatted | jq -cr '.advisories| to_entries[] | {"type": "auditAdvisory", "data": { "advisory": .value }}' >> yarn-audit-issues-advisories
-        cat yarn-audit-issues-result-summary yarn-audit-issues-advisories > yarn-audit-issues-result
+        cat yarn-audit-result-formatted \
+        | jq -c '{type: "auditSummary", data: .metadata}' \
+        > yarn-audit-issues-result-summary \
+        && cat yarn-audit-result-formatted \
+        | jq -cr '.advisories | to_entries[] | {type: "auditAdvisory", data: {advisory: .value}}' \
+        >> yarn-audit-issues-advisories \
+        && cat yarn-audit-issues-result-summary yarn-audit-issues-advisories \
+        > yarn-audit-issues-result
       """
       String issues = steps.readFile('yarn-audit-issues-result')
       String knownIssues = null
