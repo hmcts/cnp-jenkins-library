0b4218d3818873a7a6024798b4bbff95f2853e5c
Dean Cullen <Dean_cullen@next.co.uk>
2025-09-17 12:51:25 +0100

    Updated CCD URL name

--- vars/gatlingExternalLoadTest.groovy
+++ vars/gatlingExternalLoadTest.groovy
@@ -66,7 +66,6 @@ def call(Map params) {
   // Set parameter defaults - try master first as many repos still use it
   def gatlingBranch = params.gatlingBranch ?: 'master'
   def testUrl = params.testUrl ?: env.TEST_URL
-  def ccdURLPreview = env.CCD_DATA_STORE_API_PR_URL
   def gatlingUsers = params.gatlingUsers ?: 10
   def gatlingRampDuration = params.gatlingRampDuration ?: '30s'
   def gatlingTestDuration = params.gatlingTestDuration ?: '60s'
@@ -79,7 +78,7 @@ def call(Map params) {
   echo "External Repository: ${params.gatlingRepo}"
   echo "Branch: ${gatlingBranch}"
   echo "Target URL: ${testUrl}"
-  echo "CCD_DATA_STORE_API_PR_URL: ${env.CCD_DATA_STORE_API_PR_URL}"
+  echo "CCD_DATA_STORE_API_PR_URL: ${env.CCD_DATA_STORE_API_URL}"
   echo "Users: ${gatlingUsers}"
   echo "Simulation: ${gatlingSimulation ?: 'All simulations'}"
   echo "Date/Time: ${new Date().format('yyyy-MM-dd HH:mm:ss')}"
