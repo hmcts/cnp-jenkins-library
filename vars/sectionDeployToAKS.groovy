260d95d81c0fa2138eebc4929bc05441667426a4
Dean Cullen <Dean_cullen@next.co.uk>
2025-11-14 11:36:57 +0000

    Removed debug comment

--- vars/sectionDeployToAKS.groovy
+++ vars/sectionDeployToAKS.groovy
@@ -214,7 +214,7 @@ def call(params) {
               // Stage 1: Dynatrace Setup - Post build info, events, and metrics first
               if (config.performanceTestStages) {
                 stageWithAgent("Dynatrace Performance Setup - ${environment}", product) {
-                  testEnv(aksUrl) { // ** Double check If I need this here **
+                  testEnv(aksUrl) {
                     def success = true
                     try {
                       pcr.callAround("dynatracePerformanceSetup:${environment}") {
