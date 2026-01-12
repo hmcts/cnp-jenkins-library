8977922b7e69d5eda01adf4214fa26fa13320f49
Abhivan Chekuri <abhivan.chekuri@justice.gov.uk>
2026-01-12 11:09:03 +0000

    DTSPO-27708 Test with prod

--- vars/withRegistrySecrets.groovy
+++ vars/withRegistrySecrets.groovy
@@ -1,8 +1,8 @@
 def call(Closure block) {
   def registrySecrets = [
-    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'sbox-registry-name', version: '', envVariable: 'REGISTRY_NAME'],
-    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'sbox-registry-rg', version: '', envVariable: 'REGISTRY_RESOURCE_GROUP'],
-    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'sbox-registry-sub', version: '', envVariable: 'REGISTRY_SUBSCRIPTION'],
+    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'prod-registry-name', version: '', envVariable: 'REGISTRY_NAME'],
+    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'prod-registry-rg', version: '', envVariable: 'REGISTRY_RESOURCE_GROUP'],
+    [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'prod-registry-sub', version: '', envVariable: 'REGISTRY_SUBSCRIPTION'],
     [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'docker-hub-user', version: '', envVariable: 'DOCKER_HUB_USERNAME'],
     [$class: 'AzureKeyVaultSecret', secretType: 'Secret', name: 'docker-hub-jenkins-token', version: '', envVariable: 'DOCKER_HUB_PASSWORD'],
   ]
