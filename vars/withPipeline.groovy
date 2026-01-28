5fef07bd14681a957e91941d66bcb0ddab42209e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-01-28 16:34:23 +0000

    add nodeType change inside tf pr

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -123,19 +123,34 @@ def call(type, String product, String component, Closure body) {
 
           onPR {
             onTerraformChangeInPR {
-              // we always need a tf plan of aat (i.e. staging)
-              sectionDeployToEnvironment(
-                appPipelineConfig: pipelineConfig,
-                pipelineCallbacksRunner: callbacksRunner,
-                pipelineType: pipelineType,
-                subscription: subscription.nonProdName,
-                aksSubscription: aksSubscriptions.aat,
-                environment: environment.nonProdName,
-                agentType: nodeSelector,
-                product: product,
-                component: component,
-                tfPlanOnly: true
-              )
+              node(agentTypeProd) {
+                timeoutWithMsg(time: 180, unit: 'MINUTES', action: 'pipeline') {
+                try {
+                  dockerAgentSetup()
+                  env.PATH = "$env.PATH:/usr/local/bin"
+                  // we always need a tf plan of aat (i.e. staging)
+                  sectionDeployToEnvironment(
+                    appPipelineConfig: pipelineConfig,
+                    pipelineCallbacksRunner: callbacksRunner,
+                    pipelineType: pipelineType,
+                    subscription: subscription.nonProdName,
+                    aksSubscription: aksSubscriptions.aat,
+                    environment: environment.nonProdName,
+                    agentType: nodeSelector,
+                    product: product,
+                    component: component,
+                    tfPlanOnly: true
+                  )
+                }
+                catch (err) {
+                  if (err.message != null && err.message.startsWith('AUTO_ABORT')) {
+                    currentBuild.result = 'ABORTED'
+                    metricsPublisher.publish(err.message)
+                    return
+                  } else {
+                    currentBuild.result = "FAILURE"
+                    notifyBuildFailure channel: slackChannel
+              }
 
               final String LABEL_NO_TF_PLAN_ON_PROD = "not-plan-on-prod"
               def githubApi = new GithubAPI(this)
@@ -245,75 +260,44 @@ def call(type, String product, String component, Closure body) {
               )
             }
 
-            node(agentTypeProd) {
-              timeoutWithMsg(time: 180, unit: 'MINUTES', action: 'pipeline') {
-                try {
-                  dockerAgentSetup()
-                  env.PATH = "$env.PATH:/usr/local/bin"
+            sectionDeployToEnvironment(
+              appPipelineConfig: pipelineConfig,
+              pipelineCallbacksRunner: callbacksRunner,
+              pipelineType: pipelineType,
+              subscription: subscription.prodName,
+              environment: environment.prodName,
+              agentType: nodeSelectorProd,
+              product: product,
+              component: component,
+              aksSubscription: aksSubscriptions.prod,
+              tfPlanOnly: false
+            )
 
-                  sectionDeployToEnvironment(
-                    appPipelineConfig: pipelineConfig,
-                    pipelineCallbacksRunner: callbacksRunner,
-                    pipelineType: pipelineType,
-                    subscription: subscription.prodName,
-                    environment: environment.prodName,
-                    agentType: nodeSelectorProd,
-                    product: product,
-                    component: component,
-                    aksSubscription: aksSubscriptions.prod,
-                    tfPlanOnly: false
-                  )
+            highLevelDataSetup(
+              appPipelineConfig: pipelineConfig,
+              pipelineCallbacksRunner: callbacksRunner,
+              builder: pipelineType.builder,
+              environment: environment.prodName,
+              agentType: nodeSelectorProd,
+              product: product,
+            )
 
-                  highLevelDataSetup(
-                    appPipelineConfig: pipelineConfig,
-                    pipelineCallbacksRunner: callbacksRunner,
-                    builder: pipelineType.builder,
-                    environment: environment.prodName,
-                    agentType: nodeSelectorProd,
-                    product: product,
-                  )
-                } catch (err) {
-                  if (err.message != null && err.message.startsWith('AUTO_ABORT')) {
-                    currentBuild.result = 'ABORTED'
-                    metricsPublisher.publish(err.message)
-                    return
-                  } else {
-                    currentBuild.result = "FAILURE"
-                    notifyBuildFailure channel: slackChannel
-                    metricsPublisher.publish('Pipeline Failed')
-                  }
-                  callbacksRunner.call('onFailure')
-                  throw err
-                } finally {
-                  notifyPipelineDeprecations(slackChannel, metricsPublisher)
-                  if (env.KEEP_DIR_FOR_DEBUGGING != "true") {
-                    deleteDir()
-                  }
-                }
+            sectionPromoteBuildToStage(
+              appPipelineConfig: pipelineConfig,
+              pipelineCallbacksRunner: callbacksRunner,
+              pipelineType: pipelineType,
+              subscription: subscription.nonProdName,
+              product: product,
+              component: component,
+              stage: DockerImage.DeploymentStage.PROD,
+              environment: environment.nonProdName,
+              agentType: nodeSelector,
+            )
 
-                notifyBuildFixed channel: slackChannel
-
-              }
-            }
-
-            node(agentType) {
-              sectionPromoteBuildToStage(
-                appPipelineConfig: pipelineConfig,
-                pipelineCallbacksRunner: callbacksRunner,
-                pipelineType: pipelineType,
-                subscription: subscription.nonProdName,
-                product: product,
-                component: component,
-                stage: DockerImage.DeploymentStage.PROD,
-                environment: environment.nonProdName,
-                agentType: nodeSelector,
-              )
-
-              sectionSyncBranchesWithMaster(
-                branchestoSync: pipelineConfig.branchesToSyncWithMaster,
-                product: product
-              )
-            }
+            sectionSyncBranchesWithMaster(
+              branchestoSync: pipelineConfig.branchesToSyncWithMaster,
+              product: product
+            )
           }
 
           onAutoDeployBranch { subscriptionName, environmentName, aksSubscription ->
