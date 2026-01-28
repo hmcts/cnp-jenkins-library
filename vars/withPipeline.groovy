f37750e4756705174c8dfbd2aaaa538ba35829c2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-01-28 10:50:50 +0000

    update logic in nodeSelector

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -68,7 +68,7 @@ def call(type, String product, String component, Closure body) {
   String agentType = env.BUILD_AGENT_TYPE
   String nodeSelector
 
-  if (environment != "prod") {
+  if (environment == "preview") {
     nodeSelector = "dtspo-29750"
   } else {
     nodeSelector = agentType + ' && daily'
