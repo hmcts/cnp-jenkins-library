4e27c16792e685c924a6f273c7542cc99be92517
Shabbir Mama <shabbir.mama@justice.gov.uk>
2026-02-16 10:12:58 +0000

    java-17-deprcation

--- src/uk/gov/hmcts/contino/GradleBuilder.groovy
+++ src/uk/gov/hmcts/contino/GradleBuilder.groovy
@@ -303,7 +303,9 @@ EOF
   @Override
   def setupToolVersion() {
     def statusCode = steps.sh script: 'grep -F "JavaLanguageVersion.of(17)" build.gradle', returnStatus: true
-    if (statusCode == 0) {
+    def dockerfileStatusCode = steps.sh script: 'grep -E "FROM.*java.*:17|FROM.*java17" Dockerfile', returnStatus: true
+
+    if (statusCode == 0 || dockerfileStatusCode == 0) {
       def deprecationConfig = new DeprecationConfig(steps)
       def repoUrl = steps.env.GIT_URL
       def config = deprecationConfig.getDeprecationConfig(repoUrl)
