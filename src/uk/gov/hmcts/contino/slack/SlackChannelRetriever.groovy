9da0eb9e084b835276484da7bac5fd8ea25b8afe
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-02-17 11:02:42 +0000

    Reverted the changes added, during rebase

--- src/uk/gov/hmcts/contino/slack/SlackChannelRetriever.groovy
+++ src/uk/gov/hmcts/contino/slack/SlackChannelRetriever.groovy
@@ -30,27 +30,6 @@ class SlackChannelRetriever implements Serializable {
     }
 
     SlackUserMapping mappedUser = slackUserMapping.find { user -> user.github == changeAuthor }
-    if (mappedUser == null || mappedUser.slack == null || mappedUser.slack.toString().trim().empty) {
-      return null
-    }
-
-    return normaliseRecipient(mappedUser.slack.toString())
-  }
-
-  private static String normaliseRecipient(String mappedSlack) {
-    String value = mappedSlack.trim()
-    if (value ==~ /^<@[UW][A-Z0-9]{8,}>$/) {
-      return value.substring(2, value.length() - 1)
-    }
-
-    if (value.startsWith('@')) {
-      value = value.substring(1)
-    }
-
-    if (value ==~ /^[UW][A-Z0-9]{8,}$/) {
-      return value
-    }
-
-    return "@${value}"
+    return mappedUser != null? '@' + mappedUser.slack : null
   }
 }
