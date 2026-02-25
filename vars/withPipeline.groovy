aa12d4c088782448fbcbc5ec65e6dd43e9802b42
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-02-25 15:38:33 +0000

    Use same agent type

--- vars/withPipeline.groovy
+++ vars/withPipeline.groovy
@@ -71,7 +71,7 @@ def call(type, String product, String component, Closure body) {
     timeoutWithMsg(time: 180, unit: 'MINUTES', action: 'pipeline') {
       def slackChannel = env.BUILD_NOTICES_SLACK_CHANNEL
       try {
-        node("build-only") {
+        node(agentType) {
           dockerAgentSetup()
           env.PATH = "$env.PATH:/usr/local/bin"
 
