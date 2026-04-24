6829cf034f30813746e403264f2fe209a37c7005
Dan Lysiak <danlysiak@hotmail.com>
2026-04-24 14:15:44 +0100

    feat(cve): publish dashboard snapshots from Jenkins

--- README.md
+++ README.md
@@ -177,6 +177,27 @@ this output will be transposed to Uppercase s2s_url => S2S_URL and can then be u
 
 Calls `yarn test:nsp` so this command must be implemented in package.json
 
+Security checks continue to publish the trimmed CVE report to Cosmos. Services can also opt into publishing a normalized repository snapshot to the HMCTS CVE Dashboard:
+
+```
+enableCveDashboardIngestion()
+```
+
+By default the pipeline publishes CVE dashboard snapshots from `master` only and loads `cve-dashboard-url` and `cve-dashboard-api-key` from the product/environment Key Vault, for example `ccd-aat`. To use a different vault, pass its name:
+
+```
+enableCveDashboardIngestion('shared-cve-dashboard')
+```
+
+To publish from additional long-lived branches, pass a branch allow-list:
+
+```
+enableCveDashboardIngestion('', ['master', 'demo'])
+enableCveDashboardIngestion('shared-cve-dashboard', ['master', 'release/1.x'])
+```
+
+Branches outside the allow-list still run security checks and still keep the existing Cosmos publishing behaviour, but they do not update the dashboard's current repository snapshot. The secrets are exported as `CVE_DASHBOARD_URL` and `CVE_DASHBOARD_API_KEY` for the security-check stage only. Dashboard publishing is fail-open: scanner execution and Cosmos publishing keep their existing behaviour, and dashboard request failures are logged without failing the build.
+
 #### Smoke tests
 
 To check that the app is working as intended you should implement smoke tests which call your app and check that the appropriate response is received.
---
+++ test/withPipeline/SectionBuildAndTestCveDashboardTest.groovy
@@ -0,0 +1,111 @@
+package withPipeline
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import spock.lang.Specification
+import uk.gov.hmcts.contino.AppPipelineConfig
+
+class SectionBuildAndTestCveDashboardTest extends Specification {
+
+  def helper
+  def binding
+  def script
+  def keyVaultArguments
+  def withEnvArguments
+
+  def setup() {
+    def pipelineTest = new BasePipelineTest() {}
+    pipelineTest.setUp()
+    helper = pipelineTest.helper
+    binding = pipelineTest.binding
+    binding.setVariable('env', [BRANCH_NAME: 'master'])
+    helper.registerAllowedMethod("withEnv", [List.class, Closure.class], { args, body ->
+      withEnvArguments = args
+      body.call()
+    })
+    helper.registerAllowedMethod("withAzureKeyvault", [LinkedHashMap, Closure.class], { args, body ->
+      keyVaultArguments = args
+      body.call()
+    })
+    script = pipelineTest.loadScript('vars/sectionBuildAndTest.groovy')
+  }
+
+  def "does not load CVE dashboard secrets when ingestion is disabled"() {
+    given:
+      def called = false
+      def config = new AppPipelineConfig()
+
+    when:
+      script.withCveDashboardSecretsIfEnabled(config, 'ccd', 'aat') {
+        called = true
+      }
+
+    then:
+      called
+      keyVaultArguments == null
+      withEnvArguments == null
+  }
+
+  def "loads default product environment CVE dashboard secrets when ingestion is enabled"() {
+    given:
+      def called = false
+      def config = new AppPipelineConfig(cveDashboardIngestion: true)
+
+    when:
+      script.withCveDashboardSecretsIfEnabled(config, 'ccd', 'aat') {
+        called = true
+      }
+
+    then:
+      called
+      keyVaultArguments.keyVaultURLOverride == 'https://ccd-aat.vault.azure.net/'
+      keyVaultArguments.azureKeyVaultSecrets == [
+        [secretType: 'Secret', name: 'cve-dashboard-url', version: '', envVariable: 'CVE_DASHBOARD_URL'],
+        [secretType: 'Secret', name: 'cve-dashboard-api-key', version: '', envVariable: 'CVE_DASHBOARD_API_KEY']
+      ]
+      withEnvArguments == ['CVE_DASHBOARD_PUBLISH_BRANCHES=master']
+  }
+
+  def "loads explicit CVE dashboard vault when configured"() {
+    given:
+      def config = new AppPipelineConfig(cveDashboardIngestion: true, cveDashboardVaultName: 'shared-cve-dashboard')
+
+    when:
+      script.withCveDashboardSecretsIfEnabled(config, 'ccd', 'aat') {}
+
+    then:
+      keyVaultArguments.keyVaultURLOverride == 'https://shared-cve-dashboard.vault.azure.net/'
+  }
+
+  def "does not load CVE dashboard secrets when branch is not allowed"() {
+    given:
+      def called = false
+      binding.env.BRANCH_NAME = 'feature/test'
+      def config = new AppPipelineConfig(cveDashboardIngestion: true)
+
+    when:
+      script.withCveDashboardSecretsIfEnabled(config, 'ccd', 'aat') {
+        called = true
+      }
+
+    then:
+      called
+      keyVaultArguments == null
+      withEnvArguments == null
+  }
+
+  def "loads CVE dashboard secrets when branch override allows current branch"() {
+    given:
+      binding.env.BRANCH_NAME = 'demo'
+      def config = new AppPipelineConfig(
+        cveDashboardIngestion: true,
+        cveDashboardIngestionBranches: ['master', 'demo']
+      )
+
+    when:
+      script.withCveDashboardSecretsIfEnabled(config, 'ccd', 'aat') {}
+
+    then:
+      keyVaultArguments.keyVaultURLOverride == 'https://ccd-aat.vault.azure.net/'
+      withEnvArguments == ['CVE_DASHBOARD_PUBLISH_BRANCHES=master,demo']
+  }
+}
