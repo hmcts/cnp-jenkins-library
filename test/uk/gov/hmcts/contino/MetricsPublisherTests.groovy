8dff74fea272c74f6432555e65fdf513f4d22dcb
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-02 13:56:16 +0100

    Add metrics for tracking jenkins library branches (#1456)

--- test/uk/gov/hmcts/contino/MetricsPublisherTests.groovy
+++ test/uk/gov/hmcts/contino/MetricsPublisherTests.groovy
@@ -14,7 +14,8 @@ class MetricsPublisherTests extends Specification {
   def setup() {
     stubSteps = Mock(JenkinsStepMock.class)
     stubSteps.currentBuild >>  ["timeInMillis" : 1513613748925]
-    stubSteps.env >> [BRANCH_NAME: "master"]
+    stubSteps.env >> [BRANCH_NAME: "master", SHARED_LIBRARY_VERSION: "feature/test-lib-branch"]
+   
     stubSteps.azureCosmosDBCreateDocument(_) >> {}
 
     stubSteps.echo(_) >> { System.out.println(it) }
@@ -43,6 +44,8 @@ class MetricsPublisherTests extends Specification {
     assertThat(metricsMap).contains(entry("product", "testProduct"))
     assertThat(metricsMap).contains(entry("branch_name", "master"))
     assertThat(metricsMap).contains(entry("current_build_scheduled_time", "2017-12-18T16:15:48Z"))
+    assertThat(metricsMap).contains(entry("shared_library_name", "Infrastructure"))
+    assertThat(metricsMap).contains(entry("shared_library_version", "feature/test-lib-branch"))
   }
 
   def "publishes to database returned by resolver"() {
