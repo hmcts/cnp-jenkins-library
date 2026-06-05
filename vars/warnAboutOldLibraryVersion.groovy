3647090d7d6bdc219173a5218fc1c55acf83a1b3
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-05 09:05:35 +0100

    ensure looping over pattherns

--- vars/warnAboutOldLibraryVersion.groovy
+++ vars/warnAboutOldLibraryVersion.groovy
@@ -11,17 +11,32 @@ def call(String repoUrl = null) {
     writeFile file: 'check-old-library-version.sh', text: libraryResource('uk/gov/hmcts/library/check-old-library-version.sh')
 
     jenkinsLibraryDeprecationConfig.each { configKey, deprecation ->
-        try {
-            sh """
-            chmod +x check-old-library-version.sh
-            ./check-old-library-version.sh '${deprecation.pattern}'
-            """
-        } catch(ignored) {
-            WarningCollector.addPipelineWarning(
-                "old_library_version",
-                "Your code references the old library version (${deprecation.pattern}). Please update your Jenkinsfile to use the new library version: *${deprecation.version}*", 
-                LocalDate.parse(deprecation.date_deadline)
+        def patterns = []
+        if (deprecation.pattern instanceof Collection) {
+            patterns.addAll(deprecation.pattern)
+        } else {
+            patterns.addAll(
+                deprecation.pattern
+                    .toString()
+                    .split(/\|/)
+                    .collect { it.trim() }
+                    .findAll { it }
             )
+        }
+
+        patterns.each { pattern ->
+            try {
+                sh """
+                chmod +x check-old-library-version.sh
+                ./check-old-library-version.sh '${pattern}'
+                """
+            } catch(ignored) {
+                WarningCollector.addPipelineWarning(
+                    "old_library_version",
+                    "Your code references the old library version (${pattern}). Please update your Jenkinsfile to use the new library version: *${deprecation.version}*", 
+                    LocalDate.parse(deprecation.date_deadline)
+                )
+            }
         }
     }
     sh 'rm -f check-old-library-version.sh'
