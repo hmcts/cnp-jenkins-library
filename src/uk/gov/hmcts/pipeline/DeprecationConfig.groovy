3d099cda925d61192d41c29da4bb100b173f85a5
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-09 12:17:24 +0100

    revert to master

--- src/uk/gov/hmcts/pipeline/DeprecationConfig.groovy
+++ src/uk/gov/hmcts/pipeline/DeprecationConfig.groovy
@@ -13,7 +13,7 @@ class DeprecationConfig {
             def response = steps.httpRequest(
                 consoleLogResponseBody: true,
                 timeout: 10,
-                url: "https://raw.githubusercontent.com/hmcts/cnp-deprecation-map/refs/heads/DTSPO-30114/nagger-versions.yaml",
+                url: "https://raw.githubusercontent.com/hmcts/cnp-deprecation-map/master/nagger-versions.yaml",
                 validResponseCodes: '200'
             )
             deprecationConfigInternal = steps.readYaml(text: response.content)
