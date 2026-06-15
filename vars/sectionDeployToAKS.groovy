547fcb42ad0902862558fbeca583ef5c14d3e181
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-06-15 12:10:23 +0100

    don't uninstall helm if on sandbox

--- vars/sectionDeployToAKS.groovy
+++ vars/sectionDeployToAKS.groovy
@@ -21,7 +21,7 @@ def testEnv(String testUrl, block) {
 
 def clearHelmReleaseForFailure(boolean enableHelmLabel, AppPipelineConfig config, DockerImage dockerImage, Map params, PipelineCallbacksRunner pcr) {
     def projectBranch = new ProjectBranch(env.BRANCH_NAME)
-    if ((projectBranch.isMaster() && config.clearHelmReleaseOnFailure) || (projectBranch.isPR() && !enableHelmLabel)) {
+    if ((projectBranch.isMaster() && config.clearHelmReleaseOnFailure) || (projectBranch.isPR() && !enableHelmLabel) || (env.ENVIRONMENT_NAME != 'sandbox')) {
         helmUninstall(dockerImage, params, pcr)
   }
 }
