114b4e4cda7c4d271351f2f8d3547dd31abe07a7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-07-01 12:04:41 +0100

    catch unstable

--- vars/warnAboutOldLibraryVersion.groovy
+++ vars/warnAboutOldLibraryVersion.groovy
@@ -43,7 +43,9 @@ Migration guide: https://tools.hmcts.net/confluence/spaces/DTSPO/pages/197350993
 Rollout tracker: https://tools.hmcts.net/confluence/spaces/DTSPO/pages/1973305638/Migration+rollout+tracker"""
                 LocalDate deprecationDate = LocalDate.parse(deprecation.date_deadline)
 
-                unstable("Old library version detected. Update to Infrastructure@${deprecation.version}.")
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                    error("Old library version detected. Update to Infrastructure@${deprecation.version}.")
+                }
 
                 try {
                     WarningCollector.addPipelineWarning(
