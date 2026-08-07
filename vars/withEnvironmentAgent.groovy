f8abd5878c0e4224509a6590b26fb3092de60511
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-08-07 13:03:59 +0100

    trigger on env agent

--- vars/withEnvironmentAgent.groovy
+++ vars/withEnvironmentAgent.groovy
@@ -14,7 +14,7 @@ def call(String environment, String product, String agentLabelOverride, Closure 
   }
 
   String normalisedEnvironment = AgentSelector.normaliseEnvironment(environment)
-  if (AgentSelector.isRunningOnEnvironmentAgent(env, environment, product)) {
+  if (AgentSelector.currentAgentMatchesLabel(env, agentLabel)) {
     withEnvironmentContext(agentLabel, environment, body)
     return
   }
